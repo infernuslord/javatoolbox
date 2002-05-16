f3917db57b66e0c49bb01ecb4fc5113783cebf44
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-05-16 02:06:48 +0000

    Initial version

---
+++ toolbox/src/toolbox/findclass/FindClassCollector.java
@@ -0,0 +1,58 @@
+package toolbox.findclass;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Use to gather up search results. Create instance and
+ * add as listener FindClass
+ */
+public class FindClassCollector implements IFindClassListener
+{
+    /** Storage for search results **/
+    private List results_ = new ArrayList();
+    
+    /**
+     * Constructor for FindClassCollector.
+     */
+    public FindClassCollector()
+    {
+        super();
+    }
+
+    
+    /**
+     * Implemenation of IFindClassListener.
+     * Class has been found
+     * 
+     * @param  result  What was found
+     */
+    public void classFound(FindClassResult result)
+    {
+        results_.add(result);            
+    }
+    
+    
+    /**
+     * Returns array of find class results 
+     * 
+     * @return  FindClassResult[]
+     */
+    public FindClassResult[] getResults()
+    {
+        return (FindClassResult[]) 
+            results_.toArray(new FindClassResult[results_.size()]);
+    }
+    
+    /**
+     * Clears the contents of the colletor
+     * 
+     * @return  Number of elements cleared
+     */
+    public int clear() 
+    {
+        int i = results_.size();
+        results_.clear();
+        return i;
+    }
+}
---
+++ toolbox/src/toolbox/findclass/FindClassResult.java
@@ -0,0 +1,41 @@
+package toolbox.findclass;
+
+/**
+ * Data object specific to the result of a successful class search
+ */
+public class FindClassResult
+{
+    private String searchString_;
+    private String classLocation_;
+    private String classFQN_;
+    
+    /**
+     * Constructor
+     * 
+     * @param  searchString   Original search string
+     * @param  classLocation  Location where class was found (jar/path)
+     * @param  classFQN       Fully qualified name of the class found
+     */
+    public FindClassResult(String searchString, String classLocation, String classFQN)
+    {
+        searchString_ = searchString;
+        classLocation_ = classLocation;
+        classFQN_ = classFQN;    
+    }
+    
+    public String getSearchString()
+    {
+        return searchString_;
+    }
+    
+    public String getClassLocation()
+    {
+        return classLocation_;
+    }
+    
+    public String getClassFQN()
+    {
+        return classFQN_;
+    }
+        
+}
