4cfbe9271b1828957cbe24f81156d303b6379687
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-05-16 13:07:09 +0000

    Added toString()

--- trunk/toolbox/src/toolbox/findclass/FindClassResult.java
+++ trunk/toolbox/src/toolbox/findclass/FindClassResult.java
@@ -37,5 +37,10 @@ public class FindClassResult
     {
         return classFQN_;
     }
+    
+    public String toString()
+    {
+        return classLocation_ + " => " + classFQN_;
+    }
         
 }
