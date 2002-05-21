4ea0f2df5287a0132d34c8283bd45e9ded699b99
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-05-21 05:50:10 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/tail/TailAdapter.java
@@ -0,0 +1,54 @@
+package toolbox.tail;
+
+/**
+ * @author analogue
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class TailAdapter implements ITailListener
+{
+
+    /**
+     * @see ITailListener#tailStarted()
+     */
+    public void tailStarted()
+    {
+    }
+
+    /**
+     * @see ITailListener#tailStopped()
+     */
+    public void tailStopped()
+    {
+    }
+
+    /**
+     * @see ITailListener#tailEnded()
+     */
+    public void tailEnded()
+    {
+    }
+
+    /**
+     * @see ITailListener#tailPaused()
+     */
+    public void tailPaused()
+    {
+    }
+
+    /**
+     * @see ITailListener#tailUnpaused()
+     */
+    public void tailUnpaused()
+    {
+    }
+
+    /**
+     * @see ITailListener#nextLine(String)
+     */
+    public void nextLine(String line)
+    {
+    }
+
+}
