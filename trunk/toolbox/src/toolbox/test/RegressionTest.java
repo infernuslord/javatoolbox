feab8386fd9486904b9c3b1b1f2adec94b3d7ff6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-05-28 01:12:05 +0000

    Added more unit tests

--- trunk/toolbox/src/toolbox/test/RegressionTest.java
+++ trunk/toolbox/src/toolbox/test/RegressionTest.java
@@ -2,14 +2,16 @@ package toolbox.test;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
-import junit.textui.TestRunner;
 import org.apache.log4j.BasicConfigurator;
 import toolbox.util.io.test.StringInputStreamTest;
 import toolbox.util.io.test.StringOutputStreamTest;
 import toolbox.util.test.ArrayUtilTest;
 import toolbox.util.test.AssertTest;
+import toolbox.util.test.ConsoleTest;
 import toolbox.util.test.FileUtilTest;
+import toolbox.util.test.RandomUtilTest;
 import toolbox.util.test.ResourceUtilTest;
+import toolbox.util.test.SocketUtilTest;
 import toolbox.util.test.StreamUtilTest;
 import toolbox.util.test.StringUtilTest;
 import toolbox.util.test.ThreadUtilTest;
@@ -35,18 +37,21 @@ public class RegressionTest
     public static Test suite()
     {
         BasicConfigurator.configure();
-                
+        
         TestSuite suite = new TestSuite();
          
         // util
         suite.addTest(new TestSuite(ArrayUtilTest.class));
+        suite.addTest(new TestSuite(AssertTest.class));        
+        suite.addTest(new TestSuite(ConsoleTest.class));
+        suite.addTest(new TestSuite(FileUtilTest.class));        
+        suite.addTest(new TestSuite(RandomUtilTest.class));
         suite.addTest(new TestSuite(ResourceUtilTest.class));
+        suite.addTest(new TestSuite(SocketUtilTest.class));
+        suite.addTest(new TestSuite(StreamUtilTest.class));        
+        suite.addTest(new TestSuite(StringUtilTest.class));        
         suite.addTest(new TestSuite(ThreadUtilTest.class));
-        suite.addTest(new TestSuite(StreamUtilTest.class));
-        suite.addTest(new TestSuite(FileUtilTest.class));
-        suite.addTest(new TestSuite(AssertTest.class));
-        suite.addTest(new TestSuite(StringUtilTest.class));
-        
+                
         // util.io
         suite.addTest(new TestSuite(StringInputStreamTest.class));
         suite.addTest(new TestSuite(StringOutputStreamTest.class));        
