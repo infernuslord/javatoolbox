7b5f0ad44b42a41d048df263286db1d5b028ef0e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-06 01:08:51 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/ExceptionUtil.java
@@ -0,0 +1,30 @@
+package toolbox.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+/**
+ * Exception utilities
+ */
+public class ExceptionUtil
+{
+    /**
+     * Prevent construction
+     */
+    private ExceptionUtil()
+    {
+    }
+
+    /**
+     * Converts a <code>Throwable</code>'s stack trace to a string
+     *
+     * @param  throwable   Throwable to extrace stack trace from
+     * @return Stack trace of the throwable as a string
+     */
+    public static String getStackTrace(Throwable t) 
+    {
+        StringWriter sw = new StringWriter();
+        t.printStackTrace(new PrintWriter(sw));
+        return sw.toString();
+    }
+}
