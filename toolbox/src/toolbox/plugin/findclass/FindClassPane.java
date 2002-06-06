acdf3e2b7933da0563ea513b02ca30438bcb5a7f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-06 01:08:11 +0000

    Added status

--- toolbox/src/toolbox/plugin/findclass/FindClassPane.java
+++ toolbox/src/toolbox/plugin/findclass/FindClassPane.java
@@ -2,6 +2,7 @@ package toolbox.findclass;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -11,15 +12,18 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import toolbox.util.ExceptionUtil;
 import toolbox.util.StringUtil;
 import toolbox.util.ThreadUtil;
 import toolbox.util.ui.SafeListModel;
+
 /**
- * enclosing_type 
+ * JFindClass
  */
 public class JFindClass extends JFrame implements ActionListener,
     IFindClassListener
@@ -34,7 +38,11 @@ public class JFindClass extends JFrame implements ActionListener,
     private JList  resultList;
     private DefaultListModel resultModel;
     private JScrollPane resultPane;
-    
+    private JLabel statusLabel;
+
+    /**
+     * Entrypoint
+     */    
     public static void main(String[] args)
     {
         JFindClass jfc = new JFindClass();
@@ -76,6 +84,7 @@ public class JFindClass extends JFrame implements ActionListener,
         
         searchLabel = new JLabel("Find Class");
         searchField = new JTextField(15);
+        searchField.addActionListener(this);
         searchButton = new JButton("Find");
         searchButton.addActionListener(this);
                 
@@ -99,7 +108,8 @@ public class JFindClass extends JFrame implements ActionListener,
         
         JLabel resultListLabel = new JLabel("Results");
         resultModel = new SafeListModel();
-        resultList = new JList(resultModel);        
+        resultList = new JList(resultModel);
+        resultList.setFont(new Font("Lucida Console", Font.PLAIN, 10));
         resultPane = new JScrollPane(resultList);;
         
         JPanel resultPanel = new JPanel(new BorderLayout());
@@ -112,6 +122,13 @@ public class JFindClass extends JFrame implements ActionListener,
             new JSplitPane(JSplitPane.VERTICAL_SPLIT, pathPanel, resultPanel);
         
         getContentPane().add(splitPane, BorderLayout.CENTER);
+
+        //====================================================
+        
+        statusLabel = new JLabel("Enter regular expressions and hit Find!");
+        getContentPane().add(statusLabel, BorderLayout.SOUTH);
+        
+        //====================================================
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
     }
@@ -123,27 +140,39 @@ public class JFindClass extends JFrame implements ActionListener,
     {
         Object obj = e.getSource();
         
-        if (obj == searchButton)
-            ThreadUtil.run(this, "searchButtonClicked", new Object[] { "String" });
+        if (obj == searchButton || obj == searchField)
+        {
+            ThreadUtil.run(
+                this, "searchButtonClicked", new Object[] { "String" });
+        }
     }
     
     /**
      * Execute search
      */
-    public void searchButtonClicked(String s)
+    public void searchButtonClicked(String s) 
     {
-        String search = searchField.getText().trim();
-        
-        if (StringUtil.isNullOrEmpty(search))
-            return;
-        
-        resultModel.clear();
+        try
+        {
+            String search = searchField.getText().trim();
             
-        FindClass fc = new FindClass();
-        fc.addFindClassListener(this);
-        fc.findClass(search, true);
-        resultList.ensureIndexIsVisible(resultModel.getSize()-1);
-        System.out.println("Done");
+            if (StringUtil.isNullOrEmpty(search))
+                return;
+            
+            resultModel.clear();
+                
+            FindClass fc = new FindClass();
+            fc.addFindClassListener(this);
+            fc.findClass(search, true);             
+            ThreadUtil.sleep(2000);
+            resultList.ensureIndexIsVisible(resultModel.getSize()-1);
+            System.out.println("Done");
+        }
+        catch (Exception e)
+        {
+            JOptionPane.showMessageDialog(this, ExceptionUtil.getStackTrace(e), 
+                "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
     /**
@@ -152,6 +181,7 @@ public class JFindClass extends JFrame implements ActionListener,
     public void classFound(FindClassResult searchResult)
     {
         resultModel.addElement(searchResult);
-        //resultList.ensureIndexIsVisible(resultModel.getSize()-1);
+        statusLabel.setText(searchResult.toString());
+        resultList.ensureIndexIsVisible(resultModel.getSize()-1);
     }    
 }
