0e3a1383c115ca1625362e2a8fafe2d1c9108b00
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-16 05:38:33 +0000

    Added accessors/mutators

--- trunk/toolbox/src/toolbox/plugin/jsourceview/LineStatus.java
+++ trunk/toolbox/src/toolbox/plugin/jsourceview/LineStatus.java
@@ -1,14 +1,62 @@
 package toolbox.jsourceview;
 
+/**
+ * LineStatus
+ */
 public class LineStatus
 {
+    /** is the line part of a comment **/
+    private boolean inComment_;
+    
+    /** should the line be counted **/
+    private boolean countLine_;
 
+
+    /**
+     * Constuctor 
+     */
     public LineStatus()
     {
-        inComment = false;
-        countLine = false;
+        inComment_ = false;
+        countLine_ = false;
     }
 
-    public boolean inComment;
-    public boolean countLine;
+
+    /**
+     * Mutator for in comment flag
+     * 
+     * @param  inComment  True if the line is in a comment, false otherwise
+     */    
+    public void setInComment(boolean inComment)
+    {
+        inComment_ = inComment;    
+    }
+    
+
+    /**
+     * @return  Is line in comment
+     */    
+    public boolean getInComment()
+    {
+        return inComment_;
+    }
+    
+    /**
+     * Mutator for countLine flag
+     * 
+     * @param  countLine  Should the line be counted
+     */
+    public void setCountLine(boolean countLine)
+    {
+        countLine_ = countLine;
+    }
+    
+    
+    /**
+     * @return Count the line flag
+     */
+    public boolean getCountLine()
+    {
+        return countLine_;
+    }
 }
