d0d247796ce8bd4ae398f0465fbb1f6ae8b85702
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-16 05:39:38 +0000

    Migrated from JSourceview

---
+++ trunk/toolbox/src/toolbox/util/Queue.java
@@ -0,0 +1,84 @@
+package toolbox.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple (much to be desired) queue implementation
+ */
+public class Queue
+{
+    private List store_ = new ArrayList();
+
+    /**
+     * Creates a Queue
+     */    
+    public Queue()
+    {
+    }
+
+    
+    /**
+     * Adds an object to the back of the queue
+     * 
+     * @param  obj  Object to enqueue
+     */
+    public synchronized void enqueue(Object obj)
+    {
+        store_.add(obj);
+    }
+
+
+    /**
+     * Removes an object from the front of the queue
+     * 
+     * @return  Object dequeued. Null if the queue is empty
+     */
+    public synchronized Object dequeue()
+    {
+        Object obj = null;
+        
+        if (store_.size() > 0)
+            obj = store_.remove(0);
+            
+        return obj;
+    }
+
+    
+    /**
+     * Peeks at the object at the front of the queue
+     * 
+     * @return   Object at the front of the queue. Null if the queue is empty.
+     */
+    public synchronized Object peek()
+    {
+        Object obj = null;
+        
+        if (!isEmpty())
+            obj = store_.get(0);
+           
+        return obj;
+    }
+
+
+    /**
+     * Empty check
+     * 
+     * @return  True if the queue is empty, false otherwise
+     */
+    public synchronized boolean isEmpty()
+    {
+        return store_.size() == 0;
+    }
+    
+    /**
+     * Returns size of queue
+     * 
+     * @return Size of queue
+     */
+    public synchronized int size()
+    {
+        return store_.size();
+    }
+    
+}
