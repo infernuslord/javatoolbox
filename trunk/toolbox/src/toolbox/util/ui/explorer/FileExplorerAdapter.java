95525831c899aa693ddb4ca1fa99181a136a808b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-16 05:42:41 +0000

    Adapter class for JFileExplorerListener

---
+++ trunk/toolbox/src/toolbox/util/ui/explorer/FileExplorerAdapter.java
@@ -0,0 +1,35 @@
+package toolbox.util.ui;
+
+/**
+ * Adapter class for JFileExplorerListener
+ */
+public class JFileExplorerAdapter implements JFileExplorerListener
+{
+    /**
+     * Constructor for JFileExplorerAdapter.
+     */
+    public JFileExplorerAdapter()
+    {
+    }
+
+    /**
+     * @see JFileExplorerListener#fileDoubleClicked(String)
+     */
+    public void fileDoubleClicked(String file)
+    {
+    }
+
+    /**
+     * @see JFileExplorerListener#folderSelected(String)
+     */
+    public void folderSelected(String folder)
+    {
+    }
+
+    /**
+     * @see JFileExplorerListener#folderDoubleClicked(String)
+     */
+    public void folderDoubleClicked(String folder)
+    {
+    }
+}
