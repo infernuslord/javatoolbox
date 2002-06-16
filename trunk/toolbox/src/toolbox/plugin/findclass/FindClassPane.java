5603bea329d3d2f81b4e2bdcbd624e7b14131832
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-16 05:35:15 +0000

    Added file explorer to gui

--- trunk/toolbox/src/toolbox/plugin/findclass/FindClassPane.java
+++ trunk/toolbox/src/toolbox/plugin/findclass/FindClassPane.java
@@ -1,30 +1,36 @@
 package toolbox.findclass;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Vector;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
-import toolbox.util.ExceptionUtil;
 import toolbox.util.StringUtil;
 import toolbox.util.SwingUtil;
 import toolbox.util.ThreadUtil;
+import toolbox.util.ui.JFileExplorer;
 import toolbox.util.ui.JSmartOptionPane;
-import toolbox.util.ui.SafeListModel;
+import toolbox.util.ui.ThreadSafeTableModel;
 
 /**
  * JFindClass
@@ -32,20 +38,30 @@ import toolbox.util.ui.SafeListModel;
 public class JFindClass extends JFrame implements ActionListener,
     IFindClassListener
 {
-    private JLabel searchLabel;
-    private JTextField searchField;
-    private JButton searchButton;
-
-    private JList  pathList;
-    private DefaultListModel pathModel;
+    private JTextField  searchField_;
+    private JButton     searchButton_;
+    private JLabel      statusLabel_;
     
-    private JList  resultList;
-    private DefaultListModel resultModel;
-    private JScrollPane resultPane;
+    private JCheckBox   ignoreCaseCheckBox_;
     
-    private JLabel statusLabel;
+    private JList            pathList_;
+    private DefaultListModel pathModel_;
+    
+    private JTable            resultTable_;
+    private DefaultTableModel resultTableModel_;
+    private JScrollPane       resultPane_;
+    private int               resultCount_;
+        
+    private String[] resultColumns_ = new String[] 
+    {
+        "Num", 
+        "Source", 
+        "Class File"
+    };
 
-    private FindClass findClass;
+    private FindClass findClass_;
+    private JFileExplorer explorer_;
+
     
     /**
      * Entrypoint
@@ -79,6 +95,7 @@ public class JFindClass extends JFrame implements ActionListener,
         buildView();
         init();
         pack();
+        SwingUtil.centerWindow(this);
     }
 
 
@@ -87,12 +104,12 @@ public class JFindClass extends JFrame implements ActionListener,
      */
     protected void init()
     {
-        findClass = new FindClass();
-        findClass.addFindClassListener(this);        
-        List targets = findClass.getSearchTargets();
+        findClass_ = new FindClass();
+        findClass_.addFindClassListener(this);        
+        List targets = findClass_.getSearchTargets();
         
         for (Iterator i = targets.iterator(); i.hasNext(); 
-            pathModel.addElement(i.next()));
+            pathModel_.addElement(i.next()));
     }
 
 
@@ -101,62 +118,96 @@ public class JFindClass extends JFrame implements ActionListener,
      */
     protected void buildView()
     {
-        getContentPane().setLayout(new BorderLayout());
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new BorderLayout());
 
-        //====================================================
+        /* Search Panel */
         
-        searchLabel = new JLabel("Find Class");
-        searchField = new JTextField(15);
-        searchField.addActionListener(this);
-        searchField.setFont(SwingUtil.getMonospacedFont());
-        searchButton = new JButton("Find");
-        searchButton.addActionListener(this);
+        JLabel searchLabel = new JLabel("Find Class");
+        
+        searchField_ = new JTextField(12);
+        searchField_.addActionListener(this);
+        searchField_.setFont(SwingUtil.getPreferredMonoFont());
+        
+        searchButton_ = new JButton("Find");
+        searchButton_.addActionListener(this);
+        
+        ignoreCaseCheckBox_ = new JCheckBox("Ignore Case", true);
                 
         JPanel searchPanel = new JPanel(new FlowLayout());
         searchPanel.add(searchLabel);
-        searchPanel.add(searchField);
-        searchPanel.add(searchButton);
+        searchPanel.add(searchField_);
+        searchPanel.add(searchButton_);
+        searchPanel.add(new JLabel("      "));
+        searchPanel.add(ignoreCaseCheckBox_);
         
-        getContentPane().add(searchPanel, BorderLayout.NORTH);
+        contentPane.add(searchPanel, BorderLayout.NORTH);
 
-        //====================================================
+        /* Path & Explorer Panel */
 
         JLabel pathListLabel = new JLabel("Classpath");
-        pathModel = new DefaultListModel(); 
-        pathList = new JList(pathModel);
-        pathList.setFont(SwingUtil.getMonospacedFont());
+        pathModel_ = new DefaultListModel(); 
+        pathList_  = new JList(pathModel_);
+        pathList_.setFont(SwingUtil.getPreferredMonoFont());
         
         JPanel pathPanel = new JPanel(new BorderLayout());
         pathPanel.add(pathListLabel, BorderLayout.NORTH);
-        pathPanel.add(new JScrollPane(pathList), BorderLayout.CENTER);
+        pathPanel.add(new JScrollPane(pathList_), BorderLayout.CENTER);
         
-        //====================================================
+        explorer_ = new JFileExplorer();
         
-        JLabel resultListLabel = new JLabel("Results");
-        resultModel = new SafeListModel();
-        resultList = new JList(resultModel);
-        resultList.setFont(SwingUtil.getMonospacedFont());
-        resultPane = new JScrollPane(resultList);;
+        //JPanel explorerPanel = new JPanel(new BorderLayout());
+        //explorerPanel.add(explorer_, BorderLayout.WEST);
+        //explorerPanel.add(pathPanel, BorderLayout.EAST);
+        
+        JSplitPane topSplitPane = 
+            new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, explorer_, pathPanel);
+        
+        /* Result Panel */
+        
+        JLabel resultLabel = new JLabel("Results");
+        
+        resultTableModel_ = new ThreadSafeTableModel(resultColumns_,0);
+        resultTable_      = new JTable(resultTableModel_);
+        resultPane_       = new JScrollPane(resultTable_);
+        tweakTable();
         
         JPanel resultPanel = new JPanel(new BorderLayout());
-        resultPanel.add(resultListLabel, BorderLayout.NORTH);
-        resultPanel.add(resultPane, BorderLayout.CENTER);
+        resultPanel.add(resultLabel, BorderLayout.NORTH);
+        resultPanel.add(resultPane_, BorderLayout.CENTER);
         
-        //====================================================
+        /* Split pane */
         
         JSplitPane splitPane = 
-            new JSplitPane(JSplitPane.VERTICAL_SPLIT, pathPanel, resultPanel);
+            new JSplitPane(JSplitPane.VERTICAL_SPLIT, topSplitPane, resultPanel);
         
-        getContentPane().add(splitPane, BorderLayout.CENTER);
+        contentPane.add(splitPane, BorderLayout.CENTER);
 
-        //====================================================
+        /* Status bar */
         
-        statusLabel = new JLabel("Enter regular expressions and hit Find!");
-        getContentPane().add(statusLabel, BorderLayout.SOUTH);
+        statusLabel_ = new JLabel("Enter a regular expression and hit Find!");
+        contentPane.add(statusLabel_, BorderLayout.SOUTH);
         
-        //====================================================
+        /* Post tweaks */
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+    }
+
+
+    /**
+     * Tweaks the table for presentation
+     */
+    protected void tweakTable()
+    {
+        resultTable_.setFont(SwingUtil.getPreferredFont());
+        resultTable_.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
+        
+        TableColumnModel columnModel = resultTable_.getColumnModel();
+        TableColumn column = columnModel.getColumn(0);
+
+        column.setMinWidth(50);
+        column.setPreferredWidth(50);        
+        column.setMaxWidth(100);
     }
 
 
@@ -169,13 +220,14 @@ public class JFindClass extends JFrame implements ActionListener,
     {
         Object obj = e.getSource();
         
-        if (obj == searchButton || obj == searchField)
+        if (obj == searchButton_ || obj == searchField_)
         {
             /* spawn search on separate thread */
             ThreadUtil.run(
                 this, "searchButtonClicked", new Object[] { "String" });
         }
     }
+
     
     /**
      * Execute search
@@ -186,38 +238,57 @@ public class JFindClass extends JFrame implements ActionListener,
     {
         try
         {
-            String search = searchField.getText().trim();
+            String search = searchField_.getText().trim();
             
             if (StringUtil.isNullOrEmpty(search))
-                return;
-            
-            resultModel.clear();
-            Object results[] = findClass.findClass(search, true);             
-            //ThreadUtil.sleep(2000);
-            //resultList.ensureIndexIsVisible(resultModel.getSize()-1);
-            statusLabel.setText(results.length + " matches found");
+            {
+                statusLabel_.setText("Enter class to search");
+            }
+            else
+            {
+                resultTableModel_.setNumRows(0);
+                resultCount_      = 0;
+                
+                Object results[]  = findClass_.findClass(
+                    search, ignoreCaseCheckBox_.isSelected());             
+                    
+                statusLabel_.setText(results.length + " matches found");
+            }
         }
         catch (Exception e)
         {
-            JSmartOptionPane.showDetailedMessageDialog(this, e.getMessage(), 
-                ExceptionUtil.getStackTrace(e), "Error",
-                    JOptionPane.ERROR_MESSAGE);
+            JSmartOptionPane.showExceptionMessageDialog(this, e);
         }
     }
+
 
     /**
      * IFindClassListener
      */
     public void classFound(FindClassResult searchResult)
     {
-        resultModel.addElement(searchResult);
-        //statusLabel.setText(searchResult.toString());
-        //resultList.ensureIndexIsVisible(resultModel.getSize()-1);
+        Vector row = new Vector();
+        row.add(++resultCount_ + "");
+        row.add(searchResult.getClassLocation());
+        row.add(searchResult.getClassFQN());
+        resultTableModel_.addRow(row);
     }
     
+    
+    /**
+     * IFindClassListener
+     */
     public void searchingTarget(String target)
     {
-        statusLabel.setText("Searching " + target + " ...");    
+        statusLabel_.setText("Searching " + target + " ...");    
+    }
+    
+    
+    /**
+     * Notification that the search has been cancelled 
+     */
+    public void searchCancelled()
+    {
+        statusLabel_.setText("Search cancelled");
     }    
-      
 }
