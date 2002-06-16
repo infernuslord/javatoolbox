bfc72c82a1eb9d148b7c9e63afcd7983955e3445
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-16 05:41:02 +0000

    Added logger

--- trunk/toolbox/src/toolbox/util/io/ExtensionFilter.java
+++ trunk/toolbox/src/toolbox/util/io/ExtensionFilter.java
@@ -3,11 +3,17 @@ package toolbox.util.io;
 import java.io.File;
 import java.io.FilenameFilter;
 
+import org.apache.log4j.Category;
+
 /**
  * Filters files based on the file's extension
  */
 public class ExtensionFilter implements FilenameFilter
 {
+    /** Logger **/
+    private static final Category logger_ = 
+        Category.getInstance(ExtensionFilter.class);
+        
     /** Extension to filter on **/
     private String extension;
     
@@ -33,6 +39,11 @@ public class ExtensionFilter implements FilenameFilter
      */
     public boolean accept(File dir,String name)
     {
-        return name.toLowerCase().endsWith(extension.toLowerCase());
+        boolean b = name.toLowerCase().endsWith(extension.toLowerCase());
+        
+        //if (b)
+        //    logger_.debug("Accepted " + name);
+        
+        return b;
     }
 }
