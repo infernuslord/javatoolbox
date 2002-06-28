b6b91b41150572f2e9228bf05c674e545754567a
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-28 04:28:05 +0000

    Migrated some stuff to DoubleUtil

--- trunk/toolbox/src/toolbox/util/MathUtil.java
+++ trunk/toolbox/src/toolbox/util/MathUtil.java
@@ -1,5 +1,8 @@
 package toolbox.util;
 
+import java.text.DecimalFormat;
+import java.text.ParseException;
+
 /**
  * Math Utility Class
  */
@@ -12,34 +15,6 @@ public final class MathUtil
     {
     }
 
-    /**
-     * Determines of the contents of a string can be expressed as a double
-     * 
-     * @param   s   String to inspect
-     * @return  True if string contains a double, false otherwise
-     */
-    public static boolean isDouble(String s)
-    {
-        boolean b = false;
-        
-        if (StringUtil.isNullEmptyOrBlank(s))
-            b = false;
-        else
-        {
-            try
-            {
-                Double.parseDouble(s);
-                b = true;
-            }
-            catch(NumberFormatException nfe)
-            {
-                b = false;
-            }
-        }
-        
-        return b;
-    }
-
 
     /**
      * Determines if a given integer is an even number
@@ -47,7 +22,7 @@ public final class MathUtil
      * @param   i   Integer to evaluate
      * @return  True if even, false otherwise
      */
-    public static boolean isEven(int i) 
+    public static boolean isEven(long i) 
     {
         return ((i%2) == 0);
     }
@@ -59,8 +34,96 @@ public final class MathUtil
      * @param   i   Integer to evaluate
      * @return  True if odd, false otherwise
      */
-    public static boolean isOdd(int i) 
+    public static boolean isOdd(long i) 
     {
         return !isEven(i);
     }
-}
+    
+    
+    /**
+     * Adds a value to each index of an array
+     * 
+     * @param   array  Array to add value to
+     * @param   value  Value to add to each array index
+     * @return  Array with value added to each index
+     */
+    public static int[] addToAll(int[] array, int value) 
+    {
+        for(int i=0; i<array.length; i++)
+            array[i] += value;
+        return array;
+    }
+    
+    
+    /**
+     *  Calculates the inverse normal cumulative distribution
+     *
+     *  @param  p   Any number between 0 and 1
+     *  @return Inverse normal cumulative distribution
+     */
+    public static double invNormalCumDist(double p) {
+
+        double c[] = new double[3];
+        double d[] = new double[3];
+
+        double arg, t, t2, t3, xnum, xden, qinvp, x, pc;
+
+        c[0] = 2.515517;
+        c[1] = .802853;
+        c[2] = .010328;
+
+        d[0] = 1.432788;
+        d[1] = .189269;
+        d[2] = .001308;
+
+        if (p <= .5) {
+
+            arg = -2.0 * Math.log(p);
+            t = Math.sqrt(arg);
+            t2 = t * t;
+            t3 = t2 * t;
+
+            xnum = c[0] + c[1] * t + c[2] * t2;
+            xden = 1.0 + d[0] * t + d[1] * t2 + d[2] * t3;
+            qinvp = t - xnum / xden;
+            x = -qinvp;
+
+            return x;
+
+        }
+        else {
+
+            pc = 1.0 - p;
+            arg = -2.0 * Math.log(pc);
+            t = Math.sqrt(arg);
+            t2 = t * t;
+            t3 = t2 * t;
+
+            xnum = c[0] + c[1] * t + c[2] * t2;
+            xden = 1.0 + d[0] * t + d[1] * t2 + d[2] * t3;
+            x = t - xnum / xden;
+
+            return x;
+        }
+    }
+
+    
+    /**
+     *  Returns sum of array of integers
+     *
+     *  @param  d   Array of integers
+     *  @return Sum of elements in d
+     */
+    public static int sum(int[] d) {
+
+        if( d.length == 0)
+            return 0;
+            
+        int sum = 0;
+        
+        for(int i=0; i<d.length; i++)
+            sum+=d[i];
+
+        return sum;
+    }
+}    
