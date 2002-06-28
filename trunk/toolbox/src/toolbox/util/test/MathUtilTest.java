c62e6be472777e349b736b0e25f1b265255ffa58
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-06-28 04:28:41 +0000

    MIgrated some tests to DoubleUtilTest

--- trunk/toolbox/src/toolbox/util/test/MathUtilTest.java
+++ trunk/toolbox/src/toolbox/util/test/MathUtilTest.java
@@ -3,6 +3,7 @@ package toolbox.util.test;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import toolbox.util.DoubleUtil;
 import toolbox.util.MathUtil;
 
 
@@ -12,17 +13,6 @@ import toolbox.util.MathUtil;
 public class MathUtilTest extends TestCase
 {
     /**
-     * MathUtilTest constructor comment.
-     * 
-     * @param aName String
-     */
-    public MathUtilTest(String aName)
-    {
-        super(aName);
-    }
-
-    
-    /**
      * Starts the test case and runs the entire suite.
      *
      * @param  args  None recognized
@@ -30,6 +20,17 @@ public class MathUtilTest extends TestCase
     public static void main(String[] args)
     {
         TestRunner.run(MathUtilTest.class);
+    }
+    
+    
+    /**
+     * MathUtilTest constructor comment.
+     * 
+     * @param aName String
+     */
+    public MathUtilTest(String aName)
+    {
+        super(aName);
     }
 
     
@@ -58,43 +59,5 @@ public class MathUtilTest extends TestCase
         assertTrue("2 is not odd!", !MathUtil.isOdd(2));
         assertTrue("999 is odd!", MathUtil.isOdd(999));
         assertTrue("-1000 is not odd!", !MathUtil.isOdd(-1000));
-    }
-
-
-    /**
-     * Tests isDouble() for scenarios where the result is true
-     */
-    public void testIsDoublePositive() throws Exception
-    {
-        /* positive */
-        assertTrue("0 is a double", MathUtil.isDouble("0"));
-        assertTrue("0.00 is a double", MathUtil.isDouble("0.00"));
-        assertTrue("12345 is a double", MathUtil.isDouble("12345"));
-        assertTrue("-12345 is a double", MathUtil.isDouble("-12345"));
-        assertTrue("123.45 is a double", MathUtil.isDouble("123.45"));
-        assertTrue("-123.45 is a double", MathUtil.isDouble("-123.45"));
-        assertTrue("number with spaces is a double", 
-            MathUtil.isDouble("    87.774747   "));
-    }
-    
-    
-    /**
-     * Tests isDouble() for scenarios where the result is false
-     */
-    public void testIsDoubleNegative() throws Exception
-    {
-        /* negative */
-        assertTrue("null is not a double", !MathUtil.isDouble(null));
-        assertTrue("empty string is not a double", !MathUtil.isDouble(""));
-        assertTrue("alpha is not a double", !MathUtil.isDouble("a"));
-                
-        assertTrue("blank string is not a double", 
-            !MathUtil.isDouble("         "));
-        
-        assertTrue("alphanumeric is not a double", 
-            !MathUtil.isDouble("fun stuff!"));
-            
-        assertTrue("ascii junk is not a double", 
-            !MathUtil.isDouble("al;dfj0&**&&*345jklsjdf;90q354090**"));
     }
 }
