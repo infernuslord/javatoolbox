56e6e24594f7af8a9b19000e557dc0765f7fe1ed
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-12 03:04:47 +0000

    Added tests for concat()

--- trunk/toolbox/src/toolbox/util/test/ArrayUtilTest.java
+++ trunk/toolbox/src/toolbox/util/test/ArrayUtilTest.java
@@ -38,6 +38,7 @@ public class ArrayUtilTest extends TestCase
         TestRunner.run(ArrayUtilTest.class);
     }
 
+
     /**
      * Test subset() for subset equal to array
      */
@@ -124,6 +125,7 @@ public class ArrayUtilTest extends TestCase
             ei++;
         }
     }
+
 
     /**
      * Test subset(Object[]) for all
@@ -214,6 +216,7 @@ public class ArrayUtilTest extends TestCase
         logger.info("[toStr1] " + ArrayUtil.toString(s));
     }
     
+    
     /**
      * Tests indexOf() for an empty array 
      */
@@ -226,6 +229,7 @@ public class ArrayUtilTest extends TestCase
         
         assertTrue("Array is empty", idx == -1);
     }
+
 
     /**
      * Tests indexOf() for an array of length 1
@@ -241,6 +245,7 @@ public class ArrayUtilTest extends TestCase
         assertEquals("Found at wrong index", 0, idx);
     }
 
+
     /**
      * Tests indexOf() for an array of length 1 where obj not found
      */
@@ -254,6 +259,7 @@ public class ArrayUtilTest extends TestCase
         
         assertEquals("Should not have found a match", -1, idx);
     }
+
 
     /**
      * Tests indexOf() for an array of length > 1
@@ -270,6 +276,7 @@ public class ArrayUtilTest extends TestCase
         assertEquals("Found at wrong index", 2, idx);
     }
 
+
     /**
      * Tests indexOf() for an array of length > 1 where obj not found
      */
@@ -285,6 +292,7 @@ public class ArrayUtilTest extends TestCase
         assertEquals("Should not have found a match", -1, idx);
     }
     
+    
     /**
      * Tests contains() for an empty array
      */
@@ -293,6 +301,7 @@ public class ArrayUtilTest extends TestCase
         assertTrue("Should not be found in an empty array",
             !ArrayUtil.contains(new String[0], "blah"));
     }
+ 
     
     /**
      * Tests contains() for object not found in an array of size one
@@ -303,6 +312,7 @@ public class ArrayUtilTest extends TestCase
             !ArrayUtil.contains(new String[] {"this"}, "dont match"));
     }
     
+    
     /**
      * Tests contains() for object not found in an array of size > one
      */
@@ -311,6 +321,7 @@ public class ArrayUtilTest extends TestCase
         assertTrue("Should not be found in an array with size > one", 
             !ArrayUtil.contains(new String[] {"one", "two", "three" }, "zero"));
     }    
+    
     
     /**
      * Tests contains() for object found in an array of size one
@@ -321,6 +332,7 @@ public class ArrayUtilTest extends TestCase
             ArrayUtil.contains(new String[] {"this"}, "this"));
     }
     
+    
     /**
      * Tests contains() for object found in an array of size > one
      */
@@ -329,6 +341,7 @@ public class ArrayUtilTest extends TestCase
         assertTrue("Should have found in an array with size > one", 
             ArrayUtil.contains(new String[] {"one", "two", "three" }, "two"));
     }    
+    
     
     /**
      * Tests the isNullOrEmpty() method
@@ -348,4 +361,219 @@ public class ArrayUtilTest extends TestCase
         assertTrue("Should have returned false for non-empty array", 
             !ArrayUtil.isNullOrEmpty(oneArray));            
     }
+    
+
+    /*
+     * concat(head, tail) test cases:
+     * 
+     * head and tail are arrays of String[]
+     * 
+     * empty = array is empty
+     * one   = array contains one element
+     * many  = array contains > 1 elements
+     * 
+     * head     tail
+     * ==============
+     * empty    empty
+     * empty    one
+     * empty    many
+     * one      empty
+     * many     empty
+     * one      one
+     * one      many
+     * many     one
+     * many     many
+     */
+
+    
+    /**
+     * Tests concat() for two empty arrays
+     */
+    public void testConcatBothEmpty()
+    {
+        String[] head = new String[0];
+        String[] tail = new String[0];
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array should be empty", 0, concatted.length);
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+    }
+    
+    
+    /**
+     * Tests merge() for an empty head and a tail containing one element 
+     */
+    public void testConcatEmptyOne()
+    {
+        String[] head = new String[0];
+        String[] tail = new String[] { "one" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array length incorrect", 1, concatted.length);
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        assertEquals("concatted array element incorrect", "one", concatted[0]);
+    }
+
+
+    /**
+     * Tests concat() for an empty head and a tail containing many elements
+     */
+    public void testConcatEmptyMany()
+    {
+        String[] head = new String[0];
+        String[] tail = new String[] { "one", "two", "three", "four" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array len incorrect", tail.length, 
+            concatted.length);
+            
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        
+        for (int i=0; i<tail.length; i++)
+            assertEquals("concatted array contents incorrect", tail[i], 
+                concatted[i]);
+    }
+
+
+    /**
+     * Tests concat() for an a head containing one element and an empty tail
+     */
+    public void testConcatOneEmpty()
+    {
+        String[] tail = new String[0];
+        String[] head = new String[] { "one" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array length incorrect", 1, concatted.length);
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        assertEquals("concatted array element incorrect", "one", concatted[0]);
+    }
+
+
+    /**
+     * Tests merge() for an head containing many elements and an empty tail
+     */
+    public void testConcatManyEmpty()
+    {
+        String[] tail = new String[0];
+        String[] head = new String[] { "one", "two", "three", "four" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array len incorrect", head.length, 
+            concatted.length);
+            
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        
+        for (int i=0; i<head.length; i++)
+            assertEquals("concatted array contents incorrect", head[i], 
+                concatted[i]);
+    }
+ 
+ 
+    /**
+     * Tests concat() for a head and tail each containing one element 
+     */
+    public void testConcatBothOne()
+    {
+        String[] head = new String[] { "one" };
+        String[] tail = new String[] { "two" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array length incorrect", 2, concatted.length);
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        assertEquals("concatted array element incorrect", "one", concatted[0]);
+        assertEquals("concatted array element incorrect", "two", concatted[1]);
+    }
+
+
+    /**
+     * Tests concat() for an head containing many elements and tail containing
+     * one element
+     */
+    public void testConcatManyOne()
+    {
+        String[] head = new String[] { "one", "two", "three", "four" };
+        String[] tail = new String[] { "five" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array len incorrect", head.length + tail.length, 
+            concatted.length);
+            
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        
+        int i;
+        for (i=0; i<head.length; i++)
+            assertEquals("concatted array contents incorrect", head[i], 
+                concatted[i]);
+                
+        assertEquals("concatted array contents incorrect", tail[0], 
+            concatted[i]);
+    }
+
+
+    /**
+     * Tests concat() for an tail containing many elements and head containing
+     * one element
+     */
+    public void testConcatOneMany()
+    {
+        String[] tail = new String[] { "one", "two", "three", "four" };
+        String[] head = new String[] { "five" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array len incorrect", head.length + tail.length, 
+            concatted.length);
+            
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        
+        assertEquals("concatted array contents incorrect", head[0], 
+            concatted[0]);
+            
+        for (int i=0; i<tail.length; i++)
+            assertEquals("concatted array contents incorrect", tail[i], 
+                concatted[i+head.length]);
+
+    }
+
+
+    /**
+     * Tests concat() for both head and tail containing many elements
+     */
+    public void testConcatBothMany()
+    {
+        String[] head = new String[] { "one", "two", "three", "four" };
+        String[] tail = new String[] { "five", "six", "seven", "eight" };
+        
+        String[] concatted = (String[])ArrayUtil.concat(head, tail);
+        
+        assertEquals("concatted array len incorrect", head.length + tail.length, 
+            concatted.length);
+            
+        assertEquals("concatted array class type should string", String.class,
+            concatted.getClass().getComponentType());
+        
+        for (int i=0; i<head.length; i++)
+            assertEquals("concatted array contents incorrect", head[i], 
+                concatted[i]);
+
+        for (int i=0; i<tail.length; i++)
+            assertEquals("concatted array contents incorrect", tail[i], 
+                concatted[i+head.length]);
+    }    
 }
