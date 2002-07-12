1a5171cdcbbeda63db2f46765997cc97b2cd019b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-12 05:13:14 +0000

    Added stuff

--- toolbox/src/toolbox/util/DateTimeUtil.java
+++ toolbox/src/toolbox/util/DateTimeUtil.java
@@ -2,14 +2,12 @@ package toolbox.util;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 /**
- * Utility class that deals only with the DATE portions of the 
- * <code>java.util.Date</code> object
- * 
- *        
- * 
+ * Utility class that deals with both the DATE and TIME portions of the 
+ * java.util.Date object
  */
 public class DateTimeUtil
 {
@@ -21,6 +19,7 @@ public class DateTimeUtil
         super();
     }
 
+
     /**
      * @return  Date/time in dashed MM-dd-yyyy format
      */
@@ -29,6 +28,122 @@ public class DateTimeUtil
         DateFormat df = new SimpleDateFormat("MM-dd-yyyy hh:mm a");
         return df.format(d);
     }
+    
+    
+    /**
+     * Adds individual time components to a date. 
+     * The original date is returned for chaining.
+     * 
+     * @param   date     Date to modify
+     * @param   years    Number of years to add
+     * @param   months   Number of months to add
+     * @param   days     Number of days to add
+     * @param   hours    Number of hours to add
+     * @param   minutes  Number of minutes to add
+     * @param   seconds  Number of seconds to add
+     * @return  Modified date
+     */
+    public static Date add(Date date, int years, int months, int days,
+        int hours, int minutes, int seconds)
+    {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.YEAR, +years);
+        cal.add(Calendar.MONTH, +months);
+        cal.add(Calendar.DATE, +days);
+        cal.add(Calendar.HOUR, +hours);
+        cal.add(Calendar.MINUTE, +minutes);
+        cal.add(Calendar.SECOND, +seconds);
+        date.setTime(cal.getTime().getTime());
+        return date;
+    }
+    
+    
+    /**
+     * Gets the date/time of the beginning of the current day
+     * 
+     * @return  Date
+     */
+    public static Date getBeginningOfDay()
+    {
+        return getBeginningOfDay(new Date());
+    }
+    
+    
+    /**
+     * Convenience method to get the time of day immediately after is rolled 
+     * from the previous day.  Time will be 00:00:00 0ms
+     * 
+     * @param  d  Date to get beginning of
+     * @return Date set to the beginning of the day
+     */
+    public static Date getBeginningOfDay(final Date d)
+    {
+        Date start = new Date(d.getTime());
+        Calendar c = Calendar.getInstance();
+        c.setTime(start);
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 0);
+        return c.getTime();
+    }
+
+
+    /**
+     * Get earliest notion of java time
+     * 
+     * @return Beginning of time
+     */
+    public static Date getBeginningOfTime()
+    {
+        return new Date(0);
+    }
+    
+    
+    /**
+     * Convenience method to get the time of the current day preset to 
+     * 23:59:59 999 ms
+     * 
+     * @return End of current day
+     */
+    public static Date getEndOfDay()
+    {
+        return getEndOfDay(new Date());
+    }
+
+
+    /**
+     * Convenience method to get the time of the given day immediately 
+     * before it rolls over to the next day.
+     * 
+     * @param   d  Date to modify
+     * @return  Modified date for chaining
+     */
+    public static Date getEndOfDay(final Date d)
+    {
+        Date end = new Date(d.getTime());
+        Calendar c = Calendar.getInstance();
+        c.setTime(end);
+        c.set(Calendar.HOUR_OF_DAY, 23);
+        c.set(Calendar.MINUTE, 59);
+        c.set(Calendar.SECOND, 59);
+        c.set(Calendar.MILLISECOND, 999);
+        return c.getTime();
+    }
+    
+    /**
+     * Gets a date far out in the future
+     * 
+     * @return  Date
+     */
+    public static Date getEndOfTime()
+    {
+        Calendar c = Calendar.getInstance();
+        c.set(99999, 1, 1);
+        return c.getTime();
+    }
+    
 }
 
 /*
