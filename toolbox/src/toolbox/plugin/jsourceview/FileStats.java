14c711c4cd87aab4f4f7406643d261c702ed02e4
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-19 02:37:27 +0000

    Made members private

--- toolbox/src/toolbox/plugin/jsourceview/FileStats.java
+++ toolbox/src/toolbox/plugin/jsourceview/FileStats.java
@@ -1,53 +1,134 @@
 package toolbox.jsourceview;
 
 /**
- * FileStats 
+ * FileStats represents the statistics gathered for one or more source files
  */
 public class FileStats
 {
     /** Total number of lines in file **/    
-    public int totalLines;
+    private int totalLines_;
     
     /** Number of comment lines in file **/
-    public int commentLines;
+    private int commentLines_;
     
     /** Number of code lines in file **/
-    public int codeLines;
+    private int codeLines_;
     
     /** Number of blank lines in file **/
-    public int blankLines;
+    private int blankLines_;
     
     /**
      * No arg
      */
-    FileStats()
+    public FileStats()
     {
     }
     
     /**
-     * Gets percentage
+     * Gets percentage of source code lines to total lines
      * 
      * @return percentage
      */
     public int getPercent()
     {
-        if(codeLines > 0 && totalLines > 0)
+        if(codeLines_ > 0 && totalLines_ > 0)
             return (int)
-                (((float)codeLines / (float)(totalLines - blankLines)) * 100F);
+                (((float)codeLines_ / (float)
+                    (totalLines_ - blankLines_)) * 100F);
         else
             return 0;
     }
 
     /**
-     * Adds file stat to existing results
+     * Adds file stat to existing statistics
      * 
      * @param  filestats  Filestats to add
      */
     public void add(FileStats filestats)
     {
-        totalLines   += filestats.totalLines;
-        commentLines += filestats.commentLines;
-        codeLines    += filestats.codeLines;
-        blankLines   += filestats.blankLines;
+        totalLines_   += filestats.getTotalLines();
+        commentLines_ += filestats.getCommentLines();
+        codeLines_    += filestats.getCodeLines();
+        blankLines_   += filestats.getBlankLines();
+    }
+    
+    /**
+     * Returns the blankLines.
+     * 
+     * @return int
+     */
+    public int getBlankLines()
+    {
+        return blankLines_;
+    }
+
+    /**
+     * Returns the codeLines.
+     * 
+     * @return int
+     */
+    public int getCodeLines()
+    {
+        return codeLines_;
+    }
+
+    /**
+     * Returns the commentLines.
+     * 
+     * @return int
+     */
+    public int getCommentLines()
+    {
+        return commentLines_;
+    }
+
+    /**
+     * Returns the totalLines.
+     * 
+     * @return int
+     */
+    public int getTotalLines()
+    {
+        return totalLines_;
+    }
+
+    /**
+     * Sets the blankLines.
+     * 
+     * @param blankLines The blankLines to set
+     */
+    public void setBlankLines(int blankLines)
+    {
+        this.blankLines_ = blankLines;
+    }
+
+    /**
+     * Sets the codeLines.
+     * 
+     * @param codeLines The codeLines to set
+     */
+    public void setCodeLines(int codeLines)
+    {
+        this.codeLines_ = codeLines;
+    }
+
+    /**
+     * Sets the commentLines.
+     * 
+     * @param commentLines The commentLines to set
+     */
+    public void setCommentLines(int commentLines)
+    {
+        this.commentLines_ = commentLines;
+    }
+
+    /**
+     * Sets the totalLines.
+     * 
+     * @param totalLines The totalLines to set
+     */
+    public void setTotalLines(int totalLines)
+    {
+        this.totalLines_ = totalLines;
     }
 }
