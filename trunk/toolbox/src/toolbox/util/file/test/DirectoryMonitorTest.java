d90c7244a82f547423430762f9ad2398d04796cc
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-19 02:40:26 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/file/test/DirectoryMonitorTest.java
@@ -0,0 +1,23 @@
+package toolbox.util.file.test;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for DirectoryMonitor
+ */
+public class DirectoryMonitorTest extends TestCase
+{
+
+    /**
+     * Constructor for DirectoryMonitorTest.
+     * @param arg0
+     */
+    public DirectoryMonitorTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    public static void main(String[] args)
+    {
+    }
+}
