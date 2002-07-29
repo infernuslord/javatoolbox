d7bc8c1271a1e20d231973c60d0dca354e5c7560
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-29 05:26:05 +0000

    Line number and close support

--- toolbox/src/toolbox/plugin/jtail/TailPane.java
+++ toolbox/src/toolbox/plugin/jtail/TailPane.java
@@ -33,10 +33,12 @@ public class TailPane extends JPanel implements ActionListener
 	private JButton        clearButton_;
 	private JButton        pauseButton_;
 	private JButton        startButton_;
+    private JButton        closeButton_;
     private JCheckBox      autoScrollBox_;
+    private JCheckBox      lineNumbersBox_;
     
     private BlockingQueue    queue_;
-    private BatchQueueReader queueReader_;
+    private TailQueueReader  queueReader_;
     
     /** Output for tail **/
 	private JSmartTextArea tailArea_;
@@ -46,16 +48,31 @@ public class TailPane extends JPanel implements ActionListener
 
 
 	/**
-	 * Constructor for TailPanel.
+	 * Creates a TailPane with the given file. Autoscroll and line numbers
+     * are turned on by default.
      * 
      * @param  file  File to tail
 	 */
 	public TailPane(File file) throws FileNotFoundException
 	{
-		build();
-        init(file);
+        this(file, true, true);
 	}
 
+
+    /**
+     * Creates a TailPane with the given settings
+     * 
+     * @param  file             File to tail
+     * @param  autoScroll       If true, autoscroll will be enabled
+     * @param  showLineNumbers  If true, lines numbers will be shown
+     */
+    public TailPane(File file, boolean autoScroll, boolean showLineNumbers)
+        throws FileNotFoundException
+    {
+        buildView(autoScroll, showLineNumbers);
+        init(file);
+    }
+    
     
     /**
      * Init tail
@@ -71,7 +88,7 @@ public class TailPane extends JPanel implements ActionListener
         tail_.setTailFile(file);
         tail_.start();
         
-        queueReader_ = new FastQueueConsumer(queue_);
+        queueReader_ = new TailQueueReader(queue_);
         Thread consumer = new Thread(queueReader_);
         consumer.start();
         
@@ -80,18 +97,27 @@ public class TailPane extends JPanel implements ActionListener
 
 
     /**
-     * Pops thingies off the queue
+     * Pops groups of messages off the queue (as many as can be read without 
+     * waiting) and consolidates before sending then to the textarea
      */
-    class FastQueueConsumer extends BatchQueueReader
+    class TailQueueReader extends BatchQueueReader
     {
-        int lineNumber = 0;
-        
-        public FastQueueConsumer(BlockingQueue queue)
+        int lineNumber_ = 0;
+
+        /**
+         * Creates a queue consumer for the given queue
+         * 
+         * @param  queue  Queue to consume messages from
+         */        
+        public TailQueueReader(BlockingQueue queue)
         {
             super(queue);
         }
+
         
         /**
+         * Adds contents of queue to the output
+         *
          * @see toolbox.util.concurrent.BatchQueueReader#execute(Object[])
          */
         public void execute(Object[] objs)
@@ -101,9 +127,27 @@ public class TailPane extends JPanel implements ActionListener
             StringBuffer sb = new StringBuffer();
 
             for(int i=0; i<objs.length; i++)
-                sb.append(lineNumber++ + ": " + objs[i] + "\n");
+            {
+                lineNumber_++;
+                if (lineNumbersBox_.isSelected())
+                    sb.append("[" + lineNumber_ + "] ");
+                    
+                sb.append(objs[i] + "\n");
+                
+  
+            }
             
             tailArea_.append(sb.toString()); 
+        }
+        
+        
+        /**
+         * Resets line number back to zero. This is done with the tail is
+         * stopped and then restarted.
+         */
+        public void resetLines()
+        {
+            lineNumber_ = 0;
         }
     }
     
@@ -114,7 +158,8 @@ public class TailPane extends JPanel implements ActionListener
     class TailListener extends TailAdapter
     {
         /**
-         * Called when next line of input is available
+         * Called when next line of input is available. When a new line is
+         * available, just push it on the shared queue.
          */
         public void nextLine(String line)
         {
@@ -130,6 +175,9 @@ public class TailPane extends JPanel implements ActionListener
 
         
         /**
+         * When the tail is paused, change the text of the button because
+         * it has dual functionality
+         * 
          * @see toolbox.tail.TailAdapter#tailPaused()
          */
         public void tailPaused()
@@ -139,6 +187,9 @@ public class TailPane extends JPanel implements ActionListener
 
 
         /**
+         * When the tail is unpaused, change the text of the button because
+         * it has dual functionality
+         *
          * @see toolbox.tail.TailAdapter#tailUnpaused()
          */
         public void tailUnpaused()
@@ -148,16 +199,23 @@ public class TailPane extends JPanel implements ActionListener
 
 
         /**
+         * When the tail is started, change button text to "stop" and enabled
+         * the pause button
+         * 
          * @see toolbox.tail.TailAdapter#tailStarted()
          */
         public void tailStarted()
         {
             startButton_.setText("Stop");
             pauseButton_.setEnabled(true);
+            queueReader_.resetLines();
         }
 
 
         /**
+         * When the tail is stopped, change the text to "start" and disable
+         * the pause button because there is nothing to pause
+         * 
          * @see toolbox.tail.TailAdapter#tailStopped()
          */
         public void tailStopped()
@@ -171,26 +229,33 @@ public class TailPane extends JPanel implements ActionListener
 	/**
 	 * Builds the GUI
 	 */	
-	protected void build()
+	protected void buildView(boolean autoScroll, boolean showLineNumbers)
 	{
-		tailArea_ = new JSmartTextArea(false);
+		tailArea_ = new JSmartTextArea(autoScroll);
         tailArea_.setFont(SwingUtil.getPreferredMonoFont());
         
-		clearButton_   = new JButton("Clear");
-		pauseButton_   = new JButton("Pause");
-		startButton_     = new JButton("Start");
-        autoScrollBox_ = new JCheckBox("Autoscroll");
+		clearButton_    = new JButton("Clear");
+		pauseButton_    = new JButton("Pause");
+		startButton_    = new JButton("Start");
+        closeButton_    = new JButton("Close");
+        autoScrollBox_  = new JCheckBox("Autoscroll", autoScroll);
+        lineNumbersBox_ = new JCheckBox("Line Numbers", showLineNumbers);
+        
         
 		JPanel buttonPanel = new JPanel(new FlowLayout());
 		buttonPanel.add(startButton_);
 		buttonPanel.add(pauseButton_);
 		buttonPanel.add(clearButton_);
+        buttonPanel.add(closeButton_);
         buttonPanel.add(autoScrollBox_);
+        buttonPanel.add(lineNumbersBox_);
 
 		startButton_.addActionListener(this);
 		pauseButton_.addActionListener(this);
 		clearButton_.addActionListener(this);
+        closeButton_.addActionListener(this);
         autoScrollBox_.addActionListener(this);
+        lineNumbersBox_.addActionListener(this);
 		
 		setLayout(new BorderLayout());
 		add(new JScrollPane(tailArea_), BorderLayout.CENTER);
@@ -199,7 +264,7 @@ public class TailPane extends JPanel implements ActionListener
 	
     
 	/**
-	 * ActionListner interface
+	 * Handles all widget button clicks and checkbox state changes
      * 
      * @param  e  ActionEvent
 	 */
@@ -215,8 +280,12 @@ public class TailPane extends JPanel implements ActionListener
     			pauseButtonClicked();
     		else if (obj == clearButton_)
     			clearButtonClicked();
+            else if (obj == closeButton_)
+                closeButtonClicked();
             else if (obj == autoScrollBox_)
                 autoScrollBoxChanged();
+            else if (obj == lineNumbersBox_)
+                lineNumbersBoxChanged();
     		else        
     			logger_.warn("No action handler for " + e);
         }
@@ -263,13 +332,58 @@ public class TailPane extends JPanel implements ActionListener
 		logger_.info("clear");
         tailArea_.setText("");
 	}	
-    
+
+
+    /**
+     * Close button clicked
+     */
+    protected void closeButtonClicked()
+    {
+        logger_.info("close");
+        
+        if (tail_.isPaused())
+            tail_.unpause();
+            
+        if (tail_.isAlive())
+            tail_.stop();
+            
+        firePaneClosing();            
+    }   
+
+
+    /** 
+     * Spread closing event
+     */
+    protected void firePaneClosing()
+    {
+                
+    }    
+
     
     /**
-     * Sets autoscroll on text area
+     * Called when the user has toggled the autoscroll checkbox
      */
     protected void autoScrollBoxChanged()
     {
         tailArea_.setAutoScroll(autoScrollBox_.isSelected());
     }
+
+    
+    /**
+     * Called when the user has toggled the show line numbers checkbox
+     */
+    protected void lineNumbersBoxChanged()
+    {
+        // no op since the checkbox is queried directly for its state
+    }
+ 
+ 
+    /**
+     * @return Close button
+     */
+    public JButton getCloseButton()
+    {
+        return closeButton_;
+    }
+    
 }
