3e5c11f62035537d36e49e354bc8b315c6b2359d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-07-30 03:03:29 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/ui/JSmartStatusBar.java
@@ -0,0 +1,31 @@
+package toolbox.util.ui;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+/**
+ * JSmartStatusBar
+ */
+public class JSmartStatusBar extends JPanel
+{
+
+    /**
+     * Constructor for JSmartStatusBar.
+     */
+    public JSmartStatusBar()
+    {
+    }
+
+
+    /**
+     * Constructor for JSmartStatusBar.
+     * @param layout
+     */
+    public JSmartStatusBar(LayoutManager layout)
+    {
+        super(layout);
+    }
+
+
+}
