56560da04fe4b21ce6132672a2713dcbc05c3c8e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-08-06 04:21:19 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/font/test/FontSelectionDialogTest.java
@@ -0,0 +1,69 @@
+package toolbox.util.ui.font.test;
+
+import javax.swing.JFrame;
+import junit.framework.TestCase;
+
+import org.apache.log4j.Category;
+import toolbox.util.ui.font.FontSelectionDialog;
+import toolbox.util.ui.font.FontSelectionPanel;
+import toolbox.util.ui.font.IFontDialogListener;
+
+/**
+ * Unit test for FontSelectionDialog
+ */
+public class FontSelectionDialogTest extends TestCase
+{
+    private static final Category logger_ = 
+        Category.getInstance(FontSelectionDialogTest.class);
+        
+    /**
+     * Entry point
+     * 
+     * @param  args  None recognized
+     */
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(FontSelectionDialogTest.class);
+    }
+
+    /**
+     * Constructor for FontSelectionDialogTest.
+     * 
+     * @param arg0 Name
+     */
+    public FontSelectionDialogTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    /**
+     * Test for void FontSelectionPanel()
+     */
+    public void testFontSelectionPanel()
+    {
+        IFontDialogListener fsdListener = new IFontDialogListener()
+        {
+            public void okButtonPressed(FontSelectionPanel fontPanel)
+            {
+                logger_.info("OK button pressed");                
+            }
+
+            public void cancelButtonPressed(FontSelectionPanel fontPanel)
+            {
+                logger_.info("Cancel button pressed");                
+            }
+
+            public void applyButtonPressed(FontSelectionPanel fontPanel)
+            {
+                logger_.info("Apply button pressed");
+            }
+        };
+        
+        FontSelectionDialog fsd = 
+            new FontSelectionDialog(new JFrame(), "Select font", false);
+            
+        fsd.addFontDialogListener(fsdListener);
+        
+        fsd.setVisible(true);            
+    }
+}
