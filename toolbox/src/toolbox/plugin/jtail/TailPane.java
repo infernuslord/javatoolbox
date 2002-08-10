93303ff62cb77117d9b57318987f07283e5fed83
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-08-10 03:12:41 +0000

    Migrated to Action based listeners

--- toolbox/src/toolbox/plugin/jtail/TailPane.java
+++ toolbox/src/toolbox/plugin/jtail/TailPane.java
@@ -7,10 +7,14 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 import org.apache.log4j.Category;
 import toolbox.tail.Tail;
@@ -36,6 +40,7 @@ public class TailPane extends JPanel implements ActionListener
     private JButton        closeButton_;
     private JCheckBox      autoScrollBox_;
     private JCheckBox      lineNumbersBox_;
+    private JTextField     filterField_;
     
     private BlockingQueue    queue_;
     private TailQueueReader  queueReader_;
@@ -80,140 +85,11 @@ public class TailPane extends JPanel implements ActionListener
         tail_ = new Tail();
         tail_.addTailListener(new TailListener());
         tail_.setTailFile(config_.getFilename());
-        tail_.start();
+        startButton_.getAction().actionPerformed(new ActionEvent(this, 0, "Start"));
+        //tail_.start();
         
         logger_.debug("tail and reader started");
     }
-
-
-    /**
-     * Pops groups of messages off the queue (as many as can be read without 
-     * waiting) and consolidates before sending then to the textarea
-     */
-    class TailQueueReader extends BatchQueueReader
-    {
-        int lineNumber_ = 0;
-
-        /**
-         * Creates a queue consumer for the given queue
-         * 
-         * @param  queue  Queue to consume messages from
-         */        
-        public TailQueueReader(BlockingQueue queue)
-        {
-            super(queue);
-        }
-
-        
-        /**
-         * Adds contents of queue to the output
-         *
-         * @see toolbox.util.concurrent.BatchQueueReader#execute(Object[])
-         */
-        public void execute(Object[] objs)
-        {
-            if (objs.length > 1)
-                logger_.debug("Lines popped: " + objs.length);
-            
-            StringBuffer sb = new StringBuffer();
-
-            for(int i=0; i<objs.length; i++)
-            {
-                lineNumber_++;
-                if (lineNumbersBox_.isSelected())
-                    sb.append("[" + lineNumber_ + "] ");
-                    
-                sb.append(objs[i] + "\n");
-            }
-            
-            tailArea_.append(sb.toString()); 
-        }
-        
-        
-        /**
-         * Resets line number back to zero. This is done with the tail is
-         * stopped and then restarted.
-         */
-        public void resetLines()
-        {
-            lineNumber_ = 0;
-        }
-    }
-    
-    
-    /**
-     * Listener for tail
-     */
-    class TailListener extends TailAdapter
-    {
-        /**
-         * Called when next line of input is available. When a new line is
-         * available, just push it on the shared queue.
-         */
-        public void nextLine(String line)
-        {
-            try
-            {
-                queue_.push(line);
-            }
-            catch (InterruptedException ie)
-            {
-                JSmartOptionPane.showExceptionMessageDialog(null, ie);
-            }
-        }
-
-        
-        /**
-         * When the tail is paused, change the text of the button because
-         * it has dual functionality
-         * 
-         * @see toolbox.tail.TailAdapter#tailPaused()
-         */
-        public void tailPaused()
-        {
-            pauseButton_.setText("Unpause");
-        }
-
-
-        /**
-         * When the tail is unpaused, change the text of the button because
-         * it has dual functionality
-         *
-         * @see toolbox.tail.TailAdapter#tailUnpaused()
-         */
-        public void tailUnpaused()
-        {
-            pauseButton_.setText("Pause");            
-        }
-
-
-        /**
-         * When the tail is started, change button text to "stop" and enabled
-         * the pause button
-         * 
-         * @see toolbox.tail.TailAdapter#tailStarted()
-         */
-        public void tailStarted()
-        {
-            startButton_.setText("Stop");
-            pauseButton_.setEnabled(true);
-            queueReader_.resetLines();
-        }
-
-
-        /**
-         * When the tail is stopped, change the text to "start" and disable
-         * the pause button because there is nothing to pause
-         * 
-         * @see toolbox.tail.TailAdapter#tailStopped()
-         */
-        public void tailStopped()
-        {
-            startButton_.setText("Start");
-            pauseButton_.setEnabled(false);
-        }
-    }
-
     
 	/**
 	 * Builds the GUI
@@ -224,12 +100,13 @@ public class TailPane extends JPanel implements ActionListener
         tailArea_.setFont(SwingUtil.getPreferredMonoFont());
         tailArea_.setDoubleBuffered(false);
         
-		clearButton_    = new JButton("Clear");
-		pauseButton_    = new JButton("Pause");
-		startButton_    = new JButton("Start");
-        closeButton_    = new JButton("Close");
+		clearButton_    = new JButton(new ClearAction());
+		pauseButton_    = new JButton(new PauseUnpauseAction());
+		startButton_    = new JButton(new StartStopAction());
+        closeButton_    = new JButton(new CloseAction());
         autoScrollBox_  = new JCheckBox("Autoscroll");
         lineNumbersBox_ = new JCheckBox("Line Numbers");
+        filterField_    = new JTextField(12);
         
 		JPanel buttonPanel = new JPanel(new FlowLayout());
 		buttonPanel.add(startButton_);
@@ -238,11 +115,10 @@ public class TailPane extends JPanel implements ActionListener
         buttonPanel.add(closeButton_);
         buttonPanel.add(autoScrollBox_);
         buttonPanel.add(lineNumbersBox_);
+        buttonPanel.add(new JLabel("Filter"));
+        buttonPanel.add(filterField_);
+        
 
-		startButton_.addActionListener(this);
-		pauseButton_.addActionListener(this);
-		clearButton_.addActionListener(this);
-        closeButton_.addActionListener(this);
         autoScrollBox_.addActionListener(this);
         lineNumbersBox_.addActionListener(this);
 		
@@ -263,15 +139,7 @@ public class TailPane extends JPanel implements ActionListener
 		
         try
         {
-    		if (obj == startButton_)
-    			startButtonClicked();
-    		else if (obj == pauseButton_)
-    			pauseButtonClicked();
-    		else if (obj == clearButton_)
-    			clearButtonClicked();
-            else if (obj == closeButton_)
-                closeButtonClicked();
-            else if (obj == autoScrollBox_)
+            if (obj == autoScrollBox_)
                 autoScrollBoxChanged();
             else if (obj == lineNumbersBox_)
                 lineNumbersBoxChanged();
@@ -283,59 +151,6 @@ public class TailPane extends JPanel implements ActionListener
             JSmartOptionPane.showExceptionMessageDialog(null, ee);
         }
 	}
-	
-    
-	/**
-	 * Starts the tail
-	 */
-	protected void startButtonClicked() throws FileNotFoundException
-	{
-		logger_.info("start");
-        
-        if (tail_.isAlive())
-            tail_.stop();
-        else
-            tail_.start();
-	}
-	
-    
-	/**
-	 * Pauses the tail
-	 */
-	protected void pauseButtonClicked()
-	{
-		logger_.info("pause");
-        
-        if (tail_.isPaused())
-            tail_.unpause();
-        else
-            tail_.pause();
-	}
-	
-    
-	/**
-	 * Clears the tail output 
-	 */
-	protected void clearButtonClicked()
-	{
-		logger_.info("clear");
-        tailArea_.setText("");
-	}	
-
-
-    /**
-     * Close button clicked
-     */
-    protected void closeButtonClicked()
-    {
-        logger_.info("close");
-        
-        if (tail_.isPaused())
-            tail_.unpause();
-            
-        if (tail_.isAlive())
-            tail_.stop();
-    }   
 
     
     /**
@@ -414,5 +229,257 @@ public class TailPane extends JPanel implements ActionListener
     {
         tailArea_.setFont(font);
         config_.setFont(font);
+    }
+    
+    
+    /**
+     * @return Filter text
+     */
+    public String getFilter()
+    {
+        return filterField_.getText().trim();
+    }
+    
+    
+    /**
+     * Sets the filter text
+     * 
+     * @param  filter  Filter text as a regular expression
+     */
+    public void setFilter(String filter)
+    {
+        filterField_.setText(filter);
+    }
+
+
+    /**
+     * Listener for tail
+     */
+    class TailListener extends TailAdapter
+    {
+        /**
+         * Called when next line of input is available. When a new line is
+         * available, just push it on the shared queue.
+         * 
+         * @param  line  Next line read
+         */
+        public void nextLine(String line)
+        {
+            try
+            {
+                queue_.push(line);
+            }
+            catch (InterruptedException ie)
+            {
+                JSmartOptionPane.showExceptionMessageDialog(null, ie);
+            }
+        }
+    }
+
+    
+    /**
+     * Pops groups of messages off the queue (as many as can be read without 
+     * waiting) and consolidates before sending then to the textarea
+     */
+    private class TailQueueReader extends BatchQueueReader
+    {
+        int lineNumber_ = 0;
+
+        /**
+         * Creates a queue consumer for the given queue
+         * 
+         * @param  queue  Queue to consume messages from
+         */        
+        public TailQueueReader(BlockingQueue queue)
+        {
+            super(queue);
+        }
+
+        
+        /**
+         * Adds contents of queue to the output
+         *
+         * @see toolbox.util.concurrent.BatchQueueReader#execute(Object[])
+         */
+        public void execute(Object[] objs)
+        {
+            if (objs.length > 1)
+                logger_.debug("Lines popped: " + objs.length);
+            
+            StringBuffer sb = new StringBuffer();
+
+            for(int i=0; i<objs.length; i++)
+            {
+                lineNumber_++;
+                if (lineNumbersBox_.isSelected())
+                    sb.append("[" + lineNumber_ + "] ");
+                    
+                sb.append(objs[i] + "\n");
+            }
+            
+            tailArea_.append(sb.toString()); 
+        }
+        
+        
+        /**
+         * Resets line number back to zero. This is done with the tail is
+         * stopped and then restarted.
+         */
+        public void resetLines()
+        {
+            lineNumber_ = 0;
+        }
+    }
+   
+    
+    /**
+     * Starts/stops the tail
+     */
+    private class StartStopAction extends AbstractAction
+    {
+        private static final String MODE_START = "Start";
+        private static final String MODE_STOP  = "Stop";
+        
+        private String mode = MODE_START;
+            
+        /**
+         * Default constructor
+         */
+        public StartStopAction()
+        {
+            super(MODE_START);
+            putValue(MNEMONIC_KEY, new Integer('S'));
+            putValue(SHORT_DESCRIPTION, "Starts/Stops the tail");
+        }
+    
+        /**
+         * Stops the tail if it is alraedy alive and then starts the tail
+         * 
+         * @param  e    ActionEvent
+         */
+        public void actionPerformed(ActionEvent e)
+        { 
+            if (mode.equals(MODE_START))
+            {
+                try
+                {
+                    tail_.start();
+                    mode = MODE_STOP;
+                    putValue(Action.NAME, mode);
+                    pauseButton_.setEnabled(true);
+                    queueReader_.resetLines();
+                    logger_.debug("Started tail: " + tail_.getFile());                                                     
+                }
+                catch(FileNotFoundException fnfe)
+                {
+                    logger_.error(e);
+                }
+            }
+            else
+            {
+                tail_.stop();
+                mode = MODE_START;
+                putValue(Action.NAME, mode);                
+                pauseButton_.setEnabled(false);
+                logger_.debug("Stopped tail: " + tail_.getFile());                
+            }
+        }
+    }
+
+    /**
+     * Pauses/unpauses the tail
+     */
+    private class PauseUnpauseAction extends AbstractAction
+    {
+        private static final String MODE_PAUSE   = "Pause";
+        private static final String MODE_UNPAUSE = "Unpause";
+            
+        /**
+         * Default constructor
+         */
+        public PauseUnpauseAction()
+        {
+            super(MODE_PAUSE);
+            putValue(MNEMONIC_KEY, new Integer('P'));
+            putValue(SHORT_DESCRIPTION, "Pause/Unpauses the tail");
+        }
+    
+        /**
+         * Pauses/unpauses the tail based on the current state
+         * 
+         * @param  e    ActionEvent
+         */
+        public void actionPerformed(ActionEvent e)
+        { 
+            if (tail_.isPaused())
+            {
+                tail_.unpause();
+                putValue(Action.NAME, MODE_PAUSE);                
+            }
+            else
+            {
+                tail_.pause();
+                putValue(Action.NAME, MODE_UNPAUSE);                                
+            }
+        }
+    }
+    
+
+    /**
+     * Closes the tail pane
+     */
+    private class CloseAction extends AbstractAction
+    {
+        /**
+         * Default constructor
+         */
+        public CloseAction()
+        {
+            super("Close");
+            putValue(MNEMONIC_KEY, new Integer('C'));
+            putValue(SHORT_DESCRIPTION, "Closes the tail pane");
+        }
+    
+        /**
+         * Closes the tail pane
+         * 
+         * @param  e    ActionEvent
+         */
+        public void actionPerformed(ActionEvent e)
+        { 
+            if (tail_.isPaused())
+                tail_.unpause();
+                
+            if (tail_.isAlive())
+                tail_.stop();
+                
+            queueReader_.shutdown();
+        }
+    }
+
+    /**
+     * Clears the output area
+     */
+    private class ClearAction extends AbstractAction
+    {
+        /**
+         * Default constructor
+         */
+        public ClearAction()
+        {
+            super("Clear");
+            putValue(MNEMONIC_KEY, new Integer('r'));
+            putValue(SHORT_DESCRIPTION, "Clears the output area");
+        }
+    
+        /**
+         * Clears the output area
+         * 
+         * @param  e    ActionEvent
+         */
+        public void actionPerformed(ActionEvent e)
+        { 
+            tailArea_.setText("");
+        }
     }
 }
