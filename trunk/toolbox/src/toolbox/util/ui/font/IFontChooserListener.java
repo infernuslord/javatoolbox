32b4cbb440765b8ddbd523716f306f943777ddda
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-08-25 19:05:13 +0000

    Replaced IFontSelectionListener

---
+++ trunk/toolbox/src/toolbox/util/ui/font/IFontChooserListener.java
@@ -0,0 +1,12 @@
+package toolbox.util.ui.font;
+
+/**
+ * IFontChooserListener
+ */
+public interface IFontChooserListener
+{   
+	/**
+	 * Font selection has changed
+	 */
+    public void fontChanged();
+}
