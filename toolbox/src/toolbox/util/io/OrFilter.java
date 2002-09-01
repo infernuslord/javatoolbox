3f63f51ca06096c19db1a7ec091cf10273cfd765
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-09-01 04:01:03 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/io/OrFilter.java
@@ -0,0 +1,54 @@
+package toolbox.util.io;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Iterator;
+
+/**
+ * Filename filter that behaves as a logical OR for one or more given filters
+ */
+public class OrFilter extends CompoundFilter implements FilenameFilter
+{
+    
+    /**
+     * Default constructor
+     */    
+    public OrFilter()
+    {
+    }
+    
+    /**
+     * Creates a filter that ORs the given filters
+     * 
+     * @param  filterOne   First filter
+     * @param  filterTwo   Second filter
+     */   
+    public OrFilter(FilenameFilter filterOne, FilenameFilter filterTwo)
+    {
+        addFilter(filterOne);
+        addFilter(filterTwo);
+    }
+    
+    /**
+     * Accepts files based on the logical OR of the added filters
+     * 
+     * @param    dir   Directory file is contained in
+     * @param    name  Name of file
+     * @return   True if logical OR is true, false otherwise
+     */
+    public boolean accept(File dir,String name)
+    {
+        Iterator i = iterator();
+        
+        while (i.hasNext())
+        {
+            FilenameFilter f = (FilenameFilter)i.next();
+            
+            // Short circuit on first TRUE
+            if (f.accept(dir, name))     
+                return true;
+        }
+       
+        return false;
+    }
+}
