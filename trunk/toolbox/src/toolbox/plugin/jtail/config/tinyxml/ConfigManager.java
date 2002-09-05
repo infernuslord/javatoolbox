7851e5197540c4de8d71e2455929db2fa473f08b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-09-05 04:36:55 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/plugin/jtail/config/tinyxml/ConfigManager.java
@@ -0,0 +1,128 @@
+package toolbox.jtail.config.tinyxml;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.apache.log4j.Category;
+import toolbox.jtail.config.IConfigManager;
+import toolbox.jtail.config.IJTailConfig;
+import toolbox.jtail.config.ITailPaneConfig;
+import toolbox.util.FileUtil;
+import toolbox.util.SwingUtil;
+import toolbox.util.xml.XMLNode;
+import toolbox.util.xml.XMLParser;
+
+/**
+ * Configuration manager for tiny xml parser
+ */
+public class ConfigManager implements IConfigManager
+{
+    private static final Category logger_ = 
+        Category.getInstance(ConfigManager.class);
+        
+    private static final String CONFIG_FILE = ".jtail.xml";
+
+
+    //--------------------------------------------------------------------------
+    //  Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Default consturctor
+     */
+    public ConfigManager()
+    {
+    }
+
+    //--------------------------------------------------------------------------
+    //  Interfaces - IConfigManager
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.jtail.IConfigManager#save(IJTailConfig)
+     */
+    public void save(IJTailConfig jtailConfig)
+    {
+        JTailConfig config = (JTailConfig)jtailConfig;
+        
+        String userHome = System.getProperty("user.home");
+        String filename = userHome + File.separator + CONFIG_FILE;
+        File configFile = new File(filename);
+
+        try
+        {
+            String xmlString = config.marshal().toString();
+            FileUtil.setFileContents(configFile, xmlString, false);
+            logger_.debug("\n" + xmlString);
+        }
+        catch (IOException ioe)
+        {
+            throw new IllegalArgumentException(ioe.getMessage());
+        }
+    }
+
+    /**
+     * @see toolbox.jtail.IConfigManager#load()
+     */
+    public IJTailConfig load()
+    {
+        String userHome = System.getProperty("user.home");
+        String filename = userHome + File.separator + CONFIG_FILE;
+        File    xmlFile = new File(filename);
+
+        // Create jtailConfig with defaults        
+        IJTailConfig jtailConfig = new JTailConfig();
+        
+        jtailConfig.setDefaultConfig(new TailPaneConfig(
+            null,
+            ITailPaneConfig.DEFAULT_AUTOSCROLL,
+            ITailPaneConfig.DEFAULT_LINENUMBERS,
+            ITailPaneConfig.DEFAULT_ANTIALIAS,
+            SwingUtil.getPreferredMonoFont(),
+            ITailPaneConfig.DEFAULT_FILTER));
+            
+        jtailConfig.setTailConfigs(new TailPaneConfig[0]);
+                
+        if (!xmlFile.exists())
+        {
+            logger_.debug("No XML configuration present. Using defaults.");
+        }
+        else if (!xmlFile.canRead())
+        {
+            throw new IllegalArgumentException(
+                "Cannot read configuration from " + filename + ". " + 
+                "Using defaults.");
+        }
+        else if (!xmlFile.isFile())
+        {
+            throw new IllegalArgumentException(
+                "Configuration file " + filename + 
+                " cannot be a directory. Using defaults.");
+        }
+        else
+        {
+            try
+            {
+                XMLParser parser = new XMLParser();
+                XMLNode jtailNode = parser.parseXML(new FileReader(xmlFile));
+                jtailConfig = JTailConfig.unmarshal(jtailNode);
+            }
+            catch (Exception pe)
+            {
+                throw new IllegalArgumentException(pe.getMessage());       
+            }
+        }
+        return jtailConfig;
+    }
+
+    
+    /**
+     * @see toolbox.jtail.config.IConfigManager#createTailPaneConfig()
+     */
+    public ITailPaneConfig createTailPaneConfig()
+    {
+        return new TailPaneConfig();
+    }
+
+}
---
+++ trunk/toolbox/src/toolbox/plugin/jtail/config/tinyxml/JTailConfig.java
@@ -0,0 +1,290 @@
+package toolbox.jtail.config.tinyxml;
+
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Point;
+import java.io.IOException;
+import java.util.Enumeration;
+
+import org.apache.log4j.Category;
+import toolbox.jtail.config.IJTailConfig;
+import toolbox.jtail.config.ITailPaneConfig;
+import toolbox.util.ArrayUtil;
+import toolbox.util.xml.XMLNode;
+
+
+/**
+ * Tiny XML implemenation of IJTailConfig interface that marshals
+ * configuration information to/from XML
+ */
+public class JTailConfig implements IJTailConfig, XMLConstants
+{ 
+    /** Logger **/
+    private static final Category logger_ = 
+        Category.getInstance(JTailConfig.class);
+    
+    private Point               location_;
+    private Dimension           size_;
+    private String              directory_;
+    private ITailPaneConfig[]   tailPaneConfigs_;
+    private ITailPaneConfig     defaultConfig_;
+    
+    /**
+     * Constructor for JTailConfig.
+     */
+    public JTailConfig()
+    {
+        defaultConfig_ = new TailPaneConfig();
+        tailPaneConfigs_ = new TailPaneConfig[0];
+    }
+
+
+    /**
+     * Marshals from IJTailConfig -> XML
+     * 
+     * @return  JTail XML node
+     * @throws  IOExcetion on IO error
+     */
+    public XMLNode marshal()  throws IOException 
+    {
+        String method = "[marshl] ";
+        
+        // Root config node
+        XMLNode jtailNode = new XMLNode(ELEMENT_JTAIL);
+        
+        // Size
+        jtailNode.addAttr(ATTR_HEIGHT, getSize().height + "");
+        jtailNode.addAttr(ATTR_WIDTH, getSize().width + "");
+        
+        // Location
+        jtailNode.addAttr(ATTR_X, getLocation().x + "");
+        jtailNode.addAttr(ATTR_Y, getLocation().y + "");
+
+        // Directory
+        if (getDirectory() != null)
+            jtailNode.addAttr(ATTR_DIR, getDirectory());
+
+        // Defaults
+        XMLNode defaultTailNode = ((TailPaneConfig) defaultConfig_).marshal();        
+        XMLNode defaultsNode = new XMLNode(ELEMENT_DEFAULTS);
+        defaultsNode.addNode(defaultTailNode);
+        
+        jtailNode.addNode(defaultsNode);
+        
+        // Save child ITailPaneConfigs
+        logger_.debug(method + 
+            "Saving " + tailPaneConfigs_.length + " configurations");
+
+        for (int i=0; i<tailPaneConfigs_.length; i++)
+        {
+            TailPaneConfig tpc = (TailPaneConfig) tailPaneConfigs_[i];
+            jtailNode.addNode(tpc.marshal());                
+        }
+            
+        return jtailNode;
+    }    
+    
+
+    /**
+     * Converts XML -> IJTailConfig
+     * 
+     * @param   jtailNode  Element representing a IJTailConfig
+     * @return  Fully populated IJTailConfig
+     * @throws  IOException on IO error
+     */
+    public static IJTailConfig unmarshal(XMLNode jtailNode) throws IOException 
+    {
+        String method = "[unmars] ";
+        
+        IJTailConfig jtailConfig = new JTailConfig();
+        
+        // Read optional window location
+        String x = jtailNode.getAttr(ATTR_X);
+        String y = jtailNode.getAttr(ATTR_Y);
+        
+        if (x != null && y != null)
+        {
+            jtailConfig.setLocation(
+                new Point(Integer.parseInt(x), Integer.parseInt(y)));
+        }
+        else
+        {
+            // TODO: set default location
+        }
+        
+        // Read optional window size
+        String height = jtailNode.getAttr(ATTR_HEIGHT);
+        String width  = jtailNode.getAttr(ATTR_WIDTH);
+        
+        if (height != null && width != null)
+        {
+            jtailConfig.setSize(
+                new Dimension(Integer.parseInt(width),Integer.parseInt(height)));
+        }
+        else
+        {
+            // TODO: set default size
+        }
+        
+        // Read optional directory
+        String dir = jtailNode.getAttr(ATTR_DIR);
+        
+        if (dir != null)   
+        {
+            jtailConfig.setDirectory(dir);
+        }
+        else
+        {
+            // TODO: set default directory
+        }
+        
+        XMLNode defaultsNode = jtailNode.getNode(ELEMENT_DEFAULTS);
+        
+        if (defaultsNode != null)
+        {
+            XMLNode defaultTailNode = defaultsNode.getNode(ELEMENT_TAIL);
+            
+            if (defaultTailNode != null)
+            {
+                jtailConfig.setDefaultConfig(
+                    TailPaneConfig.unmarshal(defaultTailNode));
+            }
+            else
+            {
+                logger_.warn(method + "Expected XML node JTail->Defaults->Tail");
+                jtailConfig.setDefaultConfig(new TailPaneConfig());
+            }
+        }
+        else
+        {
+            logger_.warn(method + "Expected XML node JTail->Defaults");
+            jtailConfig.setDefaultConfig(new TailPaneConfig());            
+        }
+         
+                        
+        // Iterate through each "tail" element and delegate the 
+        // hydration to the TailPaneConfig object
+        ITailPaneConfig[] tailPaneConfigs = new ITailPaneConfig[0];
+        
+        for (Enumeration tails = jtailNode.enumerateNode();
+             tails.hasMoreElements();)
+        {
+            XMLNode tail = (XMLNode) tails.nextElement();
+            
+            if (tail.getTagName().equals(TailPaneConfig.ELEMENT_TAIL))
+            {
+                ITailPaneConfig tailPaneConfig = TailPaneConfig.unmarshal(tail);                    
+                
+                tailPaneConfigs = (ITailPaneConfig[])
+                    ArrayUtil.add(tailPaneConfigs, tailPaneConfig);
+            }
+        }
+
+        jtailConfig.setTailConfigs(tailPaneConfigs);
+        
+        return jtailConfig;
+    }
+    
+
+
+    /**
+     * Sets the default tail pane configuration
+     * 
+     * @param defaultConfig  The default tail pane configuration
+     */
+    public void setDefaultConfig(ITailPaneConfig defaultConfig)
+    {
+        defaultConfig_ = defaultConfig;
+    }
+
+    
+    /**
+     * Returns the default tail pane configuration
+     * 
+     * @return  ITailPaneConfig
+     */
+    public ITailPaneConfig getDefaultConfig()
+    {
+        return defaultConfig_;
+    }
+
+
+    /**
+     * Sets the location.
+     * 
+     * @param location The location to set
+     */
+    public void setLocation(Point location)
+    {
+        location_ = location;
+    }
+
+
+    /**
+     * Returns the location.
+     * 
+     * @return Point
+     */
+    public Point getLocation()
+    {
+        return location_;
+    }
+
+
+    /**
+     * Sets the size.
+     * @param size The size to set
+     */
+    public void setSize(Dimension size)
+    {
+        size_ = size;
+    }
+
+    /**
+     * Returns the size.
+     * @return Dimension
+     */
+    public Dimension getSize()
+    {
+        return size_;
+    }
+    
+    /**
+     * Returns the tailPaneConfigs.
+     * @return ITailPaneConfig[]
+     */
+    public ITailPaneConfig[] getTailConfigs()
+    {
+        return tailPaneConfigs_;
+    }
+
+    /**
+     * Sets the tailPaneConfigs.
+     * @param tailPaneConfigs The tailPaneConfigs to set
+     */
+    public void setTailConfigs(ITailPaneConfig[] tailPaneConfigs)
+    {
+        tailPaneConfigs_ = tailPaneConfigs;
+    }
+    
+
+    /**
+     * Returns the last directory selecting in the file explorer pane
+     *
+     * @return  String
+     */
+    public String getDirectory()
+    {
+        return directory_;
+    }
+
+    /**
+     * Sets the last directory selected in the file explorer pane
+     * 
+     * @param directory  Directory selected
+     */
+    public void setDirectory(String directory)
+    {
+        directory_ = directory;
+    }
+}
