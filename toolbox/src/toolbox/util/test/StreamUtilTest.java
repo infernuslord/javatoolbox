ac50e0db518406a03cde3b16b927a148d28bdb07
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-10-02 02:15:21 +0000

    Added more tests

--- toolbox/src/toolbox/util/test/StreamUtilTest.java
+++ toolbox/src/toolbox/util/test/StreamUtilTest.java
@@ -1,5 +1,7 @@
 package toolbox.util.test;
 
+import java.io.StringReader;
+
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 import toolbox.util.StreamUtil;
@@ -39,6 +41,65 @@ public class StreamUtilTest extends TestCase
         assertEquals("toString() does not match original string", 
             testString, toString);
     }
+    
+    /**
+     * Tests empty()
+     */
+    public void testEmpty() throws Exception
+    {
+        /* create stream and populate */
+        String contents = "testing";
+        StringInputStream sis = new StringInputStream(contents);
+        
+        /* verify available */
+        assertEquals("stream should have contents", contents.length(),
+            sis.available());
+        
+        /* empty the stream */
+        StreamUtil.empty(sis);
+     
+        /* verify empty */
+        assertEquals("available() should be 0", 0, sis.available());
+        assertEquals("read() should be -1", -1, sis.read());
+    }
+    
+    /**
+     * Tests readExactly() for a stream
+     */
+    public void testReadExactlyStream() throws Exception
+    {
+        /* create stream and populate */
+        String contents = "testing";
+        StringInputStream sis = new StringInputStream(contents);
+        
+        /* read in 2 passes, 4 bytes first then 3 bytes next */
+        byte[] pass1 = StreamUtil.readExactly(sis, 4);
+        byte[] pass2 = StreamUtil.readExactly(sis, 3); 
+     
+        /* verify */
+        assertEquals("first pass does not match", "test", new String(pass1));
+        assertEquals("second pass does not match", "ing", new String(pass2));
+    }
+    
+    /**
+     * Tests readExactly() for a reader
+     */
+    public void testReadExactlyReader() throws Exception
+    {
+        /* create stream and populate */
+        String contents = "testing";
+        StringReader sr = new StringReader(contents);
+        
+        /* read in 2 passes, 4 bytes first then 3 bytes next */
+        String pass1 = StreamUtil.readExactly(sr, 4);
+        String pass2 = StreamUtil.readExactly(sr, 3); 
+     
+        /* verify */
+        assertEquals("first pass does not match", "test", pass1);
+        assertEquals("second pass does not match", "ing", pass2);
+    }
+    
+    
 }
 
 
