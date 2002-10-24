4616f2e8508b0f9ffaca1cdfb3fb43b6510cb506
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-10-24 04:45:13 +0000

    Added RMB

--- toolbox/src/toolbox/util/ui/JSmartTextArea.java
+++ toolbox/src/toolbox/util/ui/JSmartTextArea.java
@@ -5,28 +5,42 @@ import java.awt.GradientPaint;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.swing.AbstractAction;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JTextArea;
-import javax.swing.text.Document;
+
+import org.apache.log4j.Category;
 
 /**
- * Extends the functionality of JTextArea with the added features of autoscroll
+ * Extends the functionality of JTextArea by adding
+ * <ul>
+ *  <li>Tailing of output</li>
+ *  <li>Toggle tailing of output via RMB</li>
+ *  <li>Anti-aliased text</li>
+ * </ul>
  */
 public class JSmartTextArea extends JTextArea
 {
-    /** auto scrolling of vertical scrollbar **/
-    private boolean autoScroll_;
+    /** Logger **/
+    private static final Category logger_ =
+        Category.getInstance(JSmartTextArea.class);
     
-    /** antialiasing of textarea font **/
-    private boolean antiAlias_ = true;
+    private JPopupMenu          popup_;
+    private JCheckBoxMenuItem   autoScrollItem_;
+    private JCheckBoxMenuItem   antiAliasItem_;
     
-    Map           renderMap_;
-    Color         darkblue   = new Color(63, 64, 124);
-    Color         darkrose   = new Color(159, 61, 100);
-    GradientPaint myGradient = new GradientPaint(0, 0, darkblue, 0, 50,
-                                   darkrose);
+    private Map           renderMap_;
+    private Color         darkblue   = new Color(63, 64, 124);
+    private Color         darkrose   = new Color(159, 61, 100);
+    private GradientPaint myGradient = new GradientPaint(0, 0, darkblue, 0, 50,
+                                       darkrose);
 
     //--------------------------------------------------------------------------
     //  Constructors
@@ -34,76 +48,44 @@ public class JSmartTextArea extends JTextArea
 
     /**
      * Constructor for JSmartTextArea.
+     * 
+     * @param text  Initial text of textarea
+     */
+    public JSmartTextArea(String text)
+    {
+        this(text, false, false);
+    }
+
+
+    /**
+     * Constructor for JSmartTextArea.
+     * 
+     * @param  autoScroll  Turns on autoscroll of output
+     * @param  antialias   Turns on antialiasing of the font
      */
     public JSmartTextArea(boolean autoScroll, boolean antiAlias)
     {
+        this("", autoScroll, antiAlias);
+    }
+
+
+    /**
+     * Constructor for JSmartTextArea.
+     * 
+     * @param  text        Initial text
+     * @param  autoScroll  Turns on autoscroll of output
+     * @param  antialias   Turns on antialiasing of the font
+     */
+    public JSmartTextArea(String text, boolean autoScroll, boolean antiAlias)
+    {
+        super(text);
+        buildView();
         setAutoScroll(autoScroll);
         setAntiAlias(antiAlias);
     }
 
-
-    /**
-     * Constructor for JSmartTextArea.
-     * 
-     * @param text
-     */
-    public JSmartTextArea(String text)
-    {
-        super(text);
-    }
-
-
-    /**
-     * Constructor for JSmartTextArea.
-     * 
-     * @param rows
-     * @param columns
-     */
-    public JSmartTextArea(int rows, int columns)
-    {
-        super(rows, columns);
-    }
-
-
-    /**
-     * Constructor for JSmartTextArea.
-     * 
-     * @param text
-     * @param rows
-     * @param columns
-     */
-    public JSmartTextArea(String text, int rows, int columns)
-    {
-        super(text, rows, columns);
-    }
-
-
-    /**
-     * Constructor for JSmartTextArea.
-     * 
-     * @param doc
-     */
-    public JSmartTextArea(Document doc)
-    {
-        super(doc);
-    }
-
-
-    /**
-     * Constructor for JSmartTextArea.
-     * 
-     * @param doc
-     * @param text
-     * @param rows
-     * @param columns
-     */
-    public JSmartTextArea(Document doc, String text, int rows, int columns)
-    {
-        super(doc, text, rows, columns);
-    }
-    
     //--------------------------------------------------------------------------
-    //  Implementation
+    //  Overridden Methods
     //--------------------------------------------------------------------------
     
     /**
@@ -138,28 +120,6 @@ public class JSmartTextArea extends JTextArea
     
     
     /**
-     * Determines if the autoscroll feature is active
-     * 
-     * @return  True is autoscroll is enable, false otherwise
-     */
-    public boolean isAutoScroll()
-    {
-        return autoScroll_;
-    }
-
-
-    /**
-     * Sets the autoScroll feature
-     * 
-     * @param autoScroll  True to enable autoscroll, false to disable autoscroll
-     */
-    public void setAutoScroll(boolean autoScroll)
-    {
-        autoScroll_ = autoScroll;
-    }
-    
-
-    /**
      * Appends a string to the textarea
      * 
      * @param   str  String to append
@@ -173,6 +133,27 @@ public class JSmartTextArea extends JTextArea
             scrollToEnd();
     }
     
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------    
+    
+    /**
+     * Adds the popupmenu to the textarea
+     */
+    protected void buildView()
+    {
+        // Build popup menu and add register with textarea
+        popup_ = new JPopupMenu("Popup");        
+        autoScrollItem_ = new JCheckBoxMenuItem(new AutoScrollAction());
+        antiAliasItem_  = new JCheckBoxMenuItem(new AntiAliasAction());
+        popup_.add(autoScrollItem_);
+        popup_.add(antiAliasItem_);
+        addMouseListener(new PopupListener());
+    }    
+    
+    //--------------------------------------------------------------------------
+    //  Public 
+    //--------------------------------------------------------------------------
     
     /**
      * Scrolls to the end of the text area
@@ -182,6 +163,31 @@ public class JSmartTextArea extends JTextArea
         setCaretPosition(getDocument().getLength());
     }
     
+    //--------------------------------------------------------------------------
+    //  Accessors/Mutators
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Determines if the autoscroll feature is active
+     * 
+     * @return  True is autoscroll is enable, false otherwise
+     */
+    public boolean isAutoScroll()
+    {
+        return autoScrollItem_.isSelected();
+    }
+
+
+    /**
+     * Sets the autoScroll feature
+     * 
+     * @param autoScroll  True to enable autoscroll, false to disable autoscroll
+     */
+    public void setAutoScroll(boolean autoScroll)
+    {
+        autoScrollItem_.setSelected(autoScroll);
+    }
+
     
     /**
      * Returns the antiAlias.
@@ -190,7 +196,7 @@ public class JSmartTextArea extends JTextArea
      */
     public boolean isAntiAlias()
     {
-        return antiAlias_;
+        return antiAliasItem_.isSelected();
     }
 
 
@@ -201,6 +207,61 @@ public class JSmartTextArea extends JTextArea
      */
     public void setAntiAlias(boolean antiAlias)
     {
-        antiAlias_ = antiAlias;
+        antiAliasItem_.setSelected(antiAlias);
     }
+    
+    //--------------------------------------------------------------------------
+    //  Inner Classes
+    //--------------------------------------------------------------------------
+    
+    class AutoScrollAction extends AbstractAction 
+    {
+        public AutoScrollAction()
+        {
+            super("AutoScroll");
+        }
+        
+        public void actionPerformed(ActionEvent e)
+        {
+            // NO OP
+        }
+    }    
+
+    class AntiAliasAction extends AbstractAction 
+    {
+        public AntiAliasAction()
+        {
+            super("AntiAlias");
+        }
+        
+        public void actionPerformed(ActionEvent e)
+        {
+            // NO OP
+        }
+    }    
+
+    
+    /**
+     * Popup menu listener for RMB on the Classpath list
+     */ 
+    class PopupListener extends MouseAdapter
+    {
+        public void mousePressed(MouseEvent e)
+        {
+            maybeShowPopup(e);
+        }
+
+        public void mouseReleased(MouseEvent e)
+        {
+            maybeShowPopup(e);
+        }
+
+        private void maybeShowPopup(MouseEvent e)
+        {
+            if (e.isPopupTrigger())
+                popup_.show(e.getComponent(), e.getX(), e.getY());
+        }
+    }
+    
+    
 }
