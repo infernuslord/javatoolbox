d81e9eb172db49e8e9bc4a21e4405e9003268309
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-11-11 01:38:45 +0000

    Initial version

---
+++ toolbox/src/toolbox/plugin/jtail/filter/ILineFilter.java
@@ -0,0 +1,32 @@
+package toolbox.jtail.filter;
+
+/**
+ * ILineFilter - filter interface for processing a line of text
+ */
+public interface ILineFilter
+{
+    
+    /**
+     * Filters the line of text
+     * 
+     * @param   line  Line of text to filter
+     * @return  Mutated line of text as processed by filter.
+     */
+    public String filter(String line);
+    
+    
+    /**
+     * Is the filter enabled?
+     * 
+     * @return  True if the filter is enabled, false otherwise
+     */
+    public boolean isEnabled();
+    
+    
+    /**
+     * Enables/disables the filter
+     * 
+     * @param  b   True to enable the filter, false to disable the filter
+     */
+    public void setEnabled(boolean b);
+}
