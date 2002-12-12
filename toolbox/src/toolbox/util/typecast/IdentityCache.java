fecb49be886307a87636edc0247d1bb2a03d491d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2002-12-12 06:30:17 +0000

    Checkstyle updates

--- toolbox/src/toolbox/util/typecast/IdentityCache.java
+++ toolbox/src/toolbox/util/typecast/IdentityCache.java
@@ -12,15 +12,20 @@ package toolbox.util.typecast;
  */
 public interface IdentityCache
 {
-
     /**
      * Gets the value based on <tt>key</tt>'s object reference
+     * 
+     * @param  key  Key
+     * @return Value for given key
      */
     public Object get(Object key);
 
     /**
      * Stores <tt>value</tt> and associates it with the <tt>key</tt>'s
      * object reference
+     * 
+     * @param  key    Key for value
+     * @param  value  Value for key
      */
     public void put(Object key, Object value);
 }
