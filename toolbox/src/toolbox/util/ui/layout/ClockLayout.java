1de3e818ccaab9a0c764a63b13f52a749ac0be15
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-01-05 09:23:33 +0000

    Javadoc updates

--- toolbox/src/toolbox/util/ui/layout/ClockLayout.java
+++ toolbox/src/toolbox/util/ui/layout/ClockLayout.java
@@ -5,85 +5,110 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Insets;
 
+/**
+ * ClockLayout
+ */
 public class ClockLayout extends ConstraintLayout
 {
 
-	public ClockLayout()
-	{
-	}
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Default constructor
+     */
+    public ClockLayout()
+    {
+    }
 
-	public void measureLayout(Container target, Dimension dimension, int type)
-	{
-		int count = target.getComponentCount();
-		if (count > 0)
-		{
-			Insets insets = target.getInsets();
-			Dimension size = target.getSize();
-			int x = 0;
-			int y = 0;
-			int maxWidth = 0;
-			int maxHeight = 0;
-			double radius =
-				Math.min(
-					size.width - insets.left - insets.right,
-					size.height - insets.top - insets.bottom)
-					/ 2;
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
 
-			Dimension[] sizes = new Dimension[count];
+    /**
+     * Measure layout
+     * 
+     * @param  target       Container
+     * @param  dimension    Dimension
+     * @param  type         Type    
+     */
+    public void measureLayout(Container target, Dimension dimension, int type)
+    {
+        int count = target.getComponentCount();
+        if (count > 0)
+        {
+            Insets insets = target.getInsets();
+            Dimension size = target.getSize();
+            int x = 0;
+            int y = 0;
+            int maxWidth = 0;
+            int maxHeight = 0;
+            
+            double radius =
+                Math.min(
+                    size.width - insets.left - insets.right,
+                    size.height - insets.top - insets.bottom) / 2;
 
-			for (int i = 0; i < count; i++)
-			{
-				Component c = target.getComponent(i);
-				if (includeComponent(c))
-				{
-					Dimension d = getComponentSize(c, type);
-					int w = d.width;
-					int h = d.height;
-					sizes[i] = d;
+            Dimension[] sizes = new Dimension[count];
 
-					maxWidth = Math.max(maxWidth, w);
-					maxHeight = Math.max(maxHeight, h);
-				}
-			}
+            for (int i = 0; i < count; i++)
+            {
+                Component c = target.getComponent(i);
+                if (includeComponent(c))
+                {
+                    Dimension d = getComponentSize(c, type);
+                    int w = d.width;
+                    int h = d.height;
+                    sizes[i] = d;
 
-			if (dimension != null)
-			{
-				dimension.width = (int) (2 * radius - maxWidth);
-				dimension.height = (int) (2 * radius - maxHeight);
-			}
-			else
-			{
-				int mx =
-					(size.width - insets.left - insets.right - 2 * hMargin) / 2;
-				int my =
-					(size.height - insets.top - insets.bottom - 2 * vMargin)
-						/ 2;
-				x = 0;
-				y = 0;
+                    maxWidth = Math.max(maxWidth, w);
+                    maxHeight = Math.max(maxHeight, h);
+                }
+            }
 
-				radius -= Math.max(maxWidth, maxHeight) / 2;
-				for (int i = 0; i < count; i++)
-				{
-					Component c = target.getComponent(i);
-					if (includeComponent(c))
-					{
-						Dimension d = sizes[i];
-						int w = d.width;
-						int h = d.height;
+            if (dimension != null)
+            {
+                dimension.width = (int) (2 * radius - maxWidth);
+                dimension.height = (int) (2 * radius - maxHeight);
+            }
+            else
+            {
+                int mx =
+                  (size.width - insets.left - insets.right - 2 * hMargin_) / 2;
+                  
+                int my =
+                  (size.height - insets.top - insets.bottom - 2 * vMargin_) / 2;
+                  
+                x = 0;
+                y = 0;
 
-						double angle = 2 * Math.PI * i / count;
-						x = mx + (int) (Math.sin(angle) * radius);
-						y = my - (int) (Math.cos(angle) * radius);
-						c.setBounds(
-							insets.left + hMargin + x - w / 2,
-							insets.top + vMargin + y - h / 2,
-							w,
-							h);
-					}
-				}
-			}
-		}
+                radius -= Math.max(maxWidth, maxHeight) / 2;
+                
+                for (int i = 0; i < count; i++)
+                {
+                    Component c = target.getComponent(i);
+                    
+                    if (includeComponent(c))
+                    {
+                        Dimension d = sizes[i];
+                        int w = d.width;
+                        int h = d.height;
 
-	}
+                        double angle = 2 * Math.PI * i / count;
+                        x = mx + (int) (Math.sin(angle) * radius);
+                        y = my - (int) (Math.cos(angle) * radius);
+                        
+                        c.setBounds(
+                            insets.left + hMargin_ + x - w / 2,
+                            insets.top + vMargin_ + y - h / 2,
+                            w,
+                            h);
+                    }
+                }
+            }
+        }
+
+    }
 
 }
--- toolbox/src/toolbox/util/ui/layout/ConstraintLayout.java
+++ toolbox/src/toolbox/util/ui/layout/ConstraintLayout.java
@@ -13,120 +13,123 @@ import java.util.Hashtable;
  */
 public class ConstraintLayout implements LayoutManager2
 {
+    protected static final int PREFERRED = 0;
+    protected static final int MINIMUM = 1;
+    protected static final int MAXIMUM = 2;
 
-	protected final static int PREFERRED = 0;
-	protected final static int MINIMUM = 1;
-	protected final static int MAXIMUM = 2;
+    protected int hMargin_ = 0;
+    protected int vMargin_ = 0;
+    private Hashtable constraints_;
+    protected boolean includeInvisible_ = false;
 
-	protected int hMargin = 0;
-	protected int vMargin = 0;
-	private Hashtable constraints;
-	protected boolean includeInvisible = false;
+    //--------------------------------------------------------------------------
+    //  Public
+    //--------------------------------------------------------------------------
+    
+    public void addLayoutComponent(String constraint, Component c)
+    {
+        setConstraint(c, constraint);
+    }
 
-	public void addLayoutComponent(String constraint, Component c)
-	{
-		setConstraint(c, constraint);
-	}
+    public void addLayoutComponent(Component c, Object constraint)
+    {
+        setConstraint(c, constraint);
+    }
 
-	public void addLayoutComponent(Component c, Object constraint)
-	{
-		setConstraint(c, constraint);
-	}
+    public void removeLayoutComponent(Component c)
+    {
+        if (constraints_ != null)
+            constraints_.remove(c);
+    }
 
-	public void removeLayoutComponent(Component c)
-	{
-		if (constraints != null)
-			constraints.remove(c);
-	}
+    public void setConstraint(Component c, Object constraint)
+    {
+        if (constraint != null)
+        {
+            if (constraints_ == null)
+                constraints_ = new Hashtable();
+            constraints_.put(c, constraint);
+        }
+        else if (constraints_ != null)
+            constraints_.remove(c);
+    }
 
-	public void setConstraint(Component c, Object constraint)
-	{
-		if (constraint != null)
-		{
-			if (constraints == null)
-				constraints = new Hashtable();
-			constraints.put(c, constraint);
-		}
-		else if (constraints != null)
-			constraints.remove(c);
-	}
+    public Object getConstraint(Component c)
+    {
+        if (constraints_ != null)
+            return constraints_.get(c);
+        return null;
+    }
 
-	public Object getConstraint(Component c)
-	{
-		if (constraints != null)
-			return constraints.get(c);
-		return null;
-	}
+    public void setIncludeInvisible(boolean includeInvisible)
+    {
+        this.includeInvisible_ = includeInvisible;
+    }
 
-	public void setIncludeInvisible(boolean includeInvisible)
-	{
-		this.includeInvisible = includeInvisible;
-	}
+    public boolean getIncludeInvisible()
+    {
+        return includeInvisible_;
+    }
 
-	public boolean getIncludeInvisible()
-	{
-		return includeInvisible;
-	}
+    protected boolean includeComponent(Component c)
+    {
+        return includeInvisible_ || c.isVisible();
+    }
 
-	protected boolean includeComponent(Component c)
-	{
-		return includeInvisible || c.isVisible();
-	}
+    public Dimension minimumLayoutSize(Container target)
+    {
+        return calcLayoutSize(target, MINIMUM);
+    }
 
-	public Dimension minimumLayoutSize(Container target)
-	{
-		return calcLayoutSize(target, MINIMUM);
-	}
+    public Dimension maximumLayoutSize(Container target)
+    {
+        return calcLayoutSize(target, MAXIMUM);
+    }
 
-	public Dimension maximumLayoutSize(Container target)
-	{
-		return calcLayoutSize(target, MAXIMUM);
-	}
+    public Dimension preferredLayoutSize(Container target)
+    {
+        return calcLayoutSize(target, PREFERRED);
+    }
 
-	public Dimension preferredLayoutSize(Container target)
-	{
-		return calcLayoutSize(target, PREFERRED);
-	}
+    public Dimension calcLayoutSize(Container target, int type)
+    {
+        Dimension dim = new Dimension(0, 0);
+        measureLayout(target, dim, type);
+        Insets insets = target.getInsets();
+        dim.width += insets.left + insets.right + 2 * hMargin_;
+        dim.height += insets.top + insets.bottom + 2 * vMargin_;
+        return dim;
+    }
 
-	public Dimension calcLayoutSize(Container target, int type)
-	{
-		Dimension dim = new Dimension(0, 0);
-		measureLayout(target, dim, type);
-		Insets insets = target.getInsets();
-		dim.width += insets.left + insets.right + 2 * hMargin;
-		dim.height += insets.top + insets.bottom + 2 * vMargin;
-		return dim;
-	}
+    public void invalidateLayout(Container target)
+    {
+    }
 
-	public void invalidateLayout(Container target)
-	{
-	}
+    public float getLayoutAlignmentX(Container parent)
+    {
+        return 0.5f;
+    }
 
-	public float getLayoutAlignmentX(Container parent)
-	{
-		return 0.5f;
-	}
+    public float getLayoutAlignmentY(Container parent)
+    {
+        return 0.5f;
+    }
 
-	public float getLayoutAlignmentY(Container parent)
-	{
-		return 0.5f;
-	}
+    public void layoutContainer(Container target)
+    {
+        measureLayout(target, null, PREFERRED);
+    }
 
-	public void layoutContainer(Container target)
-	{
-		measureLayout(target, null, PREFERRED);
-	}
+    public void measureLayout(Container target, Dimension dimension, int type)
+    {
+    }
 
-	public void measureLayout(Container target, Dimension dimension, int type)
-	{
-	}
-
-	protected Dimension getComponentSize(Component c, int type)
-	{
-		if (type == MINIMUM)
-			return c.getMinimumSize();
-		if (type == MAXIMUM)
-			return c.getMaximumSize();
-		return c.getPreferredSize();
-	}
+    protected Dimension getComponentSize(Component c, int type)
+    {
+        if (type == MINIMUM)
+            return c.getMinimumSize();
+        if (type == MAXIMUM)
+            return c.getMaximumSize();
+        return c.getPreferredSize();
+    }
 }
