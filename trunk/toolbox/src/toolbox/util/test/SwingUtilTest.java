d0f2c95d938b3d67140c9a37aeb4038724067518
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-01-16 21:59:39 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/test/SwingUtilTest.java
@@ -0,0 +1,158 @@
+package toolbox.util.test;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JInternalFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import junit.framework.TestCase;
+
+import toolbox.util.SwingUtil;
+
+/**
+ *  SwingUtil Unit Test
+ */
+public class SwingUtilTest extends TestCase
+{
+    /**
+     * Entry point
+     * 
+     * @param  args  None
+     */
+    public static void main(String[] args)
+    {
+        junit.textui.TestRunner.run(SwingUtilTest.class);
+    }
+
+    /**
+     * Constructor for SwingUtilTest.
+     * 
+     * @param   arg0    Name
+     */
+    public SwingUtilTest(String arg0)
+    {
+        super(arg0);
+    }
+
+
+    /**
+     * Tests the tiling of a JDesktopPane
+     */
+    public void testTile()
+    {
+        TestFrame frame = new TestFrame();
+        frame.setVisible(true);
+    }
+    
+    
+    class TestFrame extends JFrame
+    {
+        private JDesktopPane desktop_;
+        
+        public TestFrame()
+        {
+            super("InternalFrameDemo");
+            
+            // Make the big window be indented 50 pixels from each edge 
+            // of the screen.
+            int inset = 50;
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            
+            setBounds(
+                inset,
+                inset,
+                screenSize.width - inset * 2,
+                screenSize.height - inset * 2);
+                
+            // Quit this app when the big window closes.
+            addWindowListener(new WindowAdapter()
+            {
+                public void windowClosing(WindowEvent e)
+                {
+                    System.exit(0);
+                }
+            });
+            
+            // Set up the GUI.
+            desktop_ = new JDesktopPane(); //a specialized layered pane
+            createFrame(); //Create first window
+            setContentPane(desktop_);
+            setJMenuBar(createMenuBar());
+            
+            // Make dragging faster:
+            desktop_.putClientProperty("JDesktopPane.dragMode", "outline");
+        }
+        
+        protected JMenuBar createMenuBar()
+        {
+            JMenuBar menuBar = new JMenuBar();
+            JMenu menu = new JMenu("Document");
+            menu.setMnemonic(KeyEvent.VK_D);
+            JMenuItem menuItem = new JMenuItem("New");
+            menuItem.setMnemonic(KeyEvent.VK_N);
+            
+            menuItem.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    createFrame();
+                }
+            });
+            
+            JMenuItem tileItem = new JMenuItem("Tile");
+            tileItem.setMnemonic(KeyEvent.VK_T);
+            
+            tileItem.addActionListener(new ActionListener()
+            {
+                public void actionPerformed(ActionEvent e)
+                {
+                    SwingUtil.tileInternalFrames(desktop_);
+                }
+            });
+            
+            
+            menu.add(menuItem);
+            menu.add(tileItem);
+            menuBar.add(menu);
+            return menuBar;
+        }
+        
+        protected void createFrame()
+        {
+            MyInternalFrame frame = new MyInternalFrame();
+            frame.setVisible(true);
+            desktop_.add(frame);
+            
+            try
+            {
+                frame.setSelected(true);
+            }
+            catch (java.beans.PropertyVetoException e)
+            {
+                // Ignore
+            }
+        }
+    }
+}
+
+class MyInternalFrame extends JInternalFrame
+{
+    private static int openFrameCount_ = 0;
+    private static final int xOffset_ = 30, yOffset_ = 30;
+    
+    public MyInternalFrame()
+    {
+        super("Document #" + (++openFrameCount_), true, true, true, true);
+        setSize(300, 300);
+        setLocation(xOffset_ * openFrameCount_, yOffset_ * openFrameCount_);
+    }
+}    
