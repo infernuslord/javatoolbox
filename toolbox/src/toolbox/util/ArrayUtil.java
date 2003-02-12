1e5b99b79a80cfaa41c9c1ba92cc9033fab3938d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-02-12 03:30:10 +0000

    Removed tabs

--- toolbox/src/toolbox/util/ArrayUtil.java
+++ toolbox/src/toolbox/util/ArrayUtil.java
@@ -385,17 +385,17 @@ public class ArrayUtil
     {
         if (array1 == array2)
             return true;
-            
-       if (array1.length != array2.length)
-           return false;
-           
-       for (int i=0; i<array1.length; i++)
-       {
-           if (array1[i] != array2[i])
-               if (!array1[i].equals(array2[i]))
-                   return false;
-       }
-       
-       return true;
+
+        if (array1.length != array2.length)
+            return false;
+
+        for (int i = 0; i < array1.length; i++)
+        {
+            if (array1[i] != array2[i])
+                if (!array1[i].equals(array2[i]))
+                    return false;
+        }
+
+        return true;
     }
 }
