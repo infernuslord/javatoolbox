7b3402d0347286cdf5c26df1a097d6822bad5aa6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-02-20 22:40:32 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/test/AssertionExceptionTest.java
@@ -0,0 +1,67 @@
+package toolbox.util.test;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.Assert;
+import toolbox.util.AssertionException;
+
+/**
+ * Unit test for AssertionException
+ */
+public class AssertionExceptionTest extends TestCase
+{
+    /** Logger */
+    private static final Logger logger_ = 
+        Logger.getLogger(AssertionExceptionTest.class);
+
+    /**
+     * Entrypoint
+     * 
+     * @param args Args
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(AssertionExceptionTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+            
+    /**
+     * Constructor for AssertionExceptionTest.
+     * 
+     * @param arg0  Name
+     */
+    public AssertionExceptionTest(String arg0)
+    {
+        super(arg0);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+        
+    /**
+     * Tests default constructor
+     */
+    public void testDefaultConstructor()
+    {
+        logger_.info("Running testDefaultConstructor...");
+        
+        assertNotNull(new AssertionException());
+    }    
+    
+    /**
+     * Tests arg constructor
+     */
+    public void testArgConstructor()
+    {
+        logger_.info("Running testArgConstructor...");
+
+        assertNotNull(new AssertionException("arg"));        
+    }    
+}
