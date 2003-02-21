7bea97c7d793e78eec5f35f85c99a7866931bf6e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-02-21 03:33:05 +0000

    Added cleanup to unit test

--- toolbox/src/toolbox/util/file/test/FileCreatedActivityTest.java
+++ toolbox/src/toolbox/util/file/test/FileCreatedActivityTest.java
@@ -16,7 +16,7 @@ import toolbox.util.file.FileCreatedActivity;
  */
 public class FileCreatedActivityTest extends TestCase
 {
-    /** Logger **/
+    /** Logger */
     private static final Logger logger_ = 
         Logger.getLogger(FileCreatedActivityTest.class);
         
@@ -37,7 +37,7 @@ public class FileCreatedActivityTest extends TestCase
     /**
      * Constructor for FileCreatedActivityTest.
      * 
-     * @param arg0 Name
+     * @param  arg0  Test name
      */
     public FileCreatedActivityTest(String arg0)
     {
@@ -60,25 +60,36 @@ public class FileCreatedActivityTest extends TestCase
         // Create a base line dir with two files
         File dir = FileUtil.createTempDir();
         
-        String file1 = FileUtil.getTempFilename(dir);            
-        String file2 = FileUtil.getTempFilename(dir);
-        
-        FileUtil.setFileContents(file1, "file1", false);
-        FileUtil.setFileContents(file2, "file2", false);
-        
-        // Get list of new files..should be zero on first run
-        FileCreatedActivity activity = new FileCreatedActivity();
-        File[] firstRun = activity.getFiles(dir);
-        assertEquals("first run should be empty", 0, firstRun.length);
-        
-        // Add a file to the baseline dir
-        String file3 = FileUtil.getTempFilename(dir);
-        FileUtil.setFileContents(file3, "file3", false);
-        
-        // Run the activity again..should report 1  new file
-        File[] secondRun = activity.getFiles(dir);
-        assertEquals("second run should contain one file", 1, secondRun.length);
-        
-        logger_.info("New file activity: " + ArrayUtil.toString(secondRun));
+        try
+        {
+            String file1 = FileUtil.getTempFilename(dir);            
+            String file2 = FileUtil.getTempFilename(dir);
+            
+            FileUtil.setFileContents(file1, "file1", false);
+            FileUtil.setFileContents(file2, "file2", false);
+            
+            // Get list of new files..should be zero on first run
+            FileCreatedActivity activity = new FileCreatedActivity();
+            File[] firstRun = activity.getFiles(dir);
+            assertEquals("first run should be empty", 0, firstRun.length);
+            
+            // Add a file to the baseline dir
+            String file3 = FileUtil.getTempFilename(dir);
+            FileUtil.setFileContents(file3, "file3", false);
+            
+            // Run the activity again..should report 1  new file
+            File[] secondRun = activity.getFiles(dir);
+            assertEquals("second run should contain one file", 1, secondRun.length);
+            
+            logger_.info("New file activity: " + ArrayUtil.toString(secondRun));
+        }
+        finally
+        {
+            if (dir != null)
+            {
+                FileUtil.cleanDir(dir);
+                dir.delete();                   
+            }
+        }
     }
 }
