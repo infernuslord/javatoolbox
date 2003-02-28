d51f1ab4af39d3f3e938358d12035aeac85a761b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-02-28 21:34:57 +0000

    Updated for greater coverage of test class

--- toolbox/src/toolbox/util/file/test/DirectoryMonitorTest.java
+++ toolbox/src/toolbox/util/file/test/DirectoryMonitorTest.java
@@ -2,18 +2,25 @@ package toolbox.util.file.test;
 
 import java.io.File;
 
+import org.apache.log4j.Logger;
+
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
-
+import toolbox.util.ArrayUtil;
 import toolbox.util.FileUtil;
 import toolbox.util.ThreadUtil;
 import toolbox.util.file.DirectoryMonitor;
+import toolbox.util.file.IDirectoryListener;
+import toolbox.util.file.IFileActivity;
 
 /**
  * Unit test for DirectoryMonitor
  */
 public class DirectoryMonitorTest extends TestCase
 {
+	private static final Logger logger_ = 
+		Logger.getLogger(DirectoryMonitorTest.class);
+		
     /**
      * Entrypoint
      * 
@@ -49,10 +56,40 @@ public class DirectoryMonitorTest extends TestCase
      */
     public void testDirectoryMonitor() throws Exception
     {
+    	logger_.info("Running testDirectoryMonitor...");
+    	
         File dir = FileUtil.createTempDir();
         DirectoryMonitor dm = new DirectoryMonitor(dir);
+        dm.setDelay(500);
+        
+        // Dummy activity
+        IFileActivity activity = new IFileActivity()
+		{
+			public File[] getFiles(File dir)
+			{
+				return new File[0];
+			}
+		};
+		
+		// Dummy listener
+		IDirectoryListener listener = new IDirectoryListener()
+        {
+            public void fileActivity(IFileActivity activity, File[] files)
+                throws Exception
+            {
+            	logger_.info("File activity reported: " + 
+            		ArrayUtil.toString(files));
+            }
+        };
+		
+		dm.addDirectoryListener(listener);
+        dm.addFileActivity(activity);
         dm.start();
+        
         ThreadUtil.sleep(1000);
+        
         dm.stop();
+        dm.removeFileActivity(activity);
+        dm.removeDirectoryListener(listener);
     }
 }
