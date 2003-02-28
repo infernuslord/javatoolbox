a0d9310705eb0ae6626fb5ac175cf074191eef92
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-02-28 20:46:44 +0000

    Added blocking interface to TestListener so that events can be verified as being fired

--- trunk/toolbox/src/toolbox/tail/test/TailTest.java
+++ trunk/toolbox/src/toolbox/tail/test/TailTest.java
@@ -1,9 +1,9 @@
 package toolbox.tail.test;
 
-import java.io.OutputStreamWriter;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
@@ -13,6 +13,7 @@ import org.apache.log4j.Logger;
 import toolbox.tail.ITailListener;
 import toolbox.tail.Tail;
 import toolbox.util.ThreadUtil;
+import toolbox.util.concurrent.BlockingQueue;
 import toolbox.util.io.StringOutputStream;
 
 /**
@@ -71,21 +72,49 @@ public class TailTest extends TestCase
             new Object[] {
                 writer,
                 new Integer(5),
-                new Integer(1000),
-                "testing tail reader" });
+                new Integer(500),
+                "line " });
 
         Tail tail = new Tail();
-        tail.addOutputStream(new StringOutputStream());
-        tail.addWriter(new OutputStreamWriter(new StringOutputStream()));
-        tail.addTailListener(new TestTailListener());
+        
+        // Create sinks for tail and attach them to the tail
+        StringOutputStream sos = new StringOutputStream();
+        StringWriter sw = new StringWriter();
+        tail.addOutputStream(sos);
+        tail.addWriter(sw);
+
+		// Create a listener so we can test event
+		TestTailListener listener = new TestTailListener();        
+        tail.addTailListener(listener);
+        
         logger_.info(tail.toString());
+        
+        // Attach the input reader to the tail
         tail.setTailReader(reader);
+        
+        // Lifecycle of tail
         tail.start();
-        ThreadUtil.sleep(2000);
+        listener.waitForStart();
+        ThreadUtil.sleep(1000);
+        
         tail.pause();
-        ThreadUtil.sleep(2000);
+        listener.waitForPause();
+		ThreadUtil.sleep(1000);
+		
         tail.unpause();
-        ThreadUtil.sleep(2000);
+        listener.waitForUnpause();
+		ThreadUtil.sleep(1000);
+		
+        tail.stop();
+        listener.waitForStop();
+        
+        // Dump contents of the sinks
+        logger_.info("OutputStream sink:\n" + sos.getBuffer());
+        logger_.info("OutputWriter sink:\n" + sw.toString());
+        
+        tail.removeOutputStream(sos);
+        tail.removeWriter(sw);
+        tail.removeTailListener(listener);
     }
     
     //--------------------------------------------------------------------------
@@ -130,6 +159,13 @@ class TestTailListener implements ITailListener
     /** Logger */
     private static final Logger logger_ = TailTest.logger_;
         
+    private BlockingQueue startEvents_ = new BlockingQueue();
+    private BlockingQueue stopEvents_  = new BlockingQueue();
+    private BlockingQueue nextLineEvents_ = new BlockingQueue();
+    private BlockingQueue endedEvents_ = new BlockingQueue();
+    private BlockingQueue pauseEvents_ = new BlockingQueue();
+    private BlockingQueue unpauseEvents_ = new BlockingQueue();
+        
     /**
      * Next line is available
      * 
@@ -138,6 +174,16 @@ class TestTailListener implements ITailListener
     public void nextLine(String line)
     {
         logger_.debug(line);
+        
+		try
+		{
+			nextLineEvents_.push("nextLine");    
+		}
+		catch (Exception e)
+		{
+			logger_.error(e);
+		}
+        
     }
 
     /**
@@ -145,7 +191,16 @@ class TestTailListener implements ITailListener
      */
     public void tailStarted()
     {
-        logger_.info("tail started");    
+        logger_.info("tail started");
+        
+		try
+		{
+			startEvents_.push("start");    
+		}
+		catch (Exception e)
+		{
+			logger_.error(e);
+		}
     }
     
     
@@ -155,6 +210,15 @@ class TestTailListener implements ITailListener
     public void tailStopped()
     {
         logger_.info("tail stopped");
+        
+		try
+		{
+			stopEvents_.push("stop");    
+		}
+		catch (Exception e)
+		{
+			logger_.error(e);
+		}
     }
   
   
@@ -163,7 +227,16 @@ class TestTailListener implements ITailListener
      */
     public void tailEnded()
     {
-        logger_.info("tail ended");              
+        logger_.info("tail ended");
+        
+		try
+		{
+			endedEvents_.push("ended");    
+		}
+		catch (Exception e)
+		{
+			logger_.error(e);
+		}
     }
     
     
@@ -173,6 +246,15 @@ class TestTailListener implements ITailListener
     public void tailPaused()
     {
         logger_.info("tail paused");
+        
+        try
+        {
+			pauseEvents_.push("pause");    
+        }
+        catch (Exception e)
+        {
+            logger_.error(e);
+        }
     }
     
     
@@ -182,5 +264,34 @@ class TestTailListener implements ITailListener
     public void tailUnpaused()
     {
         logger_.info("tail unpaused");
+        
+		try
+		{
+			unpauseEvents_.push("unpause");    
+		}
+		catch (Exception e)
+		{
+			logger_.error(e);
+		}
+    }
+    
+    public void waitForStart() throws InterruptedException
+    {
+		startEvents_.pull();
+    }
+    
+    public void waitForStop() throws InterruptedException
+    {
+    	stopEvents_.pull();
+    }
+    
+    public void waitForPause() throws InterruptedException
+    {
+    	pauseEvents_.pull();
+    }
+    
+    public void waitForUnpause() throws InterruptedException
+    {
+    	unpauseEvents_.pull();
     }
 }
