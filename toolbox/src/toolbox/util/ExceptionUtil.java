b65b6bc4d60c91792d7907bdcbaaf4c1b7725baa
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-03-24 17:14:03 +0000

    Switched to using JSmartOptionPane so details optionally viewable

--- toolbox/src/toolbox/util/ExceptionUtil.java
+++ toolbox/src/toolbox/util/ExceptionUtil.java
@@ -3,9 +3,9 @@ package toolbox.util;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import javax.swing.JOptionPane;
-
 import org.apache.log4j.Logger;
+
+import toolbox.util.ui.JSmartOptionPane;
 
 /**
  * Exception utilities
@@ -41,18 +41,16 @@ public class ExceptionUtil
     }
     
     /**
-     * Generic Error handler for exceptions generated on a client side UI
+     * Generic Error handler for exceptions generated on a client side UI. 
+     * Logs the error to the given logger and displays the error via a dialog 
+     * box to the user.
      * 
-     * @param  t   Exception causing Error
-     * @param  c   Logger to log to
+     * @param  t  Exception causing Error
+     * @param  c  Logger to log to
      */
     public static void handleUI(Throwable t, Logger c)
     {
         c.warn(t.getMessage(), t);
-        
-        JOptionPane.showMessageDialog(null,
-            t.getMessage() + "\n\n" + getStackTrace(t),
-            "Error", JOptionPane.ERROR_MESSAGE);
+        JSmartOptionPane.showExceptionMessageDialog(null,t);
     }
-    
 }
