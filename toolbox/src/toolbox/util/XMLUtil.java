cba550204106c8d6ee80f7de17c707ad55507a7f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-02 04:39:34 +0000

    format() now throws SAX/IOException. Added loadElement()

--- toolbox/src/toolbox/util/XMLUtil.java
+++ toolbox/src/toolbox/util/XMLUtil.java
@@ -1,5 +1,6 @@
 package toolbox.util;
 
+import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 
@@ -8,7 +9,9 @@ import org.apache.xerces.parsers.DOMParser;
 import org.apache.xml.serialize.OutputFormat;
 import org.apache.xml.serialize.XMLSerializer;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  * XML Utility Class
@@ -24,29 +27,39 @@ public class XMLUtil
      * @param   xml  XML string to format
      * @return  Formatted XML string
      */
-    public static String format(String xml)
+    public static String format(String xml) throws SAXException, IOException
     {
         String formattedXML = null;
 
-        try
-        {
-            DOMParser parser = new DOMParser();
-            parser.parse(new InputSource(new StringReader(xml)));
-            Document doc = parser.getDocument();
-            StringWriter writer = new StringWriter();
-            OutputFormat format = new OutputFormat(doc);
-            format.setIndenting(true);
-            format.setIndent(3);
-            format.setOmitXMLDeclaration(true);
-            XMLSerializer serializer = new XMLSerializer(writer, format);
-            serializer.serialize(doc);
-            formattedXML = writer.toString();
-        }
-        catch (Exception ioe)
-        {
-            System.out.println(ioe);
-        }
+        DOMParser parser = new DOMParser();
+        parser.parse(new InputSource(new StringReader(xml)));
+        Document doc = parser.getDocument();
+        StringWriter writer = new StringWriter();
+        OutputFormat format = new OutputFormat(doc);
+        format.setIndenting(true);
+        format.setOmitXMLDeclaration(true);
+        format.setIndent(2);
+
+        XMLSerializer serializer = new XMLSerializer(writer, format);
+        serializer.serialize(doc);
+        formattedXML = writer.toString();
 
         return formattedXML;
+    }
+
+    /**
+     * Converts a string XML element into its DOM counterpart
+     * 
+     * @param   xml  XML to load into a DOM element
+     * @return  DOM Element representing xml string
+     * @throws  IOException on I/O error
+     * @throws  SAXException on XML parsing error
+     */
+    public static Element loadElement(String xml)
+        throws IOException, SAXException
+    {
+        DOMParser parser = new DOMParser();
+        parser.parse(new InputSource(new StringReader(xml)));
+        return parser.getDocument().getDocumentElement();
     }
 }
