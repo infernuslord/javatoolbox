ca178bc513b1bc1c937e6c57f349ef6d9ff8ebd9
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-11 09:29:16 +0000

    Added testStreamThroughput()

--- trunk/toolbox/src/toolbox/util/io/test/EventOutputStreamTest.java
+++ trunk/toolbox/src/toolbox/util/io/test/EventOutputStreamTest.java
@@ -1,10 +1,17 @@
 package toolbox.util.io.test;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DecimalFormat;
+import java.util.Date;
+
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
 import org.apache.log4j.Logger;
 
+import toolbox.util.DateTimeUtil;
+import toolbox.util.RandomUtil;
 import toolbox.util.concurrent.BlockingQueue;
 import toolbox.util.io.EventOutputStream;
 import toolbox.util.io.NullOutputStream;
@@ -141,6 +148,78 @@ public class EventOutputStreamTest extends TestCase
         assertNotNull(listener.waitForClose());
     }
 
+    /**
+     * Tests the streamThroughput() method on the EventOutputStream.Listener
+     * 
+     * @throws Exception on error
+     */
+    public void testStreamThroughput() throws Exception
+    {
+        logger_.info("Running testStreamThroughput...");
+        
+        EventOutputStream eos = new EventOutputStream(new NullOutputStream());
+        eos.addListener(new ThroughputListener());
+        eos.startThroughputMonitor();
+        
+        logger_.info("");
+        logger_.info("[1 byte packet]");
+        stuffStream(eos, 1, 5);
+        
+        logger_.info("");
+        logger_.info("[10 byte packet]");
+        stuffStream(eos, 10, 5);
+        
+        logger_.info("");
+        logger_.info("[100 byte packet]");
+        stuffStream(eos, 100, 5);
+        
+        logger_.info("");
+        logger_.info("[1000 byte packet]");
+        stuffStream(eos, 1000, 5);
+        
+        logger_.info("");
+        logger_.info("[10000 byte packet]");
+        stuffStream(eos, 10000, 5);
+        
+        logger_.info("");
+        logger_.info("[100000 byte packet]");
+        stuffStream(eos, 100000, 5); 
+               
+        eos.stopThroughputMonitor();
+    }
+
+    //--------------------------------------------------------------------------
+    // Helpers
+    //--------------------------------------------------------------------------
+
+    /**
+     * Stuffs a stream with a packet of random data as fast as possible for a 
+     * given duration of time.
+     * 
+     * @param   os          OutputStream to stuff
+     * @param   packetSize  Number of bytes to stuff per write
+     * @param   duration    Number of seconds to stuff the stream
+     * @throws  IOException on I/O error
+     */
+    protected void stuffStream(OutputStream os, int packetSize, int duration)
+        throws IOException
+    {
+        byte[] packet = new byte[packetSize];
+        for (int i=0; i<packet.length; i++)
+            packet[i] = (byte) RandomUtil.nextInt(1,50);
+        
+        Date now = new Date();
+        Date future = new Date(now.getTime());
+        DateTimeUtil.add(future, 0, 0, 0, 0, 0, duration);
+            
+        while (now.before(future))
+        {
+            os.write(packet);
+            os.flush();
+            now = new Date();
+        }
+    }
+
     //--------------------------------------------------------------------------
     // Inner classes 
     //--------------------------------------------------------------------------
@@ -172,7 +251,7 @@ public class EventOutputStreamTest extends TestCase
         {
             return (EventOutputStream) closeQueue_.pull();
         }
-
+        
         //----------------------------------------------------------------------
         // EvenoutOutputStream.Listener Interface
         //----------------------------------------------------------------------
@@ -211,6 +290,41 @@ public class EventOutputStreamTest extends TestCase
             {
                 logger_.warn(e);
             }
+        }
+        
+        public void streamThroughput(EventOutputStream stream, 
+            float bytesPerPeriod)
+        {
+        }
+    }
+    
+    /** 
+     * Listener used to make sure event notification is working correctly
+     */    
+    class ThroughputListener implements EventOutputStream.Listener
+    {
+        //----------------------------------------------------------------------
+        // EvenoutOutputStream.Listener Interface
+        //----------------------------------------------------------------------
+        
+        public void byteWritten(EventOutputStream stream, int b)
+        {
+        }
+        
+        public void streamClosed(EventOutputStream stream)
+        {
+        }
+
+        public void streamFlushed(EventOutputStream stream)
+        {
+        }
+        
+        public void streamThroughput(EventOutputStream stream, 
+            float bytesPerPeriod)
+        {
+            String thruput = DecimalFormat.getInstance().format(bytesPerPeriod);
+            
+            logger_.info("Transferred " + thruput + " bytes/second");    
         }
     }
 }
