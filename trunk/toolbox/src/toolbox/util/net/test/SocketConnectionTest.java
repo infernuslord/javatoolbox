12d1cab01464b341d8f01ad4b91c77f6477ba581
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-15 04:37:59 +0000

    Added waitForStart() to internal server so client no longer need to do Thread.sleep().  Improved verification in testing the connection listener by using blocking queues. Various javadoc updates

--- trunk/toolbox/src/toolbox/util/net/test/SocketConnectionTest.java
+++ trunk/toolbox/src/toolbox/util/net/test/SocketConnectionTest.java
@@ -4,14 +4,17 @@ import java.io.IOException;
 import java.net.ConnectException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+
+import org.apache.log4j.Logger;
 
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
-import org.apache.log4j.Logger;
-
 import toolbox.util.SocketUtil;
 import toolbox.util.ThreadUtil;
+import toolbox.util.concurrent.BlockingQueue;
+import toolbox.util.concurrent.Mutex;
 import toolbox.util.net.IConnection;
 import toolbox.util.net.IConnectionListener;
 import toolbox.util.net.SocketConnection;
@@ -102,30 +105,39 @@ public class SocketConnectionTest extends TestCase
         
         final int port = SocketUtil.getFreePort();
         Server server = new Server(port,false);
+        final Mutex mutex = new Mutex();
         
         new Thread(new Runnable()
         {
             public void run()
             {
                 try
-                {
+                {   
+                    logger_.info("Mutex : acquired by client");
+                    
+                    mutex.acquire();
                     SocketConnection sc = 
                         new SocketConnection("localhost", port, true, 2);
                         
-                    logger_.info("Connected after retry!");
+                    logger_.info("Client: Connected after retry!");
                     sc.connect();
                     sc.close();
+                    mutex.release();
                 }
-                catch(Exception e)
+                catch (Exception e)
                 {
-                    // Ignore
+                    logger_.error("testForceConnectConstructor", e);
                 }
             }
         }).start();
         
-        ThreadUtil.sleep(10000);
+        ThreadUtil.sleep(6000);
         
         server.start();
+        logger_.info("Mutex : server waiting to acquire...");
+        mutex.acquire();
+        logger_.info("Mutex : acquired by server");
+        server.stop();
     }        
    
     /**
@@ -139,16 +151,14 @@ public class SocketConnectionTest extends TestCase
         
         Server s = new Server();
         s.start();
-        
-        // wait for server to enter accept()
-        // TODO: add notification mechanism instead of sleeping     
-        ThreadUtil.sleep(2000);
+        s.waitForStart();
         
         int port = s.getPort();
-        
         Socket socket = new Socket("localhost", port);
         SocketConnection connection = new SocketConnection(socket);
+        
         assertNotNull("input stream is null", connection.getInputStream());
+        
         socket.close();
         s.stop();
     }
@@ -162,22 +172,18 @@ public class SocketConnectionTest extends TestCase
     {
         logger_.info("Running testGetOutputStream...");
         
-        Server s = new Server();
-        s.start();
-
-        // wait for server to enter accept()
-        // TODO: add notification mechanism instead of sleeping
-        ThreadUtil.sleep(2000);
+        Server server = new Server();
         
-        int port = s.getPort();
-        
+        server.start();
+        server.waitForStart();
+        int port = server.getPort();
         Socket socket = new Socket("localhost", port);
         SocketConnection connection = new SocketConnection(socket);
 
         assertNotNull("output stream is null", connection.getOutputStream());
         
         socket.close();
-        s.stop();
+        server.stop();
     }
 
     /**
@@ -187,19 +193,24 @@ public class SocketConnectionTest extends TestCase
      */
     public void testConnectionListener() throws Exception
     {
+        // TODO: Update test to verify connectionInterrupted() once implemented
+        
         logger_.info("Running testConnectionListener...");
         
         Server s = new Server();
         s.start();
-        
-        ThreadUtil.sleep(2000);
+        s.waitForStart();
         
         SocketConnection connection = new SocketConnection();
-        connection.addConnectionListener(new Listener());
+        Listener listener = new Listener();
+        connection.addConnectionListener(listener);
         connection.setHost("localhost");
         connection.setPort(s.getPort());
         connection.connect();
+        listener.waitForStarted();
         connection.close();
+        listener.waitForClosing();
+        listener.waitForClose();
         s.stop();
     }
 
@@ -208,21 +219,20 @@ public class SocketConnectionTest extends TestCase
      * 
      * @throws  Exception on error
      */
-    public void xtestConnectionLifeCycle() throws Exception
+    public void testConnectionLifeCycle() throws Exception
     {
         logger_.info("Running testConnectionLifeCycle...");
         
         Server s = new Server(true);
         s.start();
-        
-        ThreadUtil.sleep(2000);
-        
+        s.waitForStart();
+         
         SocketConnection connection = new SocketConnection();
         connection.addConnectionListener(new Listener());
         connection.setHost("localhost");
         connection.setPort(s.getPort());
         
-        for (int i=0; i<500; i++)
+        for (int i=0; i<100; i++)
         {
             logger_.info("Connection " + i);
             
@@ -231,9 +241,9 @@ public class SocketConnectionTest extends TestCase
                 connection.connect();
                 connection.close();
             }
-            catch(ConnectException ce)
+            catch (ConnectException ce)
             {
-                logger_.info("Failed to connect to server");
+                logger_.info("Failed to connect to server: " + ce.getMessage());
             }
             finally
             {
@@ -251,69 +261,143 @@ public class SocketConnectionTest extends TestCase
      */
     public void testIsConnected() throws Exception
     {
-        logger_.info("Running testIsConnected...");
-        
-        Server server = new Server(true);
-        server.start();        
-        
-        SocketConnection sc = 
-            new SocketConnection("localhost", server.getPort());
+        try
+        {
+            logger_.info("Running testIsConnected...");
             
-        assertTrue(sc.isConnected());
-        sc.close();
-        assertTrue(!sc.isConnected());
-        sc.connect();
-        assertTrue(sc.isConnected());
-        sc.close();
-
-        server.stop();
+            Server server = new Server(true);
+            server.start();        
+            server.waitForStart();
+            
+            SocketConnection sc = 
+                new SocketConnection("localhost", server.getPort());
+                
+            assertTrue("socket should be connted", sc.isConnected());
+            
+            sc.close();
+            
+            assertTrue("socket should be closed", !sc.isConnected());
+            
+            sc.connect();
+            assertTrue("socket should be reconnected", sc.isConnected());
+            
+            sc.close();
+            server.stop();
+        }
+        catch (Throwable t)
+        {
+            logger_.error("testIsConnected", t);
+        }
     }
-
 
     //--------------------------------------------------------------------------
     //  Test helper classes
     //--------------------------------------------------------------------------
 
     /**
-     * Dummy connection listener
+     * Socket connection listener used to make sure notification is working
      */
     class Listener implements IConnectionListener
     {
+        private BlockingQueue closed_ = new BlockingQueue();
+        private BlockingQueue closing_ = new BlockingQueue();
+        private BlockingQueue interrupted_ = new BlockingQueue();
+        private BlockingQueue started_ = new BlockingQueue(); 
+        
         public void connectionClosed(IConnection connection)
         {
             logger_.info("Notification: Connection closed " + connection);
+            
+            try
+            {
+                closed_.push("Closed");
+            }
+            catch (InterruptedException e)
+            {
+                logger_.error(e);
+            }
         }
         
         public void connectionClosing(IConnection connection)
         {
             logger_.info("Notification: Connection closing " + connection);
+            
+            try
+            {
+                closing_.push("Closing");
+            }
+            catch (InterruptedException e)
+            {
+                logger_.error(e);
+            }
         }
         
         public void connectionInterrupted(IConnection connection)
         {
             logger_.info("Notification: Connection interrupted" + connection);
+            
+            try
+            {
+                interrupted_.push("Interrupted");
+            }
+            catch (InterruptedException e)
+            {
+                logger_.error(e);
+            }
         }
         
         public void connectionStarted(IConnection connection)
         {
             logger_.info("Notification: Connection started" + connection);
+            
+            try
+            {
+                started_.push("Started");
+            }
+            catch (InterruptedException e)
+            {
+                logger_.error(e);
+            }
+        }
+        
+        public void waitForClose() throws InterruptedException
+        {
+            closed_.pull();
+        }
+        
+        public void waitForClosing() throws InterruptedException
+        {
+            closing_.pull();
+        }
+        
+        public void waitForInterrupted() throws InterruptedException
+        {
+            interrupted_.pull();
         }
 
+        public void waitForStarted() throws InterruptedException
+        {
+            started_.pull();
+        }
     }
 
-
     /**
-     * Internal socket server
+     * Internal test socket server
      */
     class Server implements Runnable
     {
+        private BlockingQueue serverStarted_;
         private ServerSocket socket_;
         private boolean longLived_ = false;
         private int port_;
         private boolean keepGoing_ = true;
                     
+        //----------------------------------------------------------------------
+        // Constructors
+        //----------------------------------------------------------------------
+                            
         /**
-         * Constructor 
+         * Default constructor 
          */
         public Server()
         {
@@ -321,19 +405,30 @@ public class SocketConnectionTest extends TestCase
         }
 
         /**
-         * Arg constructor
+         * Creates a long lived Server
          */
         public Server(boolean longLived)
         {
             this(0, longLived);
         }
 
+        /**
+         * Creates a Server
+         * 
+         * @param port
+         * @param longLived
+         */
         public Server(int port, boolean longLived)
         {
             port_ = port;
             longLived_ = longLived;
+            serverStarted_ = new BlockingQueue();
         }
 
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+        
         /**
          * Starts the socket server
          */
@@ -353,6 +448,29 @@ public class SocketConnectionTest extends TestCase
             keepGoing_ = false;
             socket_.close();
         }
+
+        /**
+         * Retrieves the server socket port
+         */
+        public int getPort() 
+        {
+            return socket_.getLocalPort();
+        }
+        
+        /**
+         * Once called, will not return until the socket server is ready to
+         * accept new connections.
+         * 
+         * @throws InterruptedException
+         */
+        public void waitForStart() throws InterruptedException
+        {
+            serverStarted_.pull();
+        }
+        
+        //----------------------------------------------------------------------
+        // Implements Runnable
+        //----------------------------------------------------------------------
         
         /**
          * Runs server on thread
@@ -364,23 +482,28 @@ public class SocketConnectionTest extends TestCase
                 try
                 {
                     logger_.info("Server: Waiting to accept...");
-                    socket_.accept();
-                    keepGoing_ = longLived_;
-                    logger_.info("Server: After accept..");
+                    serverStarted_.push("Started");
+                    
+                    if (!socket_.isClosed())
+                    {
+                        socket_.accept();
+                        keepGoing_ = longLived_;
+                        logger_.info("Server: After accept..");
+                    }
+                }
+                catch (SocketException se)
+                {
+                    logger_.info(se.getMessage());
                 }
                 catch(IOException e)
                 {
-                    logger_.info(e.toString());
+                    logger_.error("run", e);
+                } 
+                catch (InterruptedException e)
+                {
+                    logger_.error("run2", e);
                 }
             }
         }
-            
-        /**
-         * Retrieves the server socket port
-         */
-        public int getPort() 
-        {
-            return socket_.getLocalPort();
-        }           
     }
 }
