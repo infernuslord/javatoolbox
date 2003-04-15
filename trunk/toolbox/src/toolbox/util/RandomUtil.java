709732581c0f95bd5c8a2008f4d13f24606b2f65
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-15 22:15:25 +0000

    Javadoc updates

--- trunk/toolbox/src/toolbox/util/RandomUtil.java
+++ trunk/toolbox/src/toolbox/util/RandomUtil.java
@@ -36,32 +36,30 @@ public final class RandomUtil
     /**
      * Generates an unsigned random integer in the range [0..MAX_INT]
      * 
-     * @return    int
+     * @return  Random int
      */
     public static int nextInt()
     {
         return Math.abs(random_.nextInt());
     }
 
-
     /**
      * Generates a random integer in the range [0..ceiling]
      * 
      * @param   ceiling    Maximum value of the random number
-     * @return  int 
+     * @return  Random int <= ceiling
      */
     public static int nextInt(int ceiling)
     {
         return (nextInt() % (ceiling+1));
     }
 
-
     /**
      * Generates a random integer in the range [floor..ceiling]
      *
      * @param   floor        Lower boundary
      * @param   ceiling      Upper boundary
-     * @return  int
+     * @return  Random int
      */
     public static int nextInt(int floor, int ceiling)
     {
@@ -70,48 +68,46 @@ public final class RandomUtil
             
         return floor + (nextInt(ceiling - floor));
     }
-
     
     /**
      * Generates a random lowercase alpha character in the range [a..z]
      * 
-     * @return    char
+     * @return  Random lowercase car
      */
     public static char nextLowerAlpha()
     {
         return alphaChars_.charAt(nextInt(alphaChars_.length()-1));
     }
-
     
     /**
      * Generates a random uppercase alpha character in the range [A..Z]
      * 
-     * @return  char
+     * @return  Random uppercase char
      */
     public static char nextUpperAlpha()
     {
         return Character.toUpperCase(nextLowerAlpha());
     }
-
     
     /**
      * Generates a random alpha character in the range [a..z, A..Z]
      * 
-     * @return  char
+     * @return  Random case-agnostic char
      */
     public static char nextAlpha()
     {
         char c = nextLowerAlpha();
+        
         if(random_.nextBoolean())
             c = Character.toUpperCase(c);
+            
         return c;
     }
-
     
     /**
      * Generates a random double
      *  
-     * @return  double
+     * @return  Random double
      */
     public static double nextDouble()
     {
@@ -120,12 +116,11 @@ public final class RandomUtil
         return (double) (decimal + fraction / 100);
     }
 
-
     /**
      * Generates a random double with a maximum value
      * 
-     * @param  ceiling  Maximum value of double to generate
-     * @return A random signed double from 0.0 to ceiling
+     * @param   ceiling  Maximum value of double to generate
+     * @return  A random signed double from 0.0 to ceiling
      */
     public static double nextDouble(double ceiling)
     {
@@ -134,17 +129,15 @@ public final class RandomUtil
         return (double) (decimal + fraction / 100);
     }
     
-    
     /**
      * Generates a random boolean 
      * 
-     * @return  boolean
+     * @return  Random boolean
      */
     public static boolean nextBoolean()
     {
         return (nextInt() %2 == 0);
     }
-    
     
     /**
      * Returns a randomly chosen element from the passed in array
@@ -158,7 +151,6 @@ public final class RandomUtil
         return (pickList.length == 0 ? null : 
             pickList[nextInt(pickList.length-1)]);        
     }    
-
 
     /**
      * Returns a randomly chosen element from the passed in list
