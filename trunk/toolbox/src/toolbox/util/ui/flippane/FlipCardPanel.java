a81241d67d2d7bf2cd8d9eab00210cbea386d096
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-16 20:16:23 +0000

    Fixed mouse offset with dragging divider. Updated javadocs

--- trunk/toolbox/src/toolbox/util/ui/flippane/FlipCardPanel.java
+++ trunk/toolbox/src/toolbox/util/ui/flippane/FlipCardPanel.java
@@ -17,7 +17,14 @@ import javax.swing.border.Border;
  */
 public class FlipCardPanel extends JPanel
 {
+    /** 
+     * Flippane encompassing this panel
+     */
     private JFlipPane flipPane_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
     
     /**
      * Creates a FlipCardPanel
@@ -33,6 +40,10 @@ public class FlipCardPanel extends JPanel
         addMouseMotionListener(resizeMouseHandler);
     } 
 
+    //--------------------------------------------------------------------------
+    // Package
+    //--------------------------------------------------------------------------
+    
     /**
      * Shows the card with the given name
      * 
@@ -43,6 +54,10 @@ public class FlipCardPanel extends JPanel
         ((CardLayout)getLayout()).show(this,name);
     } 
 
+    //--------------------------------------------------------------------------
+    // Overridden from JComponent
+    //--------------------------------------------------------------------------
+    
     /**
      * @return  Minimum size
      */
@@ -52,7 +67,11 @@ public class FlipCardPanel extends JPanel
     } 
     
     /**
-     * @return  Preferred size
+     * Returns the preferred size of the flippane. Takes the width of the 
+     * button bar and the current width of the flipper (assuming its LEFT or
+     * RIGHT), adds them together and thats it.
+     * 
+     * @return  Preferred size of the flippane
      */
     public Dimension getPreferredSize()
     {
@@ -64,10 +83,11 @@ public class FlipCardPanel extends JPanel
         }
         else
         {
+            // Use pos to figure out if we're working on a height or a width
             int    dim = flipPane_.getDimension();
             String pos = flipPane_.getPosition();
                         
-            if(flipPane_.getDimension()  <= 0)
+            if (flipPane_.getDimension()  <= 0)
             {
                 int width = super.getPreferredSize().width;
                 
@@ -75,7 +95,7 @@ public class FlipCardPanel extends JPanel
                     width - JFlipPane.SPLITTER_WIDTH - 3);
             }
 
-            if(pos.equals(JFlipPane.TOP) || pos.equals(JFlipPane.BOTTOM))
+            if (pos.equals(JFlipPane.TOP) || pos.equals(JFlipPane.BOTTOM))
             {
                 pref = new Dimension(0,
                     dim + JFlipPane.SPLITTER_WIDTH + 3);
@@ -85,34 +105,42 @@ public class FlipCardPanel extends JPanel
                 pref = new Dimension(dim + JFlipPane.SPLITTER_WIDTH + 3, 0);
             }
         }
-
-        //fcpLogger_.info("prefsize=" + pref);
         
         return pref;
     } 
 
+    //--------------------------------------------------------------------------
+    // Inner Classes
+    //--------------------------------------------------------------------------
+    
     /**
      * Mouse handler for resizing of the pane
      */
-    class ResizeMouseHandler extends MouseAdapter 
-        implements MouseMotionListener
+    class ResizeMouseHandler extends MouseAdapter implements MouseMotionListener
     {
         private boolean canDrag_;
-        private int dragStartDimension_;
-        private Point dragStart_;
+        private int     dragStartDimension_;
+        private Point   dragStart_;
 
         /**
-         * Records start of mouse drag and dimension
+         * Takes an image of the flip pane dimension (height or width) and 
+         * the point that the mouse started to be dragged
+         * 
+         * @param  evt  Mouse pressed event
          */
         public void mousePressed(MouseEvent evt)
         {
-            dragStartDimension_ = flipPane_.getDimension();
+            dragStartDimension_ = 
+                flipPane_.getDimension() + JFlipPane.SPLITTER_WIDTH + 3;
+                
             dragStart_ = evt.getPoint();
         } 
         
         /** 
-         * Changes mouse cursor based on location over the draggable part
-         * of the border
+         * Changes mouse cursor based on location over the draggable part of 
+         * the border
+         * 
+         * @param  evt  Mouse moved event
          */
         public void mouseMoved(MouseEvent evt)
         {
@@ -131,7 +159,7 @@ public class FlipCardPanel extends JPanel
             // Top...
             if (flipPane_.getPosition().equals(JFlipPane.TOP))
             {
-                if(evt.getY() >= getHeight() - insets.bottom)
+                if (evt.getY() >= getHeight() - insets.bottom)
                 {
                     cursor = Cursor.N_RESIZE_CURSOR;
                     canDrag_ = true;
@@ -140,7 +168,7 @@ public class FlipCardPanel extends JPanel
             // Left...
             else if (flipPane_.getPosition().equals(JFlipPane.LEFT))
             {
-                if(evt.getX() >= getWidth() - insets.right)
+                if (evt.getX() >= getWidth() - insets.right)
                 {
                     cursor = Cursor.W_RESIZE_CURSOR;
                     canDrag_ = true;
@@ -149,7 +177,7 @@ public class FlipCardPanel extends JPanel
             // Bottom...
             else if (flipPane_.getPosition().equals(JFlipPane.BOTTOM))
             {
-                if(evt.getY() <= insets.top)
+                if (evt.getY() <= insets.top)
                 {
                     cursor = Cursor.S_RESIZE_CURSOR;
                     canDrag_ = true;
@@ -158,7 +186,7 @@ public class FlipCardPanel extends JPanel
             // Right...
             else if (flipPane_.getPosition().equals(JFlipPane.RIGHT))
             {
-                if(evt.getX() <= insets.left)
+                if (evt.getX() <= insets.left)
                 {
                     cursor = Cursor.E_RESIZE_CURSOR;
                     canDrag_ = true;
@@ -171,41 +199,43 @@ public class FlipCardPanel extends JPanel
         /**
          * Sets dimension on flippane if the mouse is dragged. This causes
          * the flippane to resize dynamically with the drag
+         * 
+         * @param  evt  Mouse dragged event
          */
         public void mouseDragged(MouseEvent evt)
         {
-            if(!canDrag_)
+            if (!canDrag_)
                 return;
 
-            if(dragStart_ == null) // can't happen?
+            if (dragStart_ == null) // can't happen?
                 return;
 
             // Top...
-            if(flipPane_.getPosition().equals(JFlipPane.TOP))
+            if (flipPane_.getPosition().equals(JFlipPane.TOP))
             {
-                flipPane_.setDimension(evt.getY() + dragStartDimension_ - 
-                    dragStart_.y);
+                flipPane_.setDimension(
+                    evt.getY() + dragStartDimension_ - dragStart_.y);
             } 
             // Left...
-            else if(flipPane_.getPosition().equals(JFlipPane.LEFT))
+            else if (flipPane_.getPosition().equals(JFlipPane.LEFT))
             {
-                flipPane_.setDimension(evt.getX() + dragStartDimension_ - 
-                    dragStart_.x);
+                flipPane_.setDimension(
+                    evt.getX() + dragStartDimension_ - dragStart_.x);
             } 
             // Bottom...
-            else if(flipPane_.getPosition().equals(JFlipPane.BOTTOM))
+            else if (flipPane_.getPosition().equals(JFlipPane.BOTTOM))
             {
                 flipPane_.setDimension(
                     flipPane_.getDimension() + (dragStart_.y - evt.getY()));
             } 
             // Right...
-            else if(flipPane_.getPosition().equals(JFlipPane.RIGHT))
+            else if (flipPane_.getPosition().equals(JFlipPane.RIGHT))
             {
                 flipPane_.setDimension(
                     flipPane_.getDimension() + dragStart_.x - evt.getX());
             } 
 
-            if(flipPane_.getDimension() <= 0)
+            if (flipPane_.getDimension() <= 0)
                 flipPane_.setDimension(dragStartDimension_);
 
             // TODO: find out right way to do this
@@ -220,7 +250,10 @@ public class FlipCardPanel extends JPanel
         } 
         
         /**
-         * Reset the mouse cursor to the normal cursor 
+         * Reset the mouse cursor to the normal cursor once dragging is
+         * is completed.
+         * 
+         * @param  evet  Mouse exited event 
          */
         public void mouseExited(MouseEvent evt)
         {
