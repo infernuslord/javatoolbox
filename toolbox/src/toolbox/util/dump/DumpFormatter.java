ce84f30efaef064a4145cf9d0b611961c517fbef
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-18 07:26:52 +0000

    Dropped the "I"nterface

---
+++ toolbox/src/toolbox/util/dump/DumpFormatter.java
@@ -0,0 +1,54 @@
+package toolbox.util.dump;
+
+import java.lang.reflect.Field;
+
+/**
+ * DumpFormatter defines the configurable dump and formatting options exposed 
+ * by the object dumper.
+ */
+public interface DumpFormatter
+{
+    /**
+     * Should the given class be included in the dump?
+     * 
+     * @param   clazz    Class to test for inclusion
+     * @return  boolean  True if class should be included, false otherwise
+     */
+    public boolean shouldInclude(Class clazz);
+
+    /**
+     * Should the given field be included in the dump?
+     * 
+     * @param   field    Field to test for inclusion
+     * @return  boolean  True if field should be included, false otherwise
+     */
+    public boolean shouldInclude(Field field);
+    
+    /**
+     * @return True if the inheritance tree should be shown for each object
+     *         traversed.
+     */
+    public boolean showInheritance();
+    
+    /**
+     * Formats the presentation of a classes' name
+     * 
+     * @param   className  Class name to format
+     * @return  Formatted class name
+     */
+    public String  formatClassName(String className);
+    
+    /**
+     * Formats the presentation of a classes' name
+     * 
+     * @param   clazz  Class name to format
+     * @return  Formatted classname
+     */
+    public String  formatClassName(Class clazz);
+    
+    /**
+     * @return  True if the fields in a class should be sorted alphabetically,
+     *          false otherwise.
+     */
+    public boolean sortFields();
+}
