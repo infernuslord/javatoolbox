f5625212e5993aaf658799945ddbc83bd178e8c7
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-24 19:22:49 +0000

    Initial version

---
+++ toolbox/src/toolbox/TODO.java
@@ -0,0 +1,14 @@
+package toolbox;
+
+/**
+ * TODO: SSH on CVS
+ * TODO: Move CVS server to booger2
+ * TODO: Move Tomcat server to booger2
+ * TODO: Configure VNC on special port to redirect to booger2 from router
+ * TODO: Configure toolbox to use latest Clover
+ * TODO: Upgrade to checkstyle 3.0
+ * TODO: Figure out how to launch web browser consistently for build.xml
+ */
+public class TODO
+{
+}
