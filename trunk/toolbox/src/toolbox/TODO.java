b136b1400a5122467a2491727bbca4df7b82469b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-27 09:16:28 +0000

    Removed TODO: Upgrade to latest Clover Removed TODO: Moved CVS server to goober2

--- trunk/toolbox/src/toolbox/TODO.java
+++ trunk/toolbox/src/toolbox/TODO.java
@@ -2,10 +2,8 @@ package toolbox;
 
 /**
  * TODO: SSH on CVS
- * TODO: Move CVS server to booger2
  * TODO: Move Tomcat server to booger2
  * TODO: Configure VNC on special port to redirect to booger2 from router
- * TODO: Configure toolbox to use latest Clover
  * TODO: Upgrade to checkstyle 3.0
  * TODO: Figure out how to launch web browser consistently for build.xml
  * TODO: Add vizant for visualization of build dependencies
