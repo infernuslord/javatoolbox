88fbf01aee383883b7cbf82b12eb13a4c17af066
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-04-28 18:07:26 +0000

    Completed TODO: Configure router for VNC->laptop

--- toolbox/src/toolbox/TODO.java
+++ toolbox/src/toolbox/TODO.java
@@ -3,7 +3,6 @@ package toolbox;
 /**
  * TODO: SSH on CVS
  * TODO: Move Tomcat server to booger2
- * TODO: Configure VNC on special port to redirect to booger2 from router
  * TODO: Upgrade to checkstyle 3.0
  * TODO: Figure out how to launch web browser consistently for build.xml
  * TODO: Add vizant for visualization of build dependencies
