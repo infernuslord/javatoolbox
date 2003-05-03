0444b5ada9c9742ac7a7d292f89efbccf8b81e07
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-03 01:01:26 +0000

    Completed javadocs

--- trunk/toolbox/src/toolbox/util/ui/table/TableMap.java
+++ trunk/toolbox/src/toolbox/util/ui/table/TableMap.java
@@ -1,8 +1,9 @@
 package toolbox.util.ui.table;
 
-import javax.swing.table.*;
-import javax.swing.event.TableModelListener;
 import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
 /** 
  * In a chain of data manipulators some behaviour is common. TableMap
@@ -18,64 +19,103 @@ import javax.swing.event.TableModelEvent;
  */
 public class TableMap extends AbstractTableModel implements TableModelListener
 {
-    protected TableModel model_;
+    /** Delegate table model */
+    private TableModel model_;
 
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * @return  Table model
+     */    
     public TableModel getModel()
     {
         return model_;
     }
 
+    /**
+     * Sets the table model
+     * 
+     * @param model Table model 
+     */
     public void setModel(TableModel model)
     {
         model_ = model;
         model.addTableModelListener(this);
     }
 
-    // By default, Implement TableModel by forwarding all messages 
-    // to the model. 
-
+    //--------------------------------------------------------------------------
+    // Overrides AbstractTableModel
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see javax.swing.table.TableModel#getValueAt(int, int)
+     */        
     public Object getValueAt(int aRow, int aColumn)
     {
         return model_.getValueAt(aRow, aColumn);
     }
-
+        
+    /**
+     * @see javax.swing.table.TableModel#setValueAt(java.lang.Object, int, int)
+     */
     public void setValueAt(Object aValue, int aRow, int aColumn)
     {
         model_.setValueAt(aValue, aRow, aColumn);
     }
 
+    /**
+     * @see javax.swing.table.TableModel#getRowCount()
+     */
     public int getRowCount()
     {
         return (model_ == null) ? 0 : model_.getRowCount();
     }
 
+    /**
+     * @see javax.swing.table.TableModel#getColumnCount()
+     */
     public int getColumnCount()
     {
         return (model_ == null) ? 0 : model_.getColumnCount();
     }
 
+    /**
+     * @see javax.swing.table.TableModel#getColumnName(int)
+     */
     public String getColumnName(int aColumn)
     {
         return model_.getColumnName(aColumn);
     }
 
+    /**
+     * @see javax.swing.table.TableModel#getColumnClass(int)
+     */
     public Class getColumnClass(int aColumn)
     {
         return model_.getColumnClass(aColumn);
     }
 
+    /**
+     * @see javax.swing.table.TableModel#isCellEditable(int, int)
+     */
     public boolean isCellEditable(int row, int column)
     {
         return model_.isCellEditable(row, column);
     }
     
-    //
-    // Implementation of the TableModelListener interface, 
-    //
+    //--------------------------------------------------------------------------
+    // TableModelListener Interface
+    //--------------------------------------------------------------------------
 
-    // By default forward all events to all the listeners. 
+    /**
+     * @see javax.swing.event.TableModelListener#
+     *          tableChanged(javax.swing.event.TableModelEvent)
+     */
     public void tableChanged(TableModelEvent e)
     {
+        // By default forward all events to all the listeners.
         fireTableChanged(e);
     }
 }
