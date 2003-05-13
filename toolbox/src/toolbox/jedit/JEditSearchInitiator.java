003e19c3b078b094bdacc34ed8d2afdcf4c8c9a6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-13 02:36:23 +0000

    Initial version

---
+++ toolbox/src/toolbox/jedit/JEditSearchInitiator.java
@@ -0,0 +1,56 @@
+package toolbox.jedit;
+
+import java.awt.Frame;
+
+import toolbox.util.SwingUtil;
+import toolbox.util.ui.JFindDialog;
+
+/**
+ * JEdit specific SearchInitiator which allows text searches of the contents
+ * of the text area.
+ */
+public class JEditSearchInitiator implements JFindDialog.SearchInitiator
+{
+    private String searchString_;
+    private JEditTextArea jeta_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a JEditSearchInitiator
+     * 
+     * @param  searchString  String to search for
+     * @param  jeta          JEdit text area to search
+     */
+    public JEditSearchInitiator(String searchString, JEditTextArea jeta)
+    {
+        searchString_ = searchString;
+        jeta_ = jeta;
+    }
+
+    //--------------------------------------------------------------------------
+    // SearchInitiator Interface
+    //--------------------------------------------------------------------------
+        
+    public String getSearchString()
+    {
+        return searchString_;
+    }
+
+    public String getText()
+    {
+        return jeta_.getText();
+    }
+
+    public void selectText(int start, int end)
+    {
+        jeta_.select(start, end);
+    }
+    
+    public Frame getFrame()
+    {
+        return SwingUtil.getFrameAncestor(jeta_);
+    }
+}
