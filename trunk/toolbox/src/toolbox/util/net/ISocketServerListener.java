3c5559e6c06db7e749dec61aa7b9e0aa2f4c8d04
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-18 05:13:40 +0000

    Added serverStarted()

--- trunk/toolbox/src/toolbox/util/net/ISocketServerListener.java
+++ trunk/toolbox/src/toolbox/util/net/ISocketServerListener.java
@@ -12,5 +12,13 @@ public interface ISocketServerListener
      * 
      * @param  socket  Newly created socket to communicate with the client
      */
-    public void socketAccepted(Socket socket);
+    public void socketAccepted(Socket socket, IConnection connection);
+
+    /**
+     * Notification that the socket server has started and is ready to accept
+     * client connections.
+     * 
+     * @param  server  Server that started
+     */    
+    public void serverStarted(SocketServer server);
 }
