a697d078b176cd92ad47cb53803ab33ab8be46ff
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-20 04:21:06 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/ui/TryCatchAction.java
@@ -0,0 +1,65 @@
+package toolbox.util.ui;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.ExceptionUtil;
+
+/**
+ * Convenience class that removes the need to add a try/catch block to the 
+ * actionPerformed() method of your AbstractAction implementations.
+ * TryCatchAction catches all exceptions and displays an error with stack trace
+ * into to the display. The exception is also logged via Log4J as an ERROR.
+ */
+public abstract class TryCatchAction extends AbstractAction
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(TryCatchAction.class);
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a TryCatchAction
+     * 
+     * @param  name  Name of action
+     */
+    public TryCatchAction(String name)
+    {
+        super(name);
+    }
+    
+    //--------------------------------------------------------------------------
+    // ActionListener Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see java.awt.event.ActionListener#
+     *      actionPerformed(java.awt.event.ActionEvent)
+     */
+    public final void actionPerformed(ActionEvent e)
+    {
+        try
+        {
+            tryActionPerformed(e);
+        }
+        catch (Throwable t)
+        {
+            ExceptionUtil.handleUI(t, logger_);
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Abstract 
+    //--------------------------------------------------------------------------
+    
+    /**
+     * actionPerformed delegates to this method after surrounding it with a 
+     * try/catch block to handle errors.
+     */
+    public abstract void tryActionPerformed(ActionEvent e);
+}
