84dc7768dffaa69b5c1f7ace5614568d0028badf
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-21 20:00:20 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/JConveyorPopupMenu.java
@@ -0,0 +1,71 @@
+package toolbox.util.ui;
+
+import javax.swing.Action;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+/**
+ * JPopupMenu that works like a conveyor belt. New items get inserted at the top
+ * of the menu and items get pushed off the bottom of the menu when the 
+ * capacity is reached.
+ */
+public class JConveyorPopupMenu extends JPopupMenu
+{
+    /** Max capacity of the menu */
+    private int capacity_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a JConveyorPopupMenu
+     * 
+     * @param  capacity  Max number of menu items allowed in the menu before
+     *                   items at the bottom of the menu start getting pushed
+     *                   off.
+     */
+    public JConveyorPopupMenu(int capacity)
+    {
+        this("", capacity);
+    }
+
+    /**
+     * Creates a JConveyorPopupMenu
+     * 
+     * @param  title     Popup menu title
+     * @param  capacity  Max number of menu items allowed in the menu before
+     *                   items at the bottom of the menu start getting pushed
+     *                   off.
+     */
+    public JConveyorPopupMenu(String title, int capacity)
+    {
+        super(title);
+        capacity_ = capacity;    
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides javax.swing.JPopupMenu
+    //--------------------------------------------------------------------------
+        
+    /**
+     * @see javax.swing.JPopupMenu#add(javax.swing.Action)
+     */
+    public JMenuItem add(Action action)
+    {
+        return add(createActionComponent(action));
+    }
+    
+    /**
+     * @see javax.swing.JPopupMenu#add(javax.swing.JMenuItem)
+     */
+    public JMenuItem add(JMenuItem menuItem)
+    {
+        insert(menuItem, 0);
+        
+        if (getComponentCount() > capacity_)
+            remove(capacity_);
+        
+        return menuItem;
+    }
+}
