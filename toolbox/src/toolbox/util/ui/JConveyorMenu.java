8ad1a367dbd6d2f4132dd30e8732a7b5e90bb724
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-21 06:06:29 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/JConveyorMenu.java
@@ -0,0 +1,63 @@
+package toolbox.util.ui;
+
+import javax.swing.Action;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+/**
+ * JMenu that works like a conveyor belt. New items get inserted at the top
+ * of the menu and items get pushed off the bottom of the menu when the 
+ * capacity is reached.
+ */
+public class JConveyorMenu extends JMenu
+{
+    /** Max capacity of the menu */
+    private int capacity_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a JConveyorMenu
+     * 
+     * @param  capacity  Max number of menu items allowed in the menu before
+     *                   items at the bottom of the menu start getting pushed
+     *                   off.
+     */
+    public JConveyorMenu(int capacity)
+    {
+        this("", capacity);
+    }
+
+    /**
+     * Creates a JConveyorMenu
+     * 
+     * @param  title     Menu title
+     * @param  capacity  Max number of menu items allowed in the menu before
+     *                   items at the bottom of the menu start getting pushed
+     *                   off.
+     */
+    public JConveyorMenu(String title, int capacity)
+    {
+        super(title);
+        capacity_ = capacity;    
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides javax.swing.JMenu
+    //--------------------------------------------------------------------------
+        
+    /**
+     * @see javax.swing.JMenu#add(javax.swing.Action)
+     */
+    public JMenuItem add(Action action)
+    {
+        insert(action, 0);
+        
+        if (getItemCount() > capacity_)
+            remove(capacity_);
+        
+        return getItem(0);
+    }
+}
