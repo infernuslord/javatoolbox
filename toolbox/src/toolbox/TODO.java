0a5c293a5ea4e566628310be4ee003e94db6b25c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-23 04:26:05 +0000

    Completed "recent" menu stuff with JConveyorMenu and JConveyorPopupMenu

--- toolbox/src/toolbox/TODO.java
+++ toolbox/src/toolbox/TODO.java
@@ -8,7 +8,6 @@ package toolbox;
  * TODO: Upgrade to checkstyle 3.0
  * TODO: Figure out how to launch web browser consistently for build.xml
  * TODO: Add vizant for visualization of build dependencies
- * TODO: "Recent" menu type component
  * TODO: Add package.html to all packages
  * TODO: Add support to minimize to system tray - systray4j
  * TODO: Upgrade to ant 1.5.3
