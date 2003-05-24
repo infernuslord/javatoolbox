224c56dc9268abbc43ce53a1a87021e604470d96
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-24 08:17:35 +0000

    Seeded from IPlugin

---
+++ trunk/toolbox/src/toolbox/workspace/IPreferenced.java
@@ -0,0 +1,26 @@
+package toolbox.util.ui.plugin;
+
+import java.util.Properties;
+
+/**
+ * IPreferenced provides a simple interface for saveing and restoring
+ * preferences between application instances.
+ */
+public interface IPreferenced
+{
+    /**
+     * Saves preferences to the provided preferences object.
+     *  
+     * @param  prefs  Properties object to save the implementor preferences to.
+     */
+    public void savePrefs(Properties prefs);
+    
+    /**
+     * Reads preferences from the provided preferences object an apply them to
+     * the implementor.
+     * 
+     * @param  prefs  Properties object from which to read the implementors 
+     *                preferences from. 
+     */
+    public void applyPrefs(Properties prefs);
+}
