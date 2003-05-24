ac0a2cef3d05030797025a45c23d43061b0edaa6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-24 08:18:00 +0000

    Expanded interface...not solidified yet.

--- toolbox/src/toolbox/workspace/IStatusBar.java
+++ toolbox/src/toolbox/workspace/IStatusBar.java
@@ -5,12 +5,22 @@ package toolbox.util.ui.plugin;
  */
 public interface IStatusBar
 {
+    public static final int BUSY    = 1;
+    public static final int ERROR   = 2;
+    public static final int INFO    = 3;
+    public static final int WARNING = 4;
+    
     /**
      * Sets the status text
      * 
      * @param  status  Status text
      */
     public void setStatus(String status);
+    
+    public void setBusy(String status);
+    public void setError(String status);
+    public void setInfo(String status);
+    public void setWarning(String status);
     
     /**
      * Retrieves the status text
