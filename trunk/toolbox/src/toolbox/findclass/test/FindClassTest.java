653c399265405cb88041d12697d0cebc78fe2ede
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-24 08:13:51 +0000

    Added more tests

--- trunk/toolbox/src/toolbox/findclass/test/FindClassTest.java
+++ trunk/toolbox/src/toolbox/findclass/test/FindClassTest.java
@@ -36,18 +36,72 @@ public class FindClassTest extends TestCase
     //--------------------------------------------------------------------------
     
     /**
-     * Test finding a class in a jarfile
+     * Test finding a system class
      * 
      * @throws  Exception on error
      */
-    public void testFindInJar() throws Exception
+    public void testFindClassSystem() throws Exception
     {
-        logger_.info("Running testFindInJar...");
+        logger_.info("Running testFindClassSystem...");
         
+        String searchFor = "java.lang.Object";
         FindClass finder = new FindClass();
-        FindClassResult[] results = finder.findClass("Info$", false);
         
-        logger_.info("\n" + ArrayUtil.toString(results, true));
+        String results = 
+            ArrayUtil.toString(
+                finder.findClass(searchFor, false), true);
+
+        logger_.info("Results: " + results);
+        assertTrue("Couldn't find " + searchFor, results.indexOf(searchFor)>=0);
+    }
+    
+    /**
+     * Test class not found
+     * 
+     * @throws  Exception on error
+     */
+    public void testFindClassNotFound() throws Exception
+    {
+        logger_.info("Running testFindClassNotFound...");
+        
+        String searchFor = "java.lang.Bogus";
+        FindClass finder = new FindClass();
+        FindClassResult[] results = finder.findClass(searchFor, false);
+        assertEquals("Class should not have been found", 0, results.length);
     }
 
+    /**
+     * Test case sensetivity
+     * 
+     * @throws  Exception on error
+     */
+    public void testFindClassCaseSensetivity() throws Exception
+    {
+        logger_.info("Running testFindClassCaseSensetivity...");
+        
+        String searchFor = "java.lang.object"; // <== o is lowercase
+        FindClass finder = new FindClass();
+        FindClassResult[] results = finder.findClass(searchFor, false);
+        assertEquals("Class should not have been found", 0, results.length);
+        
+        results = finder.findClass(searchFor, true); // Ignore case
+        assertEquals("Class should have been found", 1, results.length);
+    }
+    
+    /**
+     * Test finding an archive class
+     * 
+     * @throws  Exception on error
+     */
+    public void xtestFindClassInArchive() throws Exception
+    {
+        logger_.info("Running testFindClassInArchive...");
+        
+        String searchFor = getClass().getName();
+        FindClass finder = new FindClass();
+        FindClassResult[] results = finder.findClass(searchFor, false);
+
+        logger_.info("Results: " + ArrayUtil.toString(results));
+        assertEquals("Couldn't find " + searchFor, 1, results.length);
+    }
 }
