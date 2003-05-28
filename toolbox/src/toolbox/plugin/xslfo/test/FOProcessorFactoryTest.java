9a86345d196a4a976d31c98009d9c578451d6e4b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-05-28 23:28:58 +0000

    Initial version

---
+++ toolbox/src/toolbox/plugin/xslfo/test/FOProcessorFactoryTest.java
@@ -0,0 +1,90 @@
+package toolbox.util.xslfo.test;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.xslfo.FOProcessor;
+import toolbox.util.xslfo.FOProcessorFactory;
+
+/**
+ * Unit test for FOPProcessorFactory
+ */
+public class FOProcessorFactoryTest extends TestCase
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(FOProcessorFactoryTest.class);
+        
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Entrypoint
+     * 
+     * @param  args  None recognized
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(FOProcessorFactoryTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+
+    /**
+     * Simple case: create processor for Apache FOP
+     * 
+     * @throws Exception on error
+     */
+    public void testCreateProcessorFOP() throws Exception
+    {
+        logger_.info("Running testCreateProcessorFOP...");
+        
+        FOProcessor fop =
+            FOProcessorFactory.createProcessor(
+                FOProcessorFactory.FO_IMPL_APACHE);
+
+        assertNotNull(fop);
+    }
+
+    /**
+     * Simple case: create processor for RenderX XEP
+     * 
+     * @throws Exception on error
+     */
+    public void testCreateProcessorXEP() throws Exception
+    {
+        logger_.info("Running testCreateProcessorXEP...");
+                
+        FOProcessor fop =
+            FOProcessorFactory.createProcessor(
+                FOProcessorFactory.FO_IMPL_RENDERX);
+
+        assertNotNull(fop);
+    }
+
+    /**
+     * Test fault generation on unknown FO implementation
+     * 
+     * @throws Exception on error
+     */
+    public void testCreateProcessorBogus() throws Exception
+    {
+        logger_.info("Running testCreateProcessorBogus...");
+        
+        try
+        {
+            FOProcessor fop =
+                FOProcessorFactory.createProcessor("FO_IMPL_BOGUS");
+
+            fail("Exception should have thrown for unknown FO impl");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Passed.
+        }
+    }
+}
