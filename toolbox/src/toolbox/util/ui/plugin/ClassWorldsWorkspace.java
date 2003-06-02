7721d3cf7d9447b55a0945da3cbe9f5e930dc1a6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-06-02 16:29:25 +0000

    Commented out deps

--- toolbox/src/toolbox/util/ui/plugin/ClassWorldsWorkspace.java
+++ toolbox/src/toolbox/util/ui/plugin/ClassWorldsWorkspace.java
@@ -2,8 +2,8 @@ package toolbox.util.ui.plugin;
 
 import java.net.URL;
 
-import com.werken.classworlds.ClassRealm;
-import com.werken.classworlds.ClassWorld;
+//import com.werken.classworlds.ClassRealm;
+//import com.werken.classworlds.ClassWorld;
 
 public class ClassWorldsWorkspace
 {
@@ -16,9 +16,9 @@ public class ClassWorldsWorkspace
     {
         try
         {
-            ClassWorld world = new ClassWorld();
-            ClassRealm toolbox = world.newRealm("toolbox");
-            toolbox.addConstituent(new URL("file:../lib/fop.jar"));
+//            ClassWorld world = new ClassWorld();
+//            ClassRealm toolbox = world.newRealm("toolbox");
+//            toolbox.addConstituent(new URL("file:../lib/fop.jar"));
         }
         catch (Exception e)
         {
