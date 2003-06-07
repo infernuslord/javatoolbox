30560125be6801df1f681c541e77a248c8b90208
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-06-07 01:01:58 +0000

    Javadocs

--- trunk/toolbox/src/toolbox/plugin/jdbc/DBProfile.java
+++ trunk/toolbox/src/toolbox/plugin/jdbc/DBProfile.java
@@ -6,53 +6,151 @@ import java.io.StringReader;
 import toolbox.util.xml.XMLNode;
 import toolbox.util.xml.XMLParser;
 
-
-class DBProfile
+/**
+ * Database connection profile for JDBC
+ */
+public class DBProfile
 {
+    /**
+     * XML element name for a DBProfile
+     */
     private static final String ELEMENT_PROFILE = "DBProfile";
-    private static final String ATTR_DB         = "database";
-    private static final String ATTR_DRIVER     = "driver";
-    private static final String ATTR_URL        = "url";
-    private static final String ATTR_USERNAME   = "username";
-    private static final String ATTR_PASSWORD   = "password";
     
-    private String database_;
+    /**
+     * XML attribute name for the profile name
+     */
+    private static final String ATTR_PROFILE_NAME = "profilename";
+    
+    /**
+     * XML attribute name for the JDBC driver 
+     */
+    private static final String ATTR_DRIVER = "driver";
+    
+    /**
+     * XML attribute name for the JDBC url 
+     */
+    private static final String ATTR_URL = "url";
+    
+    /**
+     * XML attribute name for the JDBC user 
+     */
+    private static final String ATTR_USERNAME = "username";
+    
+    /**
+     * XML attribute name for the JDBC authentication password (clear text)
+     */
+    private static final String ATTR_PASSWORD = "password";
+    
+    /**
+     * Profile name is a user friendly term used to uniquely identify a profile 
+     */
+    private String profileName_;
+    
+    /**
+     * JDBC driver class name. Must be a FQCN!
+     */
     private String driver_;
+    
+    /**
+     * JDBC access url
+     */
     private String url_;
+    
+    /**
+     * Username for database access
+     */
     private String username_;
+    
+    /**
+     * Password to authenticate the user with
+     */
     private String password_;
     
-    DBProfile(String xml) throws IOException
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a DBProfile from XML
+     *
+     * @param  xml  String containing a valid XML persistence of DBProfile
+     * @throws IOException on I/O error 
+     */
+    public DBProfile(String xml) throws IOException
     {
         XMLNode profile = new XMLParser().parseXML(new StringReader(xml));
-        
-        setDatabase(profile.getAttr(ATTR_DB));
+        setProfileName(profile.getAttr(ATTR_PROFILE_NAME));
         setDriver(profile.getAttr(ATTR_DRIVER));
         setUrl(profile.getAttr(ATTR_URL));
         setUsername(profile.getAttr(ATTR_USERNAME));
         setPassword(profile.getAttr(ATTR_PASSWORD));   
     }
-    
-    DBProfile(
-        String database, 
+
+    /**
+     * Creates a DBProfile
+     *
+     * @param  profileName Friendly name of the profile
+     * @param  driver      JDBC driver class
+     * @param  url         JDBC access url
+     * @param  username    Username
+     * @param  password    Password in clear text       
+     */
+    public DBProfile(
+        String  profileName, 
         String driver, 
         String url, 
         String username,
         String password)
     {
-        database_ = database;
+        profileName_  = profileName;
         driver_   = driver;
         url_      = url;
         username_ = username;
         password_ = password;
     }
     
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * Returns an XML representation of the data contained in this profile.
+     * 
+     * @return  XML string
+     */
+    public String toXML()
+    {
+        return toDOM().toString();
+    }
+
+    /**
+     * Returns a DOM representation of the data contained in this profile.
+     * 
+     * @return  DOM tree
+     */    
+    public XMLNode toDOM()
+    {
+        // Tail element
+        XMLNode profile = new XMLNode(ELEMENT_PROFILE);
+        profile.addAttr(ATTR_PROFILE_NAME, profileName_);
+        profile.addAttr(ATTR_DRIVER, driver_);
+        profile.addAttr(ATTR_URL, url_);
+        profile.addAttr(ATTR_USERNAME, username_);
+        profile.addAttr(ATTR_PASSWORD, password_);
+        
+        return profile;
+    }
+
+    //--------------------------------------------------------------------------
+    // Accessors/Mutators
+    //--------------------------------------------------------------------------
+        
     /**
      * @return
      */
-    public String getDatabase()
+    public String getProfileName()
     {
-        return database_;
+        return profileName_;
     }
 
     /**
@@ -90,9 +188,9 @@ class DBProfile
     /**
      * @param string
      */
-    public void setDatabase(String string)
+    public void setProfileName(String string)
     {
-        database_ = string;
+        profileName_ = string;
     }
 
     /**
@@ -127,26 +225,19 @@ class DBProfile
         username_ = string;
     }
     
+    //--------------------------------------------------------------------------
+    // Overrides java.lang.Object
+    //--------------------------------------------------------------------------
+
+    /**
+     * Returns the name of the profile for the string representation. This is
+     * what is used by default to render the text value of this profile when
+     * added to a JComboBox.
+     * 
+     * @param  Profile name
+     */ 
     public String toString()
     {
-        return getDatabase();
-    }
-    
-    public String toXML()
-    {
-        return toDOM().toString();
-    }
-    
-    public XMLNode toDOM()
-    {
-        // Tail element
-        XMLNode profile = new XMLNode(ELEMENT_PROFILE);
-        profile.addAttr(ATTR_DB, database_);
-        profile.addAttr(ATTR_DRIVER, driver_);
-        profile.addAttr(ATTR_URL, url_);
-        profile.addAttr(ATTR_USERNAME, username_);
-        profile.addAttr(ATTR_PASSWORD, password_);
-        
-        return profile;
+        return getProfileName();
     }
 }
