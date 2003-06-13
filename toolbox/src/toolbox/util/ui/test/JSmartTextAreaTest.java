cd2a67c75cc288a77165c9f11f9b27211d7df3a8
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-06-13 07:29:00 +0000

    Added test for savePrefs() and applyPrefs()

--- toolbox/src/toolbox/util/ui/test/JSmartTextAreaTest.java
+++ toolbox/src/toolbox/util/ui/test/JSmartTextAreaTest.java
@@ -2,16 +2,23 @@ package toolbox.util.ui.test;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.io.StringReader;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
-import org.apache.log4j.Logger;
-
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.apache.log4j.Logger;
+
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Serializer;
+
 import toolbox.util.SwingUtil;
+import toolbox.util.io.StringOutputStream;
 import toolbox.util.ui.JSmartTextArea;
 
 /**
@@ -57,5 +64,45 @@ public class JSmartTextAreaTest extends TestCase
         frame.setVisible(true);
         SwingUtil.centerWindow(frame);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    /**
+     * Tests savePrefs() and applyPrefs()
+     */
+    public void testSaveApplyPrefs() throws Exception
+    {
+        logger_.info("Running testSaveApplyPrefs...");
+        
+        JSmartTextArea before = new JSmartTextArea("", true, true);
+        before.setFont(SwingUtil.getPreferredMonoFont());
+        
+        //
+        // Serialize to XML
+        //
+        Element root = new Element("root");
+        before.savePrefs(root);
+        StringOutputStream sos = new StringOutputStream();
+        Serializer serializer = new Serializer(sos, "UTF-8");
+        serializer.setIndent(4);
+        serializer.write(new Document(root));
+        String xml = sos.toString();
+        logger_.info("\n" + xml);
+        
+        // 
+        // Hydrate from XML
+        //
+        Builder builder = new Builder();
+        Element prefs = builder.build(new StringReader(xml)).getRootElement();
+        JSmartTextArea after = new JSmartTextArea();
+        after.applyPrefs(prefs);
+        
+        //
+        // Compare the "after" properties to the "before" ones
+        //
+        assertEquals(before.isAntiAlias(), after.isAntiAlias());
+        assertEquals(before.isAutoScroll(), after.isAutoScroll());
+        assertEquals(before.getFont().getName(), after.getFont().getName());
+        assertEquals(before.getFont().getSize(), after.getFont().getSize());
+        assertEquals(before.getFont().getStyle(), after.getFont().getStyle());
     }
 }
