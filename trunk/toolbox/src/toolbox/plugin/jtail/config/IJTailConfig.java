a76848ddd73ff37e705d8094bbacaee6c9e78329
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-06-17 02:58:13 +0000

    Removed size, location, and directory (not necessary of a plugin)

--- trunk/toolbox/src/toolbox/plugin/jtail/config/IJTailConfig.java
+++ trunk/toolbox/src/toolbox/plugin/jtail/config/IJTailConfig.java
@@ -1,7 +1,5 @@
 package toolbox.jtail.config;
 
-import java.awt.Dimension;
-import java.awt.Point;
 
 /**
  * Interface used for the persistence of the JTail application's preferences.
@@ -16,54 +14,12 @@ public interface IJTailConfig
     public ITailPaneConfig getDefaultConfig();
 
     /**
-     * Returns the location of the window
-     * 
-     * @return  Location of the window
-     */
-    public Point getLocation();
-
-    /**
-     * Returns the size of the window
-     * 
-     * @return  Size of the window
-     */
-    public Dimension getSize();
-
-    /**
-     * Returns the last directory selecting in the file explorer pane
-     *
-     * @return  Last selected directory
-     */
-    public String getDirectory();
-
-    /**
-     * Sets the last directory selected in the file explorer pane
-     * 
-     * @param directory  Directory selected
-     */
-    public void setDirectory(String directory);
-
-    /**
      * Sets the default tail pane configuration
      * 
      * @param config  Default tail pane configuration
      */
     public void setDefaultConfig(ITailPaneConfig config);
 
-    /**
-     * Sets the location of the window
-     * 
-     * @param  location  Location of the window
-     */
-    public void setLocation(Point location);
-
-    /**
-     * Sets the size of the window
-     * 
-     * @param  size  Size of the window
-     */
-    public void setSize(Dimension size);
-    
     /**
      * Returns the tailPaneConfigs.
      * 
