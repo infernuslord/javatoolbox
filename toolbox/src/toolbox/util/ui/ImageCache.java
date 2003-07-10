7140a17cc57b3bba6bd7947dc4db1b8dc414a03d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-10 05:47:45 +0000

    Added toolbox, delete, and cross

--- toolbox/src/toolbox/util/ui/ImageCache.java
+++ toolbox/src/toolbox/util/ui/ImageCache.java
@@ -15,11 +15,14 @@ public class ImageCache
 {
     private static final String ROOT = "/toolbox/util/ui/images/";
     
-    public static final String IMAGE_COPY   = ROOT + "Copy.gif";
-    public static final String IMAGE_PASTE  = ROOT + "Paste.gif";
-    public static final String IMAGE_SAVE   = ROOT + "Save.gif";
-    public static final String IMAGE_SAVEAS = ROOT + "SaveAs.gif";
-    public static final String IMAGE_FIND   = ROOT + "Search.gif";
+    public static final String IMAGE_TOOLBOX = ROOT + "Toolbox.gif";
+    public static final String IMAGE_COPY    = ROOT + "Copy.gif";
+    public static final String IMAGE_PASTE   = ROOT + "Paste.gif";
+    public static final String IMAGE_SAVE    = ROOT + "Save.gif";
+    public static final String IMAGE_SAVEAS  = ROOT + "SaveAs.gif";
+    public static final String IMAGE_FIND    = ROOT + "Search.gif";
+    public static final String IMAGE_DELETE  = ROOT + "Delete.gif";
+    public static final String IMAGE_CROSS   = ROOT + "Cross.gif";
     
     public static final String IMAGE_SORT_ASCENDING = ROOT+"SortAscending.gif";
     public static final String IMAGE_SORT_DESCENDING= ROOT+"SortDescending.gif";
@@ -27,6 +30,7 @@ public class ImageCache
     public static final String IMAGE_TREE_OPEN   = ROOT + "TreeOpen.gif";
     public static final String IMAGE_TREE_CLOSED = ROOT + "TreeClosed.gif";
     public static final String IMAGE_HARD_DRIVE  = ROOT + "HardDrive.gif";
+    
     
     /**
      * Hashmap for cached icons
