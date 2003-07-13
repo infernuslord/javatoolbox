ce8b2dc310a99b18ffee37c3af769664534bf5ab
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-13 05:14:48 +0000

    Implemented testGetInteger()

--- toolbox/src/toolbox/util/test/XOMUtilTest.java
+++ toolbox/src/toolbox/util/test/XOMUtilTest.java
@@ -1,8 +1,10 @@
 package toolbox.util.test;
 
 import junit.framework.TestCase;
+import junit.textui.TestRunner;
 
 import nu.xom.Element;
+import toolbox.util.XOMUtil;
 
 /**
  * 
@@ -11,21 +13,32 @@ public class XOMUtilTest extends TestCase
 {
 
     /**
-     * Constructor for XOMUtilTest.
-     * @param name
+     * Entrypoint
+     *
+     * @param args None
      */
-    public XOMUtilTest(String name)
-    {
-        super(name);
-    }
-
     public static void main(String[] args)
     {
-        junit.textui.TestRunner.run(XOMUtilTest.class);
+        TestRunner.run(XOMUtilTest.class);
     }
 
+    /**
+     * Tests getInteger() for positive, negative, and null cases
+     */
     public void testGetInteger()
     {
+        // Test get int from node
+        Element node = new Element("IntNode");
+        node.appendChild("123");
+        int i = XOMUtil.getInteger(node, -1);
+        assertEquals(123, i);
+
+        // Test node doesn't have an int
+        node.removeChildren();
+        assertEquals(-1, XOMUtil.getInteger(node, -1));
+
+        // Test node is null
+        assertEquals(-1, XOMUtil.getInteger(null, -1));
     }
 
     public void testGetString()
