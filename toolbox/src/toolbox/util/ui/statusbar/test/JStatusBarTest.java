98c3224a492d0380c01766cf3dbef21b3e57f6eb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-19 07:54:54 +0000

    Converted to JUnit test

--- toolbox/src/toolbox/util/ui/statusbar/test/JStatusBarTest.java
+++ toolbox/src/toolbox/util/ui/statusbar/test/JStatusBarTest.java
@@ -2,58 +2,83 @@ package toolbox.util.ui.statusbar.test;
 
 import java.awt.BorderLayout;
 
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JProgressBar;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
 
 import toolbox.util.RandomUtil;
 import toolbox.util.SwingUtil;
 import toolbox.util.ui.statusbar.JStatusBar;
 
-public class JStatusBarTest extends JPanel
+/**
+ * Unit test for JStatusBar
+ */
+public class JStatusBarTest extends TestCase
 {
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+
     /**
-     * TODO: Convert to JUnit test
+     * Entrypoint 
+     * 
+     * @param args None recognized
+     * @throws Exception on error
      */
-    
     public static void main(String[] args) throws Exception
     {
         SwingUtil.setPreferredLAF();
+        TestRunner.run(JStatusBarTest.class);   
+    }
 
+    //--------------------------------------------------------------------------
+    // Unit Tests 
+    //--------------------------------------------------------------------------
+
+    /**
+     * Tests JStatusBar
+     * 
+     * @throws Exception on error
+     */    
+    public void testJStatusBar() throws Exception
+    {
         JStatusBar status = new JStatusBar();
 
         // Relative, twice normal
-        status.addStatusComponent(new JLabel(" Relative x2 "), JStatusBar.RELATIVE, 2);
+        status.addStatusComponent(
+            new JLabel(" Relative x2 "), JStatusBar.RELATIVE, 2);
         
         // Relative normal
-        status.addStatusComponent(new JLabel(" Relative x1 "), JStatusBar.RELATIVE, 1);
+        status.addStatusComponent(
+            new JLabel(" Relative x1 "), JStatusBar.RELATIVE, 1);
         
         // Fixed, based on preferred size
-        status.addStatusComponent(new JLabel(" Preferred "));
+        status.addStatusComponent(
+            new JLabel(" Preferred "));
         
         // Fixed, based on specified size
-        status.addStatusComponent(new JLabel(" Fixed "), JStatusBar.FIXED, 40);
+        status.addStatusComponent(
+            new JLabel(" Fixed "), JStatusBar.FIXED, 40);
         
         // Relative normal
-        status.addStatusComponent(new JLabel(" Relative x1 "), JStatusBar.RELATIVE);
+        status.addStatusComponent(
+            new JLabel(" Relative x1 "), JStatusBar.RELATIVE);
         
         // Progress bar is based on preferred size
         JProgressBar progressBar = new JProgressBar(1, 100);
         progressBar.setValue(RandomUtil.nextInt(1, 100));
         status.addStatusComponent(progressBar);
 
-        JFrame frame = new JFrame("JStatusBar Test");
+        JDialog frame = new JDialog(new JFrame(), "JStatusBar Test", true);
+        frame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(BorderLayout.SOUTH, status);
         frame.setSize(700, 150);
         SwingUtil.centerWindow(frame);
-        frame.show();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
-
-/*
-Originally created by Claude Duguay
-Copyright (c) 2000
-*/
