b7535044007584ffcd80951ad06cbf38304a2bc3
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-20 07:51:14 +0000

    Updated javadocs

--- toolbox/src/toolbox/util/net/IConnectionHandler.java
+++ toolbox/src/toolbox/util/net/IConnectionHandler.java
@@ -1,15 +1,23 @@
 package toolbox.util.net;
 
 /**
- * Generic interface for a connection handler
+ * A connection handler is created by a SocketServer to service an incoming
+ * socket client after a connection has been established.<br>
+ * The connection handler type is specified in the SocketServerConfig<br>
+ * A connection handler is responsible for the conversation that takes place
+ * between itself and a socket client.<br>
+ * A connection handler is tasked with terminating the connection when the
+ * conversation reaches a stopping point.<br>
+ * A connection handler can choose to be full duplex or half duplex based on 
+ * the needs of the protocol.<br>
  */
 public interface IConnectionHandler
 {
     /**
-     * Handles the connection
+     * Handles a new connection.
      * 
-     * @param    conn    IConnection
-     * @return   An arbitrary object as specified by the implementor
+     * @param conn Connection to service
+     * @return An arbitrary object as specified by the implementor
      */
     public Object handle(IConnection conn);
 }
