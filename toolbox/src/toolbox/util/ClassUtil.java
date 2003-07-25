290323316e7d5c4d406671e6ac3abc0923ea86c2
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-25 08:26:59 +0000

    Added getClassLocation()

--- toolbox/src/toolbox/util/ClassUtil.java
+++ toolbox/src/toolbox/util/ClassUtil.java
@@ -2,6 +2,10 @@ package toolbox.util;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.CodeSource;
+import java.security.ProtectionDomain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -361,5 +365,77 @@ public final class ClassUtil
         }
             
         return ca;
-    }    
+    }
+    
+    /**
+     * Given a Class object, attempts to find its .class location [returns null
+     * if no such definition can be found]. Use for testing/debugging only.
+     *
+     * @param cls Class to find
+     * @return URL that points to the class definition or null if not found.
+     */
+    public static URL getClassLocation(Class cls)
+    {
+        if (cls == null)
+            throw new IllegalArgumentException("null input: cls");
+
+        URL result = null;
+        String clsAsResource = cls.getName().replace('.', '/').concat(".class");
+        ProtectionDomain pd = cls.getProtectionDomain();
+        
+        // java.lang.Class contract does not specify if 'pd' can ever be null;
+        // it is not the case for Sun's implementations, but guard against null
+        // just in case:
+        
+        if (pd != null)
+        {
+            CodeSource cs = pd.getCodeSource();
+            
+            // 'cs' can be null depending on the classloader behavior:
+            if (cs != null)
+                result = cs.getLocation();
+
+            if (result != null)
+            {
+                // Convert a code source location into a full class file 
+                // location for some common cases:
+                
+                if ("file".equals(result.getProtocol()))
+                {
+                    try
+                    {
+                        if (result.toExternalForm().endsWith(".jar") || 
+                            result.toExternalForm().endsWith(".zip"))
+                        {    
+                            result = new URL(
+                                "jar:".concat(result.toExternalForm()).
+                                    concat("!/").concat(clsAsResource));
+                        }
+                        else if (new File(result.getFile()).isDirectory())
+                        {
+                            result = new URL(result, clsAsResource);
+                        }
+                    }
+                    catch (MalformedURLException ignore)
+                    {
+                    }
+                }
+            }
+        }
+
+        if (result == null)
+        {
+            // Try to find 'cls' definition as a resource; this is not
+            // documented to be legal, but Sun's implementations seem to allow 
+            // this:
+            
+            ClassLoader clsLoader = cls.getClassLoader();
+
+            result = clsLoader != null 
+                        ? clsLoader.getResource(clsAsResource)
+                        : ClassLoader.getSystemResource(clsAsResource);
+        }
+
+        return result;
+    }
 }
