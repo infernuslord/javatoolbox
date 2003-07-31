888df66a824fdddbb1855faaf097b36bb647f142
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-07-31 02:31:39 +0000

    Added refresh image

--- trunk/toolbox/src/toolbox/util/ui/ImageCache.java
+++ trunk/toolbox/src/toolbox/util/ui/ImageCache.java
@@ -24,6 +24,7 @@ public class ImageCache
     public static final String IMAGE_DELETE  = ROOT + "Delete.gif";
     public static final String IMAGE_CROSS   = ROOT + "Cross.gif";
     public static final String IMAGE_TRIANGLE= ROOT + "Triangle.gif";
+    public static final String IMAGE_REFRESH = ROOT + "Refresh.gif";
     
     public static final String IMAGE_SORT_ASCENDING = ROOT+"SortAscending.gif";
     public static final String IMAGE_SORT_DESCENDING= ROOT+"SortDescending.gif";
