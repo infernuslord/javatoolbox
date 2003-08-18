e5941ee2a62e4ea1c7487b2af9511eccce3b1cac
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-08-18 21:36:38 +0000

    Now implements tailAggregated() so that the tab's label and tooltip are updated with the new aggregate.

--- trunk/toolbox/src/toolbox/plugin/jtail/JTailTabbedPane.java
+++ trunk/toolbox/src/toolbox/plugin/jtail/JTailTabbedPane.java
@@ -2,24 +2,27 @@ package toolbox.jtail;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+
 import javax.swing.JTabbedPane;
 
 import org.apache.log4j.Logger;
 
+import toolbox.util.ExceptionUtil;
+
 /**
- * enclosing_type
+ * JTailTabbedPane
  */
-public class JTailTabbedPane extends JTabbedPane
+public class JTailTabbedPane extends JTabbedPane implements TailPane.ITailPaneListener
 {
     private static final Logger logger_ =
         Logger.getLogger(JTailTabbedPane.class);
     
     /**
-     * Constructor for JTailTabbedPane.
+     * Creates a JTailTabbedPane
      */
     public JTailTabbedPane()
     {
-        super();
         init();
     }
 
@@ -37,12 +40,38 @@ public class JTailTabbedPane extends JTabbedPane
     /**
      * Tail pane listener
      */
-    public class TailPaneListener implements TailPane.ITailPaneListener
-    {
+//    public class TailPaneListener implements TailPane.ITailPaneListener
+//    {
+        /**
+         * @see toolbox.jtail.TailPane.ITailPaneListener#newDataAvailable(
+         *      toolbox.jtail.TailPane)
+         */
         public void newDataAvailable(TailPane tailPane)
         {
             int index = indexOfComponent(tailPane);
             setTitleAt(index, "* "+ getTitleAt(index));
         }
-    }
+        
+        /**
+         * @see toolbox.jtail.TailPane.ITailPaneListener#tailAggregated(
+         *      toolbox.jtail.TailPane)
+         */
+        public void tailAggregated(TailPane tailPane)
+        {
+            int index = indexOfComponent(tailPane);
+            
+            try
+            {
+                setTitleAt(
+                    index, JTail.makeTabLabel(tailPane.getConfiguration()));
+ 
+                setToolTipTextAt(
+                    index, JTail.makeTabToolTip(tailPane.getConfiguration()));
+            }
+            catch (IOException e)
+            {
+                ExceptionUtil.handleUI(e, logger_);
+            }
+        }
+//    }
 }
