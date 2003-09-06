1c3b6c57c3f001fb46c382fb2717a9341e823fbe
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-09-06 09:11:55 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/table/JSmartTable.java
@@ -0,0 +1,156 @@
+package toolbox.util.ui.table;
+
+import java.awt.Graphics;
+import java.util.Vector;
+
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+
+import toolbox.util.SwingUtil;
+
+/**
+ * JSmartTable adds the following behavior.
+ * <p>
+ * <ul>
+ *   <li>Support for antialised text
+ * </ul>
+ * 
+ * @see JSmartTableHeader
+ * @see JSmartTableModel
+ * @see SmartTableCellRender
+ */
+public class JSmartTable extends JTable
+{
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a JSmartTable
+     */
+    public JSmartTable()
+    {
+        setTableHeader(new JSmartTableHeader());        
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param numRows Number of rows
+     * @param numColumns Number of columns
+     */
+    public JSmartTable(int numRows, int numColumns)
+    {
+        super(numRows, numColumns);
+        setTableHeader(new JSmartTableHeader());
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param dm Table model
+     */
+    public JSmartTable(TableModel dm)
+    {
+        super(dm);
+        setTableHeader(new JSmartTableHeader());
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param rowData Row data
+     * @param columnNames Column names
+     */
+    public JSmartTable(Object[][] rowData, Object[] columnNames)
+    {
+        super(rowData, columnNames);
+        setTableHeader(new JSmartTableHeader());
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param rowData Row data
+     * @param columnNames Column names
+     */
+    public JSmartTable(Vector rowData, Vector columnNames)
+    {
+        super(rowData, columnNames);
+        setTableHeader(new JSmartTableHeader());
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param dm Table model
+     * @param cm Column model
+     */
+    public JSmartTable(TableModel dm, TableColumnModel cm)
+    {
+        super(dm, cm);
+        setTableHeader(new JSmartTableHeader(cm));
+    }
+
+    /**
+     * Creates a JSmartTable
+     * 
+     * @param dm Table model
+     * @param cm Column model
+     * @param sm Selection model
+     */
+    public JSmartTable(
+        TableModel dm,
+        TableColumnModel cm,
+        ListSelectionModel sm)
+    {
+        super(dm, cm, sm);
+        setTableHeader(new JSmartTableHeader(cm));
+    }
+
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Initializes the table
+     */
+    protected void init()
+    {
+
+    }
+    
+    //--------------------------------------------------------------------------
+    // AntiAliased Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.util.ui.AntiAliased#isAntiAlias()
+     */
+    public boolean isAntiAliased()
+    {
+        return SwingUtil.isAntiAliased();
+    }
+
+    /**
+     * @see toolbox.util.ui.AntiAliased#setAntiAlias(boolean)
+     */
+    public void setAntiAliased(boolean b)
+    {
+    }
+
+    //--------------------------------------------------------------------------
+    // Overrides JComponent
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     */
+    public void paintComponent(Graphics gc)
+    {
+        SwingUtil.makeAntiAliased(gc, isAntiAliased());
+        super.paintComponent(gc);
+    }
+}
