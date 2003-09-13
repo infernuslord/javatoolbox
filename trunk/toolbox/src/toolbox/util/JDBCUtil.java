801572d5898bf5bf9b0853ea2972dea939bbf4a1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-09-13 04:27:23 +0000

    Added javadoc header with example Added executeQueryArray() Added toArray() Added close(Statement)

--- trunk/toolbox/src/toolbox/util/JDBCUtil.java
+++ trunk/toolbox/src/toolbox/util/JDBCUtil.java
@@ -17,7 +17,42 @@ import java.util.Properties;
 import org.apache.log4j.Logger;
 
 /**
- * JDBC Utility class
+ * JDBC Utility class that makes it easy to execute SQL statements and
+ * see the results as formatted text in a tabular layout.
+ * <p>
+ * Typical usage:
+ * <p>
+ * <pre>
+ * 
+ * JdbcUtilities.init("driver", "url", "username", "password");
+ * 
+ * String results = 
+ *     JdbcUtilities.executeQuery(
+ *         "select * from users where age > 30");
+ * 
+ * System.out.println(results);
+ * 
+ * int numRows = 
+ *     JdbcUtilities.executeUpdate(
+ *         "delete from users where age < 30");
+ * 
+ * System.out.println(numrows + " rows deleted.");
+ * 
+ * </pre>
+ * <p>
+ * Sample Output 
+ * <p>
+ * <pre>
+ * 
+ * USERNAME  AGE  TITLE
+ * ---------------------------
+ * jsmith    23   Consultant
+ * abrown    34   Integrator
+ * hsimpson  45   Misc.
+ *    
+ * 4 rows
+ * 
+ * </pre>
  */
 public final class JDBCUtil
 {
@@ -27,7 +62,9 @@ public final class JDBCUtil
     // Clover private constructor workaround
     static { new JDBCUtil(); }
 
-    /** JDBC connection properties */
+    /** 
+     * JDBC connection properties 
+     */
     private static Properties connProps_;
         
     //--------------------------------------------------------------------------
@@ -44,13 +81,200 @@ public final class JDBCUtil
     //--------------------------------------------------------------------------
     //  Public
     //--------------------------------------------------------------------------
+
+    /**
+     * Initialzies the JDBC properties. Must be called before any of the other
+     * methods are use
+     * 
+     * @param driver JDBC driver to use
+     * @param url URL to database resource
+     * @param user Username used for authentication
+     * @param password Password used for authentication
+     * @throws SQLException on SQL error
+     * @throws ClassNotFoundException if the JDBC driver is not found
+     */    
+    public static void init(
+        String driver, 
+        String url, 
+        String user, 
+        String password) throws ClassNotFoundException, SQLException
+    {
+        // Will force DriverManager.register() to get called
+        Class.forName(driver);
+        connProps_ = new Properties();
+        connProps_.put("user"     , user);
+        connProps_.put("password" , password);
+        connProps_.put("url"      , url);
+        
+        // Get meta data 
+        Connection conn = getConnection();
+        DatabaseMetaData meta = conn.getMetaData();
+        
+        logger_.debug("DB Connect: " + 
+            meta.getDatabaseProductName() + meta.getDatabaseProductVersion());
+            
+        releaseConnection(conn);
+    }
+
+
+    /**
+     * Returns a connection to the database
+     * 
+     * @return Connection ready for use
+     * @throws SQLException on SQL error
+     */
+    public static Connection getConnection() throws SQLException
+    {
+        if (connProps_ == null)
+            throw new IllegalStateException(
+                "Must call init() first to set the JDBC configuration.");
+        
+        Connection conn =        
+            DriverManager.getConnection(
+                connProps_.getProperty("url"), connProps_);
+                        
+        return conn;
+    }
+
     
+    /**
+     * Executes a SQL INSERT, UPDATE, or DELETE statement
+     * 
+     * @param sql Insert/update/delete sql statement to execute
+     * @return Number of rows affected by the execution of the sql statement
+     * @throws SQLException on any errors
+     */   
+    public static int executeUpdate(String sql) throws SQLException
+    {
+        Connection conn = null;
+        int rows = -1;
+        
+        try 
+        {
+            conn = getConnection();    
+            Statement stmt = conn.createStatement();
+            rows = stmt.executeUpdate(sql);
+        } 
+        finally 
+        {
+            releaseConnection(conn); 
+        }
+        
+        return rows;
+    }   
+    
+    
+    /**
+     * Executes a SQL query statement and returns the result in a formatted
+     * string.
+     * 
+     * @param sql Select statement to execute
+     * @return Formatted contents of the result set
+     * @throws SQLException on any SQL error
+     */
+    public static String executeQuery(String sql) throws SQLException
+    {
+        String formattedResults = null;
+        Connection conn = null;
+        
+        try
+        {
+            conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet results = stmt.executeQuery();
+            formattedResults = format(results);
+        }
+        finally
+        {
+            releaseConnection(conn); 
+        }
+        
+        return formattedResults;
+    } 
+
+    /**
+     * Executes a SQL query statement and returns the result in a formatted
+     * string.
+     * 
+     * @param sql Select statement to execute
+     * @return Formatted contents of the result set
+     * @throws SQLException on any SQL error
+     */
+    public static Object[][] executeQueryArray(String sql) throws SQLException
+    {
+        Object[][] data = null;
+        Connection conn = null;
+        PreparedStatement stmt = null;
+
+        try
+        {
+            conn = getConnection();
+            stmt = conn.prepareStatement(sql);
+            ResultSet results = stmt.executeQuery();
+            data = toArray(results);
+        }
+        finally
+        {
+            close(stmt);
+            releaseConnection(conn);
+        }
+
+        return data;
+    }
+
+
+    /**
+     * Formats a result set in a table like manner.
+     * 
+     * @param rs ResultSet to format
+     * @return Contents of result set formatted in a table like format
+     * @throws SQLException on sql error
+     */
+    public static Object[][] toArray(ResultSet rs) throws SQLException
+    {
+        ResultSetMetaData meta = rs.getMetaData();
+    
+        int numCols = meta.getColumnCount();
+        List rows = new ArrayList();
+    
+        while (rs.next())
+        {
+            Object[] row = new Object[numCols];
+    
+            for (int i = 0; i < numCols; i++)
+                row[i] = rs.getObject(i + 1);
+    
+            rows.add(row);
+        }
+
+        Object[][] table = new Object[0][0];
+        
+        if (!rows.isEmpty())
+        {    
+            table = new Object[numCols][rows.size()];
+        
+            for (int i = 0, n = rows.size(); i < n; i++)
+            {
+                Object[] row = (Object[]) rows.get(i);
+        
+                for (int j = 0; j < row.length; j++)
+                    table[j][i] = row[j];
+            }
+        
+            rows.clear();
+            rows = null;
+        }
+    
+        return table;
+    }
+
+
     /**
      * Formats a results set in a table like manner
      * 
-     * @param   rs  ResultSet to format
-     * @return  Contents of result set formatted in a table like format
-     * @throws  SQLException on sql error
+     * @param rs ResultSet to format
+     * @return Contents of result set formatted in a table like format
+     * @throws SQLException on sql error
      */
     public static String format(ResultSet rs) throws SQLException 
     {
@@ -142,141 +366,11 @@ public final class JDBCUtil
         }
 
         sb.append("\n");
-        sb.append(numRows + " rows/" + time + "ms\n");
+        sb.append(numRows + " rows\n");
         
         return sb.toString();
     }
- 
-    /**
-     * Returns a connection to the database
-     * 
-     * @return  Connection ready for use
-     * @throws  SQLException on SQL error
-     */
-    public static Connection getConnection() throws SQLException
-    {
-        if (connProps_ == null)
-            throw new IllegalStateException(
-                "Must call init() first to set the JDBC configuration.");
-        
-        Connection conn =        
-            DriverManager.getConnection(
-                connProps_.getProperty("url"), connProps_);
-                        
-        return conn;
-    }
 
-    /**
-     * Releases a connection. Supresses any problems 
-     * 
-     * @param  connection  Connection to release
-     */
-    public static void releaseConnection(Connection connection) 
-    {
-        if (connection != null)
-        {
-            try 
-            { 
-                if (!connection.isClosed())
-                {
-                    connection.close();
-                    connection = null;
-                }
-            } 
-            catch(SQLException e) 
-            {
-                ; // Ignore
-            }
-        }
-    }
- 
-    /**
-     * Executes a SQL INSERT, UPDATE, or DELETE statement
-     * 
-     * @param   sql     Insert/update/delete sql statement to execute
-     * @return  Number of rows affected by the execution of the sql statement
-     * @throws  SQLException on any errors
-     */   
-    public static int executeUpdate(String sql) throws SQLException
-    {
-        Connection conn = null;
-        int rows = -1;
-        
-        try 
-        {
-            conn = getConnection();    
-            Statement stmt = conn.createStatement();
-            rows = stmt.executeUpdate(sql);
-        } 
-        finally 
-        {
-            releaseConnection(conn); 
-        }
-        
-        return rows;
-    }   
-    
-    /**
-     * Executes a SQL query statement and returns the result in a formatted
-     * string.
-     * 
-     * @param   sql  Select statement to execute
-     * @return  Formatted contents of the result set
-     * @throws  SQLException on any SQL error
-     */
-    public static String executeQuery(String sql) throws SQLException
-    {
-        String formattedResults = null;
-        Connection conn = null;
-        
-        try
-        {
-            conn = getConnection();
-            PreparedStatement stmt = conn.prepareStatement(sql);
-            ResultSet results = stmt.executeQuery();
-            formattedResults = format(results);
-        }
-        finally
-        {
-            releaseConnection(conn); 
-        }
-        
-        return formattedResults;
-    } 
-
-    /**
-     * Initialzies the JDBC properties. Must be called before any of the other
-     * methods are use
-     * 
-     * @param  driver   JDBC driver to use
-     * @param  url      URL to database resource
-     * @param  user     Username used for authentication
-     * @param  password Password used for authentication
-     * @throws SQLException on SQL error
-     * @throws ClassNotFoundException if the JDBC driver is not found
-     */    
-    public static void init(
-        String driver, 
-        String url, 
-        String user, 
-        String password) throws ClassNotFoundException, SQLException
-    {
-        // Will force DriverManager.register() to get called
-        Class.forName(driver);
-        connProps_ = new Properties();
-        connProps_.put("user"     , user);
-        connProps_.put("password" , password);
-        connProps_.put("url"      , url);
-        
-        // Get meta data 
-        Connection conn = getConnection();
-        DatabaseMetaData meta = conn.getMetaData();
-        
-        logger_.info("DB Connect: " + 
-            meta.getDatabaseProductName() + meta.getDatabaseProductVersion());
-            
-        releaseConnection(conn);
-    }
     
     /**
      * Returns the size of a given result set. The position of the cursor is
@@ -288,9 +382,9 @@ public final class JDBCUtil
      *       interface.
      * </pre>
      *
-     * @param   rs    Result set
-     * @return  Size of the result set
-     * @throws  SQLException if an SQL error occurs
+     * @param rs Result set
+     * @return Size of the result set
+     * @throws SQLException if an SQL error occurs
      */
     public static int getSize(ResultSet rs) throws SQLException 
     {
@@ -309,11 +403,12 @@ public final class JDBCUtil
             
         return last;
     }
+
     
     /**
      * Drops table w/o any complaints
      * 
-     * @param  table  Table name
+     * @param table Table name
      */
     public static void dropTable(String table)
     {
@@ -329,6 +424,7 @@ public final class JDBCUtil
             ; // Quiet please
         }
     }
+
     
     /**
      * Closes a ResultSet w/o any complaining
@@ -347,6 +443,54 @@ public final class JDBCUtil
         catch(SQLException e)
         {
             ; // Quiet please
+        }
+    }
+
+
+    /**
+     * Convenience method to close a Statement. Handles nulls and cases where
+     * an exception is thrown by logging a warning. 
+     * 
+     * @param stmt Statement to close
+     */
+    public static void close(Statement stmt)
+    {
+        if (stmt == null)
+            return;
+
+        try
+        {
+            stmt.close();
+            stmt = null;
+        }
+        catch (SQLException e)
+        {
+            ; // Ignore
+        }
+    }
+
+    
+    /**
+     * Releases a connection. Supresses any problems 
+     * 
+     * @param connection Connection to release
+     */
+    public static void releaseConnection(Connection connection) 
+    {
+        if (connection != null)
+        {
+            try 
+            { 
+                if (!connection.isClosed())
+                {
+                    connection.close();
+                    connection = null;
+                }
+            } 
+            catch(SQLException e) 
+            {
+                ; // Ignore
+            }
         }
     }
 }
