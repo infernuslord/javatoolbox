eb780304f5e3f3b5285d5d3a54259a8c7e0adfb7
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-09-21 08:24:37 +0000

    Alternative implementation backed by hamsam

---
+++ toolbox/src/toolbox/log4j/im/AIMMessenger.java
@@ -0,0 +1,341 @@
+package toolbox.log4j.im;
+
+import hamsam.api.Buddy;
+import hamsam.api.IMAdapter;
+import hamsam.api.Message;
+import hamsam.api.TextComponent;
+import hamsam.exception.IllegalStateException;
+import hamsam.net.ProxyInfo;
+import hamsam.protocol.Protocol;
+import hamsam.protocol.ProtocolManager;
+
+import java.util.Properties;
+
+import org.apache.log4j.helpers.LogLog;
+
+import toolbox.util.ArrayUtil;
+import toolbox.util.PropertiesUtil;
+import toolbox.util.ThreadUtil;
+import toolbox.util.concurrent.BlockingQueue;
+import toolbox.util.invoker.Invoker;
+import toolbox.util.invoker.QueuedInvoker;
+
+/**
+ * Yahoo Instant Messenger client that supports login, send message, and logout.
+ */
+public class AIMMessenger implements InstantMessenger
+{
+     // NOTE: Cannot use Log4J logging since this is included in the 
+     //       implementation of a Log4J appender.
+    
+    /** 
+     * Return code for a successful connection 
+     */
+    public static final String CONNECT_SUCCEEDED = "Connect succeeded!";
+    
+    /** 
+     * Return code for a failed connection 
+     */
+    public static final String CONNECT_FAILED = "Connect failed!";
+    
+    /** 
+     * Available instant messaging protocols 
+     */
+    private Protocol[] protocols;
+    
+    /** 
+     * Yahoo instant messaging protocol 
+     */
+    private Protocol aim_;
+    
+    /** 
+     * Listener for client and server side generated Yahoo events 
+     */
+    private AIMListener listener_;
+    
+    /** 
+     * Flag that tracks the connection state 
+     */
+    private boolean connected_;
+    
+    /** 
+     * Invoker used to handle the sending of messages 
+     */
+    private Invoker invoker_;
+    
+    /**
+     * Throttle delay for consecutive messages so the messenger server does 
+     * not get overwhelmed.
+     */
+    private int throttle_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a YahooMessenger
+     */
+    public AIMMessenger()
+    {
+    }
+
+    //--------------------------------------------------------------------------
+    // InstantMessenger Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#initialize(java.util.Properties)
+     */
+    public void initialize(Properties props)
+    {
+        invoker_ = new QueuedInvoker();
+        protocols = ProtocolManager.getAvailableProtocols();
+        System.out.println(ArrayUtil.toString(protocols));
+        aim_ = protocols[2];
+        aim_.setListener(listener_ = new AIMListener());
+        
+        throttle_ = PropertiesUtil.getInteger(
+            props, PROP_THROTTLE, InstantMessengerAppender.DEFAULT_THROTTLE);
+    }
+
+    /**
+     * Synchronized method since whole send/recv is async. Waiters in the
+     * queue will return immediately because the connected_ flag gets checked
+     * before anything happens. 
+     */
+    public synchronized void login(String username, String password) 
+        throws InstantMessengerException
+    {
+        if (connected_)
+            return;
+            
+        ProxyInfo info = new ProxyInfo();
+        
+        try
+        {
+            aim_.connect(username, password, info);
+            String returnCode = listener_.waitForConnect();
+            
+            if (returnCode.equals(CONNECT_SUCCEEDED))
+            {
+                connected_ = true;
+            }
+            else if (returnCode.equals(CONNECT_FAILED))
+            {
+                throw new InstantMessengerException(
+                    "Authentication failed for username '" + username + "'");
+            }
+            else
+            {
+                throw new IllegalArgumentException(
+                    "Return code '" + returnCode + "' is invalid.");
+            }
+        }
+        catch (Exception e)
+        {
+            connected_ = false;
+            throw new InstantMessengerException(e);    
+        }
+    }
+
+    /**
+     * Logs out from yahoo. 
+     */
+    public void logout() throws InstantMessengerException
+    {
+        try
+        {
+            QueuedInvoker qi = (QueuedInvoker) invoker_;
+            
+            while (!qi.isEmpty())
+            {
+                LogLog.debug("Messages waiting to be sent before logout: " + 
+                    qi.getSize());
+                    
+                ThreadUtil.sleep(throttle_);
+            }        
+            
+            aim_.disconnect();
+            LogLog.debug("Waiting for disconnect ack...");
+            listener_.waitForDisconnect();
+            connected_ = false;
+        }
+        catch (Exception e)
+        {
+            throw new InstantMessengerException(e);
+        }
+    }
+    
+    /**
+     * Sends message to the recipient using a queue invoker strategy.
+     */
+    public void send(String recipient, String message)
+    {
+        LogLog.debug("Appending: " + message);
+        
+        final Buddy buddy = new Buddy(aim_, recipient);
+        final Message msg = new Message();
+        msg.addComponent(new TextComponent(message));
+        
+        try
+        {
+            invoker_.invoke(new Runnable()
+            {
+                public void run()
+                {
+                    try
+                    {
+                        aim_.sendInstantMessage(buddy, msg);
+                    }
+                    catch (IllegalStateException e)
+                    {
+                        LogLog.error("send", e);
+                    }
+                    finally
+                    {
+                        ThreadUtil.sleep(throttle_);
+                    }
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            LogLog.error("send", e);
+        }
+    }
+
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#shutdown()
+     */
+    public void shutdown() throws InstantMessengerException 
+    {
+        try
+        {
+            invoker_.shutdown();
+        }
+        catch (Exception e)
+        {
+            throw new InstantMessengerException(e);
+        }
+    }
+    
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#isConnected()
+     */
+    public boolean isConnected()
+    {
+        return connected_;
+    }
+    
+    //--------------------------------------------------------------------------
+    // Inner Classes
+    //--------------------------------------------------------------------------
+    
+    /** 
+     * Listener for client and server side generated Yahoo events 
+     */
+    class AIMListener extends IMAdapter
+    {
+        /** 
+         * Login success and failures both go in this queue 
+         */
+        BlockingQueue connected_;
+        
+        /** 
+         * Disconnect notification goes into this queue 
+         */
+        BlockingQueue disconnected_;
+        
+        //----------------------------------------------------------------------
+        // Constructors
+        //----------------------------------------------------------------------
+
+        /** 
+         * Creates a YahooListener
+         */
+        public AIMListener()
+        {
+            connected_    = new BlockingQueue();
+            disconnected_ = new BlockingQueue();
+        }
+        
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+
+        /**
+         * Waits for a connect (failure or success)
+         * 
+         * @return CONNECT_SUCCEEDED or CONNECT_FAILED
+         * @throws InterruptedException if interrupted while pulling from the 
+         *         <code>connected_</code> queue. 
+         */                
+        public String waitForConnect() throws InterruptedException
+        {
+            return (String) connected_.pull();
+        }
+
+        /**
+         * Waits for a successful disconnect
+         * 
+         * @return Protocol that was disconnected.
+         * @throws InterruptedExceptin if interrupted while pulling from the 
+         *         <code>disconnected_</code> queue.
+         */        
+        public Protocol waitForDisconnect() throws InterruptedException
+        {
+            return (Protocol) disconnected_.pull();
+        }
+
+        //----------------------------------------------------------------------
+        // hamsam.api.IMListener Interface
+        //----------------------------------------------------------------------
+
+        /**
+         * @see hamsam.api.IMListener#connected(hamsam.protocol.Protocol)
+         */
+        public void connected(Protocol protocol)
+        {
+            LogLog.debug("Connected to Yahoo!");
+            connected_.push(CONNECT_SUCCEEDED);
+        }
+        
+        /**
+         * @see hamsam.api.IMListener#connectFailed(
+         *      hamsam.protocol.Protocol, java.lang.String)
+         */
+        public void connectFailed(Protocol protocol, String reasonMessage)
+        {
+            
+            LogLog.debug("Connect to Yahoo failed: " + reasonMessage);
+            connected_.push(CONNECT_FAILED);
+        }
+        
+        /**
+         * @see hamsam.api.IMListener#connecting(hamsam.protocol.Protocol)
+         */
+        public void connecting(Protocol protocol)
+        {
+            LogLog.debug("Connecting to Yahoo...");
+        }
+
+        /**
+         * @see hamsam.api.IMListener#disconnected(hamsam.protocol.Protocol)
+         */
+        public void disconnected(Protocol protocol)
+        {
+            disconnected_.push(protocol);
+            LogLog.debug("Disconnected from Yahoo");
+        }
+        
+        /**
+         * @see hamsam.api.IMListener#protocolMessageReceived(
+         *      hamsam.protocol.Protocol, hamsam.api.Message)
+         */
+        public void protocolMessageReceived(Protocol protocol, Message message)
+        {
+            LogLog.debug("Protocol message: " + message.toString());
+        }
+    }
+}
---
+++ toolbox/src/toolbox/log4j/im/test/AIMMessengerTest.java
@@ -0,0 +1,148 @@
+package toolbox.log4j.im.test;
+
+import java.util.Properties;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Appender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+import toolbox.log4j.im.AIMMessenger;
+import toolbox.log4j.im.InstantMessenger;
+import toolbox.log4j.im.YahooMessenger;
+import toolbox.util.ResourceUtil;
+import toolbox.util.ThreadUtil;
+import toolbox.util.XMLUtil;
+
+/**
+ * Unit tests for YahooMessenger
+ */
+public class AIMMessengerTest extends TestCase
+{
+    private static final Logger logger_ =
+        Logger.getLogger(AIMMessengerTest.class);
+
+    /**
+     * Yahoo user that will receive all test messages
+     */
+    private static final String recipient_ = "analogue";
+    
+    /**
+     * Yahoo user that the messages will originate from
+     */
+    private static final String FROM_USER = "supahfuzzy";
+    
+    /**
+     * Password of the FROM_USER
+     */
+    private static final String FROM_PASSWORD = "techno"; 
+    
+        
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Entrypoint
+     * 
+     * @param args None recognized
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(AIMMessengerTest.class);
+    }
+    
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+
+    /**
+     * Tests full lifecycle of typical usage
+     * 
+     * @throws Exception on error
+     */    
+    public void testLifeCycle() throws Exception
+    {
+        logger_.info("Running testLifeCycle...");
+        
+        InstantMessenger messenger = new AIMMessenger();
+        messenger.initialize(new Properties());
+        
+        logger_.debug("Before login...");
+        messenger.login(FROM_USER, FROM_PASSWORD);
+        
+        logger_.debug("Before send...");
+        messenger.send(recipient_, "Hello from the testLifeCycle unit test.");
+        
+        ThreadUtil.sleep(5000);
+        
+        logger_.debug("Before logout...");
+        messenger.logout();
+        
+        logger_.debug("Before shutdown...");
+        messenger.shutdown();
+        
+        logger_.debug("All done!");
+    }
+    
+//    /**
+//     * Tests sending a whole slew of messages
+//     * 
+//     * @throws Exception on error
+//     */    
+//    public void testSendMany() throws Exception
+//    {
+//        logger_.info("Running testSendMany...");
+//        
+//        InstantMessenger messenger = new YahooMessenger();
+//        messenger.initialize(new Properties());
+//        messenger.login(FROM_USER, FROM_PASSWORD);
+//        
+//        for (int i=0; i<100; i++)
+//        {
+//            //ThreadUtil.sleep(300);  // throttle
+//            messenger.send(
+//                recipient_, "This is message number " + (i+1) + " of 100");
+//        }
+//        
+//        messenger.logout();
+//        messenger.shutdown();
+//    }
+//    
+//    /**
+//     * Tests that the configuration in the xml file is read and executed
+//     * correctly.
+//     *  
+//     * @throws Exception on error
+//     */
+//    public void testConfigByXML() throws Exception
+//    {
+//        logger_.info("Running testConfigByXML...");
+//
+//        String appenderName = "testXMLInit_appender";
+//        String loggerName   = "testXMLInit_logger";
+//
+//        String xmlConfig = 
+//            ResourceUtil.getResourceAsString(
+//                "/toolbox/log4j/im/test/YahooMessengerTest.xml");
+//
+//        // Load config from xml file        
+//        DOMConfigurator.configure(XMLUtil.loadElement(xmlConfig));
+//        Logger logger = Logger.getLogger(loggerName);
+//        
+//        logger.debug("debug");
+//        logger.info("info");
+//        logger.warn("warn");
+//        logger.error("error");
+//        
+//        ThreadUtil.sleep(2000);
+//        
+//        // Cleanup
+//        Appender appender = logger.getAppender(appenderName);
+//        logger_.info("Closing appender " + appender.getName());
+//        appender.close();
+//        logger.removeAppender(appenderName);
+//    }
+}
