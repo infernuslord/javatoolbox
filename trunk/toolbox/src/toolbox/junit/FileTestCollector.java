73444d492f7ac0b6d420d18cc94d1f7dcbe6a185
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-09-28 17:15:44 +0000

    A stricter version of SimpleTestCollector that ships with JUnit

---
+++ trunk/toolbox/src/toolbox/junit/FileTestCollector.java
@@ -0,0 +1,34 @@
+package toolbox.junit;
+
+import junit.runner.ClassPathTestCollector;
+
+/**
+ * An implementation of a TestCollector that considers a class to be a test 
+ * class when it ends with the pattern "Test" in its name.
+ */
+public class FileTestCollector extends ClassPathTestCollector 
+{
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a FileTestCollector 
+     */
+    public FileTestCollector() 
+    {
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides ClassPathTestCollector
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see junit.runner.ClassPathTestCollector#isTestClass(java.lang.String)
+     */
+    protected boolean isTestClass(String classFileName) 
+    {
+        return classFileName.endsWith("Test.class") && 
+               classFileName.indexOf('$') < 0;
+    }
+}
