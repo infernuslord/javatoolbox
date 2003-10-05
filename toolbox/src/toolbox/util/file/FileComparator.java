3651c8982dfa07b2ce06d3489e99273bf376b0fb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-10-05 06:59:08 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/file/FileComparator.java
@@ -0,0 +1,86 @@
+package toolbox.util.file;
+
+import java.io.File;
+import java.util.Comparator;
+
+/**
+ * A {@java.util.File} comparator that can compare on varios file attributes.
+ * <p>
+ * <ul>
+ *   <li>File name (case insensetive)
+ *   <li>File size
+ *   <li>File timestamp
+ * </ul>
+ */
+public class FileComparator implements Comparator
+{
+    public static final int COMPARE_NAME = 1;             // File name
+    public static final int COMPARE_SIZE = 2; 			  // File size
+    public static final int COMPARE_TIMESTAMP = 3;		  // File timestamp
+    
+	/**
+	 * Field to compare
+	 */    
+    private int compareBy_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+
+    /**
+     * Creates a FileComparator
+     * 
+     * @param compareBy File attribute to base comparision on. 
+     *        Use FileComparator.COMPARE_[NAME|SIZE|TIMESTAMP]
+     */
+    public FileComparator(int compareBy)
+    {
+        // TODO: bounds checking
+        compareBy_ = compareBy;
+    }
+    
+    //--------------------------------------------------------------------------
+    // Comparator Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+     */
+    public int compare(Object a, Object b)
+    {
+        int result;
+        
+        File fileA = (File) a;
+        File fileB = (File) b;
+        
+        switch (compareBy_)
+        {
+            case COMPARE_NAME :
+             
+                result = fileA.getName().compareToIgnoreCase(fileB.getName());
+                break;
+            
+                
+            case COMPARE_SIZE :
+            
+                long sizeA = fileA.length();
+                long sizeB = fileB.length();
+                
+                if (sizeA == sizeB)
+                    result = 0;
+                else if (sizeA > sizeB)
+                    result = 1;
+                else
+                    result = -1;
+                break;
+                    
+            default:
+            
+                throw new IllegalArgumentException(
+                    "FileComparator does not support comparisons by " + 
+                    compareBy_); 
+        }
+        
+        return result;
+    }
+}
