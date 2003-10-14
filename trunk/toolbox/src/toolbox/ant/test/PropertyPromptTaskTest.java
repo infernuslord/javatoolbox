0cd6f4f1e07c7506b0d7f3205ecb501be9bccfcb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-10-14 04:12:30 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/ant/test/PropertyPromptTaskTest.java
@@ -0,0 +1,38 @@
+package toolbox.ant.test;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.tools.ant.Main;
+
+import toolbox.util.FileUtil;
+
+/**
+ * 
+ */
+public class PropertyPromptTaskTest extends TestCase
+{
+    /**
+     * 
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(PropertyPromptTaskTest.class);
+    }
+
+    /**
+     * Constructor for PropertyPromptTaskTest.
+     * @param arg0
+     */
+    public PropertyPromptTaskTest(String arg0)
+    {
+        super(arg0);
+    }
+    
+    public void testPropertyPrompt() throws Exception
+    {
+        Main.main(new String[]{"-debug", "-verbose", "-f", "c:\\workspaces\\workspace-toolbox\\toolbox\\src\\toolbox\\ant\\test\\PropertyPromptTaskTest.xml"});
+        
+    }
+}
