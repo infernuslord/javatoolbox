0001d3177ec38e9d0e38d21bcdad04835da18d50
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-10-23 06:49:57 +0000

    Javadoc updates

--- trunk/toolbox/src/toolbox/workspace/IPreferenced.java
+++ trunk/toolbox/src/toolbox/workspace/IPreferenced.java
@@ -12,7 +12,7 @@ public interface IPreferenced
      * Reads preferences from the prefs DOM and applies them to the preferenced 
      * target.
      * 
-     * @param  prefs  Document object model from which to read preferences. 
+     * @param prefs Document object model from which to read preferences. 
      * @throws Exception on error
      */
     public void applyPrefs(Element prefs) throws Exception;
@@ -20,7 +20,8 @@ public interface IPreferenced
     /**
      * Saves preferences to the prefs DOM.
      *  
-     * @param  prefs  Document object model to persist preferences to.
+     * @param prefs Document object model to persist preferences to
+     * @throws Exception on error
      */
     public void savePrefs(Element prefs) throws Exception;
     
