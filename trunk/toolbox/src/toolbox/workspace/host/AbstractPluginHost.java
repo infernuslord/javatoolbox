4481c2e5f228e0747081a6920ead8b3ec5453003
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-10-28 09:44:14 +0000

    We now have switchable plugin hosting strategies!!! (tabbed pane vs internal frames). Introduced interface PluginHost with two concrete implementations (TabbedPluginHost and DesktopPluginHost)  along with PluginHostManager which is used as the point class by PluginWorkspace to interface with.

---
+++ trunk/toolbox/src/toolbox/workspace/host/AbstractPluginHost.java
@@ -0,0 +1,172 @@
+package toolbox.workspace.host;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import org.apache.commons.collections.SequencedHashMap;
+import org.apache.log4j.Logger;
+
+import toolbox.workspace.IPlugin;
+
+/**
+ * Abstract implementation of an PluginHost that captures behavior common to 
+ * all PluginHost concrete implementations. 
+ */
+public abstract class AbstractPluginHost implements PluginHost
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(AbstractPluginHost.class);
+    
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Map of initialization parameters passed to each plugin on startup
+     */
+    private Map init_;
+    
+    /**
+     * Maps a plugin's FQCN -> IPlugin instance
+     */
+    private Map plugins_;
+
+    //--------------------------------------------------------------------------
+    // PluginHost Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Saves a copy of the initialization props that will be passed to each
+     * plugin on startup().
+     * 
+     * @see toolbox.workspace.host.PluginHost#startup(java.util.Map)
+     */
+    public void startup(Map props)
+    {
+        logger_.debug("startup");
+        
+        plugins_ = new SequencedHashMap();
+        init_ = props;
+    }
+
+    
+    /**
+     * Adds the plugin to the registry and calls startup()
+     * 
+     * @see toolbox.workspace.host.PluginHost#addPlugin(
+     *      toolbox.workspace.IPlugin)
+     */
+    public void addPlugin(IPlugin plugin)
+    {
+        logger_.debug("addPlugin: " + plugin.getClass().getName());
+        
+        importPlugin(plugin);
+        plugin.startup(init_);
+    }
+
+    
+    /**
+     * Removes the plugin from the registry and called shutdown()
+     * 
+     * @see toolbox.workspace.host.PluginHost#removePlugin(
+     *      toolbox.workspace.IPlugin)
+     */
+    public void removePlugin(IPlugin plugin)
+    {
+        logger_.debug("removePlugin: " + plugin.getClass().getName());
+        
+        exportPlugin(plugin);
+        plugin.shutdown();
+    }
+
+    
+    /**
+     * @see toolbox.workspace.host.PluginHost#importPlugin(
+     *      toolbox.workspace.IPlugin)
+     */
+    public void importPlugin(IPlugin plugin)
+    {
+        logger_.debug("importPlugin: " + plugin.getClass().getName());
+        
+        plugins_.put(plugin.getPluginName(), plugin);
+    }
+    
+    
+    /**
+     * @see toolbox.workspace.host.PluginHost#exportPlugin(
+     *      toolbox.workspace.IPlugin)
+     */
+    public void exportPlugin(IPlugin plugin)
+    {
+        logger_.debug("exportPlugin: " + plugin.getClass().getName());
+        
+        plugins_.remove(plugin.getPluginName());        
+    }    
+
+    
+    /**
+     * Retrieves the plugin from the registry.
+     * 
+     * @see toolbox.workspace.host.PluginHost#getPlugin(java.lang.String)
+     */
+    public IPlugin getPlugin(String pluginClass)
+    {
+        for (Iterator i = plugins_.values().iterator(); i.hasNext(); )
+        {
+            IPlugin plugin = (IPlugin) i.next();
+            if (plugin.getClass().getName().equals(pluginClass))
+                return plugin;
+        }        
+        
+        return null;
+    }
+
+    
+    /**
+     * Retrieves the plugins. 
+     * 
+     * @see toolbox.workspace.host.PluginHost#getPlugins()
+     */
+    public IPlugin[] getPlugins()
+    {
+        return (IPlugin[]) plugins_.values().toArray(new IPlugin[0]);
+    }
+
+    
+    /**
+     * Checks to see of the plugin exists in the registry.
+     * 
+     * @see toolbox.workspace.host.PluginHost#hasPlugin(java.lang.String)
+     */
+    public boolean hasPlugin(String pluginClass)
+    {
+        for (Iterator i = plugins_.values().iterator(); i.hasNext(); )
+            if (i.next().getClass().getName().equals(pluginClass))
+                return true;
+        
+        return false;                
+    }
+
+    
+    /**
+     * @see toolbox.workspace.host.PluginHost#getStartupConfig()
+     */
+    public Map getStartupConfig()
+    {
+        return init_;
+    }
+    
+    
+    /**
+     * Nulls out the plugin registry.
+     * 
+     * @see toolbox.workspace.host.PluginHost#shutdown()
+     */
+    public void shutdown()
+    {
+        logger_.debug("shutdown");
+        
+        plugins_.clear();
+        plugins_ = null;
+    }
+}
---
+++ trunk/toolbox/src/toolbox/workspace/host/PluginHost.java
@@ -0,0 +1,115 @@
+package toolbox.workspace.host;
+
+import java.util.Map;
+
+import javax.swing.JComponent;
+
+import toolbox.workspace.IPlugin;
+
+/**
+ * PluginHost is an interface used to manage and host IPlugins with a slant
+ * towards presentation/rendering specialization. PluginHost implementors 
+ * should be interchangeable at runtime so that the presentation framework or
+ * strategy can be changed dynamically on the fly.
+ * 
+ * @see PluginHostManager
+ */
+public interface PluginHost
+{
+    /**
+     * The actual GUI component that will host the plugins as child components.
+     * 
+     * @return JComponent
+     */
+    public JComponent getComponent();
+    
+    
+    /**
+     * Returns a UI friendly name of the plugin host.
+     * 
+     * @return String
+     */
+    public String getName();
+    
+    
+    /**
+     * Initializes the plugin host.
+     * 
+     * @param props Map of initialization properties 
+     */
+    public void startup(Map props);
+    
+    
+    /**
+     * Adds a plugin to be hosted by an PluginHost implementor.
+     * 
+     * @param plugin Plugin to add
+     */
+    public void addPlugin(IPlugin plugin);
+    
+    
+    /**
+     * Removes a plugin from PluginHost implementor.
+     * 
+     * @param plugin Plugin to remove
+     */
+    public void removePlugin(IPlugin plugin);
+    
+    
+    /**
+     * Imports an existing plugin from another host.
+     * 
+     * @param plugin Plugin to import
+     */
+    public void importPlugin(IPlugin plugin);
+    
+    
+    /**
+     * Exports an existing plugin so that the plugin can be transferred to
+     * another PluginHost via importPlugin()
+     * 
+     * @param plugin Plugin to export
+     */
+    public void exportPlugin(IPlugin plugin);
+    
+    
+    /**
+     * Checks if a given plugin is already being hosted.
+     * 
+     * @param pluginClass FQCN of the plugin to check
+     * @return True if the plugin exists, false otherwise
+     */
+    public boolean hasPlugin(String pluginClass);
+    
+    
+    /**
+     * Retrieves a plugin given its FQCN
+     * 
+     * @param pluginClass FCQN of the plugin to retrieve
+     * @return Plugin if found, null otherwise
+     */
+    public IPlugin getPlugin(String pluginClass);
+    
+    
+    /**
+     * Retrieves all of the plugins.
+     * 
+     * @return An iterator which can be used to traverse over the plugins.
+     */
+    public IPlugin[] getPlugins();
+    
+    
+    /**
+     * Necessary for transfering the contents of one host to another
+     * 
+     * @return Map containing the startup parameters
+     */
+    public Map getStartupConfig();
+    
+    
+    /**
+     * Shuts the plugin host down. A plugin host should be reusable by
+     * simply calling startup() after shutdown().
+     */
+    public void shutdown();
+}
