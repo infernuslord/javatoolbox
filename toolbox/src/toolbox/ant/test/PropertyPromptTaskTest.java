58fa42e1b5bb942fb97aca9dd4bba5729ea9bda9
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-10-31 09:40:09 +0000

    Javadoc updates

--- toolbox/src/toolbox/ant/test/PropertyPromptTaskTest.java
+++ toolbox/src/toolbox/ant/test/PropertyPromptTaskTest.java
@@ -6,31 +6,40 @@ import junit.textui.TestRunner;
 import org.apache.tools.ant.Main;
 
 /**
- * 
+ * Unit test for PropertyPromptTask.
  */
 public class PropertyPromptTaskTest extends TestCase
 {
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
     /**
+     * Entry point.
      * 
-     * @param args
+     * @param args None recognized
      */
     public static void main(String[] args)
     {
         TestRunner.run(PropertyPromptTaskTest.class);
     }
 
-    /**
-     * Constructor for PropertyPromptTaskTest.
-     * @param arg0
-     */
-    public PropertyPromptTaskTest(String arg0)
-    {
-        super(arg0);
-    }
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
     
+    /**
+     * Tests property prompt task.
+     * 
+     * @throws Exception
+     */
     public void testPropertyPrompt() throws Exception
     {
-        Main.main(new String[]{"-debug", "-verbose", "-f", "c:\\workspaces\\workspace-toolbox\\toolbox\\src\\toolbox\\ant\\test\\PropertyPromptTaskTest.xml"});
-        
+        Main.main(new String[]{
+            "-debug", 
+            "-verbose", 
+            "-f", 
+            "c:\\workspaces\\workspace-toolbox\\toolbox\\src\\toolbox\\ant\\test\\PropertyPromptTaskTest.xml"
+        });
     }
 }
