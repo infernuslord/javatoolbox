1ca053414845171b3a8ccfb41f613ec1b520c6cf
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-19 11:50:58 +0000

    Added throughtput and service status fields.

--- toolbox/src/toolbox/plugin/netmeter/ClientView.java
+++ toolbox/src/toolbox/plugin/netmeter/ClientView.java
@@ -4,28 +4,72 @@ import java.awt.BorderLayout;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
+import toolbox.util.StringUtil;
 import toolbox.util.ui.JSmartLabel;
 import toolbox.util.ui.JSmartTextField;
 import toolbox.util.ui.layout.ParagraphLayout;
 
 /**
- * ClientView
+ * ClientView.
  */
-public class ClientView extends JPanel
+public class ClientView extends JPanel implements ServiceListener, StatsListener
 {
+    /**
+     * Composite view that controls the service aspects of the client.
+     */
     private ServiceView serviceView_;
+    
+    /**
+     * Non-UI client object.
+     */
     private Client client_;
+    
+    /**
+     * Server hostname field.
+     */
     private JSmartTextField serverHostnameField_;
+    
+    /**
+     * Server port field.
+     */
     private JSmartTextField serverPortField_;
     
+    /**
+     * Throuhgput field in kilobytes per second.
+     */
+    private JTextField throughputField_;
     
+    /**
+     * Status of the service. Running, stopped, etc...
+     */
+    private JTextField statusField_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a ClientView.
+     * 
+     * @param client Client attached to this view.
+     */
     public ClientView(Client client)
     {
         client_ = client;
+        client_.addServiceListener(this);
+        client_.addStatsListener(this);
         buildView();
     }
     
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Builds the GUI.
+     */
     protected void buildView()
     {
         setBorder(BorderFactory.createEtchedBorder());
@@ -35,25 +79,106 @@ public class ClientView extends JPanel
         
         if (client_ != null)
         {
-            serverHostnameField_.setText(client_.getHostname());
-            serverPortField_.setText(client_.getPort()+"");
+            if (StringUtil.isNullOrEmpty(client_.getHostname()))
+                serverHostnameField_.setText("localhost");
+            else
+                serverHostnameField_.setText(client_.getHostname());
+
+            if (client_.getPort() == 0 )
+                serverPortField_.setText("9999");
+            else
+                serverPortField_.setText(client_.getPort() + "");
         }
-            
     }
     
+    
+    /**
+     * Creates the input panel for the server information.
+     * 
+     * @return JPanel
+     */
     protected JPanel buildInputPanel()
     {
         JPanel p = new JPanel(new ParagraphLayout());
         
+        // Hostname
         p.add(new JSmartLabel("Server"), ParagraphLayout.NEW_PARAGRAPH);
-        p.add(serverHostnameField_ = new JSmartTextField(20));
-        
-        p.add(new JSmartLabel("Port"), ParagraphLayout.NEW_PARAGRAPH);
-        p.add(serverPortField_ = new JSmartTextField(20));
-        
+        serverHostnameField_ = new JSmartTextField(10);
         serverHostnameField_.setEditable(false);
+        p.add(serverHostnameField_);
+
+        // Port
+        p.add(new JSmartLabel("Port"), ParagraphLayout.NEW_PARAGRAPH);
+        serverPortField_ = new JSmartTextField(10);
         serverPortField_.setEditable(false);
+        p.add(serverPortField_);
+        
+        // Throughput
+        p.add(new JSmartLabel("Server Throughput"),
+            ParagraphLayout.NEW_PARAGRAPH);
+        throughputField_ = new JSmartTextField(10);
+        throughputField_.setEditable(false);
+        p.add(throughputField_);
+
+        // Status
+        p.add(new JSmartLabel("Status"), ParagraphLayout.NEW_PARAGRAPH);
+        statusField_ = new JSmartTextField(10);
+        statusField_.setText("Stopped");
+        statusField_.setEditable(false);
+        p.add(statusField_);
         
         return p;
+    }
+
+    //--------------------------------------------------------------------------
+    // StatusListener Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.plugin.netmeter.StatsListener#throughput(int)
+     */
+    public void throughput(int kbs)
+    {
+        throughputField_.setText(kbs + "");
+    }
+    
+    //--------------------------------------------------------------------------
+    // ServiceListener Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.plugin.netmeter.ServiceListener#serviceStarted(
+     *      toolbox.plugin.netmeter.Service)
+     */
+    public void serviceStarted(Service service) throws ServiceException
+    {
+        statusField_.setText("Running...");
+    }
+    
+    /**
+     * @see toolbox.plugin.netmeter.ServiceListener#servicePaused(
+     *      toolbox.plugin.netmeter.Service)
+     */
+    public void servicePaused(Service service) throws ServiceException
+    {
+        statusField_.setText("Paused");
+    }
+    
+    /**
+     * @see toolbox.plugin.netmeter.ServiceListener#serviceResumed(
+     *      toolbox.plugin.netmeter.Service)
+     */
+    public void serviceResumed(Service service) throws ServiceException
+    {
+        statusField_.setText("Running...");
+    }
+    
+    /**
+     * @see toolbox.plugin.netmeter.ServiceListener#serviceStopped(
+     *      toolbox.plugin.netmeter.Service)
+     */
+    public void serviceStopped(Service service) throws ServiceException
+    {
+        statusField_.setText("Stopped");
     }
 }
