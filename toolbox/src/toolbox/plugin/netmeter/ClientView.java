30977612e91ca592f970ae122250d51d633c65f2
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-19 00:17:32 +0000

    Populated field on build.

--- toolbox/src/toolbox/plugin/netmeter/ClientView.java
+++ toolbox/src/toolbox/plugin/netmeter/ClientView.java
@@ -2,6 +2,7 @@ package toolbox.plugin.netmeter;
 
 import java.awt.BorderLayout;
 
+import javax.swing.BorderFactory;
 import javax.swing.JPanel;
 
 import toolbox.util.ui.JSmartLabel;
@@ -14,21 +15,30 @@ import toolbox.util.ui.layout.ParagraphLayout;
 public class ClientView extends JPanel
 {
     private ServiceView serviceView_;
-    private Service client_;
+    private Client client_;
     private JSmartTextField serverHostnameField_;
     private JSmartTextField serverPortField_;
     
     
-    public ClientView()
+    public ClientView(Client client)
     {
+        client_ = client;
         buildView();
     }
     
     protected void buildView()
     {
+        setBorder(BorderFactory.createEtchedBorder());
         setLayout(new BorderLayout());
         add(buildInputPanel(), BorderLayout.CENTER);
         add(new ServiceView(client_), BorderLayout.SOUTH);
+        
+        if (client_ != null)
+        {
+            serverHostnameField_.setText(client_.getHostname());
+            serverPortField_.setText(client_.getPort()+"");
+        }
+            
     }
     
     protected JPanel buildInputPanel()
@@ -40,6 +50,9 @@ public class ClientView extends JPanel
         
         p.add(new JSmartLabel("Port"), ParagraphLayout.NEW_PARAGRAPH);
         p.add(serverPortField_ = new JSmartTextField(20));
+        
+        serverHostnameField_.setEditable(false);
+        serverPortField_.setEditable(false);
         
         return p;
     }
