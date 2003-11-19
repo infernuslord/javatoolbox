93c7bf78b62b400346ad16aab9832fc502d86136
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-19 11:48:28 +0000

    Added gradient support.

--- toolbox/src/toolbox/util/ui/JSmartLabel.java
+++ toolbox/src/toolbox/util/ui/JSmartLabel.java
@@ -1,7 +1,12 @@
 package toolbox.util.ui;
 
+import java.awt.Color;
+import java.awt.GradientPaint;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Insets;
+import java.awt.geom.RoundRectangle2D;
 
 import javax.swing.Icon;
 import javax.swing.JLabel;
@@ -12,15 +17,31 @@ import toolbox.util.SwingUtil;
  * JSmartLabel adds the following behavior.
  * <p>
  * <ul>
- *   <li>Support for antialised text
+ *   <li>Antialiased text
+ *   <li>Background gradient
  * </ul>
  */
 public class JSmartLabel extends JLabel implements AntiAliased
 {
     /**
-     * Antialiased flag
+     * Antialiased flag.
      */
     private boolean antiAliased_ = SwingUtil.getDefaultAntiAlias();
+    
+    /**
+     * Flag to turn the gradient on/off.
+     */
+    private boolean gradient_;
+
+    /**
+     * Gradient start color.
+     */
+    private Color startColor_;
+    
+    /**
+     * Gradient end color.
+     */
+    private Color endColor_;
     
     //--------------------------------------------------------------------------
     // Constructors
@@ -92,6 +113,34 @@ public class JSmartLabel extends JLabel implements AntiAliased
         super(text, horizontalAlignment);
     }
 
+    
+    /**
+     * Creates a JSmartLabel with a gradient.
+     *
+     * @param text Label text
+     * @param start Gradient start color
+     * @param end Gradient end color
+     */
+    public JSmartLabel(String text, Color start, Color end) 
+    {
+        super(text);
+        gradient_ = true;
+        startColor_ = start;
+        endColor_ = end;
+    }
+    
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+
+    /**
+     * Turns the gradient on/off.
+     */
+    public void setGradient(boolean b)
+    {
+        gradient_ = b;
+    }
+    
     //--------------------------------------------------------------------------
     // AntiAliased Interface
     //--------------------------------------------------------------------------
@@ -122,9 +171,12 @@ public class JSmartLabel extends JLabel implements AntiAliased
      *      java.awt.Image, int, int, int, int, int)
      */
     public boolean imageUpdate(
-        Image img, int infoflags, 
-        int x, int y, 
-        int width, int height)
+        Image img, 
+        int infoflags, 
+        int x, 
+        int y, 
+        int width, 
+        int height)
     {
         repaint();
         return true;
@@ -141,5 +193,40 @@ public class JSmartLabel extends JLabel implements AntiAliased
     {
         SwingUtil.makeAntiAliased(gc, isAntiAliased());
         super.paintComponent(gc);
+    }
+    
+    
+    /**
+     * @see java.awt.Component#paint(java.awt.Graphics)
+     */
+    public void paint(Graphics g) 
+    {
+        if (gradient_)
+        {    
+            Insets insets = new Insets(0,0,0,0);
+            g.setColor(startColor_);
+            Graphics2D g2d = (Graphics2D) g;
+        
+            GradientPaint redtowhite =
+                new GradientPaint(
+                    insets.left, 
+                    insets.top,
+                    startColor_,  
+                    getWidth(),
+                    insets.top,
+                    endColor_); 
+                
+            g2d.setPaint(redtowhite);
+        
+            g2d.fill(
+                new RoundRectangle2D.Double(
+                    insets.left, 
+                    insets.top, 
+                    getWidth(), 
+                    getHeight(), 
+                    1, 
+                    1));
+        }
+        super.paint(g);
     }
 }
