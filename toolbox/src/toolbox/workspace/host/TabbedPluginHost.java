9a8daf77f2cc98fdc30b28c43348a5ee281a8cc3
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-25 23:41:21 +0000

    Updated to correctly behave when the tabClosing() event is fired.

--- toolbox/src/toolbox/workspace/host/TabbedPluginHost.java
+++ toolbox/src/toolbox/workspace/host/TabbedPluginHost.java
@@ -1,15 +1,20 @@
 package toolbox.workspace.host;
 
+import java.awt.Component;
 import java.util.Map;
 
 import javax.swing.JComponent;
 import javax.swing.SwingUtilities;
 
+import org.apache.commons.collections.bidimap.BidiMap;
+import org.apache.commons.collections.bidimap.DualHashBidiMap;
 import org.apache.log4j.Logger;
 
+import toolbox.util.ExceptionUtil;
 import toolbox.util.ui.tabbedpane.JSmartTabbedPane;
 import toolbox.util.ui.tabbedpane.SmartTabbedPaneListener;
 import toolbox.workspace.IPlugin;
+import toolbox.workspace.PluginWorkspace;
 
 /**
  * Plugin host that arranges plugins in a tab panel. One plugin per tab.
@@ -28,22 +33,48 @@ public class TabbedPluginHost extends AbstractPluginHost
      */
     private JSmartTabbedPane tabPanel_;
     
+    /**
+     * Reference to the workspace. Big no no..
+     */
+    private PluginWorkspace workspace_;
+    
+    /**
+     * Maps the UI component of a plugin to its associated IPlugin. The UI 
+     * component is the actual component that is added to the tab panel.
+     */
+    private BidiMap comp2plugin_;
+    
+    /**
+     * Maps an IPlugin to its associated UI component.
+     */
+    private BidiMap plugin2comp_;
+    
     //--------------------------------------------------------------------------
     // PluginHost Interface
     //--------------------------------------------------------------------------
-    
+
     /**
-     * @see toolbox.workspace.PluginHost#startup(java.util.Map)
+     * @see toolbox.workspace.host.PluginHost#startup(java.util.Map)
      */
     public void startup(Map props)
     {
         super.startup(props);
+        
+        workspace_ = (PluginWorkspace)props.get(PluginWorkspace.PROP_WORKSPACE);
+        
+        // Bidirectional hash maps
+        comp2plugin_ = new DualHashBidiMap();
+        plugin2comp_ = comp2plugin_.inverseBidiMap();
+
+        // Create tab panel and add listener so we know when one of the tabs 
+        // is closed via the user clicking the X on the tab.
         tabPanel_ = new JSmartTabbedPane(true);
         tabPanel_.addSmartTabbedPaneListener(new TabPanelListener());
         
-        SwingUtilities.invokeLater(new Runnable() {
-            
-            public void run() {
+        SwingUtilities.invokeLater(new Runnable() 
+        {
+            public void run() 
+            {
                 if (tabPanel_.getTabCount() > 0)
                     tabPanel_.setSelectedIndex(0);
             }
@@ -53,7 +84,8 @@ public class TabbedPluginHost extends AbstractPluginHost
 
     
     /**
-     * @see toolbox.workspace.PluginHost#addPlugin(toolbox.workspace.IPlugin)
+     * @see toolbox.workspace.host.PluginHost#addPlugin(
+     *      toolbox.workspace.IPlugin)
      */
     public void addPlugin(IPlugin plugin)
     {
@@ -62,12 +94,11 @@ public class TabbedPluginHost extends AbstractPluginHost
 
     
     /**
-     * @see toolbox.workspace.PluginHost#removePlugin(toolbox.workspace.IPlugin)
+     * @see toolbox.workspace.host.PluginHost#removePlugin(
+     *      toolbox.workspace.IPlugin)
      */
     public void removePlugin(IPlugin plugin)
     {
-        //      if (removeTab)
-
         super.removePlugin(plugin);
     }
     
@@ -79,7 +110,10 @@ public class TabbedPluginHost extends AbstractPluginHost
     public void importPlugin(IPlugin plugin)
     {
         super.importPlugin(plugin);
-        tabPanel_.addTab(plugin.getPluginName(), plugin.getComponent());
+        
+        JComponent comp = plugin.getComponent();
+        tabPanel_.addTab(plugin.getPluginName(), comp);
+        comp2plugin_.put(comp, plugin);
     }    
 
     
@@ -89,24 +123,39 @@ public class TabbedPluginHost extends AbstractPluginHost
      */
     public void exportPlugin(IPlugin plugin)
     {
-        tabPanel_.remove(tabPanel_.indexOfTab(plugin.getPluginName()));        
+        // make sure component still exists in the tab panel. it could already
+        // have been removed by clicking the X on the tab.
+        
+        Component c = (Component) plugin2comp_.get(plugin);
+        int i = tabPanel_.indexOfComponent(c); 
+        
+        if (i >= 0)
+            tabPanel_.remove(i);
+        else
+            logger_.info("Tab component already removed.");
+        
+        comp2plugin_.remove(c);
         super.exportPlugin(plugin);
     }
     
     
     /**
-     * @see toolbox.workspace.PluginHost#shutdown()
+     * @see toolbox.workspace.host.PluginHost#shutdown()
      */
     public void shutdown()
     {
         tabPanel_.removeAll();
         tabPanel_ = null;
+        
+        comp2plugin_.clear();
+        comp2plugin_ = null;
+        
         super.shutdown();
     }
 
     
     /**
-     * @see toolbox.workspace.PluginHost#getComponent()
+     * @see toolbox.workspace.host.PluginHost#getComponent()
      */
     public JComponent getComponent()
     {
@@ -138,22 +187,19 @@ public class TabbedPluginHost extends AbstractPluginHost
          */
         public void tabClosing(JSmartTabbedPane tabbedPane, int tabIndex)
         {
-	
-            // TODO: Fix tab closing reference cleanup.
-
-            //            String name = tabbedPane.getTitleAt(tabIndex);
-            //            //IPlugin plugin = (IPlugin) plugins_.get(name);
-            //            IPlugin plugin = (IPlugin) pluginHost_.getPlugin(name);
-            //            String clazz = plugin.getClass().getName(); 
-            //            
-            //            try
-            //            {
-            //                deregisterPlugin(clazz, false);
-            //            }
-            //            catch (Exception e)
-            //            {
-            //                ExceptionUtil.handleUI(e, logger_);
-            //            }
+            JComponent component = 
+                (JComponent) tabPanel_.getComponentAt(tabIndex);
+            
+            IPlugin plugin = (IPlugin) comp2plugin_.get(component);
+             
+            try
+            {
+                workspace_.deregisterPlugin(plugin.getClass().getName(), false);
+            }
+            catch (Exception e)
+            {
+                ExceptionUtil.handleUI(e, logger_);
+            }
         }
     }
 }
