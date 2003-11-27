4b660e9b714f4bdf03965e95fa8c0a7a038ef603
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-27 15:11:09 +0000

    Added test for formatSQL()

--- trunk/toolbox/src/toolbox/util/test/JDBCUtilTest.java
+++ trunk/toolbox/src/toolbox/util/test/JDBCUtilTest.java
@@ -574,6 +574,58 @@ public class JDBCUtilTest extends TestCase
         JDBCUtil.shutdown();
     }
     
+    
+    public void testFormatSQL()
+    {
+        logger_.info("Running testFormatSQL...");
+        
+        String sql = "select * from user";
+        String fsql = JDBCUtil.formatSQL(sql);
+        
+        assertNotNull(fsql);
+        logger_.info("\n" + fsql);
+        
+        sql = "select one, two, three from user where name like 'A%' and id = 34533 group by lastName";
+        fsql = JDBCUtil.formatSQL(sql);
+        
+        assertNotNull(fsql);
+        logger_.info("\n" + fsql);
+        
+        sql = "CREATE TABLE Location (id CHAR(8) NOT NULL, sequence CHAR(8) NOT NULL, version CHAR(8) NOT NULL, capability VARCHAR(16), sequence CHAR(8) NOT NULL, version CHAR(8) NOT NULL, capability VARCHAR(16), applyPwswTiny CHAR(1), locationNumber VARCHAR(32), localCompanyName VARCHAR(100), localCompanyNamePron VARCHAR(100), companyName VARCHAR(100), dbaName VARCHAR(100), contactAddress_addrLineOne VARCHAR(60), contactAddress_addrLineTwo VARCHAR(60), contactAddress_addrLineThree VARCHAR(60), contactAddress_addrLineFour VARCHAR(60), contactAddress_city VARCHAR(35), contactAddress_country CHAR(2), contactAddress_country_L VARCHAR(35), contactAddress_state CHAR(6), contactAddress_state_L VARCHAR(35), contactAddress_zip VARCHAR(16), hasReturnedMail CHAR(1), voiceOne_ctryCode VARCHAR(100), voiceOne_number VARCHAR(32), faxOne_ctryCode VARCHAR(100), faxOne_number VARCHAR(32), internetInfo_locationEmail VARCHAR(100), internetInfo_locationURL VARCHAR(150), parentID VARCHAR(100), ibmCustNum VARCHAR(10), federalTaxID VARCHAR(50), corporationNumber VARCHAR(8), ppaID VARCHAR(10), numberLocations INTEGER, yearStarted INTEGER, geo CHAR(2), geo_L VARCHAR(35), geoRegion CHAR(6), geoRegion_L VARCHAR(35), numEmployees INTEGER, bpdbID VARCHAR(26), cmrAction CHAR(6), cmrAction_L VARCHAR(35), cmrRetry CHAR(1), cmr_customerNumber VARCHAR(10), cmr_date DATE, cmr_existing CHAR(1), cmr_firstName VARCHAR(100), cmr_lastName VARCHAR(100), cmr_phone VARCHAR(100), cmr_email VARCHAR(100), cmr_contactNumber VARCHAR(10), cmr_deleted CHAR(1), cmr_deniedParty CHAR(1), cmr_badAddress CHAR(1), cmr_duplicate CHAR(1), cmr_duplicateNumber VARCHAR(10), cmr_duplicateName VARCHAR(35), cmr_success CHAR(1), cmr_message VARCHAR(35), profileUpdateUser VARCHAR(40), profileUpdateTimestamp DATE, dplCheck CHAR(6), dplCheck_L VARCHAR(35), supplierLists CHAR(6), supplierLists_L VARCHAR(35), russianFederation CHAR(6), russianFederation_L VARCHAR(35), PRIMARY KEY(id));";
+        fsql = JDBCUtil.formatSQL(sql);
+        
+        logger_.info("\n" + fsql);
+        
+        sql =
+        "        CREATE TABLE SAPCateg.SAPCategoryType("
+        + "                id CHAR(8) NOT NULL,"
+        + "                sequence CHAR(8) NOT NULL,"
+        + "                version CHAR(8) NOT NULL,"
+        + "                domain VARCHAR(50),"
+        + "                PRIMARY KEY(id))"
+        + "        in DATA01 index in INX01;"
+        + "        COMMENT ON TABLE SAPCateg.SAPCategoryType is '';"
+        + "        COMMENT ON COLUMN SAPCateg.SAPCategoryType.domain is ' ';"
+        + "        CREATE TABLE SAPCateg.keywordEntry("
+        + "                id CHAR(8) NOT NULL,"
+        + "                child_id CHAR(8) NOT NULL,"
+        + "                code VARCHAR(100),"
+        + "                description VARCHAR(100),"
+        + "                categoryType CHAR(6),"
+        + "                categoryType_L VARCHAR(35),"
+        + "                PRIMARY KEY(id,child_id),"
+        + "                FOREIGN KEY(id) REFERENCES SAPCateg.SAPCategoryType(id))"
+        + "        in DATA01 index in INX01;"
+        + "        COMMENT ON TABLE SAPCateg.keywordEntry is ' ';"
+        + "        COMMENT ON COLUMN SAPCateg.keywordEntry.code is ' ';"
+        + "        COMMENT ON COLUMN SAPCateg.keywordEntry.description is ' ';"
+        + "        COMMENT ON COLUMN SAPCateg.keywordEntry.categoryType is ' ';"
+        + "        COMMENT ON COLUMN SAPCateg.keywordEntry.categoryType_L is 'null (L10N)';";        
+        
+        fsql = JDBCUtil.formatSQL(sql);
+        logger_.info("\n" + fsql);
+    }
+    
     //--------------------------------------------------------------------------
     // Helpers
     //--------------------------------------------------------------------------
