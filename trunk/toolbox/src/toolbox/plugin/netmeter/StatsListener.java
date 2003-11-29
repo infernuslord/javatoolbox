e50bc702bd5a8fd2597e42dc20db9293968e1773
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-11-29 10:35:38 +0000

    Removed redundant public modifier

--- trunk/toolbox/src/toolbox/plugin/netmeter/StatsListener.java
+++ trunk/toolbox/src/toolbox/plugin/netmeter/StatsListener.java
@@ -4,12 +4,12 @@ package toolbox.plugin.netmeter;
  * StatsListener provides notification related to collected statistics based on 
  * network activity.
  */
-public interface StatsListener
+public interface StatsListener extends ServiceListener
 {
     /**
      * Notifcation of data throughput in kilobytes per second.
      * 
      * @param kbs Throughput per second.
      */
-    public void throughput(int kbs);
+    void throughput(int kbs);
 }
