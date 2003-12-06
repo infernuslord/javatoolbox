027f7f534c9ea1bed592024f19823abc63b467e2
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-06 01:00:34 +0000

    Added "null" messenger.

--- trunk/toolbox/src/toolbox/log4j/im/InstantMessengerFactory.java
+++ trunk/toolbox/src/toolbox/log4j/im/InstantMessengerFactory.java
@@ -28,6 +28,10 @@ public class InstantMessengerFactory
         {
             im = new SametimeMessenger();
         }
+        else if (network.equalsIgnoreCase("null"))
+        {
+            im = new NullMessenger();
+        }
         else
         {
             throw new IllegalArgumentException(
