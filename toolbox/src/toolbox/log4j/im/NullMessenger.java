e45cffb3a4a5850c9cac9ea8dc6aec2f7fe8ca3c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-06 01:00:12 +0000

    Inital version

---
+++ toolbox/src/toolbox/log4j/im/NullMessenger.java
@@ -0,0 +1,65 @@
+package toolbox.log4j.im;
+
+import java.util.Properties;
+
+/**
+ * NullMessenger for mock/testing purposes. 
+ */
+public class NullMessenger implements InstantMessenger
+{
+    //--------------------------------------------------------------------------
+    // InstantMessenger Interface 
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#initialize(java.util.Properties)
+     */
+    public void initialize(Properties props) throws InstantMessengerException
+    {
+    }
+
+    
+    /**
+	 * @see toolbox.log4j.im.InstantMessenger#login(java.lang.String,
+	 *      java.lang.String)
+	 */
+    public void login(String username, String password)
+        throws InstantMessengerException
+    {
+    }
+
+    
+    /**
+	 * @see toolbox.log4j.im.InstantMessenger#send(java.lang.String,
+	 *      java.lang.String)
+	 */
+    public void send(String recipient, String message)
+        throws InstantMessengerException
+    {
+    }
+
+    
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#logout()
+     */
+    public void logout() throws InstantMessengerException
+    {
+    }
+
+    
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#shutdown()
+     */
+    public void shutdown() throws InstantMessengerException
+    {
+    }
+
+    
+    /**
+     * @see toolbox.log4j.im.InstantMessenger#isConnected()
+     */
+    public boolean isConnected()
+    {
+        return false;
+    }
+}
