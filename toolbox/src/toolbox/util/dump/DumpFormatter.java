4a6af5b88d3cf2c86fa8904f529150377fa4e0c1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-06 01:41:41 +0000

    Removed redundant public access modifiers.

--- toolbox/src/toolbox/util/dump/DumpFormatter.java
+++ toolbox/src/toolbox/util/dump/DumpFormatter.java
@@ -9,57 +9,63 @@ import java.lang.reflect.Field;
 public interface DumpFormatter
 {
     /**
-     * Should the given class be included in the dump?
-     * 
-     * @param   clazz    Class to test for inclusion
-     * @return  boolean  True if class should be included, false otherwise
-     */
-    public boolean shouldInclude(Class clazz);
+	 * Should the given class be included in the dump?
+	 * 
+	 * @param clazz Class to test for inclusion
+	 * @return boolean True if class should be included, false otherwise
+	 */
+    boolean shouldInclude(Class clazz);
 
-    /**
-     * Should the given field be included in the dump?
-     * 
-     * @param   field    Field to test for inclusion
-     * @return  boolean  True if field should be included, false otherwise
-     */
-    public boolean shouldInclude(Field field);
     
     /**
-     * Returns true if the inheritance tree should be shown for each object
-     * traversed, false otherwise.
-     * 
-     * @return Inheritance flag
-     */
-    public boolean showInheritance();
+	 * Should the given field be included in the dump?
+	 * 
+	 * @param field Field to test for inclusion
+	 * @return boolean True if field should be included, false otherwise
+	 */
+    boolean shouldInclude(Field field);
+
     
     /**
-     * Formats the presentation of a classes' name
-     * 
-     * @param   className  Class name to format
-     * @return  Formatted class name
-     */
-    public String  formatClassName(String className);
+	 * Returns true if the inheritance tree should be shown for each object
+	 * traversed, false otherwise.
+	 * 
+	 * @return Inheritance flag
+	 */
+    boolean showInheritance();
+
     
     /**
-     * Formats the presentation of a classes' name
-     * 
-     * @param   clazz  Class name to format
-     * @return  Formatted classname
-     */
-    public String  formatClassName(Class clazz);
+	 * Formats the presentation of a classes' name.
+	 * 
+	 * @param className Class name to format
+	 * @return Formatted class name
+	 */
+    String formatClassName(String className);
+
     
     /**
-     * Returns true if the fields in a class should be sorted alphabetically,
-     * false otherwise.
-     * 
-     * @return Sort fields flag
-     */
-    public boolean sortFields();
+	 * Formats the presentation of a classes' name.
+	 * 
+	 * @param clazz Class name to format
+	 * @return Formatted classname
+	 */
+    String formatClassName(Class clazz);
+
     
     /**
-     * Formats a field name.
-     * 
-     * @return  Formatted field name
-     */
-    public String formatFieldName(String fieldName);
+	 * Returns true if the fields in a class should be sorted alphabetically,
+	 * false otherwise.
+	 * 
+	 * @return Sort fields flag
+	 */
+    boolean sortFields();
+
+    
+    /**
+	 * Formats a field name.
+	 * 
+	 * @return Formatted field name
+	 */
+    String formatFieldName(String fieldName);
 }
