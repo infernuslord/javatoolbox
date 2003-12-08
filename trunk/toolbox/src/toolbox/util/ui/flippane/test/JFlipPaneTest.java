b2a7c602067edf5ce03ca3b06a7357072864a88c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-08 01:57:44 +0000

    Updated to extend UITestCase

--- trunk/toolbox/src/toolbox/util/ui/flippane/test/JFlipPaneTest.java
+++ trunk/toolbox/src/toolbox/util/ui/flippane/test/JFlipPaneTest.java
@@ -2,31 +2,28 @@ package toolbox.util.ui.flippane.test;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Container;
 import java.awt.Dimension;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
-import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
 import org.apache.log4j.Logger;
 
-import toolbox.util.SwingUtil;
-import toolbox.util.ui.explorer.JFileExplorer;
+import toolbox.junit.UITestCase;
 import toolbox.util.ui.JSmartButton;
 import toolbox.util.ui.JSmartLabel;
+import toolbox.util.ui.explorer.JFileExplorer;
 import toolbox.util.ui.flippane.FlipPaneListener;
 import toolbox.util.ui.flippane.JFlipPane;
 
 /**
- * Unit test for JFlipPane
+ * Unit test for JFlipPane.
  */
-public class JFlipPaneTest extends TestCase
+public class JFlipPaneTest extends UITestCase
 {
     private static final Logger logger_ = 
         Logger.getLogger(JFlipPaneTest.class);
@@ -36,14 +33,13 @@ public class JFlipPaneTest extends TestCase
     //--------------------------------------------------------------------------
     
     /**
-     * Entry point
+     * Entry point.
      * 
-     * @param args None
+     * @param args None recognized
      * @throws Exception on error
      */    
     public static void main(String[] args) throws Exception
     {
-        SwingUtil.setPreferredLAF();
         TestRunner.run(JFlipPaneTest.class);
     }
 
@@ -51,71 +47,67 @@ public class JFlipPaneTest extends TestCase
     // Unit Tests
     //--------------------------------------------------------------------------
     
+    /**
+     * Shows multiple JFlipPanes in a window so it can be tested interactively.
+     */
     public void testJFlipPane()
     {
         logger_.info("Running testJFlipPane...");
         
-        Tester tester = new Tester();
-        tester.setSize(800, 600);
-        SwingUtil.centerWindow(tester);
-        tester.setVisible(true);
-    }
-    
-    //--------------------------------------------------------------------------
-    // Inner Classes
-    //--------------------------------------------------------------------------
+        JPanel p = new JPanel(new BorderLayout());
+
+        p.add(BorderLayout.WEST, createFlipPane(JFlipPane.LEFT));
+        p.add(BorderLayout.EAST, createFlipPane(JFlipPane.RIGHT));
+        p.add(BorderLayout.NORTH, createFlipPane(JFlipPane.TOP));
+        p.add(BorderLayout.SOUTH, createFlipPane(JFlipPane.BOTTOM));
         
-    class Tester extends JDialog
-    {
-        public Tester()
-        {
-            super(new JFrame(), "testJFlipPane", true);
-            setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-            Container c = getContentPane();
-            
-            c.setLayout(new BorderLayout());
-            
-            c.add(BorderLayout.WEST, createFlipPane(JFlipPane.LEFT));
-            c.add(BorderLayout.EAST, createFlipPane(JFlipPane.RIGHT));
-            c.add(BorderLayout.NORTH, createFlipPane(JFlipPane.TOP));
-            c.add(BorderLayout.SOUTH, createFlipPane(JFlipPane.BOTTOM));
-            
-            JLabel label = new JSmartLabel("Filler");
-            label.setFont(label.getFont().deriveFont((float)50.0));
-            label.setHorizontalAlignment(SwingConstants.CENTER);
-            label.setBackground(Color.white);
-            label.setForeground(Color.blue);
-            c.add(label, BorderLayout.CENTER);
-        }
-    
-        protected JFlipPane createFlipPane(String pos)
-        {
-            JFlipPane fp = new JFlipPane(pos);
-            JLabel card1 = new JSmartLabel("Label");
-            fp.addFlipper(card1.getText(), card1);
-            
-            JButton card2 = new JSmartButton("Button");
-            card2.setPreferredSize(new Dimension(100,100));
-            card2.setMinimumSize(new Dimension(50,50));
-            fp.addFlipper(card2.getText(), card2);
-            
-            JFileExplorer explorer = new JFileExplorer(false);
-            fp.addFlipper("File Explorer", explorer);
-            
-            fp.addFlipPaneListener(new FlipPaneListener()
-            {
-                public void collapsed(JFlipPane flipPane)
-                {
-                    logger_.info("Flipper collapsed");
-                }
-    
-                public void expanded(JFlipPane flipPane)
-                {
-                    logger_.info("Flipper expanded");
-                }
-            });
-            
-            return fp;
-        }    
+        JLabel label = new JSmartLabel("Filler");
+        label.setFont(label.getFont().deriveFont((float)50.0));
+        label.setHorizontalAlignment(SwingConstants.CENTER);
+        label.setBackground(Color.white);
+        label.setForeground(Color.blue);
+        p.add(label, BorderLayout.CENTER);
+        
+        launchInDialog(p, UITestCase.SCREEN_TWO_THIRDS);
     }
+
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
+
+    /**
+     * Creates a JFlipPane for the given position.
+     * 
+     * @param pos Position to place the flip pane. 
+     *        JFlipPane.TOP|RIGHT|LEFT|RIGHT
+     */
+    protected JFlipPane createFlipPane(String pos)
+    {
+        JFlipPane fp = new JFlipPane(pos);
+        JLabel card1 = new JSmartLabel("Label");
+        fp.addFlipper(card1.getText(), card1);
+        
+        JButton card2 = new JSmartButton("Button");
+        card2.setPreferredSize(new Dimension(100,100));
+        card2.setMinimumSize(new Dimension(50,50));
+        fp.addFlipper(card2.getText(), card2);
+        
+        JFileExplorer explorer = new JFileExplorer(false);
+        fp.addFlipper("File Explorer", explorer);
+        
+        fp.addFlipPaneListener(new FlipPaneListener()
+        {
+            public void collapsed(JFlipPane flipPane)
+            {
+                logger_.info("Flipper collapsed");
+            }
+            
+            public void expanded(JFlipPane flipPane)
+            {
+                logger_.info("Flipper expanded");
+            }
+        });
+        
+        return fp;
+    }    
 }
