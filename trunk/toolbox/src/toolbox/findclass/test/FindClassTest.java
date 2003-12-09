d997604fd54d1c3e70e746751ce1135d95067735
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-09 23:33:21 +0000

    Added additional tests to round out coverage.

--- trunk/toolbox/src/toolbox/findclass/test/FindClassTest.java
+++ trunk/toolbox/src/toolbox/findclass/test/FindClassTest.java
@@ -1,13 +1,22 @@
 package toolbox.findclass.test;
 
-import org.apache.log4j.Logger;
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
 
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.apache.log4j.Logger;
+
 import toolbox.findclass.FindClass;
+import toolbox.findclass.FindClassListener;
 import toolbox.findclass.FindClassResult;
 import toolbox.util.ArrayUtil;
+import toolbox.util.ClassUtil;
+import toolbox.util.FileUtil;
+import toolbox.util.ThreadUtil;
+import toolbox.util.concurrent.Mutex;
 
 /**
  * Unit test for Findclass.
@@ -22,7 +31,7 @@ public class FindClassTest extends TestCase
     //--------------------------------------------------------------------------
         
     /**
-     * Test entry point.
+     * Entrypoint.
      * 
      * @param args None recognized
      */
@@ -106,5 +115,171 @@ public class FindClassTest extends TestCase
 
         logger_.info("Results: " + ArrayUtil.toString(results));
         assertEquals("Couldn't find " + searchFor, 1, results.length);
+    }
+    
+    
+    /**
+     * Test adding/removing search targets.
+     * 
+     * @throws Exception on error
+     */
+    public void testSearchTargets() throws Exception
+    {
+        logger_.info("Running testSearchTargets...");
+        
+        FindClass finder = new FindClass();
+        
+        // Get the default targets
+        List targets = finder.getSearchTargets();
+        assertTrue(targets.size() > 0);
+        
+        // Remove the first one
+        String target = (String) targets.get(0);
+        finder.removeSearchTarget(target);
+        
+        // Verify it was removed
+        List removed = finder.getSearchTargets();
+        assertFalse(removed.contains(target));
+        
+        // Add it back in
+        finder.addSearchTarget(target);
+        
+        // Verify it was added
+        List added = finder.getSearchTargets();
+        assertTrue(added.contains(target));
+        
+        // Add search targets as both file and directory
+        finder.addSearchTarget(new File("."));
+        
+        for (Iterator i = added.iterator(); i.hasNext(); )
+        {    
+            String s = (String) i.next();
+            
+            logger_.info("finding jar: " + s);
+            
+            if (ClassUtil.isArchive(s))
+            {    
+                finder.addSearchTarget(new File(s));
+                break;
+            }
+        }
+
+        // Remove all
+        finder.removeSearchTargets();
+        List cleared = finder.getSearchTargets();
+        assertTrue(cleared.isEmpty());
+    }
+
+    
+    /**
+     * Test getArchivesInDir()
+     * 
+     * @throws Exception on error
+     */
+    public void testGetArchivesInDir() throws Exception
+    {
+        logger_.info("Running testGetArchivesInDir..."); 
+        
+        FindClass finder = new FindClass();
+        List archives = finder.getArchivesInDir(FileUtil.getTempDir());
+        
+        logger_.info(archives.size() + " found in temp dir.");
+    }
+     
+    
+    /**
+     * Test canceling a search.
+     * 
+     * @throws Exception on error
+     */
+    public void testCancelSearch() throws Exception
+    {
+        logger_.info("Running testCancelSearch...");
+        
+        FindClass finder = new FindClass();
+        SearchListener listener = new SearchListener();
+        finder.addSearchListener(listener);
+        
+        logger_.info("Before search...");
+        
+        ThreadUtil.run(
+                finder, 
+                "findClass", 
+                new Object[] { "xxx", new Boolean(false) });
+        
+        logger_.info("Giving finder a head start..");
+        ThreadUtil.sleep(5000); 
+        
+        logger_.info("Before cancel..");
+        finder.cancelSearch();
+        logger_.info("After cancelled..");
+        
+        listener.waitForCancel();
+        
+        finder.removeSearchListener(listener);
+        finder.removeSearchListeners();
+    }       
+    
+    //--------------------------------------------------------------------------
+    // SearchListener 
+    //--------------------------------------------------------------------------
+    
+    class SearchListener implements FindClassListener
+    {
+        Mutex cancel = new Mutex();
+        Mutex first = new Mutex();
+        
+        public SearchListener() throws InterruptedException
+        {
+            cancel.acquire();
+            first.acquire();
+        }
+        
+        public void waitForCancel() throws InterruptedException
+        {
+            cancel.acquire();
+        }
+        
+        public void waitForFirst() throws InterruptedException
+        {
+            first.acquire();
+        }
+        
+        /**
+		 * @see toolbox.findclass.FindClassListener#searchCancelled()
+		 */
+        public void searchCancelled()
+        {
+            logger_.info("SearchCanceled");
+            cancel.release();
+        }
+
+        /**
+		 * @see toolbox.findclass.FindClassListener#searchCompleted(
+         *      java.lang.String)
+		 */
+        public void searchCompleted(String search)
+        {
+            logger_.info("SearchCompleted " + search);
+        }
+
+        /**
+		 * @see toolbox.findclass.FindClassListener#classFound(
+         *      toolbox.findclass.FindClassResult)
+		 */
+        public void classFound(FindClassResult searchResult)
+        {
+            logger_.info("ClassFound " + searchResult.getClassFQN());
+        }
+
+        /**
+		 * @see toolbox.findclass.FindClassListener#searchingTarget(
+         *      java.lang.String)
+		 */
+        public void searchingTarget(String target)
+        {
+            logger_.info("SearchingTarget " + target);
+            first.release();
+        }
     }
 }
