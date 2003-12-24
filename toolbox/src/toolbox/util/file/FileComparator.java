1079f5fc7d55a390b484aec861ff794f33deb11d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-24 06:29:41 +0000

    Added ability to compare files by content.

--- toolbox/src/toolbox/util/file/FileComparator.java
+++ toolbox/src/toolbox/util/file/FileComparator.java
@@ -1,8 +1,15 @@
 package toolbox.util.file;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Comparator;
 import java.util.Date;
+
+import junit.framework.Assert;
+
+import toolbox.util.StreamUtil;
 
 /**
  * A {@java.io.File} comparator that can compare on varios file attributes.
@@ -15,12 +22,36 @@ import java.util.Date;
  */
 public class FileComparator implements Comparator
 {
-    public static final int COMPARE_NAME = 1; // File name
-    public static final int COMPARE_SIZE = 2; // File size
-    public static final int COMPARE_DATE = 3; // File timestamp
+    //--------------------------------------------------------------------------
+    // Constants
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Compare by file name (case insensetive).
+     */
+    public static final int COMPARE_NAME = 1;
+    
+    /**
+     * Compare by file size.
+     */
+    public static final int COMPARE_SIZE = 2;
+    
+    /**
+     * Compare by file timestamp.
+     */
+    public static final int COMPARE_DATE = 3;
+
+    /**
+     * Compare by file contents.
+     */
+    public static final int COMPARE_CONTENTS = 4;
+    
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
     
 	/**
-	 * Field to compare by. See COMPARE_* constants.
+	 * Field to compare by.
 	 */    
     private int compareBy_;
     
@@ -31,12 +62,15 @@ public class FileComparator implements Comparator
     /**
      * Creates a FileComparator.
      * 
-     * @param compareBy File attribute to base comparision on. 
-     *        Use FileComparator.COMPARE_[NAME|SIZE|DATE]
+     * @param compareBy Method by which to compare the files. 
+     *        Use FileComparator.COMPARE_[NAME|SIZE|DATE|CONTENTS]
      */
     public FileComparator(int compareBy)
     {
-        // TODO: bounds checking
+        // Check bounds...
+        Assert.assertTrue(compareBy >= COMPARE_NAME);
+        Assert.assertTrue(compareBy <= COMPARE_CONTENTS);
+        
         compareBy_ = compareBy;
     }
     
@@ -48,6 +82,7 @@ public class FileComparator implements Comparator
      * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
      */
     public int compare(Object a, Object b)
+
     {
         int result;
         
@@ -84,13 +119,94 @@ public class FileComparator implements Comparator
                 break;
 
                 
+            case COMPARE_CONTENTS :
+                
+                result = compareByContents(fileA, fileB);
+                break;
+                
+                
             default:
             
                 throw new IllegalArgumentException(
-                    "FileComparator does not support comparisons by " + 
+                    "File comparator does not support comparisons by " + 
                     compareBy_); 
         }
         
         return result;
+    }
+
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
+    
+    /**
+	 * Does a byte by byte comparison of the two files.
+	 * 
+	 * @param f1 File object 1
+	 * @param f2 File object 2
+	 */
+    protected int compareByContents(File f1, File f2)
+    {
+
+        if ((f1 == f2) || (f1 != null && f1.equals(f2)))
+            return 0;
+        
+        if (f1 == null)
+            return -1;
+        
+        if (f2 == null)
+            return 1;
+
+        if (f1.equals(f2))
+            return 0;
+
+        if (!f1.exists() || !f1.isFile())
+            throw new IllegalArgumentException(
+                "File '" + f1 + "' does not exist");
+        
+        if (!f2.exists() || !f2.isFile())
+            throw new IllegalArgumentException(
+                "File '" + f2 + "' does not exist");
+
+        if (f1.length() != f2.length())
+            return (int) (f1.length() - f2.length());
+
+        FileInputStream is1 = null;
+        FileInputStream is2 = null;
+        
+        try
+        {
+            is1 = new FileInputStream(f1);
+            is2 = new FileInputStream(f2);
+
+            int b1 = -2;
+            int b2 = -2;
+            
+            try
+            {
+                while ((b1 = is1.read()) != -1 && (b2 = is2.read()) != -1)
+                {
+                    if (b1 != b2)
+                        return b1 - b2;
+                    
+                    b1 = b2 = -2;
+                }
+            }
+            catch (IOException io)
+            {
+                return b1 == -2 ? -1 : 1;
+            }
+            finally
+            {
+                StreamUtil.close(is1);
+                StreamUtil.close(is2);
+            }
+            
+            return 0;
+        }
+        catch (FileNotFoundException fnf)
+        {
+            return is1 == null ? -1 : 1;
+        }
     }
 }
