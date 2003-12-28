344c11cfd736ab4516db9e9c7e877dbe59145d07
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2003-12-28 15:56:01 +0000

    Added font overrides for all components.

--- trunk/toolbox/src/toolbox/util/ui/plaf/ActivateAlloyLookAndFeelAction.java
+++ trunk/toolbox/src/toolbox/util/ui/plaf/ActivateAlloyLookAndFeelAction.java
@@ -1,10 +1,8 @@
 package toolbox.util.ui.plaf;
 
-import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.UIManager;
-import javax.swing.border.LineBorder;
 
 import com.incors.plaf.alloy.AlloyLookAndFeel;
 import com.incors.plaf.alloy.AlloyTheme;
@@ -79,16 +77,57 @@ public class ActivateAlloyLookAndFeelAction extends ActivateLookAndFeelAction
         UIManager.setLookAndFeel(alloy);
 
         // Fix alloy fonts
-        Font f = new Font("Tahoma", Font.PLAIN, 11);
+        
+        Font f = new Font("Bitstream Vera Sans", Font.PLAIN, 11);
+        //Font f = new Font("Trebuchet MS", Font.PLAIN, 11);
+        //Font f = new Font("Lucida Sans", Font.PLAIN, 11);
         //Font f = FontUtil.getPreferredSerifFont();
-        UIManager.put("Label.font", f);
+        //Font f = FontUtil.getPreferredMonoFont();
+        //Font f = new Font("Lucida Sans Typewriter", Font.PLAIN, 10);
+        
         UIManager.put("Button.font", f);
+        UIManager.put("DesktopIcon.font", f);
         UIManager.put("ComboBox.font", f);
         UIManager.put("CheckBox.font", f);
-        UIManager.put("ToolTip.border", new LineBorder(Color.black));
+        UIManager.put("CheckBoxMenuItem.font", f);
+        UIManager.put("ColorChooser.font", f);
+        UIManager.put("EditorPane.font", f);
+        UIManager.put("FormattedTextField.font", f);
+        UIManager.put("InternalFrame.titleFont", f);
+        UIManager.put("Label.font", f);
+        UIManager.put("List.font", f);
+        UIManager.put("Menu.font", f);
+        UIManager.put("MenuBar.font", f);
+        UIManager.put("MenuItem.font", f);
+        UIManager.put("MenuItem.acceleratorFont", f);
+        UIManager.put("OptionPane.font", f);
+        UIManager.put("Panel.font", f);
+        UIManager.put("PasswordField.font", f);
+        UIManager.put("PopupMenu.font", f);
+        UIManager.put("ProgressBar.font", f);
+        UIManager.put("RadioButton.font", f);
+        UIManager.put("RadioButtonMenuItem.font", f);
+        UIManager.put("RadioButtonMenuItem.acceleratorFont", f);
+        UIManager.put("RootPane.titleFont", f);
+        UIManager.put("ScrollPane.font", f);
+        UIManager.put("Spinner.font", f);
+        UIManager.put("TabbedPane.font", f);
+        UIManager.put("Table.font", f);
+        UIManager.put("TableHeader.font", f);
+        UIManager.put("TextArea.font", f);
+        UIManager.put("TextField.font", f);
+        UIManager.put("TextPane.font", f);
+        UIManager.put("TitledBorder.font", f);
+        UIManager.put("ToggleButton.font", f);
+        UIManager.put("Toolbar.font", f);
+        UIManager.put("ToolTip.font", f);
+        UIManager.put("Tree.font", f);
+        UIManager.put("Viewport.font", f);
         
-        UIManager.put(
-            "ToolTip.background", UIManager.getColor("Panel.background"));
+        //UIManager.put("ToolTip.border", new LineBorder(Color.black));
+        
+        //UIManager.put(
+        //    "ToolTip.background", UIManager.getColor("Panel.background"));
         
         UIManager.getLookAndFeel().getDefaults().put(
             LAFInfo.PROP_HIDDEN_KEY, getLookAndFeelInfo());
