172d9b87f6939e463203896e3177f620858df9d5
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-05 20:31:22 +0000

    Code cleanup

--- toolbox/src/toolbox/util/ui/plaf/ActivateSkinLookAndFeelAction.java
+++ toolbox/src/toolbox/util/ui/plaf/ActivateSkinLookAndFeelAction.java
@@ -1,7 +1,5 @@
 package toolbox.util.ui.plaf;
 
-import javax.swing.UIManager.LookAndFeelInfo;
-
 import com.l2fprod.gui.plaf.skin.Skin;
 import com.l2fprod.gui.plaf.skin.SkinLookAndFeel;
 
@@ -17,47 +15,15 @@ public class ActivateSkinLookAndFeelAction extends ActivateLookAndFeelAction
     private static final Logger logger_ =
         Logger.getLogger(ActivateSkinLookAndFeelAction.class);
     
-    /**
-     * Maps the Look And Feel name to the appropriate SkinLF themepack.
-     */
-//    private static Map themeMap_;
-    
-//    static
-//    {
-//        themeMap_ = new HashMap();
-//        themeMap_.put("Aqua", "skinlf/aqua.zip");
-//        themeMap_.put("BBJ", "skinlf/bbj.zip");
-//        themeMap_.put("Beos", "skinlf/beos.zip");
-//        themeMap_.put("Cell Shaded", "skinlf/cellshaded.zip");
-//        themeMap_.put("Mac", "skinlf/macos.zip");
-//        themeMap_.put("Modern", "skinlf/modern.zip");
-//        themeMap_.put("Toxic", "skinlf/toxic.zip");
-//        themeMap_.put("Whistler", "skinlf/whistler.zip");
-//        themeMap_.put("XP", "skinlf/xpluna.zip");
-//        themeMap_.put("SkinLF", "skinlf/toxic.zip");
-//    }
-
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
 
     /**
      * Creates a ActivateSkinLookAndFeelAction.
-     *
-     * @param info
      */
     public ActivateSkinLookAndFeelAction()
     {
-    }
-    
-    /**
-     * Creates a ActivateSkinLookAndFeelAction.
-     *
-     * @param info
-     */
-    public ActivateSkinLookAndFeelAction(LookAndFeelInfo info)
-    {
-        //super(info);
     }
 
     //--------------------------------------------------------------------------
@@ -65,13 +31,15 @@ public class ActivateSkinLookAndFeelAction extends ActivateLookAndFeelAction
     //--------------------------------------------------------------------------
     
     /**
-     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#setLookAndFeelInfo(toolbox.util.ui.plaf.LAFInfo)
+     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#setLookAndFeelInfo(
+     *      toolbox.util.ui.plaf.LAFInfo)
      */
     public void setLookAndFeelInfo(LAFInfo lookAndFeelInfo)
     {
         super.setLookAndFeelInfo(lookAndFeelInfo);
         setName(lookAndFeelInfo.getProperty("theme.name"));
     }
+    
     
     /**
      * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#activate()
