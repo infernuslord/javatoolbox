7a478a61dd9428cc518d3062a325cab9a63063d3
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-05 06:31:02 +0000

    Removed redundant public access modifiers

--- trunk/toolbox/src/toolbox/tail/TailListener.java
+++ trunk/toolbox/src/toolbox/tail/TailListener.java
@@ -1,57 +1,63 @@
 package toolbox.tail;
 
 /**
- * Listener interface for tail generated events
+ * Listener interface for tail generated events.
  */
 public interface TailListener 
 {
     /**
-     * Tail has started
+     * Tail has started.
      * 
-     * @param tail Tail that was started
+     * @param tail Tail that was started.
      */
-    public void tailStarted(Tail tail);
+    void tailStarted(Tail tail);
+    
     
     /**
-     * Tail has stopped 
+     * Tail has stopped. 
      * 
-     * @param tail Tail that was stopped
+     * @param tail Tail that was stopped.
      */
-    public void tailStopped(Tail tail);
+    void tailStopped(Tail tail);
+    
     
     /** 
-     * Tail has ended
+     * Tail has ended.
      * 
-     * @param tail Tail that ended
+     * @param tail Tail that ended.
      */
-    public void tailEnded(Tail tail);
+    void tailEnded(Tail tail);
+    
     
     /**
-     * Tail has been paused
+     * Tail has been paused.
      * 
-     * @param tail that was paused 
+     * @param tail that was paused. 
      */
-    public void tailPaused(Tail tail);
+    void tailPaused(Tail tail);
+    
     
     /**
-     * Tail has been unpaused
+     * Tail has been unpaused.
      * 
-     * @param tail Tail that was unpaused
+     * @param tail Tail that was unpaused.
      */
-    public void tailUnpaused(Tail tail);
+    void tailUnpaused(Tail tail);
+    
     
     /**
-     * Tail re-attached to source
+     * Tail re-attached to source.
      * 
-     * @param tail Tail that was reattached to its source
+     * @param tail Tail that was reattached to its source.
      */
-    public void tailReattached(Tail tail);
+    void tailReattached(Tail tail);
+    
     
     /**
-     * Notification that the next line from the tail is available
+     * Notification that the next line from the tail is available.
      * 
-     * @param tail Tail that received a new line of text
-     * @param line Next line from the tailed stream
+     * @param tail Tail that received a new line of text.
+     * @param line Next line from the tailed stream.
      */
-    public void nextLine(Tail tail, String line);
+    void nextLine(Tail tail, String line);
 }
