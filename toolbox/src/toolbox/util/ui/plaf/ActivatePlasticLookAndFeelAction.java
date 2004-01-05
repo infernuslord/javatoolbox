ea7caf0a2533e76fd3e4fc822c3169b641bad90f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-05 20:23:36 +0000

    Javadoc updates and cleanup.

--- toolbox/src/toolbox/util/ui/plaf/ActivatePlasticLookAndFeelAction.java
+++ toolbox/src/toolbox/util/ui/plaf/ActivatePlasticLookAndFeelAction.java
@@ -6,7 +6,7 @@ import com.jgoodies.plaf.plastic.PlasticTheme;
 import org.apache.log4j.Logger;
 
 /**
- * Action that sets the plastic theme.
+ * Action that activates the plastic look and feel.
  */    
 public class ActivatePlasticLookAndFeelAction extends ActivateLookAndFeelAction
 {
@@ -24,23 +24,13 @@ public class ActivatePlasticLookAndFeelAction extends ActivateLookAndFeelAction
     {
     }
     
-    /**
-     * Creates an ActivatePlasticLookAndFeelAction.
-     * 
-     * @param theme Theme to activate.
-     */
-    public ActivatePlasticLookAndFeelAction(PlasticTheme theme)
-    {
-        //super(theme.getName(), false, null, null);
-        //theme_ = theme;
-    }
-
     //--------------------------------------------------------------------------
     // Overrides ActivateLookAndFeelAction
     //--------------------------------------------------------------------------
 
     /**
-     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#setLookAndFeelInfo(toolbox.util.ui.plaf.LAFInfo)
+     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#setLookAndFeelInfo(
+     *      toolbox.util.ui.plaf.LAFInfo)
      */
     public void setLookAndFeelInfo(LAFInfo info)
     {
@@ -55,16 +45,10 @@ public class ActivatePlasticLookAndFeelAction extends ActivateLookAndFeelAction
     public void activate() throws Exception
     {
         LAFInfo info = getLookAndFeelInfo();
-     
-        String name  = info.getProperty("theme.name");
+        //String name  = info.getProperty("theme.name");
         String clazz = info.getProperty("theme.class");
-        
         PlasticTheme theme = (PlasticTheme) Class.forName(clazz).newInstance();
         PlasticLookAndFeel.setMyCurrentTheme(theme);
-        
-        //UIManager.setLookAndFeel(UIManager.getLookAndFeel());
-        //LookAndFeelUtil.propagateChangeInLAF();
-        
         super.activate();
     }
 }
