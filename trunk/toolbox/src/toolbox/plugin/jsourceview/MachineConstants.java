0e65f691b199be41274c38a01958b7f44b20eb7a
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-06 23:25:10 +0000

    Checkstyle updates

--- trunk/toolbox/src/toolbox/plugin/jsourceview/MachineConstants.java
+++ trunk/toolbox/src/toolbox/plugin/jsourceview/MachineConstants.java
@@ -6,56 +6,56 @@ package toolbox.jsourceview;
 public interface MachineConstants
 {
     /** Start state */
-    public static final int STARTSTATE = 0;
+    int STARTSTATE = 0;
     
     /** State 1 */
-    public static final int STATE1 = 1;
+    int STATE1 = 1;
     
     /** State 2 */
-    public static final int STATE2 = 2;
+    int STATE2 = 2;
     
     /** State 3 */
-    public static final int STATE3 = 3;
+    int STATE3 = 3;
     
     /** State 4 */
-    public static final int STATE4 = 4;
+    int STATE4 = 4;
     
     /** State 5 */
-    public static final int STATE5 = 5;
+    int STATE5 = 5;
     
     /** State 6 */
-    public static final int STATE6 = 6;
+    int STATE6 = 6;
     
     /** State 7 */
-    public static final int STATE7 = 7;
+    int STATE7 = 7;
     
     /** State 8 */
-    public static final int STATE8 = 8;
+    int STATE8 = 8;
     
     /** EndState */
-    public static final int ENDSTATE = 9;
+    int ENDSTATE = 9;
     
     /** Any character */
-    public static final int ANY_CHAR = 0;
+    int ANY_CHAR = 0;
     
     /** End of line */
-    public static final int EOL = 1;
+    int EOL = 1;
     
     /** Line comment */
-    public static final int LINE_COMMENT = 2;
+    int LINE_COMMENT = 2;
     
     /** Begin of comment */
-    public static final int COMMENT_BEGIN = 3;
+    int COMMENT_BEGIN = 3;
     
     /** End of comment */
-    public static final int COMMENT_END = 4;
+    int COMMENT_END = 4;
     
     /** Line comment string */
-    public static final String STR_LINE_COMMENT = "//";
+    String STR_LINE_COMMENT = "//";
     
     /** Comment begin string */
-    public static final String STR_COMMENT_BEGIN = "/*";
+    String STR_COMMENT_BEGIN = "/*";
     
     /** Comment end string */
-    public static final String STR_COMMENT_END = "*/";
+    String STR_COMMENT_END = "*/";
 }
