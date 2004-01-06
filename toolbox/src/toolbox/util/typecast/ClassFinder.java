454aa61591af00aa5950a3c0b6af19527e4e0db5
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-06 21:08:45 +0000

    Javadoc updates

--- toolbox/src/toolbox/util/typecast/ClassFinder.java
+++ toolbox/src/toolbox/util/typecast/ClassFinder.java
@@ -47,15 +47,29 @@ import toolbox.util.collections.LRUMap;
  */
 public class ClassFinder
 {
+    //--------------------------------------------------------------------------
+    // Fields 
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Search packages.
+     */
     private Set additionalSearchPackages_ = new HashSet(10);
     
     /** 
-     * Create an LRUMap with a max size of 10000 and a timelimit of 60 minutes 
+     * Create an LRUMap with a max size of 10000 and a timelimit of 60 minutes. 
      */
     private Map cache_ = 
         Collections.synchronizedMap(new LRUMap(1000, 60 * 60000));
-        
+    
+    /**
+     * Search string.
+     */
     private String search_;
+    
+    /**
+     * Replacement string.
+     */
     private String replace_;
 
     //--------------------------------------------------------------------------
@@ -63,17 +77,18 @@ public class ClassFinder
     //--------------------------------------------------------------------------
     
     /**
-     * Default constructor
+     * Creates a ClassFinder.
      */
     public ClassFinder()
     {
     }
 
+    
     /**
-     * Constructor
+     * Creates a ClassFinder.
      * 
-     * @param  search  Search string
-     * @param  replace Replace string
+     * @param search Search string.
+     * @param replace Replace string.
      */
     public ClassFinder(String search, String replace)
     {
@@ -86,50 +101,54 @@ public class ClassFinder
     //--------------------------------------------------------------------------
 
     /**
-     * Adds package to search
-     * 
-     * @param  pckgName  Package name
-     */
-    public void addSearchPackage(String pckgName)
+	 * Adds package to search.
+	 * 
+	 * @param pkgName Package name.
+	 */
+    public void addSearchPackage(String pkgName)
     {
-        additionalSearchPackages_.add(pckgName);
+        additionalSearchPackages_.add(pkgName);
     }
 
+    
     /**
-     * Adds a classes package to the search
-     * 
-     * @param  aClassInPackage  Class with package to add
-     */
+	 * Adds a classes package to the search.
+	 * 
+	 * @param aClassInPackage Class with package to add.
+	 */
     public void addSearchPackage(Class aClassInPackage)
     {
         additionalSearchPackages_.add(getPackageName(aClassInPackage));
     }
 
+    
     /**
-     * Finds a class
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @return Class found
-     * @throws ClassNotFoundException when no class found
-     */
+	 * Finds a class.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @return Class found.
+	 * @throws ClassNotFoundException when no class found.
+	 */
     public Class findClass(Class fromClass, Class toClass)
         throws ClassNotFoundException
     {
         return findClass(fromClass, toClass, null);
     }
 
+    
     /**
-     * Finds a class
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @param  prefix     Prefix
-     * @return Class if found
-     * @throws ClassNotFoundException when no class found
-     */
-    public Class findClass(Class fromClass, Class toClass, String prefix)
-        throws ClassNotFoundException
+	 * Finds a class.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @param prefix Prefix.
+	 * @return Class if found.
+	 * @throws ClassNotFoundException when no class found.
+	 */
+    public Class findClass(Class fromClass, 
+                           Class toClass, 
+                           String prefix) throws ClassNotFoundException
     {
 
         return findClass(
@@ -139,20 +158,22 @@ public class ClassFinder
             Thread.currentThread().getContextClassLoader());
     }
 
+    
     /**
-     * Finds a class
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @param  prefix     Prefix
-     * @param  loader     Classloader to search
-     * @return Class if found
-     * @throws ClassNotFoundException when no class found
-     */
-    public Class findClass(Class fromClass, Class toClass, String prefix,
-        ClassLoader loader) throws ClassNotFoundException
+	 * Finds a class.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @param prefix Prefix.
+	 * @param loader Classloader to search.
+	 * @return Class if found.
+	 * @throws ClassNotFoundException when no class found.
+	 */
+    public Class findClass(Class fromClass, 
+                           Class toClass, 
+                           String prefix,
+						   ClassLoader loader) throws ClassNotFoundException
     {
-
         // Check Cache
         Class fClass = checkCache(fromClass, toClass, prefix, loader);
 
@@ -201,19 +222,22 @@ public class ClassFinder
     //--------------------------------------------------------------------------
 
     /**
-     * Checks the cache for a class
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @param  prefix     Prefix
-     * @param  loader     Classloader to search
-     * @return Class if found
-     * @throws ClassNotFoundException when no class found
-     */
-    protected Class checkCache(Class fromClass, Class toClass, String prefix,
-        ClassLoader loader) throws ClassNotFoundException
+	 * Checks the cache for a class.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @param prefix Prefix.
+	 * @param loader Classloader to search.
+	 * @return Class if found.
+	 * @throws ClassNotFoundException when no class found.
+	 */
+    protected Class checkCache(
+        Class fromClass,
+        Class toClass,
+        String prefix,
+        ClassLoader loader)
+        throws ClassNotFoundException
     {
-
         String className =
             (String)cache_.get(getCacheKey(fromClass, toClass, prefix, loader));
 
@@ -223,45 +247,55 @@ public class ClassFinder
         return null;
     }
 
+    
     /**
-     * Checks the cache for a class
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @param  prefix     Prefix
-     * @param  loader     Classloader to search
-     * @return Key
-     */
-    protected Object getCacheKey(Class fromClass, Class toClass, String prefix,
+	 * Checks the cache for a class.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @param prefix Prefix.
+	 * @param loader Classloader to search.
+	 * @return Key
+	 */
+    protected Object getCacheKey(
+        Class fromClass,
+        Class toClass,
+        String prefix,
         ClassLoader loader)
     {
         return fromClass.getName() + toClass.getName() + 
             (prefix == null ? "" : prefix) + System.identityHashCode(loader);
     }
 
+    
     /**
-     * Puts to the cache
-     * 
-     * @param  fromClass  From a class
-     * @param  toClass    To a class
-     * @param  prefix     Prefix
-     * @param  loader     Classloader to search
-     * @param  classname  Name of class
-     */
-    protected void putCache(Class fromClass, Class toClass, String prefix,
-        ClassLoader loader, String className)
+	 * Puts to the cache.
+	 * 
+	 * @param fromClass From a class.
+	 * @param toClass To a class.
+	 * @param prefix Prefix.
+	 * @param loader Classloader to search.
+	 * @param classname Name of class.
+	 */
+    protected void putCache(
+        Class fromClass,
+        Class toClass,
+        String prefix,
+        ClassLoader loader,
+        String className)
     {
         cache_.put(getCacheKey(fromClass, toClass, prefix, loader), className);
     }
 
+    
     /**
-     * Combine all of the fromClass packages and their heirarchy along with the 
-     * toClass heirarchy.
-     * 
-     * @param  fromClass  From class
-     * @param  toClass    To class
-     * @return List of packages
-     */
+	 * Combine all of the fromClass packages and their heirarchy along with the
+	 * toClass heirarchy.
+	 * 
+	 * @param fromClass From class.
+	 * @param toClass To class.
+	 * @return List of packages.
+	 */
     protected List getAllPackages(Class fromClass, Class toClass)
     {
         List allPackages = getPackageNames(getClassHierarchy(fromClass));
@@ -274,12 +308,13 @@ public class ClassFinder
         return allPackages;
     }
 
+    
     /**
-     * Gets a classes' hierarchy
-     * 
-     * @param  aClass  Class
-     * @return List of classes in hierarchy
-     */
+	 * Gets a classes' hierarchy.
+	 * 
+	 * @param aClass Class.
+	 * @return List of classes in hierarchy.
+	 */
     protected static List getClassHierarchy(Class aClass)
     {
         List classes = new ArrayList(20);
@@ -292,19 +327,22 @@ public class ClassFinder
         return classes;
     }
 
+    
     /**
-     * Gets list of classes combinations
-     *
-     * @param  packages         List of packages
-     * @param  fromClassNames   List of names for from classes
-     * @param  toClassName      Name of To class
-     * @param  prefix           Prefix
-     * @return  List of classes matching combination
-     */
-    protected List getCombinations(List packages, List fromClassNames,
-        String toClassName, String prefix)
+	 * Gets list of classes combinations.
+	 * 
+	 * @param packages List of packages.
+	 * @param fromClassNames List of names for from classes.
+	 * @param toClassName Name of To class.
+	 * @param prefix Prefix.
+	 * @return List of classes matching combination.
+	 */
+    protected List getCombinations(
+        List packages,
+        List fromClassNames,
+        String toClassName,
+        String prefix)
     {
-
         List classNames =
             new ArrayList(packages.size() * fromClassNames.size());
 
@@ -332,15 +370,15 @@ public class ClassFinder
         return classNames;
     }
 
+    
     /**
-     * Gets list of package names given a list of classes
-     * 
-     * @param  classes  List of classes
-     * @return List of package names for those classes
-     */
+	 * Gets list of package names given a list of classes.
+	 * 
+	 * @param classes List of classes.
+	 * @return List of package names for those classes.
+	 */
     protected List getPackageNames(List classes)
     {
-
         List pckgs = new ArrayList(classes.size());
 
         for (Iterator i = classes.iterator(); i.hasNext();)
@@ -354,11 +392,12 @@ public class ClassFinder
         return pckgs;
     }
 
+    
     /**
-     * Transposes a stringbuffer
-     * 
-     * @param  name  Name to transpose
-     */
+	 * Transposes a stringbuffer.
+	 * 
+	 * @param name Name to transpose.
+	 */
     protected void transpose(StringBuffer name)
     {
         if (search_ == null || replace_ == null)
@@ -375,34 +414,36 @@ public class ClassFinder
     //--------------------------------------------------------------------------
 
     /**
-     * Retrieves classes package name
-     * 
-     * @param  aClass  Class to get package name of
-     * @return Classes packagename
-     */
+	 * Retrieves classes package name.
+	 * 
+	 * @param aClass Class to get package name of.
+	 * @return Classes packagename.
+	 */
     public static String getPackageName(Class aClass)
     {
         Package pckg = aClass.getPackage();
         return pckg == null ? removePackage(aClass.getName()) : pckg.getName();
     }
 
+    
     /**
-     * Capitalizes class name
-     * 
-     * @param  name  Name to capitalize
-     * @return Capitalized name
-     */
+	 * Capitalizes class name.
+	 * 
+	 * @param name Name to capitalize.
+	 * @return Capitalized name.
+	 */
     public static String capitalize(String name)
     {
         return Character.toUpperCase(name.charAt(0)) + name.substring(1);
     }
 
+    
     /**
-     * Gets a classes class name
-     * 
-     * @param  aClass  Class to get name of
-     * @return Class name without the package qualifier
-     */
+	 * Gets a classes class name.
+	 * 
+	 * @param aClass Class to get name of.
+	 * @return Class name without the package qualifier.
+	 */
     public static String getClassName(Class aClass)
     {
         String name = aClass.getName();
@@ -411,12 +452,13 @@ public class ClassFinder
         return index > 0 ? name.substring(index + 1) : name;
     }
 
+    
     /**
-     * Removes the package from a name
-     * 
-     * @param  name  Name to remove package from
-     * @return Name with package removed
-     */
+	 * Removes the package from a name.
+	 * 
+	 * @param name Name to remove package from.
+	 * @return Name with package removed.
+	 */
     protected static String removePackage(String name)
     {
         int index = name.lastIndexOf('.');
@@ -424,12 +466,13 @@ public class ClassFinder
         return index > 0 ? name.substring(0, index) : name;
     }
 
+    
     /**
-     * Gets list of class names in a classes hierarchy
-     * 
-     * @param  aClass  Class to get hierarchy names from
-     * @return List of class names in hierarchy
-     */
+	 * Gets list of class names in a classes hierarchy.
+	 * 
+	 * @param aClass Class to get hierarchy names from.
+	 * @return List of class names in hierarchy.
+	 */
     protected static List getClassNames(Class aClass)
     {
         List classes = getClassHierarchy(aClass);
--- toolbox/src/toolbox/util/typecast/TypecastException.java
+++ toolbox/src/toolbox/util/typecast/TypecastException.java
@@ -4,70 +4,92 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 
 /**
- * TypeCase exception
+ * Typecast exception.
  */
 public class TypecastException extends RuntimeException
 {
+    //--------------------------------------------------------------------------
+    // Fields 
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Root cause of the error.
+     */
     private Throwable rootCause_;
 
+    //--------------------------------------------------------------------------
+    // Constructors 
+    //--------------------------------------------------------------------------
+    
     /**
-     * Constructor
-     * 
-     * @param  message  Exception message
-     */ 
+	 * Creates a TypecastException.
+	 * 
+	 * @param message Exception message.
+	 */
     public TypecastException(String message)
     {
         super(message);
     }
 
+    
     /**
-     * Constructor
-     * 
-     * @param  message      Exception message
-     * @param  rootCause    Exception at root cause
-     */ 
+	 * Creates a TypecastException.
+	 * 
+	 * @param message Exception message.
+	 * @param rootCause Exception at root cause.
+	 */
     public TypecastException(String message, Throwable rootCause)
     {
         super(message);
         rootCause_ = rootCause;
     }
 
+    //--------------------------------------------------------------------------
+    // Public 
+    //--------------------------------------------------------------------------
+    
     /**
-     * Prints stack trace to system.out
-     */
+	 * Prints stack trace to system.out.
+	 */
     public void printStackTrace()
     {
         printStackTrace(System.out);
     }
 
+    
     /**
-     * Prints stack trace to a print stream
-     * 
-     * @param  s  PrintStream
-     */
+	 * Prints stack trace to a print stream.
+	 * 
+	 * @param s PrintStream.
+	 */
     public void printStackTrace(PrintStream s)
     {
         if (rootCause_ != null)
             rootCause_.printStackTrace(s);
-            
+
         rootCause_.printStackTrace(s);
     }
 
+    
     /**
-     * Prints stack trace to a Writer
-     * 
-     * @param  s  PrintWriter
-     */
+	 * Prints stack trace to a Writer.
+	 * 
+	 * @param s PrintWriter.
+	 */
     public void printStackTrace(PrintWriter s)
     {
         if (rootCause_ != null)
             rootCause_.printStackTrace(s);
-            
+
         rootCause_.printStackTrace(s);
     }
 
+    //--------------------------------------------------------------------------
+    // Overrides java.lang.Object 
+    //--------------------------------------------------------------------------
+    
     /**
-     * @return  String representation
+     * @see java.lang.Object#toString()
      */
     public String toString()
     {
