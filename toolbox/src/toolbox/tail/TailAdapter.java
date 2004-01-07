f25aa9fd18b04e7401833b4eb914cc84cbafe59c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-07 11:56:24 +0000

    Checkstyle updates

--- toolbox/src/toolbox/tail/TailAdapter.java
+++ toolbox/src/toolbox/tail/TailAdapter.java
@@ -5,49 +5,49 @@ package toolbox.tail;
  */
 public class TailAdapter implements TailListener
 {
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailStarted(toolbox.tail.Tail)
      */
     public void tailStarted(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailStopped(toolbox.tail.Tail)
      */
     public void tailStopped(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailEnded(toolbox.tail.Tail)
      */
     public void tailEnded(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailPaused(toolbox.tail.Tail)
      */
     public void tailPaused(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailUnpaused(toolbox.tail.Tail)
      */
     public void tailUnpaused(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#tailReattached(toolbox.tail.Tail)
      */
     public void tailReattached(Tail tail)
     {
     }
 
-    /*
+    /**
      * @see toolbox.tail.TailListener#nextLine(toolbox.tail.Tail, 
      *      java.lang.String)
      */
