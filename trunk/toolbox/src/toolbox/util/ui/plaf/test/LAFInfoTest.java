a19bc7d2b0fdfc0ddd362ead41a305279d904d64
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-12 21:51:12 +0000

    Added perf test for equals but its commented out

--- trunk/toolbox/src/toolbox/util/ui/plaf/test/LAFInfoTest.java
+++ trunk/toolbox/src/toolbox/util/ui/plaf/test/LAFInfoTest.java
@@ -7,6 +7,7 @@ import nu.xom.Element;
 
 import org.apache.log4j.Logger;
 
+import toolbox.util.ElapsedTime;
 import toolbox.util.StringUtil;
 import toolbox.util.XOMUtil;
 import toolbox.util.ui.plaf.LAFInfo;
@@ -90,6 +91,8 @@ public class LAFInfoTest extends TestCase
         Element root = XOMUtil.toElement(xml);
         LAFInfo info = new LAFInfo();
         info.applyPrefs(root);
+
+        logger_.info("toString:" + info);
         
         assertEquals("name", info.getName());
         assertEquals("class", info.getClassName());
@@ -133,46 +136,55 @@ public class LAFInfoTest extends TestCase
     /**
      * Tests the equals() method.
      */
-    public void testEquals()
+    public void xxtestEqualsPerformance()
     {
-        logger_.info("Running testEquals...");
+        logger_.info("Running testEqualsPerformance...");
         
-        // Equality to self
-        LAFInfo info = new LAFInfo();
-        info.setName("Foo");
-        info.setClassName("com.foo.Bar");
-        info.setAction("com.foo.Action");
-        info.getProperties().put("key", "value");
-        info.getProperties().put("xxx", "yyy");
-        assertTrue(info.equals(info));
+        ElapsedTime timer = new ElapsedTime();
         
+        for (int i=0; i<100000; i++)
+        {    
+            // Equality to self
+            LAFInfo info = new LAFInfo();
+            info.setName("Foo");
+            info.setClassName("com.foo.Bar");
+            info.setAction("com.foo.Action");
+            info.getProperties().put("key", "value");
+            info.getProperties().put("xxx", "yyy");
+            assertTrue(info.equals(info));
+            
+            
+            // Equality by content
+            LAFInfo replica = new LAFInfo();
+            replica.setName("Foo");
+            replica.setClassName("com.foo.Bar");
+            replica.setAction("com.foo.Action");
+            replica.getProperties().put("key", "value");
+            replica.getProperties().put("xxx", "yyy");
+            assertTrue(info.equals(replica));
+    
+            
+            // Equality by content but with props out of order
+            LAFInfo outOfOrder = new LAFInfo();
+            outOfOrder.setName("Foo");
+            outOfOrder.setClassName("com.foo.Bar");
+            outOfOrder.setAction("com.foo.Action");
+            outOfOrder.getProperties().put("xxx", "yyy");
+            outOfOrder.getProperties().put("key", "value");
+            assertTrue(info.equals(outOfOrder));
+            
+            // Inequality by property
+            LAFInfo different = new LAFInfo();
+            different.setName("Foo");
+            different.setClassName("com.foo.Bar");
+            different.setAction("com.foo.Action");
+            different.getProperties().put("key", "value");
+            different.getProperties().put("DIFFERENT", "PROPERTY");
+            assertTrue(!info.equals(different));
+        }
         
-        // Equality by content
-        LAFInfo replica = new LAFInfo();
-        replica.setName("Foo");
-        replica.setClassName("com.foo.Bar");
-        replica.setAction("com.foo.Action");
-        replica.getProperties().put("key", "value");
-        replica.getProperties().put("xxx", "yyy");
-        assertTrue(info.equals(replica));
-
+        timer.setEndTime();
         
-        // Equality by content but with props out of order
-        LAFInfo outOfOrder = new LAFInfo();
-        outOfOrder.setName("Foo");
-        outOfOrder.setClassName("com.foo.Bar");
-        outOfOrder.setAction("com.foo.Action");
-        outOfOrder.getProperties().put("xxx", "yyy");
-        outOfOrder.getProperties().put("key", "value");
-        assertTrue(info.equals(outOfOrder));
-        
-        // Inequality by property
-        LAFInfo different = new LAFInfo();
-        different.setName("Foo");
-        different.setClassName("com.foo.Bar");
-        different.setAction("com.foo.Action");
-        different.getProperties().put("key", "value");
-        different.getProperties().put("DIFFERENT", "PROPERTY");
-        assertTrue(!info.equals(different));
+        logger_.info("ElapsedTime: " + timer);
     }
 }
