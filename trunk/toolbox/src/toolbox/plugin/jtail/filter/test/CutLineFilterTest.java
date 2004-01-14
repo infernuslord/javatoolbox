5b8eaeec0c168098df11f83e3c21dc931ec1f18f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-14 07:17:05 +0000

    Added loads of tests to get 100% clover coverage.

--- trunk/toolbox/src/toolbox/plugin/jtail/filter/test/CutLineFilterTest.java
+++ trunk/toolbox/src/toolbox/plugin/jtail/filter/test/CutLineFilterTest.java
@@ -6,6 +6,7 @@ import junit.textui.TestRunner;
 import org.apache.log4j.Logger;
 
 import toolbox.jtail.filter.CutLineFilter;
+import toolbox.util.StringUtil;
 
 /**
  * Unit test for CutLineFilter.
@@ -66,5 +67,129 @@ public class CutLineFilterTest extends TestCase
         // Last char + subset
         filter.setCut("3-5");
         assertEquals("12", filter.filter("12345"));
+        
+        // Out of range
+        filter.setCut("20-30");
+        assertEquals("12345", filter.filter("12345"));
+        
+        // Overlapping out of range
+        filter.setCut("3-30");
+        assertEquals("12", filter.filter("12345"));
+        
+        logger_.info(filter.toString());
+    }
+    
+    
+    /**
+     * Tests the filter when disabled.
+     */
+    public void testFilterDisabled()
+    {
+        logger_.info("Running testFilterDisabled...");
+        
+        String s = "12345";
+        CutLineFilter filter = new CutLineFilter();
+        filter.setEnabled(false);
+        filter.setCut("1-1");        
+        assertEquals(s, filter.filter(s));
+    }
+    
+    
+    /**
+     * Tests the filter for null input.
+     */
+    public void testFilterNull()
+    {
+        logger_.info("Running testFilterNull...");
+        
+        CutLineFilter filter = new CutLineFilter();
+        filter.setEnabled(true);
+        filter.setCut("1-1");        
+        assertNull(filter.filter(null));
+    }
+    
+    
+    /**
+     * Tests the filter for empty input.
+     */
+    public void testFilterEmpty()
+    {
+        logger_.info("Running testFilterEmpty...");
+        
+        CutLineFilter filter = new CutLineFilter();
+        filter.setEnabled(true);
+        filter.setCut("1-1");        
+        assertEquals("", filter.filter(""));
+    }
+    
+    
+    /**
+     * Tests the filter for an invalid cut range.
+     */
+    public void testFilterInvalidRange()
+    {
+        logger_.info("Running testFilterInvalidRange...");
+        
+        CutLineFilter filter = new CutLineFilter();
+        filter.setEnabled(true);
+        
+        // Begin is greater than end.
+        try
+        {
+            String range = "4-2";
+            filter.setCut(range);
+            fail("Test should have failed for invalid range: " + range);
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Passed test.
+            logger_.debug("Induced error: " + iae.getMessage());
+        }
+        
+        // Begin is equal to end.
+        try
+        {
+            String range = "5-4";
+            filter.setCut(range);
+            logger_.debug(StringUtil.addBars(filter.toString()));
+            fail("Test should have failed for invalid range: " + range);
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Passed
+            logger_.debug("Induced error: " + iae.getMessage());
+        }
+        
+        // Not even a range
+        try
+        {
+            String range = "this should fail";
+            filter.setCut(range);
+            logger_.debug(StringUtil.addBars(filter.toString()));
+            fail("Test should have failed for invalid range: " + range);
+        }
+        catch (IllegalArgumentException iae)
+        {
+            // Passed
+            logger_.debug("Induced error: " + iae.getMessage());
+        }
+    }
+
+    
+    /**
+     * Tests setCut(null/empty) which should disable the filter.
+     */
+    public void testSetCutNullEmpty()
+    {
+        logger_.info("Running testSetCutNullEmpty...");
+        
+        CutLineFilter f = new CutLineFilter();
+        f.setEnabled(true);
+        f.setCut(null);
+        assertTrue(!f.isEnabled());
+        
+        f.setEnabled(true);
+        f.setCut("");
+        assertTrue(!f.isEnabled());
     }
 }
