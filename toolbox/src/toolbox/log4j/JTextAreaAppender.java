c8e1f180321c9f594cf0297bee935f8bc3df933d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-01-18 06:01:52 +0000

    Added another constructor so an existing JTextArea can be used.

--- toolbox/src/toolbox/log4j/JTextAreaAppender.java
+++ toolbox/src/toolbox/log4j/JTextAreaAppender.java
@@ -8,6 +8,7 @@ import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.PatternLayout;
 import org.apache.log4j.spi.LoggingEvent;
 
+import toolbox.util.SwingUtil;
 import toolbox.util.ui.JSmartTextArea;
 
 /**
@@ -23,7 +24,7 @@ public class JTextAreaAppender extends AppenderSkeleton
     /** 
      * Text area that logging statements are directed to. 
      */ 
-    private JSmartTextArea textArea_;
+    private JTextArea textArea_;
     
     /** 
      * Layout for logging statements.
@@ -39,11 +40,22 @@ public class JTextAreaAppender extends AppenderSkeleton
      */
     public JTextAreaAppender()
     {
-        textArea_ = new JSmartTextArea(true, false);
+        this(new JSmartTextArea(true, SwingUtil.getDefaultAntiAlias()));
+    }
+
+    
+    /** 
+     * Creates a new text area appender.
+     * 
+     * @param textArea Text area to connect the appender to.
+     */
+    public JTextAreaAppender(JTextArea textArea)
+    {
+        textArea_ = textArea;
         textArea_.getDocument().addDocumentListener(this);
         layout_ = new PatternLayout("%-5p %3x - %m%n");
     }
-
+    
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
@@ -119,48 +131,6 @@ public class JTextAreaAppender extends AppenderSkeleton
      */
     public void insertUpdate(DocumentEvent event)
     {
-        textArea_.scrollToEnd();
+        //textArea_.scrollToEnd();
     }
 }
-
-/*
- * Jacareto Software License, Version 1.0
- *
- * Copyright (c) 2002 Applied Computer Science Research Group,
- * Darmstadt University of Technology, and Mathematics & Computer Science
- * Department, Ludwigsburg University of Education. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided  that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- *
- * 3. The end-user documentation included with the redistribution, if any,
- *    must include the following acknowledgment:
- *      "This product includes software developed by the Applied Computer
- *       Science Research Group, Darmstadt University of Technology
- *       (http://www.pi.informatik.tu-darmstadt.de), and the Mathematics &
- *       Computer Science Department, Ludwigsburg University of Education
- *       (http://www.ph-ludwigsburg.de/mathematik/)."
- *    Alternately, this acknowledgment may appear in the software itself,
- *    if and wherever such third-party acknowledgments normally appear.
- *
- * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESSED OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE APPLIED COMPUTER SCIENCE RESEARCH GROUP, DARMSTADT UNIVERSITY OF
- * TECHNOLOGY, AND THE MATHEMATICS & COMPUTER SCIENCE DEPARTMENT, LUDWIGSBURG
- * UNIVERSITY OF EDUCATION, OR THEIR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
- * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES 
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
- * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR 
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
- * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
