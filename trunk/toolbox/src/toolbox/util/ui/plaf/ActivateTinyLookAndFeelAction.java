5e342aef8987bcab53b6152e2a5a9806911a604f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-02-14 18:49:45 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/util/ui/plaf/ActivateTinyLookAndFeelAction.java
@@ -0,0 +1,58 @@
+package toolbox.util.ui.plaf;
+
+import java.io.File;
+
+import de.muntjak.tinylookandfeel.Theme;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.ResourceUtil;
+
+/**
+ * Action that activates the Tiny Look and Feel and its varios themes.
+ */    
+public class ActivateTinyLookAndFeelAction extends ActivateLookAndFeelAction
+{
+    private static final Logger logger_ =
+        Logger.getLogger(ActivateTinyLookAndFeelAction.class);
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates an ActivateTinyLookAndFeelAction.
+     */
+    public ActivateTinyLookAndFeelAction()
+    {
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides ActivateLookAndFeelAction
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#setLookAndFeelInfo(
+     *      toolbox.util.ui.plaf.LAFInfo)
+     */
+    public void setLookAndFeelInfo(LAFInfo info)
+    {
+        super.setLookAndFeelInfo(info);
+        setName(info.getProperty("theme.name"));
+    }
+    
+    
+    /**
+     * @see toolbox.util.ui.plaf.ActivateLookAndFeelAction#activate()
+     */
+    public void activate() throws Exception
+    {
+        LAFInfo info = getLookAndFeelInfo();
+        String themeResource = info.getProperty("theme.file");
+        File themeFile = ResourceUtil.getResourceAsTempFile(themeResource);
+        int themeStyle = Integer.parseInt(info.getProperty("theme.style"));
+        boolean b = Theme.loadTheme(themeFile, themeStyle);
+        logger_.debug("Tiny theme loaded: " + b);
+        super.activate();
+    }
+}
