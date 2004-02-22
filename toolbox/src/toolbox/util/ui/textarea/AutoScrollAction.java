bdf05f714c809ff225b8487a62e34b91644c544b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-02-22 13:30:50 +0000

    Added icon.

--- toolbox/src/toolbox/util/ui/textarea/AutoScrollAction.java
+++ toolbox/src/toolbox/util/ui/textarea/AutoScrollAction.java
@@ -4,37 +4,51 @@ import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
 
+import toolbox.util.ui.ImageCache;
 import toolbox.util.ui.JSmartTextArea;
-
 
 /**
  * Toggles autoscrolling in a JSmartTextArea.
  */    
 public class AutoScrollAction extends AbstractAction 
 {
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
 	/**
      * Text area.
      */
     private final JSmartTextArea area_;
 
-
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
     /**
      * Creates a AutoScrollAction.
+     * 
+     * @param area Text area to link this action to.
      */
     public AutoScrollAction(JSmartTextArea area)
     {
-        super("AutoScroll");
+        super("AutoScroll", ImageCache.getIcon(ImageCache.IMAGE_LOCK));
         area_ = area;
     }
     
+    //--------------------------------------------------------------------------
+    // ActionListener Interface
+    //--------------------------------------------------------------------------
     
     /**
      * @see java.awt.event.ActionListener#actionPerformed(
      *      java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent e)
-    {
+    { 
+        area_.setAutoScroll(!area_.isAutoScroll());
+        
         if (area_.isAutoScroll())
-            area_.scrollToEnd(); 
+            area_.scrollToEnd();
     }
 }
