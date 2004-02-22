929417a12b2594638759a1ac42d6ffba9f0534f7
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-02-22 13:23:44 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/action/AntiAliasAction.java
@@ -0,0 +1,51 @@
+package toolbox.util.ui.action;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import toolbox.util.ui.AntiAliased;
+
+/**
+ * Toggles antialiasing.
+ */
+public class AntiAliasAction extends AbstractAction 
+{
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Antialias aware component.
+     */
+    private Component component_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates an AntiAliasAction.
+     */
+    public AntiAliasAction(Component component)
+    {
+        super("AntiAlias");
+        component_ = component;
+    }
+    
+    //--------------------------------------------------------------------------
+    // ActionListener Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(
+     *      java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        AntiAliased aa = (AntiAliased) component_;
+        aa.setAntiAliased(!aa.isAntiAliased());
+        component_.repaint();
+    }
+}
