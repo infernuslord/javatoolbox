66d4764694678658cf37025c5fb4455f2bf0bf85
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-03-06 04:15:05 +0000

    Added: plugin dialog double click

--- trunk/toolbox/src/toolbox/TODO.java
+++ trunk/toolbox/src/toolbox/TODO.java
@@ -21,7 +21,8 @@ package toolbox;
  *       the addition of large sections of text. Unhook model, insert, rehook 
  *       model is a possiblity.
  * TODO: Convert all gifs to pngs.
- *  * =============================================================================
+ * TODO: Make plugin dialog double click automatically add plugin.
+ * =============================================================================
  * TODO: DocViewer: Fix colors in pollo doc viewer.
  * =============================================================================
  * TODO: FindClass: Reduce startup time.
