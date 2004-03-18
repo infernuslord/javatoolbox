01daf101e794c2c0cc80a0a1d18d9bcb6e1f04d1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-03-18 23:27:32 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/workspace/host/PluginHostListener.java
@@ -0,0 +1,27 @@
+package toolbox.workspace.host;
+
+import toolbox.workspace.IPlugin;
+
+/**
+ * Defines the interface for notification when a plugin is added or removed
+ * from a PluginHost.
+ */
+public interface PluginHostListener
+{
+    /**
+     * Notification that a plugin was added to the plugin host.
+     * 
+     * @param pluginHost Plugin host that the plugin was added to.
+     * @param plugin Plugin that was added.
+     */
+    void pluginAdded(PluginHost pluginHost, IPlugin plugin);
+    
+    
+    /**
+     * Notification that a plugin was removed from a plugin host.
+     * 
+     * @param pluginHost Plugin host that the plugin was removed from.
+     * @param plugin Plugin that was removed.
+     */
+    void pluginRemoved(PluginHost pluginHost, IPlugin plugin);
+}
