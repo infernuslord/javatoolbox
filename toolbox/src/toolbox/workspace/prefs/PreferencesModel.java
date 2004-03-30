164edb2dd19f4cd2a6a9f9b24f3386dc3a83971d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-03-30 22:34:32 +0000

    Checkstyle updates

--- toolbox/src/toolbox/workspace/prefs/PreferencesModel.java
+++ toolbox/src/toolbox/workspace/prefs/PreferencesModel.java
@@ -12,103 +12,103 @@ public class PreferencesModel implements IPreferenced
     //--------------------------------------------------------------------------
     // Constants
     //--------------------------------------------------------------------------
-    
+
     private static final String NODE_WORKSPACE_PREFERENCES = "Preferences";
     private static final String NODE_HTTP_PROXY = "HttpProxy";
     private static final String ATTR_HTTP_PROXY_ENABLED = "enabled";
     private static final String ATTR_HTTP_PROXY_HOST = "hostname";
     private static final String ATTR_HTTP_PROXY_PORT = "port";
-    
+
     //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
-    
+
     private boolean httpProxyEnabled_;
     private String httpProxyHost_;
     private String httpProxyPort_;
-    
+
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
-    
+
     public PreferencesModel()
     {
     }
-    
+
     //--------------------------------------------------------------------------
     // IPreferenced Interface
     //--------------------------------------------------------------------------
-    
+
     /**
      * @see toolbox.workspace.IPreferenced#applyPrefs(nu.xom.Element)
      */
     public void applyPrefs(Element prefs) throws Exception
     {
-        Element root = 
+        Element root =
             XOMUtil.getFirstChildElement(
-                prefs, 
-                NODE_WORKSPACE_PREFERENCES, 
+                prefs,
+                NODE_WORKSPACE_PREFERENCES,
                 new Element(NODE_WORKSPACE_PREFERENCES));
-        
+
         Element httpProxy =
             XOMUtil.getFirstChildElement(
                 root, NODE_HTTP_PROXY, new Element(NODE_HTTP_PROXY));
-                
+
         setHttpProxyEnabled(
             XOMUtil.getBooleanAttribute(
                 httpProxy,
                 ATTR_HTTP_PROXY_ENABLED,
                 false));
-        
+
         setHttpProxyHost(
             XOMUtil.getStringAttribute(
                 httpProxy,
                 ATTR_HTTP_PROXY_HOST,
                 ""));
-            
+
         setHttpProxyPort(
             XOMUtil.getStringAttribute(
                 httpProxy,
                 ATTR_HTTP_PROXY_PORT,
                 ""));
     }
-    
-    
+
+
     /**
      * @see toolbox.workspace.IPreferenced#savePrefs(nu.xom.Element)
      */
     public void savePrefs(Element prefs) throws Exception
     {
         Element root = new Element(NODE_WORKSPACE_PREFERENCES);
-        
+
         Element httpProxy = new Element(NODE_HTTP_PROXY);
-        
+
         httpProxy.addAttribute(
             new Attribute(
-                ATTR_HTTP_PROXY_ENABLED, 
+                ATTR_HTTP_PROXY_ENABLED,
                 isHttpProxyEnabled() + ""));
-        
+
         if (!StringUtil.isNullOrBlank(getHttpProxyHost()))
             httpProxy.addAttribute(
                 new Attribute(
-                    ATTR_HTTP_PROXY_HOST, 
+                    ATTR_HTTP_PROXY_HOST,
                     getHttpProxyHost().trim()));
-        
+
         if (!StringUtil.isNullOrBlank(getHttpProxyPort()))
             httpProxy.addAttribute(
                 new Attribute(
-                    ATTR_HTTP_PROXY_PORT, 
+                    ATTR_HTTP_PROXY_PORT,
                     getHttpProxyPort().trim()));
-        
+
         root.appendChild(httpProxy);
-        
+
         XOMUtil.insertOrReplace(prefs, root);
     }
-    
+
     //--------------------------------------------------------------------------
     // Accessors/Mutators
     //--------------------------------------------------------------------------
-    
+
     /**
      * @return Returns the httpProxyEnabled.
      */
@@ -116,8 +116,8 @@ public class PreferencesModel implements IPreferenced
     {
         return httpProxyEnabled_;
     }
-    
-    
+
+
     /**
      * @param httpProxyEnabled The httpProxyEnabled to set.
      */
@@ -125,8 +125,8 @@ public class PreferencesModel implements IPreferenced
     {
         httpProxyEnabled_ = httpProxyEnabled;
     }
-    
-    
+
+
     /**
      * @return Returns the httpProxyHost.
      */
@@ -134,8 +134,8 @@ public class PreferencesModel implements IPreferenced
     {
         return httpProxyHost_;
     }
-    
-    
+
+
     /**
      * @param httpProxyHost The httpProxyHost to set.
      */
@@ -143,8 +143,8 @@ public class PreferencesModel implements IPreferenced
     {
         httpProxyHost_ = httpProxyHost;
     }
-    
-    
+
+
     /**
      * @return Returns the httpProxyPort.
      */
@@ -152,8 +152,8 @@ public class PreferencesModel implements IPreferenced
     {
         return httpProxyPort_;
     }
-    
-    
+
+
     /**
      * @param httpProxyPort The httpProxyPort to set.
      */
