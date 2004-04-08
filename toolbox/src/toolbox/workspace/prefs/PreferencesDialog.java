61da1bfe94ca5403cef1bf3cac52e096de948485
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-04-08 23:56:49 +0000

    Javadoc updates

--- toolbox/src/toolbox/workspace/prefs/PreferencesDialog.java
+++ toolbox/src/toolbox/workspace/prefs/PreferencesDialog.java
@@ -30,7 +30,16 @@ import toolbox.util.ui.tree.JSmartTree;
 import toolbox.util.ui.tree.SmartTreeCellRenderer;
 
 /**
- * Workspace preferences dialog box.
+ * The PreferencesDialog is spawned by the PluginWorkspace to allow the user 
+ * to change various preferences supported by the program. Preferences are
+ * grouped together on different panels and can be switched between be selecting
+ * that panel's node on the configuration tree. New panels can easily be
+ * added by implementing the PreferencesView interface and providing the 
+ * necessary information.
+ * 
+ * @see toolbox.workspace.PluginWorkspace
+ * @see toolbox.workspace.prefs.PreferencesManager
+ * @see toolbox.workspace.prefs.PreferencesView
  */
 public class PreferencesDialog extends JDialog
 {
@@ -38,44 +47,44 @@ public class PreferencesDialog extends JDialog
         Logger.getLogger(PreferencesDialog.class);
 
     //--------------------------------------------------------------------------
-    // Constants
-    //--------------------------------------------------------------------------
-
-    private static final String ACTION_OK     = "OK";
-    private static final String ACTION_CANCEL = "Cancel";
-
-
-    //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
 
     /**
-     * Preferences tree.
+     * Preferences tree. Each node in the tree corresponds to a PreferencesView
+     * which is displayed on the right side of the dialog.
      */
     private JSmartTree tree_;
-    
+
+    /**
+     * Card panel used to switch out the currently visisble PreferencesView
+     * based on which node in the tree is selected.
+     */
     private JPanel cardPanel_;
+
+    /**
+     * Layout for switching out the PreferencesViews.
+     */
     private CardLayout cardLayout_;
 
+    /**
+     * Source of the PreferencesViews.
+     */
     private PreferencesManager preferencesManager_;
 
     //--------------------------------------------------------------------------
     // Main
     //--------------------------------------------------------------------------
-    
+
     public static void main(String[] args) throws Exception
     {
-        //LookAndFeelUtil.setPreferredLAF();
-        
         JFrame frame = new JFrame();
         SwingUtil.centerWindow(frame);
         JDialog d = new PreferencesDialog(frame, new PreferencesManager());
         d.setVisible(true);
         SwingUtil.centerWindow(d);
-        
     }
-    
-    
+
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
@@ -84,9 +93,10 @@ public class PreferencesDialog extends JDialog
      * Constructor for PreferencesView.
      *
      * @param parent Parent frame.
+     * @param preferencesManager PreferencesManager from the workspace.
      */
     public PreferencesDialog(
-        Frame parent, 
+        Frame parent,
         PreferencesManager preferencesManager)
     {
         super(parent, "Toolbox Preferences", true);
@@ -99,23 +109,23 @@ public class PreferencesDialog extends JDialog
     //--------------------------------------------------------------------------
     // Public
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Adds a PreferenceView as a node in the tree and adds it to the stack of
+     * cards in the card layout.
+     * 
+     * @param view Preferences view to add to the prefs tree.
+     */
     public void registerView(PreferencesView view)
     {
         logger_.debug("Registering prefs view = " + view.getLabel());
-        
+
         DefaultTreeModel model = (DefaultTreeModel) tree_.getModel();
         DefaultMutableTreeNode root = (DefaultMutableTreeNode) model.getRoot();
         root.add(new DefaultMutableTreeNode(view));
-        
-        // Since the view is created by the PlugWorkspace during initialization,
-        // the look and feel may have changed so we have to update it manually.
-        
-        // SwingUtilities.updateComponentTreeUI(view.getView());
-        
         cardPanel_.add(view.getView(), view.getLabel());
     }
-    
+
     //--------------------------------------------------------------------------
     // Build View
     //--------------------------------------------------------------------------
@@ -127,28 +137,27 @@ public class PreferencesDialog extends JDialog
     {
         Container view = getContentPane();
         view.setLayout(new BorderLayout());
-        
-        JSmartSplitPane splitter = 
+
+        JSmartSplitPane splitter =
             new JSmartSplitPane(
                 JSmartSplitPane.HORIZONTAL_SPLIT,
                 buildTreePanel(),
                 cardPanel_ = new JPanel(
                     cardLayout_ = new CardLayout()));
-        
+
         view.add(BorderLayout.CENTER, splitter);
         view.add(BorderLayout.SOUTH, buildButtonPanel());
-        //setContentPane(view);
-        
+
         PreferencesView[] prefs = preferencesManager_.getPreferences();
         for (int i = 0; i < prefs.length; registerView(prefs[i++]));
-        
+
         SwingUtil.expandAll(tree_, true);
     }
 
-    
+
     /**
      * Builds the ok/cancel/apply button panel.
-     * 
+     *
      * @return JPanel
      */
     protected JPanel buildButtonPanel()
@@ -160,10 +169,10 @@ public class PreferencesDialog extends JDialog
         return p;
     }
 
-    
+
     /**
      * Builds the tree panel with the preference views as children.
-     * 
+     *
      * @return JPanel
      */
     protected JPanel buildTreePanel()
@@ -172,28 +181,32 @@ public class PreferencesDialog extends JDialog
         DefaultTreeModel treeModel = new DefaultTreeModel(root);
         tree_ = new JSmartTree(root);
         tree_.setCellRenderer(new PrefsTreeCellRenderer());
-        
-        JHeaderPanel p = 
+
+        JHeaderPanel p =
             new JHeaderPanel(
-                ImageCache.getIcon(ImageCache.IMAGE_CONFIG), 
+                ImageCache.getIcon(ImageCache.IMAGE_CONFIG),
                 "Preferences",
                 null,
                 new JScrollPane(tree_));
-        
+
         tree_.addTreeSelectionListener(new PrefsTreeSelectionListener());
         return p;
     }
-    
+
     //--------------------------------------------------------------------------
     // OKAction
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Propagates the OK event to all the views and dismisses the dialog box.
+     */
     class OKAction extends AbstractAction
     {
         public OKAction()
         {
             super("OK");
         }
+
         
         /**
          * @see java.awt.event.ActionListener#actionPerformed(
@@ -201,23 +214,26 @@ public class PreferencesDialog extends JDialog
          */
         public void actionPerformed(ActionEvent e)
         {
-            System.out.println("ok!");
             PreferencesView[] prefs = preferencesManager_.getPreferences();
             for (int i = 0; i < prefs.length; prefs[i++].onOK());
             dispose();
         }
     }
-    
+
     //--------------------------------------------------------------------------
     // ApplyAction
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Propagates the Apply event to all the views.
+     */
     class ApplyAction extends AbstractAction
     {
         public ApplyAction()
         {
             super("Apply");
         }
+
         
         /**
          * @see java.awt.event.ActionListener#actionPerformed(
@@ -225,7 +241,6 @@ public class PreferencesDialog extends JDialog
          */
         public void actionPerformed(ActionEvent e)
         {
-            System.out.println("applying!");
             PreferencesView[] prefs = preferencesManager_.getPreferences();
             for (int i = 0; i < prefs.length; prefs[i++].onApply());
         }
@@ -234,13 +249,18 @@ public class PreferencesDialog extends JDialog
     //--------------------------------------------------------------------------
     // CancelAction
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Propagates the Cancel event to all the views and disposes of the 
+     * dialog box.
+     */
     class CancelAction extends AbstractAction
     {
         public CancelAction()
         {
             super("Cancel");
         }
+
         
         /**
          * @see java.awt.event.ActionListener#actionPerformed(
@@ -254,57 +274,74 @@ public class PreferencesDialog extends JDialog
             dispose();
         }
     }
-    
+
     //--------------------------------------------------------------------------
     // PrefsTreeCellRenderer
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Custom cell renderer for the prefs tree. The user object in each non-root
+     * node is expected to be an instance of PreferencesView. The label of the
+     * node is set to the name of the view.
+     */
     class PrefsTreeCellRenderer extends SmartTreeCellRenderer
     {
+        /**
+         * @see javax.swing.tree.TreeCellRenderer#getTreeCellRendererComponent(
+         *      javax.swing.JTree, java.lang.Object, boolean, boolean, boolean, 
+         *      int, boolean)
+         */
         public Component getTreeCellRendererComponent(
             JTree tree,
-            Object value, 
-            boolean selected, 
-            boolean expanded, 
+            Object value,
+            boolean selected,
+            boolean expanded,
             boolean leaf,
-            int row, 
+            int row,
             boolean hasFocus)
         {
             super.getTreeCellRendererComponent(
-                tree, 
-                value, 
-                selected, 
-                expanded, 
-                leaf, 
-                row, 
+                tree,
+                value,
+                selected,
+                expanded,
+                leaf,
+                row,
                 hasFocus);
-            
+
             DefaultMutableTreeNode d = (DefaultMutableTreeNode) value;
-            
-            if (d.getUserObject() != null && 
+
+            if (d.getUserObject() != null &&
                 d.getUserObject() instanceof PreferencesView)
             {
                 PreferencesView pp = (PreferencesView) d.getUserObject();
                 setText(pp.getLabel());
             }
-            
-            return this; 
-            
+
+            return this;
         }
     }
-    
+
     //--------------------------------------------------------------------------
     // PrefsTreeSelectionListener
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Activates PreferenceViews in the card stack based on the currently
+     * selected node.
+     */
     class PrefsTreeSelectionListener implements TreeSelectionListener
     {
+        /**
+         * @see javax.swing.event.TreeSelectionListener#valueChanged(
+         *      javax.swing.event.TreeSelectionEvent)
+         */
         public void valueChanged(TreeSelectionEvent e)
         {
-            DefaultMutableTreeNode node = 
+            DefaultMutableTreeNode node =
                 (DefaultMutableTreeNode) e.getPath().getLastPathComponent();
 
-            if (node.getUserObject() != null && 
+            if (node.getUserObject() != null &&
                 node.getUserObject() instanceof PreferencesView)
             {
                 PreferencesView pp = (PreferencesView) node.getUserObject();
