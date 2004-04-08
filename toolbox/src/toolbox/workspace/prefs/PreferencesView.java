64dfd47bec6407b291ac6abb885f9429c3e50943
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-04-08 19:13:19 +0000

    Extends IPreferenced. Added javadocs

--- toolbox/src/toolbox/workspace/prefs/PreferencesView.java
+++ toolbox/src/toolbox/workspace/prefs/PreferencesView.java
@@ -2,20 +2,48 @@ package toolbox.workspace.prefs;
 
 import javax.swing.JComponent;
 
+import toolbox.workspace.IPreferenced;
+
 /**
  * PreferencesPane is an interface for components that will be shown by the
- * Prefreences dialog box in a card like layout once the pane's node is selected
- * in the preferences tree view.
+ * Prefreences dialog box in a card like layout once the view's node is selected
+ * in the configuration tree.
  */
-public interface PreferencesView
+public interface PreferencesView extends IPreferenced
 {
-    public String getLabel();
-    
-    public JComponent getView();
-    
-    public void onOK();
-    
-    public void onApply();
-    
-    public void onCancel();
+    /**
+     * The label of the view in the configuration tree.
+     *
+     * @return String
+     */
+    String getLabel();
+
+
+    /**
+     * Returns the views user interface component.
+     *
+     * @return JComponent
+     */
+    JComponent getView();
+
+
+    /**
+     * Called when the user click on the OK button to accept the current
+     * preferences.
+     */
+    void onOK();
+
+
+    /**
+     * Called when the user clicks on the Apply button to apply the current
+     * preferences.
+     */
+    void onApply();
+
+
+    /**
+     * Called when the user clicks on the Cancel button to dismiss the
+     * preferences dialog box without saving any changed preferences.
+     */
+    void onCancel();
 }
