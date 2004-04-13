f68d5d4c27c6744f16ecb3abc3df5dd1cd0d1ba1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-04-13 05:09:52 +0000

    Initial version

---
+++ toolbox/test/toolbox/log4j/im/InstantMessengerFactoryTest.java
@@ -0,0 +1,93 @@
+package toolbox.log4j.im;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Unit test for InstantMessengerFactory.
+ */
+public class InstantMessengerFactoryTest extends TestCase
+{
+    private static final Logger logger_ =
+        Logger.getLogger(InstantMessengerFactoryTest.class);
+
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+
+    /**
+     * Entrypoint
+     *
+     * @param args None recognized.
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(InstantMessengerFactoryTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+
+    /**
+     * Tests creation of the Yahoo messenger.
+     */
+    public void testCreateYahoo()
+    {
+        logger_.info("Running testCreateYahoo...");
+
+        InstantMessenger im = InstantMessengerFactory.create("yahoo");
+        assertNotNull(im);
+        assertFalse(im.isConnected());
+        assertTrue(im instanceof YahooMessenger);
+    }
+
+
+    /**
+     * Tests creation of the Msn messenger.
+     */
+    public void testCreateMSN()
+    {
+        logger_.info("Running testCreateMSN...");
+
+        InstantMessenger im = InstantMessengerFactory.create("msn");
+        assertNotNull(im);
+        assertFalse(im.isConnected());
+        assertTrue(im instanceof MSNMessenger);
+    }
+
+
+    /**
+     * Tests creation of the AOL messenger.
+     */
+    public void testCreateAOL()
+    {
+        logger_.info("Running testCreateAOL...");
+
+        InstantMessenger im = InstantMessengerFactory.create("aol");
+        assertNotNull(im);
+        assertFalse(im.isConnected());
+        assertTrue(im instanceof AIMMessenger);
+    }
+
+
+    /**
+     * Tests creation of an invalid messenger.
+     */
+    public void testCreateFailure()
+    {
+        logger_.info("Running testCreateFailure...");
+
+        try
+        {
+            InstantMessenger im = InstantMessengerFactory.create("xyz");
+            fail("Should have failed for bogus messenger xyz");
+        }
+        catch (IllegalArgumentException iae)
+        {
+            assertTrue(true);
+        }
+    }
+}
