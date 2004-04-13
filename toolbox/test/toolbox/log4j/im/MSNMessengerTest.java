933dde489c33476fd64fbf7fb49340fdf3847cc0
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-04-13 04:55:54 +0000

    Initial version

---
+++ toolbox/test/toolbox/log4j/im/MSNMessengerTest.java
@@ -0,0 +1,79 @@
+package toolbox.log4j.im;
+
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
+
+import toolbox.junit.StandaloneTestCase;
+
+/**
+ * Unit test for YahooMessenger.
+ */
+public class MSNMessengerTest extends AbstractMessengerTest implements 
+	StandaloneTestCase
+{
+    private static final Logger logger_ =
+        Logger.getLogger(MSNMessengerTest.class);
+
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Entrypoint.
+     * 
+     * @param args None recognized.
+     */
+    public static void main(String[] args)
+    {
+        TestRunner.run(MSNMessengerTest.class);
+    }
+    
+    //--------------------------------------------------------------------------
+    // Abstract Implementation
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see toolbox.log4j.im.AbstractMessengerTest#getFromUser()
+     */
+    public String getFromUser()
+    {
+        return "supahfuzz";
+    }
+    
+    
+    /**
+     * @see toolbox.log4j.im.AbstractMessengerTest#getFromPassword()
+     */
+    public String getFromPassword()
+    {
+        return "techno";
+    }
+    
+    
+    /**
+     * @see toolbox.log4j.im.AbstractMessengerTest#getToUser()
+     */
+    public String getToUser()
+    {
+        return "sourcedirect@hotmail.com";
+    }
+    
+    
+    /**
+     * @see toolbox.log4j.im.AbstractMessengerTest#getLog4JXML()
+     */
+    public String getLog4JXML()
+    {
+    	return "/toolbox/log4j/im/MSNMessengerTest.xml";        
+    }
+    
+    
+    /**
+     * @see toolbox.log4j.im.AbstractMessengerTest#getMessenger()
+     */
+    public InstantMessenger getMessenger()
+    {
+        return new MSNMessenger();
+    }
+}
