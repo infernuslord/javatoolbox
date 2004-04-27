f4ae199ad72be0e50fc9326e020559d2927609bb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-04-27 03:20:45 +0000

    Extracted SQLReferenceAction from QueryPlugin

---
+++ toolbox/src/toolbox/plugin/jdbc/action/SQLReferenceAction.java
@@ -0,0 +1,64 @@
+package toolbox.plugin.jdbc.action;
+
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+
+import toolbox.plugin.jdbc.QueryPlugin;
+import toolbox.util.ClassUtil;
+import toolbox.util.FileUtil;
+import toolbox.util.ResourceUtil;
+
+/**
+ * Dumps a short SQL reference text to the SQL editor.
+ */
+public class SQLReferenceAction extends AbstractAction
+{
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Parent plugin.
+     */
+    private final QueryPlugin plugin_;
+
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a SQLReferenceAction.
+     * 
+     * @param plugin Parent plugin.
+     */
+    public SQLReferenceAction(QueryPlugin plugin)
+    {
+        plugin_ = plugin;
+    }
+
+    //--------------------------------------------------------------------------
+    // ActionListener Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see java.awt.event.ActionListener#actionPerformed(
+     *      java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        String sqlRef = File.separator +
+            FileUtil.trailWithSeparator(
+                ClassUtil.packageToPath(
+                    ClassUtil.stripClass(
+                        QueryPlugin.class.getName()))) +
+                            "sqlref.txt";
+
+        sqlRef = sqlRef.replace(File.separatorChar, '/');
+
+        plugin_.getSQLEditor().setText(
+            plugin_.getSQLEditor().getText() + "\n" +
+                ResourceUtil.getResourceAsString(sqlRef));
+    }
+}
