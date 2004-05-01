d79ef8ed3d340da30ec44e440f89a4663b68e9e6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-05-01 00:49:15 +0000

    Javadoc updates

--- trunk/toolbox/src/toolbox/junit/StandaloneTestCase.java
+++ trunk/toolbox/src/toolbox/junit/StandaloneTestCase.java
@@ -3,6 +3,9 @@ package toolbox.junit;
 /**
  * Marker interface to tag test cases which can only be run standalone and are
  * not appropriate for running in a batch sequence unattended.
+ * 
+ * @see toolbox.junit.CloverTestFilter
+ * @see toolbox.junit.CloverTestSuite
  */
 public interface StandaloneTestCase
 {
