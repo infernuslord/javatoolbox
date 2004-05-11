bc8c4e1fc97ada6f169dae27ed4e33d560a94909
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-05-11 07:04:45 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/ui/SortedComboBoxModel.java
@@ -0,0 +1,80 @@
+package toolbox.util.ui;
+
+import java.util.Vector;
+
+import javax.swing.DefaultComboBoxModel;
+
+/**
+ * SortedComboBoxModel sorts combobox items on insertion. Items in the model
+ * must implement the comparable interface.
+ */
+public class SortedComboBoxModel extends DefaultComboBoxModel
+{
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a SortedComboBoxModel.
+     */
+    public SortedComboBoxModel()
+    {
+    }
+
+    
+    /**
+     * Creates a SortedComboBoxModel.
+     * 
+     * @param items ComboBox elements.
+     */
+    public SortedComboBoxModel(Object[] items)
+    {
+        super(items);
+    }
+
+    
+    /**
+     * Creates a SortedComboBoxModel.
+     * 
+     * @param items ComboBox elements.
+     */
+    public SortedComboBoxModel(Vector items)
+    {
+        super(items);
+    }
+
+    //--------------------------------------------------------------------------
+    // Overrides DefaultComboBoxModel
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see javax.swing.MutableComboBoxModel#addElement(java.lang.Object)
+     */
+    public void addElement(Object element)
+    {
+        int index = 0;
+        int size = getSize();
+
+        //  Determine where to insert element to keep list in sorted order
+
+        for (index = 0; index < size; index++)
+        {
+            Comparable c = (Comparable) getElementAt(index);
+
+            if (c.compareTo(element) > 0)
+                break;
+        }
+
+        super.insertElementAt(element, index);
+    }
+
+
+    /**
+     * @see javax.swing.MutableComboBoxModel#insertElementAt(
+     *      java.lang.Object, int)
+     */
+    public void insertElementAt(Object element, int index)
+    {
+        addElement(element);
+    }
+}
