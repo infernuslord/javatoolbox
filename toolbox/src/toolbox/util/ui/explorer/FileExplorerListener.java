9eb740a0275d0a907f4310711bf39361f74eff18
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-05-22 06:46:37 +0000

    Added todo

--- toolbox/src/toolbox/util/ui/explorer/FileExplorerListener.java
+++ toolbox/src/toolbox/util/ui/explorer/FileExplorerListener.java
@@ -5,34 +5,36 @@ package toolbox.util.ui.explorer;
  */
 public interface FileExplorerListener
 {
+    // TODO: Change String parameter to a File object.
+
     /**
      * Called when a file is selected in the file list.
-     * 
+     *
      * @param file Name of the file selected.
      */
-    void fileSelected(String file);    
-    
-    
+    void fileSelected(String file);
+
+
     /**
      * Called when a file is double clicked in the details view.
      *
      * @param file Name of the file that was clicked.
      */
     void fileDoubleClicked(String file);
-    
-    
+
+
     /**
      * Called when a directory folder is selected in the tree view.
-     * 
+     *
      * @param folder Name of the directory selected.
      */
-    void folderSelected(String folder);    
-    
-    
+    void folderSelected(String folder);
+
+
     /**
      * Called when a folder is doubleclicked in the tree view.
-     * 
+     *
      * @param folder Name of directory double clicked.
      */
-    void folderDoubleClicked(String folder);    
+    void folderDoubleClicked(String folder);
 }
