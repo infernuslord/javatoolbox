36415d7f495c5ee75fff472c87907c3e3234f3a9
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-06-03 09:53:25 +0000

    Moved AutoTailAction from toolbox.util.ui.table to toolbox.util.ui.table.action

---
+++ toolbox/src/toolbox/util/ui/table/action/AutoTailAction.java
@@ -0,0 +1,57 @@
+package toolbox.util.ui.table.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+
+import toolbox.util.ui.ImageCache;
+import toolbox.util.ui.table.JSmartTable;
+
+/**
+ * Creates an action for toggling the autotail flag in a JSmartTable. Comes
+ * prewired with an icon and a shortcut.
+ */    
+public class AutoTailAction extends AbstractAction
+{
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Table to tail.
+     */
+    private final JSmartTable table_;
+
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates an AutoTailAction.
+     * 
+     * @param table Table to tail.
+     */
+    public AutoTailAction(JSmartTable table)
+    {
+        super("Tail");
+        table_ = table;
+        putValue(Action.MNEMONIC_KEY, new Integer('A'));
+        putValue(Action.SMALL_ICON, ImageCache.getIcon(ImageCache.IMAGE_LOCK));
+    }
+    
+    //--------------------------------------------------------------------------
+    // ActionPerformed Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Basically toggle the autotail flag.
+     * 
+     * @see java.awt.event.ActionListener#actionPerformed(
+     *      java.awt.event.ActionEvent)
+     */
+    public void actionPerformed(ActionEvent e)
+    {
+        table_.setAutoTail(!table_.isAutoTail());
+    }
+}
