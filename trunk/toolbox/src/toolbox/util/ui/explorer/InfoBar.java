8d995e5170cd0bdb290a7d2af4cf9fadc727a0b9
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-06-03 09:03:48 +0000

    Extracted from JFileExplorer

---
+++ trunk/toolbox/src/toolbox/util/ui/explorer/InfoBar.java
@@ -0,0 +1,159 @@
+package toolbox.util.ui.explorer;
+
+import java.awt.event.ItemEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.Date;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+import toolbox.util.DateTimeUtil;
+import toolbox.util.FileUtil;
+import toolbox.util.ui.ImageCache;
+import toolbox.util.ui.JSmartLabel;
+import toolbox.util.ui.statusbar.JStatusBar;
+
+/**
+ * Simple file information status bar. Shows the following information:
+ * <ul>
+ *  <li>File size
+ *  <li>File last modified date
+ *  <li>Read/write attribute
+ * </ul>
+ * <p>
+ * Also contains a button to refresh the contents of the file explorer.
+ * 
+ * @see toolbox.util.ui.explorer.JFileExplorer
+ */    
+public class InfoBar extends JStatusBar
+{
+    //--------------------------------------------------------------------------
+    // Constants
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Formatter for the file size.
+     */
+    private static final DecimalFormat sizeFormatter_ = new DecimalFormat();
+
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+
+    /**
+     * Refererence to the parent file explorer.
+     */
+    private final JFileExplorer explorer_;
+    
+    /**
+     * Shows the size of the file in bytes formatted for the current locale.
+     */
+	private JLabel sizeLabel_;
+    
+    /**
+     * Shows the last modified timestamp of the file.
+     */
+    private JLabel modifiedLabel_;
+    
+    /**
+     * Shows an R if the file is readonly and/or a W if the file is writeable. 
+     */
+    private JLabel attribLabel_;
+    
+    /**
+     * Contains an icon the user can click on to refresh the file explorer.
+     */
+    private JLabel refreshLabel_;
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+
+    /**
+     * Creates an InfoBar.
+     * 
+     * @param exporer File explorer to associate this infobar with.
+     */
+    InfoBar(JFileExplorer explorer)
+    {
+		explorer_ = explorer;
+        sizeLabel_ = new JSmartLabel();
+        sizeLabel_.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        modifiedLabel_ = new JSmartLabel();
+        modifiedLabel_.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        attribLabel_ = new JSmartLabel();
+
+        refreshLabel_ = 
+            new JSmartLabel(ImageCache.getIcon(ImageCache.IMAGE_REFRESH));
+        
+        refreshLabel_.addMouseListener(new MouseAdapter()
+        {
+            public void mousePressed(MouseEvent e)
+            {
+                String folder = explorer_.getCurrentPath();
+                String file   = FileUtil.stripPath(explorer_.getFilePath());
+                
+                explorer_.new DriveComboListener().itemStateChanged(
+                    new ItemEvent(explorer_.getRootsComboBox(), 0, null, 
+                        ItemEvent.ITEM_STATE_CHANGED));
+                        
+                explorer_.selectFolder(folder);
+                explorer_.setFileList(folder);
+                explorer_.getFileList().setSelectedValue(file, true);
+            }
+        });
+        
+        addStatusComponent(sizeLabel_, false);
+        addStatusComponent(modifiedLabel_, true);
+        addStatusComponent(attribLabel_, false);
+        addStatusComponent(refreshLabel_, false);
+    }
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Updates the bar to show information for the given file.
+     * 
+     * @param file File to show info for.
+     */
+    public void showInfo(File file)
+    {
+        String size = sizeFormatter_.format(file.length()) + " bytes";
+        sizeLabel_.setText(size);
+        sizeLabel_.setToolTipText(size);
+        
+        String date = DateTimeUtil.format(new Date(file.lastModified()));
+        modifiedLabel_.setText(date);
+        modifiedLabel_.setToolTipText(date);
+            
+        attribLabel_.setText(
+            (file.canRead() ? "R" : "") +
+            (file.canWrite() ? "W" : ""));
+    }
+    
+    //--------------------------------------------------------------------------
+    // InfoBar
+    //--------------------------------------------------------------------------
+    
+    /** 
+     * Updates the infobar with the currently selected file.
+     */
+    class InfoBarUpdater extends FileExplorerAdapter
+    {
+        /**
+         * @see toolbox.util.ui.explorer.FileExplorerListener#fileSelected(
+         *      java.lang.String)
+         */
+        public void fileSelected(String file)
+        {
+            showInfo(new File(file));
+        }
+    }
+}
