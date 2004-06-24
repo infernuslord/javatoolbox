b33d4f001212216c453ff880481348746590bbc7
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-06-24 14:57:30 +0000

    Initial version

---
+++ trunk/toolbox/test/toolbox/util/io/BandwidthTest.java
@@ -0,0 +1,40 @@
+package toolbox.util.io;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.StringUtil;
+
+/**
+ * Unit test for Bandwidth.
+ * 
+ * @see toolbox.util.io.Bandwidth 
+ */
+public class BandwidthTest extends TestCase
+{
+    private static final Logger logger_ = Logger.getLogger(BandwidthTest.class);
+    
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args)
+    {
+        TestRunner.run(BandwidthTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+    
+    public void testToString() 
+    {
+        logger_.info("Running testToString...");
+        
+        Bandwidth bw = new Bandwidth(1000, 1000, Bandwidth.TYPE_BOTH);
+        
+        logger_.info(StringUtil.addBars(bw.toString()));
+    }
+}
