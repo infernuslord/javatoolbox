eba825e55aab304b3e3e01102ca99f8404c68ab8
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-06-28 20:35:20 +0000

    Switched to a cleaner map based transition table with multi-value keys.

--- toolbox/src/toolbox/util/service/AbstractService.java
+++ toolbox/src/toolbox/util/service/AbstractService.java
@@ -1,5 +1,11 @@
 package toolbox.util.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.collections.keyvalue.MultiKey;
+import org.apache.commons.lang.Validate;
+
 import toolbox.util.ArrayUtil;
 
 /**
@@ -8,16 +14,134 @@ import toolbox.util.ArrayUtil;
 public abstract class AbstractService implements Service
 {
     //--------------------------------------------------------------------------
+    // Constants
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Strict state transitions are turned off by default.
+     */
+    private static final boolean DEFAULT_STRICT = false;
+    
+    /**
+     * Mapping of the strict set of transitions.
+     * <pre>  
+     * Key   = (ServiceState beginState, ServiceActivity activity)
+     * Value = (ServiceState endState)
+     * </pre>
+     */
+    private static final Map TRANSITIONS_STRICT;
+    
+    /**
+     * Mapping of the relaxed or unchecked set of transitions.
+     * <pre>  
+     * Key   = (ServiceActivity activity)
+     * Value = (ServiceState endState)
+     * </pre>
+     */
+    private static final Map TRANSITIONS_RELAXED;
+    
+    //--------------------------------------------------------------------------
+    // Static Initializers
+    //--------------------------------------------------------------------------
+    
+    static 
+    {
+        // Populate strict service state transitions
+        TRANSITIONS_STRICT = new HashMap(7);
+
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.UNINITIALIZED, 
+                ServiceActivity.INITIALIZE), 
+            ServiceState.INITIALIZED);
+
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.INITIALIZED, 
+                ServiceActivity.START), 
+            ServiceState.RUNNING);
+        
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.RUNNING, 
+                ServiceActivity.STOP), 
+            ServiceState.STOPPED); 
+        
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.RUNNING, 
+                ServiceActivity.SUSPEND), 
+            ServiceState.SUSPENDED); 
+        
+        TRANSITIONS_STRICT.put(
+             new MultiKey(
+                ServiceState.SUSPENDED, 
+                ServiceActivity.RESUME), 
+             ServiceState.RUNNING);
+
+        TRANSITIONS_STRICT.put(
+                new MultiKey(
+                   ServiceState.SUSPENDED, 
+                   ServiceActivity.START), 
+                ServiceState.RUNNING);
+        
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.SUSPENDED, 
+                ServiceActivity.STOP), 
+            ServiceState.STOPPED); 
+        
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.STOPPED, 
+                ServiceActivity.START), 
+            ServiceState.RUNNING); 
+        
+        TRANSITIONS_STRICT.put(
+            new MultiKey(
+                ServiceState.STOPPED, 
+                ServiceActivity.DESTROY), 
+            ServiceState.DESTROYED); 
+        
+        // Populate relaxed service state transitions
+        TRANSITIONS_RELAXED = new HashMap(6);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.INITIALIZE, 
+            ServiceState.INITIALIZED);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.START, 
+            ServiceState.RUNNING);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.STOP, 
+            ServiceState.STOPPED);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.SUSPEND, 
+            ServiceState.SUSPENDED);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.RESUME, 
+            ServiceState.RUNNING);
+        
+        TRANSITIONS_RELAXED.put(
+            ServiceActivity.DESTROY, 
+            ServiceState.DESTROYED);
+    }
+    
+    //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
 
     /**
-     * State of the service.
+     * State of this service.
      */
     private ServiceState state_;
 
     /**
-     * Previous state of the service.
+     * Previous state of this service.
      */
     private ServiceState previousState_;
 
@@ -25,16 +149,40 @@ public abstract class AbstractService implements Service
      * Array of listeners interested in events that this service generates.
      */
     private ServiceListener[] listeners_;
+     
+    /**
+     * If true, this service will observe strict state transitions, otherwise
+     * relaxed state transitions will apply.
+     * 
+     * @see #TRANSITIONS_RELAXED
+     * @see #TRANSITIONS_STRICT
+     */
+    private boolean strict_;
 
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
-    
+
     /**
-     * Default constructor.
+     * Creates an AbstractService with the default value for strict state
+     * transitions.
+     * 
+     * @see #DEFAULT_STRICT
      */
     protected AbstractService()
     {
+        this(DEFAULT_STRICT);
+    }
+
+    
+    /**
+     * Creates an AbstractService.
+     * 
+     * @param strict True for strict state transitions, false otherwise.
+     */
+    protected AbstractService(boolean strict)
+    {
+        setStrict(strict);
         setPreviousState(ServiceState.UNINITIALIZED);
         setState(ServiceState.UNINITIALIZED);
         listeners_ = new ServiceListener[0];
@@ -97,8 +245,7 @@ public abstract class AbstractService implements Service
      */
     public void initialize() throws ServiceException
     {
-        setState(ServiceState.INITIALIZED);
-        fireServiceStateChanged();
+        transition(ServiceActivity.INITIALIZE);
     }
     
     
@@ -107,31 +254,16 @@ public abstract class AbstractService implements Service
      */
     public void start() throws ServiceException
     {
-        if (getState() == ServiceState.INITIALIZED || 
-            getState() == ServiceState.STOPPED)
-        {    
-            setState(ServiceState.RUNNING);
-            fireServiceStateChanged();
-        }
-        else
-            throw new IllegalStateException(
-                "Cannot start service from the current state of " + getState());
+        transition(ServiceActivity.START);
     }
 
     
     /**
      * @see toolbox.util.service.Service#suspend()
      */
-    public void suspend() throws ServiceException
+    public void suspend() throws ServiceException 
     {
-        if (getState() == ServiceState.RUNNING)
-        {
-            setState(ServiceState.SUSPENDED);
-            fireServiceStateChanged();
-        }
-        else
-            throw new IllegalStateException(
-                "Cannot suspend from the current state of " + getState());
+        transition(ServiceActivity.SUSPEND);
     }
 
     
@@ -140,14 +272,7 @@ public abstract class AbstractService implements Service
      */
     public void resume() throws ServiceException
     {
-        if (getState() == ServiceState.SUSPENDED)
-        {
-            setState(ServiceState.RUNNING);
-            fireServiceStateChanged();
-        }
-        else
-            throw new IllegalStateException(
-                "Cannot resume service from the current state of " + getState());
+        transition(ServiceActivity.RESUME);
     }
 
     
@@ -156,14 +281,7 @@ public abstract class AbstractService implements Service
      */
     public void stop() throws ServiceException
     {
-        if (getState() == ServiceState.RUNNING)
-        {
-            setState(ServiceState.STOPPED);
-            fireServiceStateChanged();
-        }
-        else
-            throw new IllegalStateException(
-                "Cannot stop service from the current state of " + getState());
+        transition(ServiceActivity.STOP);
     }
 
     
@@ -172,14 +290,7 @@ public abstract class AbstractService implements Service
      */
     public void destroy() throws ServiceException
     {
-        if (getState() == ServiceState.STOPPED)
-        {
-            setState(ServiceState.DESTROYED);
-            fireServiceStateChanged();
-        }
-        else
-            throw new IllegalStateException(
-                "Cannot stop service from the current state of " + getState());
+        transition(ServiceActivity.DESTROY);
     }
 
     
@@ -220,12 +331,30 @@ public abstract class AbstractService implements Service
         listeners_ = (ServiceListener[]) ArrayUtil.remove(listeners_, listener);
     }
     
+    
+    /**
+     * @see toolbox.util.service.Service#isStrict()
+     */
+    public boolean isStrict() 
+    {
+        return strict_;
+    }
+    
+    
+    /**
+     * @see toolbox.util.service.Service#setStrict(boolean)
+     */
+    public void setStrict(boolean b) 
+    {
+        strict_ = b;
+    }
+
     //--------------------------------------------------------------------------
     // Protected
     //--------------------------------------------------------------------------
 
     /**
-     * Notifies registered listeners that the service has been initialized.
+     * Notifies registered listeners that this service's state has changed.
      * 
      * @throws ServiceException on service related error.
      */
@@ -233,5 +362,42 @@ public abstract class AbstractService implements Service
     {
         for (int i = 0; i < listeners_.length; 
             listeners_[i++].serviceStateChanged(this));
+    }
+    
+    
+    /**
+     * Validates that a service state transition is valid if the strict state
+     * transition flag is enabled.
+     * 
+     * @param activity Service activity.
+     * @throws ServiceException if the state transition is invalid.
+     */
+    protected void transition(ServiceActivity activity) throws ServiceException
+    {
+        if (isStrict()) 
+        {
+            ServiceState nextState = (ServiceState) 
+                TRANSITIONS_STRICT.get(new MultiKey(getState(), activity));
+
+            try
+            {
+                Validate.isTrue(nextState != null);
+                setState(nextState);
+            }
+            catch (IllegalArgumentException iae)
+            {
+                throw new ServiceException(
+                        "Invalid service state transition from " 
+                        + getState()
+                        + " with activity "
+                        + activity);
+            }
+        }
+        else
+        {
+            setState((ServiceState) TRANSITIONS_RELAXED.get(activity));
+        }
+            
+        fireServiceStateChanged();
     }
 }
