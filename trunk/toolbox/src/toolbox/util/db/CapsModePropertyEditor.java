8ff08d215a91fbeecf7922a69b3cb37cea8a5812
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-06-30 21:12:25 +0000

    Javadoc updates

--- trunk/toolbox/src/toolbox/util/db/CapsModePropertyEditor.java
+++ trunk/toolbox/src/toolbox/util/db/CapsModePropertyEditor.java
@@ -2,17 +2,17 @@ package toolbox.util.db;
 
 import com.l2fprod.common.beans.editor.ComboBoxPropertyEditor;
 
-import toolbox.util.StringUtil;
-
 /**
- * Propery editor for CapsMode.
+ * Propery editor for CapsMode that renders each of the available capitalization
+ * modes in a combobox.
  */
 public class CapsModePropertyEditor extends ComboBoxPropertyEditor
 {
+    /**
+     * Creates a CapsModePropertyEditor.
+     */
     public CapsModePropertyEditor()
     {
-        System.out.println(StringUtil.banner("creating caps mode prop editor!!!"));
-        
         Object[] values = new Object[]
         {
             CapsMode.LOWERCASE,
