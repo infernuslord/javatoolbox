3503eeb88ed29f2a531942108aecc272ce46c1a2
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-06 22:28:27 +0000

    Updated to use a property sheet

--- toolbox/src/toolbox/plugin/jdbc/DBPrefsView.java
+++ toolbox/src/toolbox/plugin/jdbc/DBPrefsView.java
@@ -1,272 +1,146 @@
 package toolbox.plugin.jdbc;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.BorderLayout;
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyDescriptor;
+import java.util.ArrayList;
+import java.util.List;
 
-import javax.swing.JComponent;
 import javax.swing.JPanel;
-import javax.swing.SwingConstants;
 
-import nu.xom.Attribute;
-import nu.xom.Element;
+import com.l2fprod.common.propertysheet.Property;
+import com.l2fprod.common.propertysheet.PropertySheetPanel;
 
-import toolbox.util.XOMUtil;
-import toolbox.util.ui.JHeaderPanel;
-import toolbox.util.ui.JSmartCheckBox;
-import toolbox.util.ui.JSmartLabel;
-import toolbox.util.ui.JSmartTextField;
-import toolbox.workspace.prefs.PreferencesView;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.log4j.Logger;
+
+import toolbox.util.ArrayUtil;
+import toolbox.util.ExceptionUtil;
+import toolbox.util.StringUtil;
+import toolbox.util.collections.AsMap;
 
 /**
- * JDBC Plugin Preferences.
+ * DBPrefsView allows the QueryPlugin preferences to be edited. 
  */
-public class DBPrefsView extends JHeaderPanel implements PreferencesView
+public class DBPrefsView extends JPanel
 {
-    //--------------------------------------------------------------------------
-    // XML Constants
-    //--------------------------------------------------------------------------
-
-    public  static final String NODE_JDBC_PLUGIN          = "DBPrefsView";
-    private static final String ATTR_ERROR_PANE_ENABLED   = "errorPaneEnabled";
-    private static final String ATTR_AUTOSCROLL_THRESHOLD = "autoscrollThreshold";
-    private static final String ATTR_SQL_TERMINATOR       = "sqlTerminator";
+    private static final Logger logger_ = Logger.getLogger(DBPrefsView.class);
 
     //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
-
+    
     /**
-     * Enables routing of JDBC error messages to an error page instead of 
-     * popping up a modal dialog box each time an error occurs. This is not 
-     * enabled by default.
+     * Property sheet panel.
      */
-    private JSmartCheckBox errorPaneEnabledCheckBox_;
-
+    private PropertySheetPanel sheet_;
+    
     /**
-     * Sequence of characters that the SQL parser will use to determine the 
-     * termination of a SQL in the editor pane. The default value is a 
-     * semicolon.
+     * Query plugin.
      */
-    private JSmartTextField sqlTerminatorField_;
-
-    /**
-     * Holds a positive number that specifies the number of lines of output 
-     * that must occur for the output pane to automatically scroll to the bottom
-     * after the execution of a sql statement.
-     */
-    private JSmartTextField autoscrollThresholdField_;
-
-//    /**
-//     * Field for the username if the proxy requires authentication. If the
-//     * field is left blank, then it us assumed that the proxy does not require
-//     * authentication.
-//     */
-//    private JSmartTextField proxyUserNameField_;
-//
-//    /**
-//     * Field for the passward if the proxy requires authentication. The password
-//     * is stored in clear text.
-//     */
-//    protected JSmartTextField proxyPasswordField_;
-
+    private QueryPlugin plugin_;
+    
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
-
+    
     /**
      * Creates a DBPrefsView.
+     * 
+     * @param formatter SQL formatter.
      */
-    public DBPrefsView()
+    public DBPrefsView(QueryPlugin plugin)
     {
-        super("");
-        setTitle(getLabel());
         buildView();
+        setPlugin(plugin);
     }
 
     //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Sets the plugin and refreshes the property sheet.
+     * 
+     * @param plugin Query plugin.
+     */
+    public void setPlugin(QueryPlugin plugin)
+    {
+        plugin_ = plugin;
+        sheet_.readFromObject(plugin);
+    }
+    
+    //--------------------------------------------------------------------------
     // Protected
     //--------------------------------------------------------------------------
-
+    
     /**
      * Constructs the user interface.
      */
     protected void buildView()
     {
-        JPanel p = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
+        BeanInfo beanInfo = null;
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
-        gbc.insets = new Insets(0, 4, 7, 4);
+        try
+        {
+            beanInfo = Introspector.getBeanInfo(QueryPlugin.class);
+        }
+        catch (IntrospectionException ie)
+        {
+            ExceptionUtil.handleUI(ie, logger_);
+        }
 
-        p.add(new JSmartLabel("Use Error Pane", SwingConstants.RIGHT), gbc);
+        // Build the list of properties to support
+        PropertyDescriptor[] dArray = beanInfo.getPropertyDescriptors();
+        List descriptors = new ArrayList();
+        CollectionUtils.addAll(descriptors, dArray);
+        
+        // Remove unsupported properties
+        CollectionUtils.filter(descriptors, new Predicate()
+        {
+            public boolean evaluate(Object object)
+            {
+                PropertyDescriptor pd = (PropertyDescriptor) object;
+                return ArrayUtil.contains(
+                    QueryPlugin.SAVED_PROPS, pd.getName());
+            }
+        });
 
-        gbc.gridx++;
-        p.add(errorPaneEnabledCheckBox_ = new JSmartCheckBox(), gbc);
+        dArray = (PropertyDescriptor[]) 
+            descriptors.toArray(new PropertyDescriptor[0]);
 
-        gbc.gridy++;
-        gbc.gridx--;
-        p.add(new JSmartLabel("SQL Terminator", SwingConstants.RIGHT), gbc);
+        if (logger_.isDebugEnabled())
+            for (int i = 0; i < dArray.length; i++)
+                logger_.info(StringUtil.banner(AsMap.of(dArray[i]) + ""));
 
-        gbc.gridx++;
-        p.add(sqlTerminatorField_ = new JSmartTextField(14), gbc);
-
-        gbc.gridy++;
-        gbc.gridx--;
-        p.add(new JSmartLabel("Autoscroll Threshold", SwingConstants.RIGHT),
-            gbc);
-
-        gbc.gridx++;
-        p.add(autoscrollThresholdField_ = new JSmartTextField(4), gbc);
-
-//        gbc.gridy++;
-//        gbc.gridx--;
-//        p.add(new JSmartLabel("Username", SwingConstants.RIGHT), gbc);
-//
-//        gbc.gridx++;
-//        p.add(proxyUserNameField_ = new JSmartTextField(14), gbc);
-//
-//        gbc.gridy++;
-//        gbc.gridx--;
-//        p.add(new JSmartLabel("Password", SwingConstants.RIGHT), gbc);
-//
-//        gbc.gridx++;
-//        p.add(proxyPasswordField_ = new JSmartTextField(14), gbc);
-//
-//        gbc.gridy++;
-//        p.add(new JSmartButton(new TestProxyAction()), gbc);
-
-        setContent(p);
+        // Create and init the property sheet
+        sheet_ = new PropertySheetPanel();
+        sheet_.setProperties(dArray);
+        sheet_.addPropertySheetChangeListener(new MyPropertyChangeListener());
+        
+        // Build the GUI
+        setLayout(new BorderLayout());
+        add(BorderLayout.CENTER, sheet_);
     }
 
-    //--------------------------------------------------------------------------
-    // PreferencesView Interface
-    //--------------------------------------------------------------------------
+    //----------------------------------------------------------------------
+    // MyPropertyChangeListener
+    //----------------------------------------------------------------------
 
     /**
-     * @see toolbox.workspace.prefs.PreferencesView#getLabel()
+     * Writes the changed property back to the source javabean.
      */
-    public String getLabel()
+    class MyPropertyChangeListener implements PropertyChangeListener
     {
-        return "JDBC Plugin Preferences";
-    }
-
-
-    /**
-     * @see toolbox.workspace.prefs.PreferencesView#getView()
-     */
-    public JComponent getView()
-    {
-        return this;
-    }
-
-
-    /**
-     * @see toolbox.workspace.prefs.PreferencesView#onOK()
-     */
-    public void onOK()
-    {
-        onApply();
-    }
-
-
-    /**
-     * @see toolbox.workspace.prefs.PreferencesView#onApply()
-     */
-    public void onApply()
-    {
-    }
-
-
-    /**
-     * @see toolbox.workspace.prefs.PreferencesView#onCancel()
-     */
-    public void onCancel()
-    {
-    }
-
-    //--------------------------------------------------------------------------
-    // IPreferenced Interface
-    //--------------------------------------------------------------------------
-
-    /**
-     * @see toolbox.workspace.IPreferenced#applyPrefs(nu.xom.Element)
-     */
-    public void applyPrefs(Element prefs) throws Exception
-    {
-        Element prefsView =
-            XOMUtil.getFirstChildElement(
-                prefs, NODE_JDBC_PLUGIN, new Element(NODE_JDBC_PLUGIN));
-
-        errorPaneEnabledCheckBox_.setSelected(
-            XOMUtil.getBooleanAttribute(
-                prefsView,
-                ATTR_ERROR_PANE_ENABLED,
-                false));
-
-        sqlTerminatorField_.setText(
-            XOMUtil.getStringAttribute(
-                prefsView,
-                ATTR_SQL_TERMINATOR,
-                ";"));
-
-        autoscrollThresholdField_.setText(
-            XOMUtil.getStringAttribute(
-                prefsView,
-                ATTR_AUTOSCROLL_THRESHOLD,
-                "50"));
-
-//        proxyUserNameField_.setText(
-//            XOMUtil.getStringAttribute(
-//                prefsView,
-//                ATTR_HTTP_PROXY_USERNAME,
-//                ""));
-//
-//        proxyPasswordField_.setText(
-//            XOMUtil.getStringAttribute(
-//                prefsView,
-//                ATTR_HTTP_PROXY_PASSWORD,
-//                ""));
-        onApply();
-    }
-
-
-    /**
-     * @see toolbox.workspace.IPreferenced#savePrefs(nu.xom.Element)
-     */
-    public void savePrefs(Element prefs) throws Exception
-    {
-        Element prefsView = new Element(NODE_JDBC_PLUGIN);
-
-        prefsView.addAttribute(
-            new Attribute(
-                ATTR_ERROR_PANE_ENABLED,
-                errorPaneEnabledCheckBox_.isSelected() + ""));
-
-        prefsView.addAttribute(
-            new Attribute(
-                ATTR_SQL_TERMINATOR,
-                sqlTerminatorField_.getText()));
-
-        prefsView.addAttribute(
-            new Attribute(
-                ATTR_AUTOSCROLL_THRESHOLD,
-                autoscrollThresholdField_.getText().trim()));
-
-//        prefsView.addAttribute(
-//            new Attribute(
-//                ATTR_HTTP_PROXY_USERNAME,
-//                proxyUserNameField_.getText().trim()));
-//
-//        prefsView.addAttribute(
-//            new Attribute(
-//                ATTR_HTTP_PROXY_PASSWORD,
-//                proxyPasswordField_.getText().trim()));
-
-        XOMUtil.insertOrReplace(prefs, prefsView);
+        public void propertyChange(PropertyChangeEvent evt)
+        {
+            Property p = (Property) evt.getSource();
+            p.writeToObject(plugin_);
+        }
     }
 }
