b00e31bf4fdf08908f27e159768c17ce9b245fdf
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-13 18:24:25 +0000

    Javadoc updates

--- toolbox/src/toolbox/util/random/RandomSequenceFactory.java
+++ toolbox/src/toolbox/util/random/RandomSequenceFactory.java
@@ -2,21 +2,50 @@ package toolbox.util.random;
 
 import java.util.List;
 
-
+/**
+ * RandomSequenceFactory is a factory for various types of randomized sequences.
+ * 
+ * @see toolbox.util.random.RandomSequence
+ */
 public class RandomSequenceFactory
 {
-    public static RandomSequence create(boolean repeating, int low, int high)
+    /**
+     * Creates a randomized integer sequence.
+     * 
+     * @param low Lower bound.
+     * @param high Upper bound.
+     * @param nonRepeating True if the sequence should not repeat values.
+     * @return RandomSequence
+     */
+    public static RandomSequence create(int low, int high, boolean nonRepeating)
     {
-        return new IntSequence(low, high, repeating);
+        return new IntSequence(low, high, nonRepeating);
     }
     
-    public static RandomSequence create(boolean repeating, List objectList)
+    
+    /**
+     * Creates a randomized sequence from a given list of existing objects.
+     *  
+     * @param objectList List of objects from which to create the sequence.
+     * @param nonRepeating True if the sequence should not repeat values.
+     * @return RandomSequence
+     */
+    public static RandomSequence create(List objectList, boolean nonRepeating)
     {
-        return new ObjectSequence(objectList, repeating);
+        return new ObjectSequence(objectList, nonRepeating);
     }
-
-    public static RandomSequence create(boolean repeating, Object[] objectList)
+    
+    
+    /**
+     * Creates a randomized sequence from a given array of existing objects.
+     *  
+     * @param objectList Array of objects from which to create the sequence.
+     * @param nonRepeating True if the sequence should not repeat values.
+     * @return RandomSequence
+     */
+    public static RandomSequence create(
+        Object[] objectList, boolean nonRepeating)
     {
-        return new ObjectSequence(objectList, repeating);
+        return new ObjectSequence(objectList, nonRepeating);
     }
 }
