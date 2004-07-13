db058c55b2620a5ce8969c887e226c35ac9b8739
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-13 18:23:39 +0000

    Renamed isRepeating() to isNonRepeating()

--- trunk/toolbox/src/toolbox/util/random/RandomSequence.java
+++ trunk/toolbox/src/toolbox/util/random/RandomSequence.java
@@ -1,11 +1,10 @@
 package toolbox.util.random;
 
-
 /**
- * A RandomSequence is responsible for generating a sequence of 
- * non-repreating values. If all possible values have been generated, the
- * sequence has ended and will throw a SequenceEndedException if asked to
- * create additional values.
+ * A RandomSequence is responsible for generating a sequence of random values 
+ * that can either be repeating or non-repeating. If all possible values have 
+ * been generated for a non-repeating sequence, a SequenceEndedException is
+ * thrown to on subsequent calls to nextValue().
  */
 interface RandomSequence
 {
@@ -13,24 +12,25 @@ interface RandomSequence
      * Returns the next value in the sequence.
      * 
      * @return Object
-     * @throws SequenceEndedException if the sequence has been exhausted and 
-     *         there are no more values left to generate.
+     * @throws SequenceEndedException if the sequence is non-repeating and has 
+     *         no more unique values.
      */
     Object nextValue() throws SequenceEndedException;
     
     
     /**
-     * Returns true if this sequence may repeat values in the sequence, false
-     * otherwise.
+     * Returns true if this sequence guarantees that generated values will 
+     * not repeat, false otherwise.
      * 
      * @return boolean
      */
-    boolean isRepeating();
+    boolean isNonRepeating();
     
     
     /**
      * Returns true if the sequence is non-repeating and has at least one more 
-     * value that can be generated, false otherwise.
+     * value that can be generated, false otherwise. Always returns true for
+     * repeating sequences.
      * 
      * @return boolean
      */
