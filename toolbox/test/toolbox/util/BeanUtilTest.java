aa02b5b7d49b90038570c894817b8ac94b680934
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-16 23:18:00 +0000

    Initial version

---
+++ toolbox/test/toolbox/util/BeanUtilTest.java
@@ -0,0 +1,50 @@
+package toolbox.util;
+
+import javax.swing.JLabel;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
+/**
+ * Unit test for BeanUtil.
+ * 
+ * @see toolbox.util.BeanUtil
+ */
+public class BeanUtilTest extends TestCase
+{
+    private static final Logger logger_ = Logger.getLogger(BeanUtilTest.class);
+    
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args)
+    {
+        TestRunner.run(BeanUtilTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Tests toString(javabean)
+     */
+    public void testToStringObject()
+    {
+        logger_.info("Running testToStringObject...");
+        
+        JLabel label = new JLabel("Whoopee!");
+        
+        logger_.info(
+            StringUtil.banner(
+                StringUtils.center("JLabel BeanInfo", 80)
+                + "\n"
+                + StringUtil.BR 
+                + "\n" 
+                + BeanUtil.toString(label)));
+    }
+}
