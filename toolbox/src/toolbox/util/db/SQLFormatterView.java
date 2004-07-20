c652de4e40fa10e89e0d86ad0268634adbb7209e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-20 21:25:05 +0000

    Updated to use BeanPropertyFilter for narrowing down the list of editable properties. Also set description to visible and wrapped in a JHeaderPanel so the flipper has a title.

--- toolbox/src/toolbox/util/db/SQLFormatterView.java
+++ toolbox/src/toolbox/util/db/SQLFormatterView.java
@@ -1,6 +1,5 @@
 package toolbox.util.db;
 
-import java.awt.BorderLayout;
 import java.beans.BeanInfo;
 import java.beans.IntrospectionException;
 import java.beans.Introspector;
@@ -10,28 +9,29 @@ import java.beans.PropertyDescriptor;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JPanel;
-
 import com.l2fprod.common.propertysheet.Property;
 import com.l2fprod.common.propertysheet.PropertyEditorRegistry;
 import com.l2fprod.common.propertysheet.PropertySheetPanel;
 
 import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.collections.Predicate;
 import org.apache.log4j.Logger;
 
-import toolbox.util.ArrayUtil;
 import toolbox.util.ExceptionUtil;
 import toolbox.util.StringUtil;
+import toolbox.util.beans.BeanPropertyFilter;
 import toolbox.util.collections.AsMap;
+import toolbox.util.ui.ImageCache;
+import toolbox.util.ui.JHeaderPanel;
 
 /**
- * SQLFormatterView is view onto a SQLFormatter allowing javabean properties 
- * to be changed via the user interface.
+ * SQLFormatterView is a UI component that allows for the configuration of a
+ * SQLFormatter. The editable properties of the SQLFormatter are presented in
+ * a property sheet. Updates to the properties are reflected in the SQLFormatter
+ * immediately upon pressing the enter key for a given property. 
  * 
  * @see toolbox.util.db.SQLFormatter
  */
-public class SQLFormatterView extends JPanel
+public class SQLFormatterView extends JHeaderPanel
 {
     private static final Logger logger_ = 
         Logger.getLogger(SQLFormatterView.class);
@@ -41,12 +41,12 @@ public class SQLFormatterView extends JPanel
     //--------------------------------------------------------------------------
     
     /**
-     * SQLFormatter that serves as the model for this view.
+     * SQLFormatter to configure.
      */
     private SQLFormatter formatter_;
 
     /**
-     * Property sheet panel.
+     * Property sheet that displays the SQLFormatter's configurable properties.
      */
     private PropertySheetPanel sheet_;
     
@@ -56,34 +56,37 @@ public class SQLFormatterView extends JPanel
 
     /**
      * Creates a SQLFormatterView.
-     */
-    public SQLFormatterView()
-    {
-        buildView();
-    }
-
-    
-    /**
-     * Creates a SQLFormatterView.
      * 
-     * @param formatter SQL formatter.
+     * @param formatter SQL formatter to configure.
      */
     public SQLFormatterView(SQLFormatter formatter)
     {
+        super(
+            ImageCache.getIcon(ImageCache.IMAGE_CONFIG), 
+            "SQL Formatter Properties");
+        
         buildView();
         setFormatter(formatter);
     }
 
     //--------------------------------------------------------------------------
-    // Protected
+    // Public
     //--------------------------------------------------------------------------
     
+    /**
+     * Sets the SQL formatter and updates the property sheet.
+     * 
+     * @param formatter SQL formatter.
+     */
     public void setFormatter(SQLFormatter formatter)
     {
         formatter_ = formatter;
         sheet_.readFromObject(formatter_);
     }
     
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
     
     /**
      * Constructs the user interface.
@@ -110,20 +113,18 @@ public class SQLFormatterView extends JPanel
         List descriptors = new ArrayList();
         CollectionUtils.addAll(descriptors, dArray);
         
-        // Remove unsupported properties
-        CollectionUtils.filter(descriptors, new Predicate()
-        {
-            public boolean evaluate(Object object)
-            {
-                PropertyDescriptor pd = (PropertyDescriptor) object;
-                return ArrayUtil.contains(
-                    SQLFormatter.SAVED_PROPERTIES, pd.getName());
-            }
-        });
-
+        // Filter out only the properties that the SQLFormatter saves as 
+        // preferences.
+        CollectionUtils.filter(
+            descriptors, 
+            new BeanPropertyFilter(
+                "name", 
+                SQLFormatter.SAVED_PROPERTIES));
+                
         dArray = (PropertyDescriptor[]) 
             descriptors.toArray(new PropertyDescriptor[0]);
 
+        // DEBUG
         if (logger_.isDebugEnabled())
             for (int i = 0; i < dArray.length; i++)
                 logger_.info(StringUtil.banner(AsMap.of(dArray[i]) + ""));
@@ -132,11 +133,10 @@ public class SQLFormatterView extends JPanel
         sheet_ = new PropertySheetPanel();
         sheet_.setEditorRegistry(registry);
         sheet_.setProperties(dArray);
+        sheet_.setDescriptionVisible(true);
         sheet_.addPropertySheetChangeListener(new MyPropertyChangeListener());
         
-        // Build the GUI
-        setLayout(new BorderLayout());
-        add(BorderLayout.CENTER, sheet_);
+        setContent(sheet_);
     }
 
     //----------------------------------------------------------------------
@@ -150,15 +150,8 @@ public class SQLFormatterView extends JPanel
     {
         public void propertyChange(PropertyChangeEvent evt)
         {
-            //logger_.debug(
-            //    "old = " + evt.getOldValue() 
-            //    + " new = " + evt.getNewValue() 
-            //    + " class= " + evt.getSource().getClass());
-
             Property p = (Property) evt.getSource();
             p.writeToObject(formatter_);
-
-            //logger_.debug(StringUtil.banner(AsMap.of(formatter_).toString()));
         }
     }
 }
