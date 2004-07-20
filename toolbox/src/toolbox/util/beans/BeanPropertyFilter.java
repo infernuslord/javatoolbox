3a6fcc21e4cf3c514fe23a6b069ccdfec4e78b8c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-20 21:23:10 +0000

    Initial version

---
+++ toolbox/src/toolbox/util/beans/BeanPropertyFilter.java
@@ -0,0 +1,156 @@
+package toolbox.util.beans;
+
+import java.util.Collection;
+
+import org.apache.commons.beanutils.PropertyUtils;
+import org.apache.commons.collections.Predicate;
+import org.apache.log4j.Logger;
+
+import toolbox.util.ArrayUtil;
+
+/**
+ * Filters java beans using the value of a bean property as the selector. 
+ * Filter criteria can be one of the following:
+ * <ul>
+ *   <li>Single object 
+ *   <li>Collection of objects
+ *   <li>Array of objects
+ * </ul>
+ * <p>
+ * Usage:
+ * 
+ * <pre>
+ * 
+ * // Plain old java bean with two properties, 'first' and 'last' 
+ * class Name
+ * {
+ *     String first;
+ *     String last;
+ * 
+ * 
+ *     public String getFirst()
+ *     {
+ *         return first;
+ *     };
+ * 
+ * 
+ *     public void setFirst(Sting s)
+ *     {
+ *         first = s;
+ *     };
+ * 
+ * 
+ *     public String getLast()
+ *     {
+ *         return last;
+ *     };
+ * 
+ * 
+ *     public void setLast(Sting s)
+ *     {
+ *         last = s;
+ *     };
+ * }
+ * 
+ * // Lets filter out all the joe's 
+ * Collection names = new ArrayList();
+ * names.add(new Name(&quot;joe&quot;, &quot;schmo&quot;));
+ * names.add(new Name(&quot;flo&quot;, &quot;jo&quot;));
+ * 
+ * Predicate joeFilter = new BeanPropertyFilter(&quot;first&quot;, &quot;joe&quot;);
+ * Collection joes = CollectionUtils.select(names, joeFilter);
+ * </pre>
+ * 
+ * @see org.apache.commons.collections.CollectionUtils
+ */
+public class BeanPropertyFilter implements Predicate
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(BeanPropertyFilter.class);
+    
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+
+    /**
+     * Name of the javabean property.
+     */
+    private String propertyName_;
+
+    /**
+     * Values to filter.
+     */
+    private Object[] filterValues_;
+
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+
+    /**
+     * Creates a BeanPropertyFilter with the given javabean property name and
+     * value.
+     * 
+     * @param name Name of the javabean property.
+     * @param value Value to compare with the value of the javabean property.
+     */
+    public BeanPropertyFilter(String name, Object value)
+    {
+        this(name, new Object[] {value});
+    }
+
+    
+    /**
+     * Creates a BeanPropertyFilter.
+     * 
+     * @param name Name of the javabean property.
+     * @param values Collection of values to match the value of the javabean
+     *        property against.
+     */
+    public BeanPropertyFilter(String name, Collection values)
+    {
+        this(name, values.toArray());
+    }
+
+    
+    /**
+     * Creates a BeanPropertyFilter.
+     * 
+     * @param name Name of the javabean property.
+     * @param values Array of values to match the value of the javabean property
+     *        against.
+     */
+    public BeanPropertyFilter(String name, Object[] values)
+    {
+        propertyName_ = name;
+        filterValues_ = values;
+    }
+
+    //--------------------------------------------------------------------------
+    // Predicate Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see org.apache.commons.collections.Predicate#evaluate( java.lang.Object)
+     */
+    public boolean evaluate(Object javabean)
+    {
+        // Extract the named javabean property value from the subject and
+        // compare it to the value passed in via the constructor.
+
+        Object propertyValue = null;
+
+        try
+        {
+            propertyValue = PropertyUtils.getProperty(javabean, propertyName_);
+        }
+        catch (Exception e)
+        {
+            logger_.error(e);
+        }
+        
+        if (propertyValue == null)
+            return false;
+        else    
+            return ArrayUtil.contains(filterValues_, propertyValue);
+    }
+}
---
+++ toolbox/test/toolbox/util/beans/BeanPropertyFilterTest.java
@@ -0,0 +1,369 @@
+package toolbox.util.beans;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * Unit test for BeanPropertyFilter.
+ * 
+ * @see toolbox.util.beans.BeanPropertyFilter
+ */
+public class BeanPropertyFilterTest extends TestCase
+{
+    private static final Log logger_ = 
+        LogFactory.getLog(BeanPropertyFilterTest.class);
+
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+
+    private MockBean vanilla = new MockBean("vanilla", 7);
+    private MockBean roast = new MockBean("roast", 1);
+    private MockBean french = new MockBean("french", 7);
+    private MockBean origin = new MockBean(
+        "ignore", 0, new Origin("south america"));
+    
+    private List beans;
+
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+
+    public static void main(String[] args)
+    {
+        TestRunner.run(BeanPropertyFilterTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Overrides TestCase
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see junit.framework.TestCase#setUp()
+     */
+    protected void setUp() throws Exception
+    {
+        beans = new ArrayList();
+        beans.add(vanilla);
+        beans.add(roast);
+        beans.add(french);
+        beans.add(origin);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+
+    /**
+     * Filter matches zero elements.
+     */
+    public void testEvaluateZero()
+    {
+        logger_.info("Running testEvaluateZero...");
+
+        // Match 0
+        BeanPropertyFilter bogusFilter = new BeanPropertyFilter("flavor",
+            "bogus");
+        Collection bogii = CollectionUtils.select(beans, bogusFilter);
+        assertTrue(bogii.isEmpty());
+    }
+
+
+    /**
+     * Filter matches one element.
+     */
+    public void testEvaluateOne()
+    {
+        logger_.info("Running testEvaluateOne...");
+
+        // Match 1
+        BeanPropertyFilter vanillaFilter = new BeanPropertyFilter("flavor",
+            "vanilla");
+        Collection vanillas = CollectionUtils.select(beans, vanillaFilter);
+        assertEquals(1, vanillas.size());
+        assertEquals(vanilla, vanillas.iterator().next());
+    }
+
+
+    /**
+     * Filter matches > 1 element.
+     */
+    public void testEvaluateMany()
+    {
+        logger_.info("Running testEvaluateMany...");
+
+        // Match > 1
+        BeanPropertyFilter manyFilter = new BeanPropertyFilter("acidity",
+            new Integer(7));
+
+        Collection many = CollectionUtils.select(beans, manyFilter);
+        assertEquals(2, many.size());
+        assertTrue(many.contains(vanilla));
+        assertTrue(many.contains(french));
+    }
+
+
+    /**
+     * Filter that matches a non-trivial javabean property (not primitive, not
+     * string).
+     */
+    public void testEvaluateObject()
+    {
+        logger_.info("Running testEvaluateObject...");
+
+        BeanPropertyFilter filter = new BeanPropertyFilter("origin",
+            new Origin("south america"));
+        Collection match = CollectionUtils.select(beans, filter);
+        assertEquals(1, match.size());
+        assertEquals(origin, match.iterator().next());
+    }
+
+    
+    /**
+     * Filter is an array.
+     */
+    public void testEvaluateArrayZero()
+    {
+        logger_.info("Running testEvaluateArrayZero...");
+
+        BeanPropertyFilter filter =
+            new BeanPropertyFilter("flavor", new String[] {});
+        
+        Collection result = CollectionUtils.select(beans, filter);
+        assertTrue(result.isEmpty());
+    }
+
+
+    /**
+     * Filter matches one element in a array of size one.
+     */
+    public void testEvaluateArrayOneMatch()
+    {
+        logger_.info("Running testEvaluateArrayOneMatch...");
+
+        BeanPropertyFilter filter = 
+            new BeanPropertyFilter("flavor", new Object[] {"vanilla"});
+        
+        Collection results = CollectionUtils.select(beans, filter);
+        assertEquals(1, results.size());
+        assertEquals(vanilla, results.iterator().next());
+    }
+
+    
+    /**
+     * Filter does not match one element in a array of size one.
+     */
+    public void testEvaluateArrayOneMismatch()
+    {
+        logger_.info("Running testEvaluateArrayOneMismatch...");
+
+        BeanPropertyFilter filter = 
+            new BeanPropertyFilter("flavor", new Object[] {"bogus"});
+        
+        Collection results = CollectionUtils.select(beans, filter);
+        assertEquals(0, results.size());
+    }
+
+    
+    /**
+     * Filter matches one element in a array of size > 1.
+     */
+    public void testEvaluateArrayManyMatch()
+    {
+        logger_.info("Running testEvaluateArrayManyMatch...");
+
+        BeanPropertyFilter filter = 
+            new BeanPropertyFilter("flavor", 
+                new Object[] {"bogus1", "vanilla", "bogus2"});
+        
+        Collection results = CollectionUtils.select(beans, filter);
+        assertEquals(1, results.size());
+        assertEquals(vanilla, results.iterator().next());
+    }
+
+    
+    /**
+     * Filter matches one element in a array of size > 1.
+     */
+    public void testEvaluateCollectionManyMatch()
+    {
+        logger_.info("Running testEvaluateCollectionManyMatch...");
+
+        List values = new ArrayList();
+        values.add("bogus1");
+        values.add("vanilla");
+        values.add("bogus2");
+        
+        BeanPropertyFilter filter = new BeanPropertyFilter("flavor", values); 
+        Collection results = CollectionUtils.select(beans, filter);
+        assertEquals(1, results.size());
+        assertEquals(vanilla, results.iterator().next());
+    }
+    
+    //--------------------------------------------------------------------------
+    // MockBean
+    //--------------------------------------------------------------------------
+
+    /**
+     * Simple class with two bean properties.
+     */
+    public class MockBean
+    {
+        /**
+         * Bean propery name = flavor.
+         */
+        private String flavor;
+
+        /**
+         * Bean property name = acidity.
+         */
+        private int acidity;
+
+        /**
+         * Bean that does not have a default equals() impl.
+         */
+        private Origin origin;
+
+        //----------------------------------------------------------------------
+        // Constructors
+        //----------------------------------------------------------------------
+
+        /**
+         * Creates a MockBean.
+         * 
+         * @param flavor Flavor
+         * @param acidity Acidity
+         */
+        public MockBean(String flavor, int acidity)
+        {
+            setFlavor(flavor);
+            setAcidity(acidity);
+        }
+
+
+        /**
+         * Creates a MockBean.
+         * 
+         * @param flavor Flavor
+         * @param acidity Acidity
+         */
+        public MockBean(String flavor, int acidity, Origin origin)
+        {
+            setFlavor(flavor);
+            setAcidity(acidity);
+            setOrigin(origin);
+        }
+
+        //----------------------------------------------------------------------
+        // Accessors/Mutators
+        //----------------------------------------------------------------------
+
+        /**
+         * Returns the acidity.
+         * 
+         * @return int
+         */
+        public int getAcidity()
+        {
+            return acidity;
+        }
+
+
+        /**
+         * Sets the value of acidity.
+         * 
+         * @param acidity The acidity to set.
+         */
+        public void setAcidity(int acidity)
+        {
+            this.acidity = acidity;
+        }
+
+
+        /**
+         * Returns the flavor.
+         * 
+         * @return String
+         */
+        public String getFlavor()
+        {
+            return flavor;
+        }
+
+
+        /**
+         * Sets the value of flavor.
+         * 
+         * @param flavor The flavor to set.
+         */
+        public void setFlavor(String flavor)
+        {
+            this.flavor = flavor;
+        }
+
+
+        /**
+         * @return Origin
+         */
+        public Origin getOrigin()
+        {
+            return origin;
+        }
+
+
+        /**
+         * @param origin
+         */
+        public void setOrigin(Origin origin)
+        {
+            this.origin = origin;
+        }
+
+    }
+
+    //--------------------------------------------------------------------------
+    // Origin
+    //--------------------------------------------------------------------------
+
+    public class Origin
+    {
+
+        String name;
+
+
+        public Origin(String s)
+        {
+            name = s;
+        }
+
+
+        public boolean equals(Object obj)
+        {
+            if (obj == null)
+                return false;
+
+            return name.equals(((Origin) obj).getName());
+        }
+
+
+        public String getName()
+        {
+            return name;
+        }
+
+
+        public void setName(String string)
+        {
+            name = string;
+        }
+    }
+
+}
