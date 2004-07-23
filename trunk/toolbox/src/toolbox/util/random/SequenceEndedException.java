2593217f2d0f1353f3fbab2b85945492f6ce0a31
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-23 22:22:35 +0000

    Removed unused constructors

--- trunk/toolbox/src/toolbox/util/random/SequenceEndedException.java
+++ trunk/toolbox/src/toolbox/util/random/SequenceEndedException.java
@@ -10,42 +10,11 @@ public class SequenceEndedException extends Exception
 {
     /**
      * Creates a SequenceEndedException.
-     */
-    public SequenceEndedException()
-    {
-    }
-
-
-    /**
-     * Creates a SequenceEndedException.
      * 
      * @param message Error message.
      */
     public SequenceEndedException(String message)
     {
         super(message);
-    }
-
-
-    /**
-     * Creates a SequenceEndedException.
-     * 
-     * @param cause Cause.
-     */
-    public SequenceEndedException(Throwable cause)
-    {
-        super(cause);
-    }
-
-
-    /**
-     * Creates a SequenceEndedException.
-     * 
-     * @param message Error message.
-     * @param cause Cause
-     */
-    public SequenceEndedException(String message, Throwable cause)
-    {
-        super(message, cause);
     }
 }
