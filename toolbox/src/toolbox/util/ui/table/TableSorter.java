1028299c7c0eec187798815d468e75f12a7ac03c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-26 05:26:31 +0000

    Checkstyle updates

--- toolbox/src/toolbox/util/ui/table/TableSorter.java
+++ toolbox/src/toolbox/util/ui/table/TableSorter.java
@@ -611,20 +611,40 @@ public class TableSorter extends AbstractTableModel
         }
 
 
-        public Component getTableCellRendererComponent(JTable table,
-            Object value, boolean isSelected, boolean hasFocus, int row,
+        /**
+         * @see javax.swing.table.TableCellRenderer
+         *      #getTableCellRendererComponent(javax.swing.JTable,
+         *      java.lang.Object, boolean, boolean, int, int)
+         */
+        public Component getTableCellRendererComponent(
+            JTable table,
+            Object value, 
+            boolean isSelected, 
+            boolean hasFocus, 
+            int row,
             int column)
         {
-            Component c = tableCellRenderer.getTableCellRendererComponent(
-                table, value, isSelected, hasFocus, row, column);
+            Component c = 
+                tableCellRenderer.getTableCellRendererComponent(
+                    table, 
+                    value, 
+                    isSelected, 
+                    hasFocus, 
+                    row, 
+                    column);
+            
             if (c instanceof JLabel)
             {
-                JLabel l = (JLabel) c;
-                l.setHorizontalTextPosition(JLabel.LEFT);
+                JLabel label = (JLabel) c;
+                label.setHorizontalTextPosition(JLabel.LEFT);
                 int modelColumn = table.convertColumnIndexToModel(column);
-                l.setIcon(getHeaderRendererIcon(modelColumn, l.getFont()
-                    .getSize()));
+                
+                label.setIcon(
+                    getHeaderRendererIcon(
+                        modelColumn, 
+                        label.getFont().getSize()));
             }
+            
             return c;
         }
     }
