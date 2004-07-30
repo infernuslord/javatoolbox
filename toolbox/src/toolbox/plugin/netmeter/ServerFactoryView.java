36ae1ce3c91cca5f1ab37c52553a37414ccd056d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-07-30 19:29:24 +0000

    Refactored to use FormLayout

--- toolbox/src/toolbox/plugin/netmeter/ServerFactoryView.java
+++ toolbox/src/toolbox/plugin/netmeter/ServerFactoryView.java
@@ -1,18 +1,18 @@
 package toolbox.plugin.netmeter;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.CellConstraints;
+import com.jgoodies.forms.layout.FormLayout;
+
+import toolbox.forms.SmartComponentFactory;
 import toolbox.util.ui.JHeaderPanel;
 import toolbox.util.ui.JSmartButton;
-import toolbox.util.ui.JSmartLabel;
 import toolbox.util.ui.JSmartTextField;
 import toolbox.util.ui.SmartAction;
-import toolbox.util.ui.layout.ParagraphLayout;
 
 /**
  * ServerFactoryView concepts.
@@ -24,7 +24,7 @@ import toolbox.util.ui.layout.ParagraphLayout;
  *      NetMeterPlugin.
  * </ul>
  * 
- * @see ServerFactoryView
+ * @see toolbox.plugin.netmeter.ServerView
  */
 public class ServerFactoryView extends JHeaderPanel
 {
@@ -69,24 +69,28 @@ public class ServerFactoryView extends JHeaderPanel
      */
     protected void buildView()
     {
-        JPanel content = new JPanel(new BorderLayout());
-        JPanel inputPanel = new JPanel(new ParagraphLayout());
-
-        // Port
-        inputPanel.add(
-            new JSmartLabel("Server Port"), 
-            ParagraphLayout.NEW_PARAGRAPH);
-        
         serverPortField_ = new JSmartTextField(6);
         serverPortField_.setText(NetMeterPlugin.DEFAULT_PORT + "");
-        inputPanel.add(serverPortField_);
-
-        JPanel buttonPanel = new JPanel(new FlowLayout());
-        buttonPanel.add(new JSmartButton(new CreateAction()));
-        content.add(inputPanel, BorderLayout.CENTER);
-        content.add(buttonPanel, BorderLayout.SOUTH);
         
-        setContent(content);
+        FormLayout layout = new FormLayout("r:p:g, p, l:p:g", "");
+        DefaultFormBuilder builder = new DefaultFormBuilder(layout);
+        builder.setComponentFactory(SmartComponentFactory.getInstance());
+        builder.setDefaultDialogBorder();
+        
+        builder.append("Server &Port", serverPortField_);
+        builder.nextLine();
+        
+        builder.appendRelatedComponentsGapRow();
+        builder.nextLine();
+
+        builder.appendRow("p");
+        CellConstraints cc = new CellConstraints();
+        
+        builder.add(
+            new JSmartButton(new CreateAction()), 
+            cc.xyw(builder.getColumn(), builder.getRow(), 3, "c,f"));
+        
+        setContent(builder.getPanel());
     }
     
     //--------------------------------------------------------------------------
