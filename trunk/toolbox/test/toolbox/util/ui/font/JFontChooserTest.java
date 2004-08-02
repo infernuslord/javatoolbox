f13caee63df1cc25f64ffea36e613e9caa3caac1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-02 21:23:52 +0000

    Added additional variations of chooser component.

--- trunk/toolbox/test/toolbox/util/ui/font/JFontChooserTest.java
+++ trunk/toolbox/test/toolbox/util/ui/font/JFontChooserTest.java
@@ -1,10 +1,15 @@
 package toolbox.util.ui.font;
 
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+
 import junit.textui.TestRunner;
 
 import org.apache.log4j.Logger;
 
 import toolbox.junit.UITestCase;
+import toolbox.util.FontUtil;
 
 /**
  * Unit test for JFontChooser.
@@ -35,11 +40,30 @@ public class JFontChooserTest extends UITestCase
     /**
      * Test for void JFontChooser().
      */
-    public void testFontSelectionPanel()
+    public void testJFontChooser()
     {
-        logger_.info("Running testFontSelectionPanel...");
+        logger_.info("Running testJFontChooser...");
         
-        JFontChooser chooser = new JFontChooser();
-        launchInDialog(chooser);
+        JPanel p = new JPanel(new GridLayout(2,2));
+        
+        JFontChooser fc = null;
+        
+        fc = new JFontChooser(FontUtil.getPreferredMonoFont());
+        p.add(fc);
+        
+        fc = new JFontChooser(FontUtil.getPreferredSerifFont());
+        fc.setMonospaceEmphasized(true);
+        p.add(fc);
+        
+        fc = new JFontChooser(FontUtil.getPreferredMonoFont());
+        fc.setRenderedUsingFont(true);
+        p.add(fc);
+        
+        fc = new JFontChooser(FontUtil.getPreferredSerifFont());
+        fc.setRenderedUsingFont(true);
+        fc.setMonospaceEmphasized(true);
+        p.add(fc);
+        
+        launchInDialog(p);
     }
 }
