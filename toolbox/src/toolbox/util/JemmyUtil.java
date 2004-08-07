124f79d8874c7c8b0fb2a26701666ff1df50b0a6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-07 00:27:58 +0000

    Added dump() methods

--- toolbox/src/toolbox/util/JemmyUtil.java
+++ toolbox/src/toolbox/util/JemmyUtil.java
@@ -1,28 +1,43 @@
 package toolbox.util;
 
+import java.awt.Component;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JList;
+
+import org.apache.log4j.Logger;
 
 import org.netbeans.jemmy.operators.ContainerOperator;
 import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JDialogOperator;
 import org.netbeans.jemmy.operators.JListOperator;
+import org.netbeans.jemmy.util.Dumper;
 import org.netbeans.jemmy.util.NameComponentChooser;
 
 /**
- * JemmyUtil is responsible for ___.
+ * Utility methods to use in conjunction with Jemmy.
  */
 public class JemmyUtil
 {
+    private static final Logger logger_ = Logger.getLogger(JemmyUtil.class);
 
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
     /**
-     * Creates a JemmyUtil.
+     * Prevent construction of this static singleton.
      */
-    public JemmyUtil()
+    private JemmyUtil()
     {
     }
 
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
     
     /**
      * Convenience method to find a button operatator withing a given container
@@ -39,18 +54,57 @@ public class JemmyUtil
         return new JButtonOperator(operator, new NameComponentChooser(name));
     }
     
+    
+    /**
+     * @param operator
+     * @return
+     */
     public static JButton getSource(JButtonOperator operator)
     {
         return (JButton) operator.getSource();
     }
     
+    
+    /**
+     * @param operator
+     * @return
+     */
     public static JList getSource(JListOperator operator)
     {
         return (JList) operator.getSource();
     }
     
+    
+    /**
+     * @param operator
+     * @return
+     */
     public static JDialog getSource(JDialogOperator operator)
     {
         return (JDialog) operator.getSource();
+    }
+
+    
+    /**
+     * Convenience method to dump all to the logger as debug output. 
+     */
+    public static void dump()
+    {
+        StringWriter writer = new StringWriter();
+        Dumper.dumpAll(new PrintWriter(writer));
+        logger_.debug("\n" + writer);
+    }
+    
+    
+    /**
+     * Convenience method to dump a component to the logger as debug output.
+     * 
+     * @param c Component to dump.
+     */
+    public static void dump(Component c)
+    {
+        StringWriter writer = new StringWriter();
+        Dumper.dumpComponent(c, new PrintWriter(writer));
+        logger_.debug("\n" + writer);
     }
 }
