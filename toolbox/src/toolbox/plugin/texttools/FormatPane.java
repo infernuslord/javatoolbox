8fe80d1218f1682bafc8b4dff90f87bd336a86c5
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-11 23:17:33 +0000

    Added java formatter. Updated to use Formatter interface for other formats

--- toolbox/src/toolbox/plugin/texttools/FormatPane.java
+++ toolbox/src/toolbox/plugin/texttools/FormatPane.java
@@ -2,20 +2,15 @@ package toolbox.plugin.texttools;
 
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
-import java.io.InputStream;
-import java.io.OutputStream;
 
-import javax.swing.AbstractAction;
 import javax.swing.JPanel;
 
-import org.w3c.tidy.Tidy;
-
-import toolbox.util.XMLUtil;
-import toolbox.util.io.StringInputStream;
-import toolbox.util.io.StringOutputStream;
+import toolbox.util.formatter.Formatter;
+import toolbox.util.formatter.HTMLFormatter;
+import toolbox.util.formatter.JavaFormatter;
+import toolbox.util.formatter.XMLFormatter;
 import toolbox.util.ui.JSmartButton;
-import toolbox.util.ui.flippane.JFlipPane;
-import toolbox.workspace.WorkspaceAction;
+import toolbox.util.ui.SmartAction;
 
 /**
  * Flipper for formatting various text formats.
@@ -31,11 +26,6 @@ public class FormatPane extends JPanel
      */
     private final TextToolsPlugin plugin_;
     
-    /**
-     * Flipper for the preferences.
-     */
-    private JFlipPane flipper_;
-
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
@@ -61,73 +51,34 @@ public class FormatPane extends JPanel
     protected void buildView()
     {
         setLayout(new FlowLayout());
-        add(new JSmartButton(new FormatHTMLAction()));
-        add(new JSmartButton(new FormatXMLAction()));
+        
+        add(new JSmartButton(
+            new FormatAction("Format HTML", new HTMLFormatter())));
+        
+        add(new JSmartButton(
+            new FormatAction("Format XML", new XMLFormatter())));
+        
+        add(new JSmartButton(
+            new FormatAction("Format Java", new JavaFormatter())));
     }
 
     //----------------------------------------------------------------------
     // FormatHTMLAction
     //----------------------------------------------------------------------
 
-    /**
-     * Uses JTidy to format the HTML in the input text area.
-     */
-    class FormatHTMLAction extends AbstractAction
+    class FormatAction extends SmartAction
     {
+        private Formatter formatter_;
+        
         /**
-         * Creates a FormatHTMLAction.
+         * Creates a FormatAction.
          */
-        FormatHTMLAction()
+        FormatAction(String label, Formatter formatter)
         {
-            super("Format HTML");
+            super(label, true, false, null);
+            formatter_ = formatter;
         }
 
-
-        /**
-         * @see java.awt.event.ActionListener#actionPerformed(
-         *      java.awt.event.ActionEvent)
-         */
-        public void actionPerformed(ActionEvent e)
-        {
-            // TODO: Add UI to manipulate the configuration
-
-            Tidy tidy = new Tidy();
-
-            tidy.setIndentContent(true);
-            //tidy.setIndentAttributes(true);
-            tidy.setWrapAttVals(true);
-            tidy.setBreakBeforeBR(true);
-            tidy.setWraplen(100);
-            //tidy.setSpaces(2);
-            //tidy.setTabsize()
-            //tidy.setSmartIndent(true);
-            tidy.setMakeClean(true);
-            tidy.setWrapScriptlets(true);
-
-            InputStream input = new StringInputStream(plugin_.getInputText());
-            OutputStream output = new StringOutputStream();
-            tidy.parse(input, output);
-            plugin_.getOutputArea().setText(output.toString());
-        }
-    }
-
-    //--------------------------------------------------------------------------
-    // FormatXMLAction
-    //--------------------------------------------------------------------------
-    
-    /**
-     * Formats the XML with correct indentation and spacing.
-     */
-    class FormatXMLAction extends WorkspaceAction 
-    {
-        /**
-         * Creates a FormatXMLAction. 
-         */
-        FormatXMLAction()
-        {
-            super("Format XML", false, null, null);
-        }
-    
         
         /**
          * @see toolbox.util.ui.SmartAction#runAction(
@@ -136,7 +87,7 @@ public class FormatPane extends JPanel
         public void runAction(ActionEvent e) throws Exception
         {
             plugin_.getOutputArea().setText(
-                XMLUtil.format(plugin_.getInputText()));
+                formatter_.format(plugin_.getInputText()));
         }
     }
 }
