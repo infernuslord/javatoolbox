01612a880bdbbe53bebbe5ec2195e38d3cf8d111
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-12 06:45:29 +0000

    Added format() signature that takes streams

--- trunk/toolbox/src/toolbox/util/formatter/Formatter.java
+++ trunk/toolbox/src/toolbox/util/formatter/Formatter.java
@@ -1,5 +1,8 @@
 package toolbox.util.formatter;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import toolbox.util.service.Initializable;
 import toolbox.util.service.Nameable;
 
@@ -12,7 +15,18 @@ public interface Formatter extends Initializable, Nameable
      * Returns a formatted version of the input string.
      * 
      * @param input String contained element to be formatted.
-     * @throws Exception on error.
+     * @throws Exception on formatting error.
      */
     String format(String input) throws Exception;
+    
+    
+    /**
+     * Formats the contents of the given input stream and writes the results to
+     * the given output stream.
+     * 
+     * @param input Source of data to format.
+     * @param output Destination of formatted data.
+     * @throws Exception on formatting error.
+     */
+    void format(InputStream input, OutputStream output) throws Exception;
 }
