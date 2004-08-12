1b0bf147b7d02057356e18da3416c0443fc2a6b4
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-12 18:57:37 +0000

    Updates for Invoker now extending the Destroyable interface.

--- toolbox/src/toolbox/util/invoker/BlockingInvoker.java
+++ toolbox/src/toolbox/util/invoker/BlockingInvoker.java
@@ -2,6 +2,8 @@ package toolbox.util.invoker;
 
 import org.apache.commons.beanutils.MethodUtils;
 
+import toolbox.util.service.ServiceException;
+
 /**
  * Invoker that invokes a method synchronously and blocks until the method has
  * completed execution.
@@ -31,11 +33,15 @@ public class BlockingInvoker implements Invoker
         MethodUtils.invokeMethod(target, method, params);
     }
     
+    //--------------------------------------------------------------------------
+    // Destroyable Interface
+    //--------------------------------------------------------------------------
     
     /**
-     * @see toolbox.util.invoker.Invoker#shutdown()
+     * @see toolbox.util.service.Destroyable#destroy()
      */
-    public void shutdown() throws Exception
+    public void destroy() throws ServiceException
     {
+        ; // No-op
     }
 }
--- toolbox/src/toolbox/util/invoker/Invoker.java
+++ toolbox/src/toolbox/util/invoker/Invoker.java
@@ -1,15 +1,18 @@
 package toolbox.util.invoker;
 
+import toolbox.util.service.Destroyable;
+import toolbox.util.service.ServiceException;
+
 /**
  * Interface for invoking a method on an object or a {@link java.lang.Runnable}.
  */
-public interface Invoker
+public interface Invoker extends Destroyable
 {
     /**
-     * Invokes run() on a Runnable.
+     * Invokes the run() method on class that implements Runnable.
      * 
-     * @param invokable Runnable 
-     * @throws Exception on error
+     * @param invokable Object instance that implements Runnable.
+     * @throws Exception on error.
      */
     void invoke(Runnable invokable) throws Exception;
 
@@ -17,10 +20,10 @@ public interface Invoker
     /**
      * Invokes the method on the given target object.
      * 
-     * @param target Object to invoke method on
-     * @param method Name of method to invoke
-     * @param params Method parameters
-     * @throws Exception on error
+     * @param target Object to invoke method on.
+     * @param method Name of method to invoke.
+     * @param params Method's parameters.
+     * @throws Exception on error.
      */
     void invoke(Object target, String method, Object[] params) throws Exception;
     
@@ -28,7 +31,7 @@ public interface Invoker
     /**
      * Shuts down the invoker.
      * 
-     * @throws Exception on error
+     * @throws Exception on destroy error.
      */    
-    void shutdown() throws Exception;
+    void destroy() throws ServiceException;
 }
