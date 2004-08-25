b1122085727aaafb3d52515b6ebc75e723e216bf
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-08-25 22:17:01 +0000

    Iniital version

---
+++ toolbox/test/toolbox/plugin/jdbc/QueryPluginTest.java
@@ -0,0 +1,71 @@
+package toolbox.plugin.jdbc;
+import java.util.Collections;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import nu.xom.Element;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.StringUtil;
+import toolbox.util.formatter.XMLFormatter;
+
+/**
+ * Unit test for QueryPlugin.
+ * 
+ * @see toolbox.plugin.statcvs.QueryPlugin
+ */
+public class QueryPluginTest extends TestCase
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(QueryPluginTest.class);
+    
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args)
+    {
+        TestRunner.run(QueryPluginTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+    
+    public void testPreferenced() throws Exception
+    {
+        logger_.info("Running testApplyPrefs...");
+
+        // Save ---------------------------------------------------------------
+        
+        QueryPlugin p = new QueryPlugin();
+        p.initialize(Collections.EMPTY_MAP);
+        
+        p.setAutoScrollThreshold(100);
+        p.setMaxHistory(99);
+        p.setSqlTerminator("#");
+        p.setSendErrorToConsole(false);
+        
+        Element root = new Element("root");
+        p.savePrefs(root);
+        
+        logger_.debug(StringUtil.banner(
+            new XMLFormatter().format(root.toXML())));
+
+        // Apply ---------------------------------------------------------------
+        
+        QueryPlugin p2 = new QueryPlugin();
+        p2.initialize(Collections.EMPTY_MAP);
+        p2.applyPrefs(root);
+        
+        // Verify --------------------------------------------------------------
+        
+        assertEquals(p.getAutoScrollThreshold(), p2.getAutoScrollThreshold());
+        assertEquals(p.getMaxHistory(), p2.getMaxHistory());
+        assertEquals(p.getSqlTerminator(), p2.getSqlTerminator());
+        assertEquals(p.isSendErrorToConsole(), p2.isSendErrorToConsole());
+        assertEquals(p.isContinueOnError(), p2.isContinueOnError());
+    }
+}
