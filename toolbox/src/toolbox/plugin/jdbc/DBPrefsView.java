5e0240f0a430e6fbf0492b030f35bfcfd1c82615
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-01 22:43:03 +0000

    Factored out common component BeanSheet

--- toolbox/src/toolbox/plugin/jdbc/DBPrefsView.java
+++ toolbox/src/toolbox/plugin/jdbc/DBPrefsView.java
@@ -1,24 +1,10 @@
 package toolbox.plugin.jdbc;
 
-import java.beans.BeanInfo;
-import java.beans.IntrospectionException;
-import java.beans.Introspector;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyDescriptor;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.swing.Icon;
 
-import com.l2fprod.common.propertysheet.Property;
-import com.l2fprod.common.propertysheet.PropertySheetPanel;
-
-import org.apache.commons.collections.CollectionUtils;
 import org.apache.log4j.Logger;
 
-import toolbox.util.ExceptionUtil;
-import toolbox.util.beans.BeanPropertyFilter;
+import toolbox.util.ui.BeanSheet;
 import toolbox.util.ui.ImageCache;
 import toolbox.util.ui.JHeaderPanel;
 
@@ -48,7 +34,7 @@ public class DBPrefsView extends JHeaderPanel
     /**
      * Property sheet panel.
      */
-    private PropertySheetPanel sheet_;
+    private BeanSheet sheet_;
     
     /**
      * Query plugin.
@@ -67,6 +53,7 @@ public class DBPrefsView extends JHeaderPanel
     public DBPrefsView(QueryPlugin plugin)
     {
         super(ICON_DBPREFS, "Plugin Properties");
+        plugin_ = plugin;
         buildView();
         setPlugin(plugin);
     }
@@ -95,58 +82,11 @@ public class DBPrefsView extends JHeaderPanel
      */
     protected void buildView()
     {
-        BeanInfo beanInfo = null;
-
-        try
-        {
-            beanInfo = Introspector.getBeanInfo(QueryPlugin.class);
-        }
-        catch (IntrospectionException ie)
-        {
-            ExceptionUtil.handleUI(ie, logger_);
-        }
-
-        // Build the list of properties to support
-        PropertyDescriptor[] dArray = beanInfo.getPropertyDescriptors();
-        List descriptors = new ArrayList();
-        CollectionUtils.addAll(descriptors, dArray);
-        
-        // Filter out only the properties that the QueryPlugin can save. 
-        CollectionUtils.filter(
-            descriptors, 
-            new BeanPropertyFilter(
-                "name", 
-                QueryPlugin.SAVED_PROPS));
-        
-        dArray = (PropertyDescriptor[]) 
-            descriptors.toArray(new PropertyDescriptor[0]);
-        
-        //if (logger_.isDebugEnabled())
-        //    for (int i = 0; i < dArray.length; i++)
-        //        logger_.info(StringUtil.banner(AsMap.of(dArray[i]) + ""));
-
-        // Create and init the property sheet
-        sheet_ = new PropertySheetPanel();
-        sheet_.setProperties(dArray);
-        sheet_.setDescriptionVisible(true);
-        sheet_.addPropertySheetChangeListener(new MyPropertyChangeListener());
+        sheet_ = 
+            new BeanSheet(
+                plugin_,
+                QueryPluginConstants.SAVED_PROPS);
         
         setContent(sheet_);
-    }
-
-    //----------------------------------------------------------------------
-    // MyPropertyChangeListener
-    //----------------------------------------------------------------------
-
-    /**
-     * Writes the changed property back to the source javabean.
-     */
-    class MyPropertyChangeListener implements PropertyChangeListener
-    {
-        public void propertyChange(PropertyChangeEvent evt)
-        {
-            Property p = (Property) evt.getSource();
-            p.writeToObject(plugin_);
-        }
     }
 }
