9856621f090672e44c465b525034902a9b5025b0
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-01 22:49:47 +0000

    INitial version

---
+++ trunk/toolbox/test/toolbox/plugin/texttools/TextToolsPluginTest.java
@@ -0,0 +1,58 @@
+package toolbox.plugin.texttools;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import nu.xom.Element;
+
+import org.apache.commons.collections.MapUtils;
+import org.apache.log4j.Logger;
+
+import toolbox.util.StringUtil;
+import toolbox.util.formatter.XMLFormatter;
+
+/**
+ * Unit test for TextToolsPlugin.
+ * 
+ * @see toolbox.plugin.texttools.TextToolsPlugin
+ */
+public class TextToolsPluginTest extends TestCase
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(TextToolsPluginTest.class);
+    
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args)
+    {
+        TestRunner.run(TextToolsPluginTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+    
+    public void testPreferenced() throws Exception
+    {
+        logger_.info("Running testPreferenced...");
+        
+        Element prefs = new Element("testPreferenced");
+        
+        TextToolsPlugin plugin = new TextToolsPlugin();
+        plugin.initialize(MapUtils.EMPTY_MAP);
+        plugin.buildView();
+        plugin.savePrefs(prefs);
+        plugin.destroy();
+        
+        logger_.debug(StringUtil.banner(
+            new XMLFormatter().format(prefs.toXML())));
+
+        TextToolsPlugin plugin2 = new TextToolsPlugin();
+        plugin2.initialize(MapUtils.EMPTY_MAP);
+        plugin2.buildView();
+        plugin2.applyPrefs(prefs);
+        plugin2.destroy();
+    }
+}
