f9021567c0b62bd87067efad9b840e02ea5b57c8
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-09 18:41:10 +0000

    Implements IPreferenced but nothing saved yet

--- trunk/toolbox/src/toolbox/jedit/JEditPopupMenu.java
+++ trunk/toolbox/src/toolbox/jedit/JEditPopupMenu.java
@@ -1,19 +1,33 @@
 package toolbox.jedit;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import nu.xom.Element;
+
 import org.apache.log4j.Logger;
 
+import toolbox.util.XOMUtil;
 import toolbox.util.ui.JPopupListener;
 import toolbox.util.ui.JSmartMenuItem;
 import toolbox.util.ui.JSmartPopupMenu;
+import toolbox.workspace.IPreferenced;
 
 /**
  * Popup menu with commonly used functionality for JEditTextArea subclasses.
  */
-public class JEditPopupMenu extends JSmartPopupMenu
+public class JEditPopupMenu extends JSmartPopupMenu implements IPreferenced
 {
     private static final Logger logger_ = 
         Logger.getLogger(JEditPopupMenu.class); 
 
+    //--------------------------------------------------------------------------
+    // IPreferenced Constants
+    //--------------------------------------------------------------------------
+    
+    private static final String NODE_JEDITPOPUPMENU = "JEditPopupMenu";
+    
     //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
@@ -22,6 +36,11 @@ public class JEditPopupMenu extends JSmartPopupMenu
      * Text component to associate this popup menu with. 
      */
     private JEditTextArea textArea_;
+
+    /**
+     * Preferenced subcomponents to trickle save/applyPrefs to.
+     */
+    private List subComponents_;
 
     //--------------------------------------------------------------------------
     //  Constructors
@@ -32,6 +51,7 @@ public class JEditPopupMenu extends JSmartPopupMenu
      */
     public JEditPopupMenu()
     {
+        subComponents_ = new ArrayList(0);
     }
     
     
@@ -42,9 +62,9 @@ public class JEditPopupMenu extends JSmartPopupMenu
      */
     public JEditPopupMenu(JEditTextArea textArea)
     {
-        textArea_ = textArea;
+        subComponents_ = new ArrayList(2);
+        setTextArea(textArea);
         buildView();
-        
     }
     
     //--------------------------------------------------------------------------
@@ -74,9 +94,39 @@ public class JEditPopupMenu extends JSmartPopupMenu
         addSeparator();
         add(new JSmartMenuItem(new JEditActions.SetFontAction(textArea_)));
         add(new JSmartMenuItem(new JEditActions.FindAction(textArea_)));
-        add(new JSmartMenuItem(new JEditActions.InsertFileAction(textArea_)));
-        add(new JSmartMenuItem(new JEditActions.SaveAsAction(textArea_)));
+        add(new JSmartMenuItem(textArea_.getInsertFileAction()));
+        add(new JSmartMenuItem(textArea_.getSaveAsAction()));
+        
+        //subComponents_.add(save);
         
         textArea_.addMouseListener(new JPopupListener(this));
+    }
+    
+    //--------------------------------------------------------------------------
+    // IPreferenced Interface
+    //--------------------------------------------------------------------------
+
+    /**
+     * @see toolbox.workspace.IPreferenced#applyPrefs(nu.xom.Element)
+     */
+    public void applyPrefs(Element prefs) throws Exception
+    {
+        Element root = XOMUtil.getFirstChildElement(prefs, NODE_JEDITPOPUPMENU,
+            new Element(NODE_JEDITPOPUPMENU));
+     
+        for (Iterator iter = subComponents_.iterator(); iter.hasNext();)
+            ((IPreferenced) iter.next()).applyPrefs(root);
+    }
+
+
+    /**
+     * @see toolbox.workspace.IPreferenced#savePrefs(nu.xom.Element)
+     */
+    public void savePrefs(Element prefs) throws Exception
+    {
+        Element root = new Element(NODE_JEDITPOPUPMENU);
+        for (Iterator iter = subComponents_.iterator(); iter.hasNext();)
+            ((IPreferenced) iter.next()).savePrefs(root);
+        XOMUtil.insertOrReplace(prefs, root);
     }
 }
