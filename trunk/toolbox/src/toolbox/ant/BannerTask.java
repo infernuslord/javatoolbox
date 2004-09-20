f0c9b8a90600997cd6fac6852dbf12a6b3098e58
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-20 08:23:29 +0000

    Added example usage and sample output to header

--- trunk/toolbox/src/toolbox/ant/BannerTask.java
+++ trunk/toolbox/src/toolbox/ant/BannerTask.java
@@ -6,8 +6,31 @@ import org.apache.tools.ant.Task;
 import toolbox.util.Banner;
 
 /**
- * BannerTask is an Ant task thats converts text to an ASCII banner and write 
- * the result to standard output stream.
+ * BannerTask is an Ant task thats converts text to an ASCII banner and writes 
+ * the result to the standard output stream.
+ * <p>
+ * To use as an ant task, place the following in your ant script:
+ * <pre class="snippet">
+ * 
+ *   &lt;taskdef 
+ *     name=&quot;banner&quot; 
+ *     classname=&quot;toolbox.ant.BannerTask
+ *     classpath=&quot;/path/to/toolbox-ant.jar&quot; /&gt;
+ *   
+ *   &lt;target name=&quot;testBannerTask&quot;&gt;
+ *     &lt;banner message=&quot;Yippee!&quot; /&gt;
+ *   &lt;/target&gt;
+ * 
+ * </pre>
+ * Sample Output:
+ * <pre class="snippet">
+ *    __   __  _                                 _ 
+ *    \ \ / / (_)  _ __    _ __     ___    ___  | |
+ *     \ V /  | | | '_ \  | '_ \   / _ \  / _ \ | |
+ *      | |   | | | |_) | | |_) | |  __/ |  __/ |_|
+ *      |_|   |_| | .__/  | .__/   \___|  \___| (_)
+ *                |_|     |_|                      
+ * </pre>
  * 
  * @see toolbox.util.Banner
  */
@@ -41,6 +64,8 @@ public class BannerTask extends Task
     //--------------------------------------------------------------------------
 
     /**
+     * Writes banner to System.out.
+     * 
      * @see org.apache.tools.ant.Task#execute()
      */
     public void execute() throws BuildException
