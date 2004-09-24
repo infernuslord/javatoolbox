49fe62ac0bf7a915c0ab9603f5da09667ba9dd2e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-24 22:32:32 +0000

    Moved nextColor() to RandomUtil

--- trunk/toolbox/test/toolbox/util/ui/JSmartTextPaneTest.java
+++ trunk/toolbox/test/toolbox/util/ui/JSmartTextPaneTest.java
@@ -1,15 +1,12 @@
 package toolbox.util.ui;
 
-import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.SwingConstants;
 
 import junit.textui.TestRunner;
 
-import org.apache.commons.lang.math.RandomUtils;
 import org.apache.log4j.Logger;
 
 import toolbox.junit.testcase.UITestCase;
@@ -51,14 +48,8 @@ public class JSmartTextPaneTest extends UITestCase
     {
         logger_.info("Running testColors...");
         
-        JSmartSplitPane splitter = 
-            new JSmartSplitPane(SwingConstants.HORIZONTAL, true);
-        
         JSmartTextPane tp = new JSmartTextPane();
         tp.setFont(FontUtil.getPreferredMonoFont());
-        
-        splitter.setTopComponent(
-            new JHeaderPanel("Test Component", null, new JScrollPane(tp)));
         
         class ButtonPanel extends JPanel
         {
@@ -67,34 +58,28 @@ public class JSmartTextPaneTest extends UITestCase
                 setLayout(new FlowLayout());
             }
         }
-
-        splitter.setBottomComponent(new ButtonPanel());
-        splitter.setDividerLocation(200);
-        
         
         for (int i = 0; i < 100; i++)
         {
+            tp.setActiveForeground(RandomUtil.nextColor());
+            tp.setActiveBackground(RandomUtil.nextColor());
+            
             for (int j = 0; j < 10; j++)
-                tp.append(RandomUtil.nextString(RandomUtil.nextInt(5,15)), nextColor(), nextColor());
+                //tp.append(RandomUtil.nextString(RandomUtil.nextInt(5,15)), 
+                //    RandomUtil.nextColor(), RandomUtil.nextColor());
+                tp.append(RandomUtil.nextString(RandomUtil.nextInt(5,15)));
                 
-            tp.append("\n", nextColor(), nextColor());
+            tp.append("\n");
         }
+        
+        logger_.debug("launching..");
         
         //tp.append("hello", Color.red, Color.white);
         //tp.append("world!", Color.green, Color.white);
-         
         
-        
-        launchInDialog(splitter, UITestCase.SCREEN_ONE_HALF);
+        launchInDialog(
+            new JHeaderPanel("testing", null, new JScrollPane(tp)), 
+            UITestCase.SCREEN_ONE_HALF);
     }
     
-    Color nextColor()
-    {
-        Color c = new Color(
-            RandomUtils.nextInt(256), 
-            RandomUtils.nextInt(256), 
-            RandomUtils.nextInt(256)); 
-        
-        return c;
-    }
 }
