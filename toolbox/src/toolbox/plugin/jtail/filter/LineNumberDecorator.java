f81d880a406418628b91d644c0bcb780dd8f6177
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-27 18:55:45 +0000

    Added style formatting to the line of filtered text

--- toolbox/src/toolbox/plugin/jtail/filter/LineNumberDecorator.java
+++ toolbox/src/toolbox/plugin/jtail/filter/LineNumberDecorator.java
@@ -1,5 +1,14 @@
 package toolbox.plugin.jtail.filter;
 
+import java.awt.Color;
+import java.util.List;
+
+import javax.swing.JTextPane;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyleContext;
+import javax.swing.text.StyledDocument;
+
 /**
  * Appends a line number to the beginning of the line.
  */
@@ -14,6 +23,11 @@ public class LineNumberDecorator extends AbstractLineFilter
      */
     private int lineNumber_;
     
+    /**
+     * Formatting style for the number line number.
+     */
+    private Style style_;
+    
     //--------------------------------------------------------------------------
     //  Constructors
     //--------------------------------------------------------------------------
@@ -23,6 +37,11 @@ public class LineNumberDecorator extends AbstractLineFilter
      */
     public LineNumberDecorator()
     {
+        StyleContext context = new StyleContext();
+        style_ = context.new NamedStyle();
+        StyleConstants.setForeground(style_, Color.green);
+        StyleConstants.setBackground(style_, Color.black);
+        
         reset();
     }
 
@@ -54,5 +73,25 @@ public class LineNumberDecorator extends AbstractLineFilter
             line.insert(0,"[" + lineNumber_++ + "] ");
         
         return true;
+    }
+    
+    
+    /**
+     * @see toolbox.plugin.jtail.filter.ILineFilter#filter(
+     *      java.lang.StringBuffer, java.util.List)
+     */
+    public boolean filter(StringBuffer line, List segments)
+    {
+        boolean result = filter(line);
+        
+        if (result && isEnabled())
+        {
+            StyledSegment segment = new StyledSegment(
+                0, ((lineNumber_ - 1) + "").length() + 2 - 1, style_);
+            
+            segments.add(segment);
+        }
+            
+        return result;
     }
 }
