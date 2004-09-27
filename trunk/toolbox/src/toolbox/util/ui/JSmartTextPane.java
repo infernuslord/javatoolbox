0d727105aebf5bbca740c4fbf485531f56c9a4b4
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-27 09:28:40 +0000

    Added line wrapping flag, antialiasing, and execution of append() on the event dispatch thread

--- trunk/toolbox/src/toolbox/util/ui/JSmartTextPane.java
+++ trunk/toolbox/src/toolbox/util/ui/JSmartTextPane.java
@@ -3,8 +3,11 @@ package toolbox.util.ui;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
+import java.lang.reflect.InvocationTargetException;
 
 import javax.swing.JTextPane;
+import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
@@ -12,10 +15,12 @@ import javax.swing.text.StyledDocument;
 
 import org.apache.log4j.Logger;
 
+import toolbox.util.SwingUtil;
+
 /**
- * JSmartTextPane is responsible for ___.
+ * JSmartTextPane 
  */
-public class JSmartTextPane extends JTextPane
+public class JSmartTextPane extends JTextPane implements AntiAliased
 {
     private static final Logger logger_ = 
         Logger.getLogger(JSmartTextPane.class);
@@ -26,6 +31,16 @@ public class JSmartTextPane extends JTextPane
     private StyledDocument doc_;
     private Style style_;
     private String styleName_;
+    
+    /**
+     * Flag to activate line wrapping of text.
+     */
+    private boolean lineWrap_;
+    
+    /**
+     * Flag that toggles antialiasing of text.
+     */
+    private boolean antiAlias_ = SwingUtil.getDefaultAntiAlias();
 
     //--------------------------------------------------------------------------
     // Constructors
@@ -40,6 +55,8 @@ public class JSmartTextPane extends JTextPane
         setEditable(false);
         styleName_ = getClass().getName();
         style_ = doc_.addStyle(styleName_, null);
+        
+        setLineWrap(false);
     }
 
     //--------------------------------------------------------------------------
@@ -47,23 +64,48 @@ public class JSmartTextPane extends JTextPane
     //--------------------------------------------------------------------------
 
     /**
-     * Appends a string to the end of the textpane.
+     * Appends a string to the end of the textpane. Makes sure operation is
+     * executed on the event dispatch thread otherwise weird things happen!
      * 
      * @param str String to append.
      */
-    public void append(String str)
+    public void append(final String str)
     {
-        int len = doc_.getLength();
-        
-        try
+        if (SwingUtilities.isEventDispatchThread())
         {
-            doc_.insertString(len, str, style_);
+            int len = doc_.getLength();
+            
+            try
+            {
+                doc_.insertString(len, str, style_);
+                setCaretPosition(len);
+            }
+            catch (BadLocationException e)
+            {
+                logger_.error(e);
+            }
         }
-        catch (BadLocationException e)
+        else
         {
-            logger_.error(e);
+            try
+            {
+                SwingUtilities.invokeAndWait(
+                    new Runnable() {
+                        public void run()
+                        {
+                            append(str);
+                        }
+                    });
+            }
+            catch (InterruptedException e)
+            {
+                logger_.error(e);
+            }
+            catch (InvocationTargetException e)
+            {
+                logger_.error(e);
+            }
         }
-        setCaretPosition(len);
     }
 
     
@@ -85,9 +127,9 @@ public class JSmartTextPane extends JTextPane
 
     
     /**
-     * Clears the contents of the textpane. 
+     * Clears the contents of the textpane.
      */
-    public void clear()
+    public synchronized void clear()
     {
         setText("");
     }
@@ -128,6 +170,24 @@ public class JSmartTextPane extends JTextPane
         StyleConstants.setForeground(style_, fg);
     }
 
+    
+    /**
+     * @return
+     */
+    protected boolean isLineWrap()
+    {
+        return lineWrap_;
+    }
+    
+    
+    /**
+     * @param lineWrap
+     */
+    protected void setLineWrap(boolean lineWrap)
+    {
+        lineWrap_ = lineWrap;
+    }
+    
     //--------------------------------------------------------------------------
     // Overrides JTextPane
     //--------------------------------------------------------------------------
@@ -137,10 +197,10 @@ public class JSmartTextPane extends JTextPane
      */
     public boolean getScrollableTracksViewportWidth()
     {
-        if (getSize().width < getParent().getSize().width)
-            return true;
+        if (isLineWrap())
+            return super.getScrollableTracksViewportWidth();
         else
-            return false;
+            return getSize().width < getParent().getSize().width;
     }
 
 
@@ -149,8 +209,54 @@ public class JSmartTextPane extends JTextPane
      */
     public void setSize(Dimension d)
     {
-        if (d.width < getParent().getSize().width)
-            d.width = getParent().getSize().width;
+        if (!isLineWrap())
+        {
+            if (d.width < getParent().getSize().width)
+                d.width = getParent().getSize().width;
+        }
+        
         super.setSize(d);
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides java.awt.Component
+    //--------------------------------------------------------------------------
+
+    /**
+     * Overriden to enable antialiasing.
+     *
+     * @see java.awt.Component#paint(java.awt.Graphics)
+     */
+    public void paint(Graphics g)
+    {
+        SwingUtil.makeAntiAliased(g, isAntiAliased());
+        super.paint(g);
+    }
+    
+    //--------------------------------------------------------------------------
+    // Antialiased Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Returns true if antialiasing is enabled, false otherwise.
+     *
+     * @return boolean
+     */
+    public boolean isAntiAliased()
+    {
+        return antiAlias_;
+    }
+
+
+    /**
+     * Activates antialiasing of text.
+     *
+     * @param antiAlias True turns antialiasing on; false turns it off.
+     */
+    public void setAntiAliased(boolean antiAlias)
+    {
+        boolean old = antiAlias_;
+        antiAlias_ = antiAlias;
+        firePropertyChange(AntiAliased.PROP_ANTIALIAS, old, antiAlias_);
     }
 }
