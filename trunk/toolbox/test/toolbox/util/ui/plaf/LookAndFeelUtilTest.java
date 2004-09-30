21dba870c8c469350ae40b524266a0cb26dd3019
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-30 06:06:31 +0000

    Test for figuring out wjy tiny laf changes are not working...

--- trunk/toolbox/test/toolbox/util/ui/plaf/LookAndFeelUtilTest.java
+++ trunk/toolbox/test/toolbox/util/ui/plaf/LookAndFeelUtilTest.java
@@ -22,7 +22,7 @@ import toolbox.util.ui.list.JSmartList;
 import toolbox.util.ui.tree.JSmartTree;
 
 /**
- * Unit test for LookAndFeelUtil.
+ * Unit test for {@link toolbox.util.ui.plaf.LookAndFeelUtil}.
  */
 public class LookAndFeelUtilTest extends UITestCase
 {
@@ -40,6 +40,19 @@ public class LookAndFeelUtilTest extends UITestCase
         TestRunner.run(LookAndFeelUtilTest.class);
     }
 
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a LookAndFeelUtilTest and explicitly tells the superclass
+     * not to set the look and feel.
+     */
+    public LookAndFeelUtilTest()
+    {
+        super(false);  // Don't let the LAF get set
+    }
+    
     //--------------------------------------------------------------------------
     // Unit Tests
     //--------------------------------------------------------------------------
@@ -87,5 +100,56 @@ public class LookAndFeelUtilTest extends UITestCase
             })));
         
         launchInFrame(p);
+    }
+    
+    public void testCreateThemesMenu2() throws Exception
+    {
+        LAFInfo info = new LAFInfo();
+        info.setAction(ActivateTinyLookAndFeelAction.class.getName());
+        info.setClassName("de.muntjak.tinylookandfeel.TinyLookAndFeel");
+        info.setName("Tiny");
+        info.getProperties().put("group.name", "Tiny");
+        info.getProperties().put("theme.name", "XP Forest");
+        info.getProperties().put("theme.file", "/tinylf/XP Forest.theme");
+        info.getProperties().put("theme.style", "3");
+        LookAndFeelUtil.setLookAndFeel(info);
+        
+        SwingUtil.setDefaultAntiAlias(true);
+        
+        JMenuBar mb = new JMenuBar();
+        JMenu lafMenu = LookAndFeelUtil.createLookAndFeelMenu();
+        mb.add(new JMenu("Testing"));
+        mb.add(lafMenu);
+        setMenuBar(mb);
+        
+        JPanel p = new JPanel(new FlowLayout());
+        p.add(new JSmartButton("Button"));
+        p.add(new JSmartTextField("TextField"));
+        p.add(new JSmartCheckBox("CheckBox", true));
+        p.add(new JSmartComboBox(new String[] {"Combo", "Box"}));
+        p.add(new JSmartLabel("Label"));
+        
+        p.add(new JScrollPane(new JSmartList(
+            new String[]{"List Item 1", "List Item 2", "List Item 3"})));
+        
+        p.add(new JScrollPane(new JTable(
+            new String[][] { 
+                new String[] {"Cell A", "Cell B"}, 
+                new String[] {"Cell C", "Cell D"}
+            }, 
+            new String[] {"Column 1", "Column 2"})));
+        
+        p.add(new JScrollPane(new JSmartTextArea(
+            "TextArea line 1\nTextArea line2\nTextArea line3")));
+        
+        p.add(new JScrollPane(new JSmartTree(
+            new Object[] 
+            {
+                "Node 1", 
+                new String[] {"Node 1.1", "Node 1.2"} , 
+                "Node3"
+            })));
+        
+        launchInDialog(p);
     }
 }
