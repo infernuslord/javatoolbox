1d5d07a2498db5e074e453319f050809701f81ab
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-09-30 06:07:36 +0000

    Added getDialogs()

--- toolbox/src/toolbox/util/ui/JSmartDialog.java
+++ toolbox/src/toolbox/util/ui/JSmartDialog.java
@@ -4,6 +4,8 @@ import java.awt.Dialog;
 import java.awt.Frame;
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JDialog;
@@ -21,12 +23,17 @@ import toolbox.workspace.IPreferenced;
 /**
  * JSmartDialog is an extension of JDialog. Features:
  * <ul>
- *  <li>Esc will dispose of the dialog.
+ *  <li>Pressing the escape key (ESC) will dismiss the dialog.
  *  <li>Size and location can be saved/restored via the IPreferenced interface
+ *  <li>A list of all active JSmartDialog instances can obtained (useful when
+ *      changing look and feel without restarting)
  * </ul>
  */
 public class JSmartDialog extends JDialog implements IPreferenced
 {
+    // TODO: Write unit test to make sure initDialog() and dispose() are 
+    //       maintaining a proper list of active dialogs.
+    
     private static final Logger logger_ = Logger.getLogger(JSmartDialog.class);
     
     //--------------------------------------------------------------------------
@@ -43,6 +50,15 @@ public class JSmartDialog extends JDialog implements IPreferenced
      * XML element name.
      */
     private static final String NODE_JDIALOG = "JDialog";
+
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+
+    /**
+     * Store a ref to each active instance.
+     */
+    private static List instances_ = new ArrayList();
     
     //--------------------------------------------------------------------------
     // Constructors
@@ -182,6 +198,8 @@ public class JSmartDialog extends JDialog implements IPreferenced
      */
     protected void initDialog()
     {
+        instances_.add(this);
+        
         // Bind ESC to the dispose action.
         getRootPane()
             .getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW)
@@ -189,6 +207,36 @@ public class JSmartDialog extends JDialog implements IPreferenced
 
         getRootPane().getActionMap().put(KEY_ESC, new DisposeAction(this));
     }
+    
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Returns an array of all known active JSmartDialogs.
+     * 
+     * @return JDialog[]
+     */
+    public static JDialog[] getDialogs() 
+    {
+        return (JDialog[]) instances_.toArray(new JDialog[0]);
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides java.awt.Dialog
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Removes the store references that was saved during #initDialog().
+     * 
+     * @see java.awt.Dialog#dispose()
+     */
+    public void dispose()
+    {
+        instances_.remove(this);
+        super.dispose();
+    }
+    
     
     //--------------------------------------------------------------------------
     // IPreferenced Interface
