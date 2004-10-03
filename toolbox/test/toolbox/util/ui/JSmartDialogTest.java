bc2fb7c6333cf37cb6a197bf007ec9d462bb6beb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-03 05:59:46 +0000

    Test now exits the VM after completion

--- toolbox/test/toolbox/util/ui/JSmartDialogTest.java
+++ toolbox/test/toolbox/util/ui/JSmartDialogTest.java
@@ -9,12 +9,9 @@ import org.apache.log4j.Logger;
 
 import toolbox.junit.testcase.UITestCase;
 import toolbox.util.SwingUtil;
-import toolbox.util.ThreadUtil;
 
 /**
- * Unit test for JSmartDialog.
- * 
- * @see toolbox.util.ui.JSmartDialog
+ * Unit test for {@link toolbox.util.ui.JSmartDialog}.
  */
 public class JSmartDialogTest extends UITestCase
 {
@@ -33,42 +30,21 @@ public class JSmartDialogTest extends UITestCase
     //--------------------------------------------------------------------------
     // Unit Tests
     //--------------------------------------------------------------------------
-    
+
+    /**
+     * Tests to make sure the esc key dismisses the dialog box.
+     */
     public void testJSmartDialog()
     {
         logger_.info("Running testJSmartDialog...");
         
-        JSmartDialog dlg = new JSmartDialog(new JFrame(), false);
+        JFrame f = new JFrame();
+        JSmartDialog dlg = new JSmartDialog(f, true);
         dlg.getContentPane().add(new JSmartLabel("Hit esc to exit!"));
         dlg.getContentPane().add(new JButton("Hit esc to exit"));
         dlg.pack();
         SwingUtil.centerWindow(dlg);
         dlg.setVisible(true);
-        
-        ThreadUtil.sleep(10000);
+        f.dispose();
     }
-
-//    public void testJSmartDialog()
-//    {
-//        logger_.info("Running testJSmartDialog...");
-//        
-//        JSmartDialog dlg = new MySmartDialog();
-//        dlg.pack();
-//        SwingUtil.centerWindow(dlg);
-//        dlg.setVisible(true);
-//        
-//        ThreadUtil.sleep(10000);
-//    }
-//
-//    class MySmartDialog extends JSmartDialog
-//    {
-//        public MySmartDialog()
-//        {
-//            super(new JFrame(), true);
-//            Container c = getContentPane();
-//            c.setLayout(new FlowLayout());
-//            c.add(new JButton("Hello"));
-//            c.add(new JTextField("bye!"));
-//        }
-//    }
 }
