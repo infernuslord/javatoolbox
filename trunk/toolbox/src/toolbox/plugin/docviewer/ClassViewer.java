233ad5eb1876eb039f3db7447016ff0851803c29
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-03 10:37:36 +0000

    Initial version

---
+++ trunk/toolbox/src/toolbox/plugin/docviewer/ClassViewer.java
@@ -0,0 +1,90 @@
+package toolbox.plugin.docviewer;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Logger;
+
+import toolbox.util.FileUtil;
+import toolbox.util.decompiler.Decompiler;
+import toolbox.util.decompiler.DecompilerException;
+import toolbox.util.decompiler.DecompilerFactory;
+import toolbox.util.io.StringInputStream;
+
+/**
+ * Java class file viewer.
+ */
+public class ClassViewer extends JavaViewer
+{
+    private static final Logger logger_ = Logger.getLogger(ClassViewer.class);
+    
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Creates a ClassViewer. Necessary for instantiation via reflection.
+     */
+    public ClassViewer()
+    {
+        super("Class Viewer");
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides JavaViewer
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.plugin.docviewer.DocumentViewer#view(java.io.InputStream)
+     */
+    public void view(InputStream is) throws DocumentViewerException
+    {
+        try
+        {
+            byte[] bytecode = IOUtils.toByteArray(is);
+            String name = FileUtil.createTempFilename();
+            FileUtil.setFileContents(name, bytecode, false);
+            File f = new File(name);
+            view(f);
+        }
+        catch (IOException e)
+        {
+            throw new DocumentViewerException(e);
+        }
+        
+    }
+    
+    
+    /**
+     * @see toolbox.plugin.docviewer.DocumentViewer#view(java.io.File)
+     */
+    public void view(File file) throws DocumentViewerException
+    {
+            Decompiler d;
+            
+            try
+            {
+                d = DecompilerFactory.create(
+                    DecompilerFactory.DECOMPILER_JREVERSEPRO);
+                
+                String javacode = d.decompile(file);
+                super.view(new StringInputStream(javacode));
+                
+            }
+            catch (DecompilerException e)
+            {
+                logger_.error(e);
+            }
+    }
+
+    
+    /**
+     * @see toolbox.plugin.docviewer.DocumentViewer#getViewableFileTypes()
+     */
+    public String[] getViewableFileTypes()
+    {
+        return new String[] {"class"};
+    }
+}
