a34aaa4fe63037e4fda1a1557d93ab9be8bbe7cc
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-05 15:01:17 +0000

    Now extends AbstractDirAction

--- trunk/toolbox/src/toolbox/util/ui/explorer/action/CreateDirAction.java
+++ trunk/toolbox/src/toolbox/util/ui/explorer/action/CreateDirAction.java
@@ -1,10 +1,8 @@
 package toolbox.util.ui.explorer.action;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
 import java.io.File;
 
-import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JOptionPane;
 
@@ -13,27 +11,18 @@ import org.apache.log4j.Logger;
 
 import toolbox.util.SwingUtil;
 import toolbox.util.ui.JSmartOptionPane;
-import toolbox.util.ui.explorer.JFileExplorerProxy;
-import toolbox.util.ui.explorer.listener.DriveComboListener;
+import toolbox.util.ui.explorer.JFileExplorer;
 
 /**
  * CreateDirAction is responsible for creating a new directory in the 
- * JFileExplorer.
+ * currently selected directory. A dialog box is used to prompt the user for the
+ * name of the new directory.
  */
-public class CreateDirAction extends AbstractAction
+public class CreateDirAction extends AbstractDirAction
 {
     private static final Logger logger_ = 
         Logger.getLogger(CreateDirAction.class);
     
-    //--------------------------------------------------------------------------
-    // Fields
-    //--------------------------------------------------------------------------
-
-    /**
-     * Parent explorer.
-     */
-    private final JFileExplorerProxy explorer_;
-
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
@@ -41,13 +30,12 @@ public class CreateDirAction extends AbstractAction
     /**
      * Creates a CreateDirAction.
      * 
-     * @param explorer File explorer proxy.
+     * @param explorer File explorer.
      */
-    public CreateDirAction(JFileExplorerProxy explorer)
+    public CreateDirAction(JFileExplorer explorer)
     {
-        super("Create");
+        super("Create", explorer);
         putValue(Action.MNEMONIC_KEY, new Integer('C'));
-        explorer_ = explorer;
     }
 
     //--------------------------------------------------------------------------
@@ -61,15 +49,15 @@ public class CreateDirAction extends AbstractAction
     public void actionPerformed(ActionEvent e)
     {
         String path = StringUtils.chomp(
-            explorer_.getOriginal().getCurrentPath(), File.separator);
+            getExplorer().getCurrentPath(), File.separator);
         
         int lastDirIdx = path.lastIndexOf(File.separator);
         String lastDir = lastDirIdx < 0 ? path : path.substring(lastDirIdx + 1);
         
         String newDir = 
             JOptionPane.showInputDialog(
-                explorer_.getOriginal(), 
-                "Enter the new name",
+                getExplorer(), 
+                "Enter the name of the directory to create",
                 lastDir);
         
         if (!StringUtils.isBlank(newDir))
@@ -77,29 +65,16 @@ public class CreateDirAction extends AbstractAction
             File parent = new File(path);
             File createdDir = new File(parent, newDir);
             logger_.debug("Creating dir " + createdDir);
-            boolean success = createdDir.mkdir();
+            
 
-            if (success)
+            if (createdDir.mkdir())
             {
-                String folder = createdDir.getAbsolutePath();
-                
-                new DriveComboListener(
-                    explorer_.getOriginal()).itemStateChanged(
-                        new ItemEvent(
-                            explorer_.getRootsComboBox(), 
-                            0, 
-                            null, 
-                            ItemEvent.SELECTED));
-                        
-                explorer_.selectFolder(folder);
-                
-                //setFileList(folder);
-                //getFileList().setSelectedValue(file, true);
+                refresh(createdDir.getAbsolutePath());
             }
             else
             {
                 JSmartOptionPane.showMessageDialog(
-                    SwingUtil.getFrameAncestor(explorer_.getOriginal()),
+                    SwingUtil.getFrameAncestor(getExplorer()),
                     "Create directory failed",
                     "Error - Create",
                     JOptionPane.ERROR_MESSAGE);
--- trunk/toolbox/src/toolbox/util/ui/explorer/action/RenameDirAction.java
+++ trunk/toolbox/src/toolbox/util/ui/explorer/action/RenameDirAction.java
@@ -1,10 +1,8 @@
 package toolbox.util.ui.explorer.action;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ItemEvent;
 import java.io.File;
 
-import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JOptionPane;
 
@@ -13,27 +11,17 @@ import org.apache.log4j.Logger;
 
 import toolbox.util.SwingUtil;
 import toolbox.util.ui.JSmartOptionPane;
-import toolbox.util.ui.explorer.JFileExplorerProxy;
-import toolbox.util.ui.explorer.listener.DriveComboListener;
+import toolbox.util.ui.explorer.JFileExplorer;
 
 /**
  * RenameDirAction is responsible for renaming the currently selected directory
  * in the JFileExplorer.
  */
-public class RenameDirAction extends AbstractAction
+public class RenameDirAction extends AbstractDirAction
 {
     private static final Logger logger_ = 
         Logger.getLogger(RenameDirAction.class);
     
-    //--------------------------------------------------------------------------
-    // Fields
-    //--------------------------------------------------------------------------
-
-    /**
-     * Parent explorer.
-     */
-    private final JFileExplorerProxy explorer_;
-
     //--------------------------------------------------------------------------
     // Constructors
     //--------------------------------------------------------------------------
@@ -41,13 +29,12 @@ public class RenameDirAction extends AbstractAction
     /**
      * Creates a RenameDirAction.
      * 
-     * @param explorer File explorer proxy.
+     * @param explorer File explorer.
      */
-    public RenameDirAction(JFileExplorerProxy explorer)
+    public RenameDirAction(JFileExplorer explorer)
     {
-        super("Rename");
+        super("Rename", explorer);
         putValue(Action.MNEMONIC_KEY, new Integer('R'));
-        explorer_ = explorer;
     }
 
     //--------------------------------------------------------------------------
@@ -61,14 +48,14 @@ public class RenameDirAction extends AbstractAction
     public void actionPerformed(ActionEvent e)
     {
         String path = StringUtils.chomp(
-            explorer_.getOriginal().getCurrentPath(), File.separator);
+            getExplorer().getCurrentPath(), File.separator);
         
         int lastDirIdx = path.lastIndexOf(File.separator);
         String lastDir = lastDirIdx < 0 ? path : path.substring(lastDirIdx + 1);
         
         String newName = 
             JOptionPane.showInputDialog(
-                explorer_.getOriginal(), 
+                getExplorer(), 
                 "Enter the new name",
                 lastDir);
         
@@ -80,29 +67,14 @@ public class RenameDirAction extends AbstractAction
             
             logger_.debug("From: " + from + "  To: " + to);
             
-            boolean success  = from.renameTo(to);
-
-            if (success)
+            if (from.renameTo(to))
             {
-                String folder = to.getAbsolutePath();
-                
-                new DriveComboListener(
-                    explorer_.getOriginal()).itemStateChanged(
-                        new ItemEvent(
-                            explorer_.getRootsComboBox(), 
-                            0, 
-                            null, 
-                            ItemEvent.SELECTED));
-                        
-                explorer_.selectFolder(folder);
-                
-                //setFileList(folder);
-                //getFileList().setSelectedValue(file, true);
+                refresh(to.getAbsolutePath());
             }
             else
             {
                 JSmartOptionPane.showMessageDialog(
-                    SwingUtil.getFrameAncestor(explorer_.getOriginal()),
+                    SwingUtil.getFrameAncestor(getExplorer()),
                     "File rename failed",
                     "Error - Rename",
                     JOptionPane.ERROR_MESSAGE);
