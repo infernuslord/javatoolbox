1930e31bd9582ece72fe82fdee952870c34c5f6d
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-07 22:31:45 +0000

    Updated to create its temp file the system temp directory

--- trunk/toolbox/test/toolbox/util/AbstractConstantTest.java
+++ trunk/toolbox/test/toolbox/util/AbstractConstantTest.java
@@ -1,5 +1,6 @@
 package toolbox.util;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -12,9 +13,7 @@ import junit.textui.TestRunner;
 import org.apache.log4j.Logger;
 
 /**
- * Unit test for AbstractConstant.
- * 
- * @see toolbox.util.AbstractConstant
+ * Unit test for {@link toolbox.util.AbstractConstant}.
  */
 public class AbstractConstantTest extends TestCase
 {
@@ -25,7 +24,17 @@ public class AbstractConstantTest extends TestCase
     // Constants
     //--------------------------------------------------------------------------
 
-    private static final String FNAME = "const.ser";
+    /**
+     * File that the serilaized data of the constant will be read from/written 
+     * to.
+     */
+    private static final File FILE_SERIALIZED = 
+        new File(FileUtil.getTempDir(), 
+            AbstractConstant.class.getName() + ".ser");
+    
+    /**
+     * Instance of the constant to serialize.
+     */
     private static final MockConstant CONST = MockConstant.TWO;
 
     //--------------------------------------------------------------------------
@@ -41,23 +50,43 @@ public class AbstractConstantTest extends TestCase
     // Unit Tests
     //--------------------------------------------------------------------------
 
+    /**
+     * Tests passivation/activation of a constant to make sure it retains its
+     * single instance identity.
+     * 
+     * @throws Exception on error.
+     */
     public void testSerialization() throws Exception
     {
         logger_.info("Running testSerialization...");
 
-        save(CONST);
-        MockConstant nc = (MockConstant) load();
-        assertTrue("Failed to match using ==", CONST == nc);
+        try
+        {
+            save(CONST);
+            MockConstant nc = (MockConstant) load();
+            assertTrue("Failed to match using ==", CONST == nc);
+        }
+        finally
+        {
+            FileUtil.delete(FILE_SERIALIZED);
+        }
     }
 
     //--------------------------------------------------------------------------
     // Helpers
     //--------------------------------------------------------------------------
 
+    /**
+     * Loads an instance of a constant from a serialized file.
+     * 
+     * @return Loaded instance.
+     * @throws IOException on I/O error.
+     * @throws ClassNotFoundException on class not found error.
+     */
     static Object load() throws IOException, ClassNotFoundException
     {
         Object o = null;
-        FileInputStream fis = new FileInputStream(FNAME);
+        FileInputStream fis = new FileInputStream(FILE_SERIALIZED);
         ObjectInputStream ois = new ObjectInputStream(fis);
         o = ois.readObject();
         ois.close();
@@ -65,9 +94,15 @@ public class AbstractConstantTest extends TestCase
     }
 
 
+    /**
+     * Saves an instance of a constant to a file in serialzied form.
+     * 
+     * @param o Constant to serialize.
+     * @throws IOException on I/O error.
+     */
     static void save(Object o) throws IOException
     {
-        FileOutputStream fos = new FileOutputStream(FNAME);
+        FileOutputStream fos = new FileOutputStream(FILE_SERIALIZED);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
         oos.writeObject(o);
         oos.flush();
