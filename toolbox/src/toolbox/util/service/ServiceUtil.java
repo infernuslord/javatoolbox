2571508eaacff20c26eceedad9ec541207f32e91
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-08 21:42:01 +0000

    Refactored

--- toolbox/src/toolbox/util/service/ServiceUtil.java
+++ toolbox/src/toolbox/util/service/ServiceUtil.java
@@ -8,7 +8,6 @@ import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
 
-import toolbox.util.ArrayUtil;
 import toolbox.util.statemachine.StateMachine;
 import toolbox.util.statemachine.StateMachineFactory;
 
@@ -40,118 +39,178 @@ public class ServiceUtil
      */
     public static StateMachine createStateMachine(Class[] serviceClasses)
     {
-        StateMachine machine = 
-            StateMachineFactory.createStateMachine("ServiceStateMachine");
+        Set serviceAttr = new HashSet();
+        CollectionUtils.addAll(serviceAttr, serviceClasses);
+
+        Set initSet = new HashSet(
+            Arrays.asList(new Class[] { Initializable.class }));
         
-        Set natures = new HashSet();
-        CollectionUtils.addAll(natures, serviceClasses);
-    
-        Set initDestroy = new HashSet(
-            Arrays.asList(new Class[]{Initializable.class, Destroyable.class}));
-    
-        // Initializable/Destroyable -------------------------------------------
+        if (CollectionUtils.isEqualCollection(initSet, serviceAttr))
+            return createInitializable();
         
-        if (CollectionUtils.isEqualCollection(natures, initDestroy))
-        {
-            // uninit    --> init
-            // init      --> destroyed
-            // destroyed --> init
-            
-            machine.addState(ServiceState.UNINITIALIZED);
-            machine.addState(ServiceState.INITIALIZED);
-            machine.addState(ServiceState.DESTROYED);
-            
-            machine.setBeginState(ServiceState.UNINITIALIZED);
-            
-            machine.addTransition(
-                ServiceTransition.INITIALIZE, 
-                ServiceState.UNINITIALIZED, 
-                ServiceState.INITIALIZED);
-            
-            machine.addTransition(
-                ServiceTransition.DESTROY, 
-                ServiceState.INITIALIZED,
-                ServiceState.DESTROYED);
-                
-            machine.addTransition(
-                ServiceTransition.INITIALIZE, 
-                ServiceState.DESTROYED, 
-                ServiceState.INITIALIZED);
-            
-            machine.reset();
-            return machine;
-        }            
+        Set startSet = new HashSet(
+            Arrays.asList(new Class[] { Startable.class }));
         
+        if (CollectionUtils.isEqualCollection(startSet, serviceAttr))
+            return createStartable();
+
+        //----------------------------------------------------------------------
         
-        if (ArrayUtil.contains(serviceClasses, Startable.class))
-        {
-            machine.addState(ServiceState.RUNNING);
-            machine.addState(ServiceState.STOPPED);
-            machine.setBeginState(ServiceState.STOPPED);
-            
-            machine.addTransition(
-                ServiceTransition.START, 
-                ServiceState.STOPPED, 
-                ServiceState.RUNNING);
-            
-            machine.addTransition(
-                ServiceTransition.STOP, 
-                ServiceState.RUNNING,
-                ServiceState.STOPPED);
-        }
+        Set initStartSet = new HashSet(
+            Arrays.asList(new Class[] { Initializable.class, Startable.class }));
         
-        if (ArrayUtil.contains(serviceClasses,Suspendable.class))
-        {
-            machine.addState(ServiceState.SUSPENDED);
-            
-            machine.addTransition(
-                ServiceTransition.SUSPEND, 
-                ServiceState.RUNNING, 
-                ServiceState.SUSPENDED);
-            
-            machine.addTransition(
-                ServiceTransition.RESUME, 
-                ServiceState.SUSPENDED,
-                ServiceState.RUNNING);
-        }
+        if (CollectionUtils.isEqualCollection(initStartSet, serviceAttr))
+            return createInitializableStartable();
+
+        //----------------------------------------------------------------------
         
-        if (ArrayUtil.contains(serviceClasses, Initializable.class))
-        {
-            machine.addState(ServiceState.UNINITIALIZED);
-            machine.addState(ServiceState.INITIALIZED);
-            machine.setBeginState(ServiceState.UNINITIALIZED);
-            
-            machine.addTransition(
-                ServiceTransition.INITIALIZE, 
-                ServiceState.UNINITIALIZED, 
-                ServiceState.INITIALIZED);
-            
-            machine.addTransition(
-                ServiceTransition.START, 
-                ServiceState.INITIALIZED,
-                ServiceState.RUNNING);
-        }
+        Set startSuspendSet = new HashSet(
+            Arrays.asList(new Class[] { Startable.class, Suspendable.class }));
         
-        if (ArrayUtil.contains(serviceClasses, Destroyable.class))
-        {
-            machine.addState(ServiceState.DESTROYED);
-            
-            machine.addTransition(
-                ServiceTransition.DESTROY, 
-                ServiceState.STOPPED, 
-                ServiceState.DESTROYED);
-            
-            if ((ArrayUtil.contains(serviceClasses, Initializable.class)))
-            {
-                machine.addTransition(
-                    ServiceTransition.DESTROY, 
-                    ServiceState.INITIALIZED, 
-                    ServiceState.DESTROYED);
-            }
-        }
+        if (CollectionUtils.isEqualCollection(startSuspendSet, serviceAttr))
+            return createStartableSuspendable();
+
+        Set allSet = new HashSet(
+            Arrays.asList(new Class[] { 
+                Initializable.class, 
+                Startable.class, 
+                Suspendable.class,
+                Destroyable.class
+            }));
         
-        machine.reset();
-        return machine;
+        if (CollectionUtils.isEqualCollection(allSet, serviceAttr))
+            return createAll();
+
+        Set initDestroySet = new HashSet(
+            Arrays.asList(new Class[] { Initializable.class, Destroyable.class }));
+        
+        if (CollectionUtils.isEqualCollection(initDestroySet, serviceAttr))
+            return createInitializableDestroyable();
+
+        Set initStartDestroySet = new HashSet(
+            Arrays.asList(new Class[] { 
+                Initializable.class, 
+                Startable.class, 
+                Destroyable.class 
+            }));
+        
+        if (CollectionUtils.isEqualCollection(initStartDestroySet, serviceAttr))
+            return createInitializableStartableDestroyable();
+        
+        return null;
+        
+//        StateMachine machine = 
+//            StateMachineFactory.createStateMachine("ServiceStateMachine");
+//        
+//        Set natures = new HashSet();
+//        CollectionUtils.addAll(natures, serviceClasses);
+//    
+//        Set initDestroy = new HashSet(
+//            Arrays.asList(new Class[]{Initializable.class, Destroyable.class}));
+//    
+//        // Initializable/Destroyable -------------------------------------------
+//        
+//        if (CollectionUtils.isEqualCollection(natures, initDestroy))
+//        {
+//            // uninit    --> init
+//            // init      --> destroyed
+//            // destroyed --> init
+//            
+//            machine.addState(ServiceState.UNINITIALIZED);
+//            machine.addState(ServiceState.INITIALIZED);
+//            machine.addState(ServiceState.DESTROYED);
+//            
+//            machine.setBeginState(ServiceState.UNINITIALIZED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.INITIALIZE, 
+//                ServiceState.UNINITIALIZED, 
+//                ServiceState.INITIALIZED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.DESTROY, 
+//                ServiceState.INITIALIZED,
+//                ServiceState.DESTROYED);
+//                
+//            machine.addTransition(
+//                ServiceTransition.INITIALIZE, 
+//                ServiceState.DESTROYED, 
+//                ServiceState.INITIALIZED);
+//            
+//            machine.reset();
+//            return machine;
+//        }            
+//        
+//        
+//        if (ArrayUtil.contains(serviceClasses, Startable.class))
+//        {
+//            machine.addState(ServiceState.RUNNING);
+//            machine.addState(ServiceState.STOPPED);
+//            machine.setBeginState(ServiceState.STOPPED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.START, 
+//                ServiceState.STOPPED, 
+//                ServiceState.RUNNING);
+//            
+//            machine.addTransition(
+//                ServiceTransition.STOP, 
+//                ServiceState.RUNNING,
+//                ServiceState.STOPPED);
+//        }
+//        
+//        if (ArrayUtil.contains(serviceClasses,Suspendable.class))
+//        {
+//            machine.addState(ServiceState.SUSPENDED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.SUSPEND, 
+//                ServiceState.RUNNING, 
+//                ServiceState.SUSPENDED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.RESUME, 
+//                ServiceState.SUSPENDED,
+//                ServiceState.RUNNING);
+//        }
+//        
+//        if (ArrayUtil.contains(serviceClasses, Initializable.class))
+//        {
+//            machine.addState(ServiceState.UNINITIALIZED);
+//            machine.addState(ServiceState.INITIALIZED);
+//            machine.setBeginState(ServiceState.UNINITIALIZED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.INITIALIZE, 
+//                ServiceState.UNINITIALIZED, 
+//                ServiceState.INITIALIZED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.START, 
+//                ServiceState.INITIALIZED,
+//                ServiceState.RUNNING);
+//        }
+//        
+//        if (ArrayUtil.contains(serviceClasses, Destroyable.class))
+//        {
+//            machine.addState(ServiceState.DESTROYED);
+//            
+//            machine.addTransition(
+//                ServiceTransition.DESTROY, 
+//                ServiceState.STOPPED, 
+//                ServiceState.DESTROYED);
+//            
+//            if ((ArrayUtil.contains(serviceClasses, Initializable.class)))
+//            {
+//                machine.addTransition(
+//                    ServiceTransition.DESTROY, 
+//                    ServiceState.INITIALIZED, 
+//                    ServiceState.DESTROYED);
+//            }
+//        }
+//        
+//        machine.reset();
+//        return machine;
     }
 
     /**
@@ -180,5 +239,264 @@ public class ServiceUtil
         //logger_.debug("Natures: " + ArrayUtil.toString(natures.toArray()));
         return createStateMachine((Class[]) natures.toArray(new Class[0]));
     }
+    
+    //--------------------------------------------------------------------------
+    // Private
+    //--------------------------------------------------------------------------
+    
+    private static StateMachine createInitializable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Initializable");
+        
+        // uninit    --> init
+        // init      --> destroyed
+        // destroyed --> init
+        
+        machine.addState(ServiceState.UNINITIALIZED);
+        machine.addState(ServiceState.INITIALIZED);
+        
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.UNINITIALIZED, 
+            ServiceState.INITIALIZED);
+        
+        machine.setBeginState(ServiceState.UNINITIALIZED);
+        machine.reset();
+        return machine;
+    }
+    
+    
+    private static StateMachine createStartable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Startable");
 
+        machine.addState(ServiceState.RUNNING);
+        machine.addState(ServiceState.STOPPED);
+        machine.setBeginState(ServiceState.STOPPED);
+        
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.STOPPED, 
+            ServiceState.RUNNING);
+        
+        machine.addTransition(
+            ServiceTransition.STOP, 
+            ServiceState.RUNNING,
+            ServiceState.STOPPED);
+        
+        machine.reset();
+        return machine;
+    }
+    
+    
+    private static StateMachine createStartableSuspendable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Startable/Suspendable");
+
+        machine.addState(ServiceState.RUNNING);
+        machine.addState(ServiceState.STOPPED);
+        machine.addState(ServiceState.SUSPENDED);
+        machine.setBeginState(ServiceState.STOPPED);
+        
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.STOPPED, 
+            ServiceState.RUNNING);
+        
+        machine.addTransition(
+            ServiceTransition.STOP, 
+            ServiceState.RUNNING,
+            ServiceState.STOPPED);
+    
+        machine.addTransition(
+            ServiceTransition.SUSPEND, 
+            ServiceState.RUNNING,
+            ServiceState.SUSPENDED);
+        
+        machine.addTransition(
+            ServiceTransition.RESUME, 
+            ServiceState.SUSPENDED,
+            ServiceState.RUNNING);
+        
+        machine.reset();
+        return machine;
+    }
+
+    
+    private static StateMachine createAll()
+    {
+        StateMachine machine = StateMachineFactory.createStateMachine("All");
+
+        machine.addState(ServiceState.UNINITIALIZED);
+        machine.addState(ServiceState.INITIALIZED);
+        machine.addState(ServiceState.DESTROYED);
+        machine.addState(ServiceState.RUNNING);
+        machine.addState(ServiceState.STOPPED);
+        machine.addState(ServiceState.SUSPENDED);
+        
+        machine.setBeginState(ServiceState.UNINITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.UNINITIALIZED, 
+            ServiceState.INITIALIZED);
+      
+        machine.addTransition(
+            ServiceTransition.DESTROY, 
+            ServiceState.INITIALIZED,
+            ServiceState.DESTROYED);
+          
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.DESTROYED, 
+            ServiceState.INITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.INITIALIZED, 
+            ServiceState.RUNNING);
+
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.STOPPED, 
+            ServiceState.RUNNING);
+        
+        machine.addTransition(
+            ServiceTransition.STOP, 
+            ServiceState.RUNNING,
+            ServiceState.STOPPED);
+    
+        machine.addTransition(
+            ServiceTransition.SUSPEND, 
+            ServiceState.RUNNING,
+            ServiceState.SUSPENDED);
+        
+        machine.addTransition(
+            ServiceTransition.RESUME, 
+            ServiceState.SUSPENDED,
+            ServiceState.RUNNING);
+        
+        machine.reset();
+        return machine;
+    }
+
+    
+    private static StateMachine createInitializableDestroyable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Initializable/Destroyable");
+
+        machine.addState(ServiceState.UNINITIALIZED);
+        machine.addState(ServiceState.INITIALIZED);
+        machine.addState(ServiceState.DESTROYED);
+
+        machine.setBeginState(ServiceState.UNINITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.INITIALIZE,
+            ServiceState.UNINITIALIZED, 
+            ServiceState.INITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.DESTROY,
+            ServiceState.INITIALIZED, 
+            ServiceState.DESTROYED);
+
+        machine.addTransition(
+            ServiceTransition.INITIALIZE,
+            ServiceState.DESTROYED, 
+            ServiceState.INITIALIZED);
+        
+        machine.reset();
+        return machine;
+    }
+    
+    
+    private static StateMachine createInitializableStartableDestroyable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Init/Start/Destroy");
+
+        machine.addState(ServiceState.UNINITIALIZED);
+        machine.addState(ServiceState.INITIALIZED);
+        machine.addState(ServiceState.DESTROYED);
+        machine.addState(ServiceState.RUNNING);
+        machine.addState(ServiceState.STOPPED);
+        
+        machine.setBeginState(ServiceState.UNINITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.UNINITIALIZED, 
+            ServiceState.INITIALIZED);
+      
+        machine.addTransition(
+            ServiceTransition.DESTROY, 
+            ServiceState.INITIALIZED,
+            ServiceState.DESTROYED);
+          
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.DESTROYED, 
+            ServiceState.INITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.INITIALIZED, 
+            ServiceState.RUNNING);
+
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.STOPPED, 
+            ServiceState.RUNNING);
+        
+        machine.addTransition(
+            ServiceTransition.STOP, 
+            ServiceState.RUNNING,
+            ServiceState.STOPPED);
+        
+        machine.reset();
+        return machine;
+    }
+    
+    
+    private static StateMachine createInitializableStartable()
+    {
+        StateMachine machine = 
+            StateMachineFactory.createStateMachine("Init/Start");
+
+        machine.addState(ServiceState.UNINITIALIZED);
+        machine.addState(ServiceState.INITIALIZED);
+        machine.addState(ServiceState.RUNNING);
+        machine.addState(ServiceState.STOPPED);
+        
+        machine.setBeginState(ServiceState.UNINITIALIZED);
+
+        machine.addTransition(
+            ServiceTransition.INITIALIZE, 
+            ServiceState.UNINITIALIZED, 
+            ServiceState.INITIALIZED);
+      
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.INITIALIZED, 
+            ServiceState.RUNNING);
+
+        machine.addTransition(
+            ServiceTransition.START, 
+            ServiceState.STOPPED, 
+            ServiceState.RUNNING);
+        
+        machine.addTransition(
+            ServiceTransition.STOP, 
+            ServiceState.RUNNING,
+            ServiceState.STOPPED);
+        
+        machine.reset();
+        return machine;
+    }
+    
 }
