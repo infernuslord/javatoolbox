e13b461ed1d4a135f1e819090e1a78c5beb737d1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-08 21:39:26 +0000

    Implemented getStateMachine()

--- trunk/toolbox/src/toolbox/plugin/netmeter/Client.java
+++ trunk/toolbox/src/toolbox/plugin/netmeter/Client.java
@@ -401,6 +401,15 @@ public class Client implements Initializable, Startable, Destroyable,
         notifier_.removeServiceListener(listener);
     }
     
+    
+    /**
+     * @see toolbox.util.service.ObservableService#getStateMachine()
+     */
+    public StateMachine getStateMachine()
+    {
+        return machine_;
+    }
+    
     //--------------------------------------------------------------------------
     // ThroughputCollector
     //--------------------------------------------------------------------------
