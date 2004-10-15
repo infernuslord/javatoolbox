6ab729779e56e391cc2527abdbc256fc8146eaf6
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-15 21:21:08 +0000

    Added exitForTesting() method that is used to exit the application during unit testing

--- trunk/toolbox/src/toolbox/workspace/action/ExitAction.java
+++ trunk/toolbox/src/toolbox/workspace/action/ExitAction.java
@@ -8,6 +8,7 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
 import toolbox.util.ExceptionUtil;
+import toolbox.workspace.IPlugin;
 import toolbox.workspace.PluginWorkspace;
 
 /**
@@ -35,24 +36,66 @@ public class ExitAction extends BaseAction
 
 
     /**
+     * Routes method invocation to exit().
+     * 
      * @see java.awt.event.ActionListener#actionPerformed(
      *      java.awt.event.ActionEvent)
      */
     public void actionPerformed(ActionEvent ae)
     {
+        exit();
+    }
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * The real exit method.
+     */
+    public void exit()
+    {
+        exitForTesting();
+        System.exit(0);
+    }
+    
+    /**
+     * Exits the application but does not exit the JVM.
+     */
+    public void exitForTesting()
+    {
         try
         {
+            // Save all preferences first...
             (new SavePreferencesAction(getWorkspace())).runAction();
+            
+            // Release resources retained by the plugins by "destroying" them
+            IPlugin[] plugins = getWorkspace().getPluginHost().getPlugins();
+            
+            logger_.debug("Plugins to destroy:" + plugins.length);
+            for (int i = 0; i < plugins.length; i++)
+            {
+                try
+                {
+                    logger_.debug(
+                        "Destroying plugin " + plugins[i].getPluginName());
+                    
+                    plugins[i].destroy();
+                }
+                catch (Exception e)
+                {
+                    logger_.error("destroy", e);
+                }
+            }
         }
         catch (Exception e)
         {
             ExceptionUtil.handleUI(e, logger_);
         }
         
-        getWorkspace().setVisible(false);
+        //getWorkspace().setVisible(false);
         getWorkspace().dispose();
         logger_.debug("Goodbye!");
         LogManager.shutdown();
-        System.exit(0);
     }
 }
