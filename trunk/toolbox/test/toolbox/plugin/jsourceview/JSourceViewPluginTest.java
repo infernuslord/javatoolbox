b2cc9cbe4fa20bd68728c0e3fac275fb27e9181c
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-15 21:20:28 +0000

    Fixed problem with the test exiting the JVM during JUnit testing from maven by creating a separate exitForTesting() method that does not call System.exit()

--- trunk/toolbox/test/toolbox/plugin/jsourceview/JSourceViewPluginTest.java
+++ trunk/toolbox/test/toolbox/plugin/jsourceview/JSourceViewPluginTest.java
@@ -2,6 +2,7 @@ package toolbox.plugin.jsourceview;
 
 import java.io.IOException;
 
+import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
 import org.apache.log4j.Logger;
@@ -18,16 +19,17 @@ import org.netbeans.jemmy.operators.JPopupMenuOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
 import org.netbeans.jemmy.util.NameComponentChooser;
 
-import toolbox.junit.testcase.UITestCase;
 import toolbox.util.FileUtil;
 import toolbox.util.JemmyUtil;
+import toolbox.util.ThreadUtil;
 import toolbox.workspace.PluginMenu;
 import toolbox.workspace.PluginWorkspace;
+import toolbox.workspace.action.ExitAction;
 
 /**
  * Unit test for {@link toolbox.plugin.jsourceview.JSourceViewPlugin}.
  */
-public class JSourceViewPluginTest extends UITestCase
+public class JSourceViewPluginTest extends TestCase
 {
     private static final Logger logger_ = 
         Logger.getLogger(JSourceViewPluginTest.class);
@@ -53,7 +55,6 @@ public class JSourceViewPluginTest extends UITestCase
     public static void main(String[] args) throws Exception
     {
         TestRunner.run(JSourceViewPluginTest.class);
-        //System.exit(0); 
     }
 
     //--------------------------------------------------------------------------
@@ -65,24 +66,25 @@ public class JSourceViewPluginTest extends UITestCase
      */
     protected void setUp() throws Exception
     {
+        // TODO: SunAWTRobot does not let the JVM exit properly on linux so
+        //       we need to make sure it isn't used on linux especially.
+        //       Move behavior up to superclass on wich model to use based on
+        //       operating system.
+        
+        //JemmyProperties.setCurrentDispatchingModel(
+        //    JemmyProperties.ROBOT_MODEL_MASK);
+
         JemmyProperties.setCurrentDispatchingModel(
-            JemmyProperties.ROBOT_MODEL_MASK);
+            JemmyProperties.QUEUE_MODEL_MASK);
+
+        //JemmyProperties.setCurrentDispatchingModel(
+        //    JemmyProperties.SHORTCUT_MODEL_MASK);
 
         JemmyProperties.setCurrentOutput(TestOut.getNullOutput());
         
         prefsFile_ = FileUtil.createTempFilename() + "-toolbox.xml";
         workspace_ = new PluginWorkspace(prefsFile_);
         window_ = new JFrameOperator(workspace_);
-    }
-   
-    
-    /**
-     * @see junit.framework.TestCase#tearDown()
-     */
-    protected void tearDown() throws Exception
-    {
-        window_.close();
-        FileUtil.delete(prefsFile_);
     }
     
     //--------------------------------------------------------------------------
@@ -97,45 +99,81 @@ public class JSourceViewPluginTest extends UITestCase
     public void testJSourceViewPlugin() throws IOException
     {
         logger_.info("Running testJSourceViewPlugin...");
-        
-        JMenuBarOperator mbo = new JMenuBarOperator(window_);
-        mbo.pushMenu(PluginMenu.LABEL);
-        JPopupMenuOperator pmo = new JPopupMenuOperator(window_);
-        new JCheckBoxMenuItemOperator(window_, JSourceViewPlugin.NAME).push();
-        
-        // Start search
-        JemmyUtil.findTextField(window_, JSourceView.NAME_DIRECTORY_FIELD)
-            .enterText(FileUtil.getTempDir().getAbsolutePath());
-                
-        //"J:\\tmp\\wsad\\workspace\\toolbox\\src\\toolbox\\findclass");
-
-        // Wait for search to complete
-        JLabelOperator.waitJLabel(workspace_, "Elapsed", false, false);
-        
-        // Show chart in dialog box
-        new JButtonOperator(window_, 
-            new NameComponentChooser(
-                JSourceView.NAME_CHART_BUTTON)).pushNoBlock();
-        
-        // Dismiss dialog box
-        JDialogOperator.waitJDialog("Message", true, true);
-        new JButtonOperator(new JDialogOperator("Message"), "OK").push();
-        
-        // Save to file 
-        new JButtonOperator(window_, 
-            new NameComponentChooser(
-                JSourceView.NAME_SAVE_BUTTON)).pushNoBlock();
-        
-        // Set file name and save
-        JDialogOperator saveOp = new JDialogOperator("Save to file");
+     
         String saveFile = FileUtil.createTempFilename() + ".txt";
-        new JTextFieldOperator(saveOp).enterText(saveFile);
-        saveOp.waitClosed();
+        String javaFile = FileUtil.createTempFilename() + ".java"; 
         
-        // Verify
-        int len = FileUtil.getFileContents(saveFile).length();
-        logger_.debug("Saved file is " + len + " bytes");
-        assertTrue(len > 0);
-        FileUtil.delete(saveFile);
+        try
+        {
+            JMenuBarOperator mbo = new JMenuBarOperator(window_);
+            mbo.pushMenu(PluginMenu.LABEL);
+            JPopupMenuOperator pmo = new JPopupMenuOperator(window_);
+            
+            new JCheckBoxMenuItemOperator(
+                window_, 
+                JSourceViewPlugin.NAME).push();
+            
+            // Write a java file out to the system temp dir so we atleast have
+            // one file that is picked up
+            
+            String javaSource = 
+                  "public class JavaFile { \n" 
+                + "  \n"
+                + "  public static void main(String args[]) { \n"
+                + "    // This is a comment \n"
+                + "    System.out.println(\"Hello world!\"); \n"
+                + "  } \n"
+                + "} \n";
+            
+            FileUtil.setFileContents(javaFile, javaSource, false);
+                
+            // Start search in the system temp directory
+            JemmyUtil.findTextField(window_, 
+                JSourceView.NAME_DIRECTORY_FIELD).enterText(
+                    FileUtil.getTempDir().getAbsolutePath());
+                    
+            // Wait for search to complete
+            JLabelOperator.waitJLabel(workspace_, "Elapsed", false, false);
+            
+            // Show chart in dialog box
+            new JButtonOperator(window_, 
+                new NameComponentChooser(
+                    JSourceView.NAME_CHART_BUTTON)).pushNoBlock();
+            
+            // Dismiss dialog box
+            JDialogOperator.waitJDialog("Message", true, true);
+            new JButtonOperator(new JDialogOperator("Message"), "OK").push();
+            
+            
+            // Save to file 
+            new JButtonOperator(window_, 
+                new NameComponentChooser(
+                    JSourceView.NAME_SAVE_BUTTON)).pushNoBlock();
+            
+            // Set file name and save
+            JDialogOperator saveOp = new JDialogOperator("Save to file");
+            
+            new JTextFieldOperator(saveOp).enterText(saveFile);
+            saveOp.waitClosed();
+            
+            // FIXME:
+            // If this delay is not here, the output file is not ready to be
+            // read for some reason on linux only...
+            logger_.debug("Sleeping (linux workaround)...");
+            ThreadUtil.sleep(5000);
+            
+            // Verify
+            int len = FileUtil.getFileContents(saveFile).length();
+            logger_.debug("Saved file is " + len + " bytes");
+            assertTrue("Save file should not be empty", len > 0);
+            
+        }
+        finally 
+        {
+            new ExitAction(workspace_).exitForTesting();
+            FileUtil.delete(saveFile);
+            FileUtil.delete(javaFile);
+            FileUtil.delete(prefsFile_);
+        }
     }
 }
