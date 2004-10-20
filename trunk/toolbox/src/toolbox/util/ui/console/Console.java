24a96834a5992b50dbf722d9b61faeb715f9b6de
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2004-10-20 20:02:57 +0000

    Updates

--- trunk/toolbox/src/toolbox/util/ui/console/Console.java
+++ trunk/toolbox/src/toolbox/util/ui/console/Console.java
@@ -1,16 +1,20 @@
 package toolbox.util.ui.console;
 
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.OutputStream;
+
+import toolbox.util.service.Destroyable;
+import toolbox.util.service.Nameable;
 
 /**
  * Basic interface to an application console that supports intput/output via
  * streams.
  */
-public interface Console
+public interface Console extends Nameable, Destroyable
 {
     /**
-     * Returns the stream used for reading input from the console.
+     * Stream used to read input from the console.
      * 
      * @return InputStream
      */
@@ -18,11 +22,11 @@ public interface Console
 
 
     /**
-     * Returns the stream used to write output to the console.
+     * Stream used to write output to the console.
      * 
      * @return PrintStream
      */
-    PrintStream getOutputStream();
+    OutputStream getOutputStream();
 
 
     /**
@@ -30,31 +34,14 @@ public interface Console
      * 
      * @param text Text to send to the console.
      */
-    public void send(String text);
+    void send(String text) throws IOException;
 
 
     /**
      * Clears the contents of the console.
      */
     void clear();
-
-
-    /**
-     * Release any resources held by the console.
-     */
-    void dispose();
+    
+    
+    String getPrompt();
 }
-
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU Lesser General Public
-//License as published by the Free Software Foundation; either
-//version 2 of the License, or (at your option) any later version.
-//
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-//Lesser General Public License for more details.
-//
-//You should have received a copy of the GNU Lesser General Public
-//License along with this library; if not, write to the Free Software
-//Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
