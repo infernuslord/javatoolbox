e3372d94a18b41121c2571cfc432033df1850172
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-04 23:53:56 +0000

    Added test for size and location retention

--- trunk/toolbox/test/toolbox/util/ui/JSmartDialogTest.java
+++ trunk/toolbox/test/toolbox/util/ui/JSmartDialogTest.java
@@ -1,6 +1,7 @@
 package toolbox.util.ui;
 
-import javax.swing.JButton;
+import java.awt.FlowLayout;
+
 import javax.swing.JFrame;
 
 import junit.textui.TestRunner;
@@ -9,6 +10,7 @@ import org.apache.log4j.Logger;
 
 import toolbox.junit.testcase.UITestCase;
 import toolbox.util.SwingUtil;
+import toolbox.util.ui.action.DisposeAction;
 
 /**
  * Unit test for {@link toolbox.util.ui.JSmartDialog}.
@@ -40,11 +42,59 @@ public class JSmartDialogTest extends UITestCase
         
         JFrame f = new JFrame();
         JSmartDialog dlg = new JSmartDialog(f, true);
+        dlg.getContentPane().setLayout(new FlowLayout());
+        dlg.getContentPane().add(new JSmartButton("Hit esc to exit"));
         dlg.getContentPane().add(new JSmartLabel("Hit esc to exit!"));
-        dlg.getContentPane().add(new JButton("Hit esc to exit"));
         dlg.pack();
         SwingUtil.centerWindow(dlg);
         dlg.setVisible(true);
         f.dispose();
+    }
+
+    
+    public void testRememberSizeAndPosition()
+    {
+        logger_.info("Running testRememberSizeAndPosition...");
+        
+        JFrame f = new JFrame();
+        JSmartDialog dlg = new MockDialog(f,"Remember Size and Position",  true);
+        dlg.setResizable(true);
+        dlg.setSize(300,200);
+        dlg.getContentPane().add(new JSmartLabel("Resize me and hit esc. I should come back the same size"));
+        dlg.getContentPane().add(new JSmartButton(new DisposeAction("Nuke me", dlg)));
+        dlg.pack();
+        SwingUtil.centerWindow(dlg);
+        dlg.setVisible(true);
+        
+        logger_.info("Launching second window");
+        
+        // Reinstantiate same dialog box and make sure the new size was 
+        // retained.
+        
+        dlg = new MockDialog(f, "This is the new dialog", true);
+        dlg.setResizable(true);
+        dlg.getContentPane().add(new JSmartLabel("I should be same size and pos"));
+        dlg.getContentPane().add(new JSmartButton("Hit esc to exit"));
+        //dlg.pack();
+        dlg.setVisible(true);
+        
+        f.dispose();
+    }
+    
+    
+    class MockDialog extends JSmartDialog
+    {
+        public MockDialog(JFrame f, String string, boolean b)
+        {
+            super(f, string, b);
+        }
+
+        /**
+         * @see java.awt.Component#getName()
+         */
+        public String getName()
+        {
+            return "MockDialog";
+        }
     }
 }
