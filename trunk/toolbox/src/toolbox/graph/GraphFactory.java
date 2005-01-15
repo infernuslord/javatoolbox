1eae4ed7609673b2e3024e134e14c18150efb4e7
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-15 20:56:15 +0000

    Added createView() and getLayouts()

--- trunk/toolbox/src/toolbox/graph/GraphFactory.java
+++ trunk/toolbox/src/toolbox/graph/GraphFactory.java
@@ -1,5 +1,7 @@
 package toolbox.graph;
 
+import java.util.List;
+
 /**
  * GraphFactory is responsible for ___.
  */
@@ -9,6 +11,9 @@ public interface GraphFactory
     
     Edge createEdge(Vertex from, Vertex to);
     
-    Vertex createVertex(String name);
+    Vertex createVertex(Graph graph, String label);
     
+    GraphView createView(Graph graph);
+    
+    List getLayouts(Graph graph);
 }
