28859622b30864cb556e2c8a8664a34c46353e36
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-15 20:57:34 +0000

    Added methods to create a view and get the list of available layouts

--- toolbox/src/toolbox/graph/jung/JungGraphFactory.java
+++ toolbox/src/toolbox/graph/jung/JungGraphFactory.java
@@ -1,8 +1,15 @@
 package toolbox.graph.jung;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.uci.ics.jung.visualization.SpringLayout;
+import edu.uci.ics.jung.visualization.contrib.CircleLayout;
+
 import toolbox.graph.Edge;
 import toolbox.graph.Graph;
 import toolbox.graph.GraphFactory;
+import toolbox.graph.GraphView;
 import toolbox.graph.Vertex;
 
 /**
@@ -23,7 +30,7 @@ public class JungGraphFactory implements GraphFactory
      */
     public Edge createEdge(Vertex from, Vertex to)
     {
-        Edge e = new JungEdge();
+        Edge e = new JungEdge(from, to);
         return e;
     }
     
@@ -38,12 +45,34 @@ public class JungGraphFactory implements GraphFactory
     }
     
     
-    /**
-     * @see toolbox.graph.GraphFactory#createVertex(java.lang.String)
-     */
-    public Vertex createVertex(String label)
+    public Vertex createVertex(Graph graph, String label)
     {
-        Vertex v = new JungVertex(label);
+        Vertex v = new JungVertex(graph, label);
         return v;
+    }
+    
+    
+    /**
+     * @see toolbox.graph.GraphFactory#createView(toolbox.graph.Graph)
+     */
+    public GraphView createView(Graph graph)
+    {
+        GraphView view = new JungGraphView(graph);
+        return view;
+    }
+    
+    
+    /**
+     * @see toolbox.graph.GraphFactory#getLayouts()
+     */
+    public List getLayouts(Graph graph)
+    {
+        edu.uci.ics.jung.graph.Graph g = 
+            (edu.uci.ics.jung.graph.Graph) graph.getDelegate();
+        
+        List layouts = new ArrayList();
+        layouts.add(new JungLayout(new CircleLayout(g))); 
+        layouts.add(new JungLayout(new SpringLayout(g)));
+        return layouts;
     }
 }
