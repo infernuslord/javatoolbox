001555d6e366e0472bce29f2017ccebf723e412f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-15 20:58:24 +0000

    Added label support

--- toolbox/src/toolbox/graph/jung/JungVertex.java
+++ toolbox/src/toolbox/graph/jung/JungVertex.java
@@ -1,7 +1,12 @@
 package toolbox.graph.jung;
 
+import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.Vertex;
+import edu.uci.ics.jung.graph.decorators.StringLabeller;
+import edu.uci.ics.jung.graph.decorators.StringLabeller.UniqueLabelException;
 import edu.uci.ics.jung.graph.impl.DirectedSparseVertex;
+
+import org.apache.log4j.Logger;
 
 
 /**
@@ -9,16 +14,28 @@ import edu.uci.ics.jung.graph.impl.DirectedSparseVertex;
  */
 public class JungVertex implements toolbox.graph.Vertex
 {
+    private static final Logger logger_ = Logger.getLogger(JungVertex.class);
+    
     Vertex vertex_;
     
     /**
      * Creates a JungVertex.
      * 
      */
-    public JungVertex(String label)
+    public JungVertex(toolbox.graph.Graph graph, String label)
     {
         vertex_ = new DirectedSparseVertex();
-
+        Graph g = (Graph) graph.getDelegate();
+        g.addVertex(vertex_);
+        
+        try
+        {
+            StringLabeller.getLabeller(g).setLabel(vertex_, label);
+        }
+        catch (UniqueLabelException e)
+        {
+            logger_.error(e);
+        }
     }
 
     
