2adf0dceb68413aee84a6485434a9a08017dd3df
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-17 22:41:17 +0000

    Refactored bulk of test to abstract base class AbstractGraphTest

--- trunk/toolbox/test/toolbox/graph/jung/JungTest.java
+++ trunk/toolbox/test/toolbox/graph/jung/JungTest.java
@@ -1,81 +1,38 @@
 package toolbox.graph.jung;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import junit.textui.TestRunner;
 
 import org.apache.log4j.Logger;
 
-import toolbox.graph.Edge;
-import toolbox.graph.Graph;
+import toolbox.graph.AbstractGraphTest;
 import toolbox.graph.GraphLib;
-import toolbox.graph.GraphView;
-import toolbox.graph.Vertex;
-import toolbox.graph.prefuse.PrefuseGraphLib;
-import toolbox.junit.testcase.UITestCase;
-import toolbox.util.RandomUtil;
+import toolbox.graph.GraphLibFactory;
 
 /**
- * JungTest is responsible for _____.
+ * JungTest is responsible for testing classes in package toolbox.graph.jung. 
  */
-public class JungTest extends UITestCase
+public class JungTest extends AbstractGraphTest
 {
     private static final Logger logger_ = Logger.getLogger(JungTest.class);
+   
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
     
     public static void main(String[] args)
     {
         TestRunner.run(JungTest.class);
     }
 
-    public void testJung1() throws Exception
-    {
-        logger_.info("Running testJung1...");
-        
-        GraphLib factory = new PrefuseGraphLib();
-        Graph graph = factory.createGraph();
-        
-        Vertex sun = factory.createVertex(graph, "sun");
-        graph.addVertex(sun);
-        
-        Vertex moon = factory.createVertex(graph, "moon");
-        graph.addVertex(moon);
-        
-        Edge edge = factory.createEdge(sun, moon);
-        graph.addEdge(edge);
-        
-        GraphView view = factory.createView(graph);
-        
-        launchInDialog(view.getComponent()); //, SCREEN_TWO_THIRDS);
-    }
+    //--------------------------------------------------------------------------
+    // Abstract Methods
+    //--------------------------------------------------------------------------
     
-    
-    public void testJung2() throws Exception
+    /**
+     * @see toolbox.graph.AbstractGraphTest#getGraphLib()
+     */
+    public GraphLib getGraphLib()
     {
-        logger_.info("Running testJung2...");
-        
-        GraphLib lib = new PrefuseGraphLib();
-        Graph graph = lib.createGraph();
-        
-        List nodes = new ArrayList();
-        
-        for (int i = 0; i < 50; i++)
-        {
-            Vertex v = lib.createVertex(graph, i + 1 + "");
-            nodes.add(v);
-            graph.addVertex(v);
-        }
-        
-        for (int i = 0; i < 50; i++)
-        {
-            Vertex from = (Vertex) nodes.get(i);
-            Vertex to = (Vertex) RandomUtil.nextElement(nodes);
-            Edge edge = lib.createEdge(from, to);
-            graph.addEdge(edge);
-        }
-        
-        GraphView view = lib.createView(graph);
-        
-        launchInDialog(view.getComponent());
+        return GraphLibFactory.create(GraphLibFactory.TYPE_JUNG);
     }
 }
