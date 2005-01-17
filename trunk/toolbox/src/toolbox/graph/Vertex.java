94f67affa8c25bff88911eaf61a71f318215f9c0
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-17 22:38:48 +0000

    Added text label support

--- trunk/toolbox/src/toolbox/graph/Vertex.java
+++ trunk/toolbox/src/toolbox/graph/Vertex.java
@@ -3,9 +3,30 @@ package toolbox.graph;
 import java.util.Set;
 
 /**
- * A Vertex is a single node in a graph.
+ * A Vertex is a single node in a {@link Graph}
  */
 public interface Vertex extends Delegator
 {
+    /**
+     * Returns all edges attached to this vertex.
+     * 
+     * @return Set<Edge>
+     */
     Set getEdges();
+    
+    
+    /**
+     * Returns the text of this vertex.
+     * 
+     * @return String
+     */
+    String getText();
+    
+    
+    /**
+     * Sets the text label of this vertex.
+     * 
+     * @param text Label
+     */
+    void setText(String text);
 }
