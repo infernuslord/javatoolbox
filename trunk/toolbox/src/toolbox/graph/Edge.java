61cf8d03583b15b2edce3b784bc62e1bd368bf1e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-17 22:39:48 +0000

    Javadoc updates

--- trunk/toolbox/src/toolbox/graph/Edge.java
+++ trunk/toolbox/src/toolbox/graph/Edge.java
@@ -1,11 +1,23 @@
 package toolbox.graph;
 
 /**
- * An edge connects two vertices. It can be directed (have an arrow) or
+ * An Edge connects two Vertices. It can be directed (have an arrow) or
  * undirected.
  */
 public interface Edge extends Delegator
 {
+    /**
+     * Returns the source vertex.
+     * 
+     * @return Vertex
+     */
     Vertex getSource();
+    
+    
+    /**
+     * Returns the destination vertex.
+     * 
+     * @return Vertex
+     */
     Vertex getDestination();
 }
