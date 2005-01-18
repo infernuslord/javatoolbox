2fe60647348f683fb03698648237d7d0318867b4
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-01-18 07:39:40 +0000

    Assigned name

--- trunk/toolbox/src/toolbox/graph/prefuse/PrefuseLayout.java
+++ trunk/toolbox/src/toolbox/graph/prefuse/PrefuseLayout.java
@@ -1,6 +1,6 @@
 package toolbox.graph.prefuse;
 
-import edu.uci.ics.jung.visualization.Layout;
+import edu.berkeley.guir.prefuse.action.assignment.Layout;
 
 /**
  * Prefuse implementation of a {@link toolbox.graph.Layout}.
@@ -10,6 +10,11 @@ public class PrefuseLayout implements toolbox.graph.Layout
     //--------------------------------------------------------------------------
     // Fields
     //--------------------------------------------------------------------------
+    
+    /**
+     * Name of this layout.
+     */
+    private String name_;
     
     /**
      * Prefuse layout delegate.
@@ -23,11 +28,23 @@ public class PrefuseLayout implements toolbox.graph.Layout
     /**
      * Creates a PrefuseLayout.
      */
-    public PrefuseLayout(Layout layout)
+    public PrefuseLayout(String name, Layout layout)
     {
+        setName(name);
         delegate_ = layout;
     }
     
+    //--------------------------------------------------------------------------
+    // Layout Interface
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see toolbox.graph.Layout#getName()
+     */
+    public String getName()
+    {
+        return name_;
+    }
     //--------------------------------------------------------------------------
     // Delegator Interface
     //--------------------------------------------------------------------------
@@ -38,5 +55,31 @@ public class PrefuseLayout implements toolbox.graph.Layout
     public Object getDelegate()
     {
         return delegate_;
+    }
+    
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Sets the value of name.
+     * 
+     * @param name The name to set.
+     */
+    public void setName(String name)
+    {
+        name_ = name;
+    }
+    
+    //--------------------------------------------------------------------------
+    // Overrides java.lang.Object
+    //--------------------------------------------------------------------------
+    
+    /**
+     * @see java.lang.Object#toString()
+     */
+    public String toString()
+    {
+        return getName();
     }
 }
