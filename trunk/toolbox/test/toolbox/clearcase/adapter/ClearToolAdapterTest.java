b97b9a4b838f6e6465aa20ddfa20c23387d68216
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-05-20 16:36:54 +0000

    Initial version

---
+++ trunk/toolbox/test/toolbox/clearcase/adapter/ClearToolAdapterTest.java
@@ -0,0 +1,68 @@
+package toolbox.clearcase.adapter;
+
+import java.io.File;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+import org.apache.commons.io.filefilter.SuffixFileFilter;
+import org.apache.log4j.Logger;
+
+import toolbox.clearcase.IClearCaseAdapter;
+import toolbox.clearcase.domain.Revision;
+import toolbox.clearcase.domain.VersionedFile;
+
+/**
+ * Unit test for {@link toolbox.clearcase.adapter.ClearToolAdapter}. 
+ */
+public class ClearToolAdapterTest extends TestCase
+{
+    private static final Logger logger_ = 
+        Logger.getLogger(ClearToolAdapterTest.class);
+    
+    //--------------------------------------------------------------------------
+    // Main
+    //--------------------------------------------------------------------------
+    
+    public static void main(String[] args)
+    {
+        TestRunner.run(ClearToolAdapterTest.class);
+    }
+
+    //--------------------------------------------------------------------------
+    // Unit Tests
+    //--------------------------------------------------------------------------
+    
+    public void testFindChangedFiles() throws Exception
+    {
+        logger_.info("Running testFindChangedFiles...");
+        
+        IClearCaseAdapter cc = ClearCaseAdapterFactory.create();
+        cc.setViewPath(new File("m:\\x1700_sandbox\\staffplanning"));
+        
+        List changed = 
+            cc.findChangedFiles(
+                new Date(), 
+                new Date(), 
+                new SuffixFileFilter(".java"));
+        
+        for (Iterator iter = changed.iterator(); iter.hasNext();)
+        {
+            VersionedFile file = (VersionedFile) iter.next();
+            logger_.debug("File   : " + file.getName());
+            
+            for (Iterator iterator = file.getRevisions().iterator(); 
+                 iterator.hasNext();)
+            {
+                Revision revision = (Revision) iterator.next();
+                logger_.debug("Action : " + revision.getAction());
+                logger_.debug("User   : " + revision.getUser());
+                logger_.debug("Comment: " + revision.getComment());
+                logger_.debug("");
+            }
+        }
+    }
+}
