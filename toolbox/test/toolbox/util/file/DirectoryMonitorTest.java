243e33384547aa9b946ed57a0c9fa108f5980787
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-10-19 23:24:52 +0000

    Added testDirectoryMonitorWithSubDirs()

--- toolbox/test/toolbox/util/file/DirectoryMonitorTest.java
+++ toolbox/test/toolbox/util/file/DirectoryMonitorTest.java
@@ -14,117 +14,152 @@ import toolbox.util.ThreadUtil;
 /**
  * Unit test for {@link toolbox.util.file.DirectoryMonitor}.
  */
-public class DirectoryMonitorTest extends TestCase
-{
+public class DirectoryMonitorTest extends TestCase {
+
     private static final Logger logger_ = 
         Logger.getLogger(DirectoryMonitorTest.class);
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Main
-    //--------------------------------------------------------------------------
-        
+    // --------------------------------------------------------------------------
+
     /**
      * Entrypoint.
      * 
      * @param args None recognized.
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TestRunner.run(DirectoryMonitorTest.class);
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Unit Tests
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
-     * Tests a full lifecycle of the DirectoryMonitor. 
+     * Tests a full lifecycle of the DirectoryMonitor.
      * 
      * @throws Exception on I/O error.
      */
-    public void testDirectoryMonitor() throws Exception
-    {
+    public void testDirectoryMonitor() throws Exception {
+
         logger_.info("Running testDirectoryMonitor...");
-        
+
         File dir = FileUtil.createTempDir();
-        
-        try
-        {
+
+        try {
             DirectoryMonitor dm = new DirectoryMonitor(dir);
             dm.setDelay(500);
-            
+
             // Dummy activity
-            IFileActivity activity = new IFileActivity()
-            {
-                public File[] getFiles(File dir)
-                {
+            IFileActivity activity = new IFileActivity() {
+
+                public File[] getFiles(File dir) {
+
                     return new File[0];
                 }
             };
-            
+
             // Dummy listener
-            IDirectoryListener listener = new IDirectoryListener()
-            {
+            IDirectoryListener listener = new IDirectoryListener() {
+
                 public void fileActivity(IFileActivity activity, File[] files)
-                    throws Exception
-                {
-                    logger_.info("File activity reported: " + 
-                        ArrayUtil.toString(files));
+                    throws Exception {
+
+                    logger_.info("File activity reported: "
+                        + ArrayUtil.toString(files));
                 }
             };
-            
+
             dm.addDirectoryListener(listener);
             dm.addFileActivity(activity);
             dm.start();
-            
+
             ThreadUtil.sleep(1000);
-            
+
             dm.stop();
             dm.removeFileActivity(activity);
             dm.removeDirectoryListener(listener);
         }
-        finally
-        {
+        finally {
+            FileUtil.removeDir(dir);
+        }
+    }
+
+
+    /**
+     * Tests failure of an attempt to start an already running directory
+     * monitor.
+     * 
+     * @throws Exception on I/O error.
+     */
+    public void testDirectoryMonitorFalseStart() throws Exception {
+
+        logger_.info("Running testDirectoryMonitorFalseStart...");
+
+        File dir = FileUtil.createTempDir();
+
+        DirectoryMonitor dm = new DirectoryMonitor(dir);
+        dm.setDelay(250);
+
+        dm.start();
+
+        try {
+            dm.start();
+            fail("Expected failure on attempt to start twice");
+        }
+        catch (IllegalStateException ise) {
+            // Success
+            logger_.debug("SUCCESS: start twice failed.");
+        }
+        catch (Exception e) {
+            fail("Expected IllegalStateException");
+        }
+        finally {
+            dm.stop();
             FileUtil.removeDir(dir);
         }
     }
     
     
     /**
-     * Tests failure of an attempt to start an already running directory 
-     * monitor. 
-     * 
-     * @throws Exception on I/O error.
+     * @throws Exception
      */
-    public void testDirectoryMonitorFalseStart() throws Exception
-    {
-        logger_.info("Running testDirectoryMonitorFalseStart...");
+    public void testDirectoryMonitorWithSubDirs() throws Exception {
         
-        File dir = FileUtil.createTempDir();
+        logger_.info("Running testDirectoryMonitorWithSubDirs...");
         
-        DirectoryMonitor dm = new DirectoryMonitor(dir);
-        dm.setDelay(250);
+        File root = FileUtil.createTempDir();
+        File sub1 = FileUtil.createTempDir(root);
+        File sub11 = FileUtil.createTempDir(sub1);
+        File sub2 = FileUtil.createTempDir(root);
         
-        dm.start();
+        root = new File("c:\\eclipse-3.0");
         
-        try
-        {
+        try {
+            DirectoryMonitor dm = new DirectoryMonitor(root, true);
+            dm.addFileActivity(new FileCreatedActivity());
+            dm.addDirectoryListener(new IDirectoryListener() {
+            
+                public void fileActivity(IFileActivity activity, File[] files)
+                    throws Exception {
+                    
+                    logger_.info(
+                        "FileActivity: " 
+                        + activity 
+                        + " " 
+                        + ArrayUtil.toString(files));
+                }
+            });
+            
             dm.start();
-            fail("Expected failure on attempt to start twice");
+            
+            ThreadUtil.sleep(999999999);
+            
+            //dm.stop();
         }
-        catch (IllegalStateException ise)
-        {
-            // Success
-            logger_.debug("SUCCESS: start twice failed."); 
-        }
-        catch (Exception e)
-        {
-            fail("Expected IllegalStateException");
-        }
-        finally
-        {
-            dm.stop();
-            FileUtil.removeDir(dir);
+        finally {
+            //FileUtils.cleanDirectory(root);
         }
     }
 }
