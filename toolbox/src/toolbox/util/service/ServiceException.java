d441b04a2b64a78443edf7340f1140c506618851
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-11-07 15:01:10 +0000

    Formatting updates

--- toolbox/src/toolbox/util/service/ServiceException.java
+++ toolbox/src/toolbox/util/service/ServiceException.java
@@ -3,34 +3,31 @@ package toolbox.util.service;
 /**
  * ServiceException is thrown in the event of Service related failures.
  */
-public class ServiceException extends RuntimeException
-{
+public class ServiceException extends RuntimeException {
+
     /**
      * Creates a ServiceException.
      */
-    public ServiceException()
-    {
+    public ServiceException() {
     }
 
-    
+
     /**
      * Creates a ServiceException.
      * 
      * @param message Error message.
      */
-    public ServiceException(String message)
-    {
+    public ServiceException(String message) {
         super(message);
     }
-    
-    
+
+
     /**
      * Creates a ServiceException.
      * 
      * @param t Originating exception.
      */
-    public ServiceException(Throwable t)
-    {
+    public ServiceException(Throwable t) {
         super(t);
     }
 }
