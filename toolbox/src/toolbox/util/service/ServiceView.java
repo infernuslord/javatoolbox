68fd39960f4c34ab02b4ee444b59aaa2ea3b92e1
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-11-08 14:22:40 +0000

    Added label to show current state

--- toolbox/src/toolbox/util/service/ServiceView.java
+++ toolbox/src/toolbox/util/service/ServiceView.java
@@ -15,10 +15,11 @@ import org.apache.log4j.Logger;
 
 import toolbox.util.statemachine.StateMachine;
 import toolbox.util.ui.JSmartButton;
+import toolbox.util.ui.JSmartLabel;
 import toolbox.util.ui.SmartAction;
 
 /**
- * ServiceView is a user interface component that presents a view on an object 
+ * ServiceView is a user interface component that presents a view on an object
  * that implements the Service interface. Features include:
  * <ul>
  *  <li>Initialize, start, stop, suspend, resume, and destroy a Service
@@ -28,11 +29,11 @@ import toolbox.util.ui.SmartAction;
 public class ServiceView extends JPanel
 {
     private static final Logger logger_ = Logger.getLogger(ServiceView.class);
-    
-    //--------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------
     // Fields
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Service thats services as the model for this view.
      */
@@ -42,321 +43,309 @@ public class ServiceView extends JPanel
      * Maps an action's name to its corresponding action.
      */
     private Map actions_;
-    
+
     /**
      * Internal listener for this service.
      */
     private ServiceListener myServiceListener_;
-    
-    //--------------------------------------------------------------------------
+
+    /**
+     * Label that displays the current state of serivce associated with this 
+     * view. 
+     */
+    private JSmartLabel currentStateLabel_;
+
+    // --------------------------------------------------------------------------
     // Constructors
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * Creates a ServiceView.
      * 
      * @param service Service that acts as the model for this view.
      */
-    public ServiceView(ObservableService service)
-    {
+    public ServiceView(ObservableService service){
         myServiceListener_ = new MyServiceListener();
+        currentStateLabel_ = new JSmartLabel();
         service.addServiceListener(myServiceListener_);
         setService(service);
         actions_ = new HashMap(4);
         buildView();
     }
 
-    //--------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------
     // Public
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Returns the service.
      * 
      * @return Service
      */
-    public ObservableService getService()
-    {
+    public ObservableService getService(){
         return service_;
     }
-    
-    
+
+
     /**
      * Sets the value of service.
      * 
      * @param service The service to set.
      */
-    public void setService(ObservableService service)
-    {
+    public void setService(ObservableService service){
         // Remove the old one if one exists
         if (service_ != null)
             service_.removeServiceListener(myServiceListener_);
-        
+
         service_ = service;
-        
+
         // Transsfer the listener to the newly appointed service.
         service_.addServiceListener(myServiceListener_);
+        
+        currentStateLabel_.setText(
+            service_.getStateMachine().getState().toString());
     }
-    
-    //--------------------------------------------------------------------------
+
+
+    // --------------------------------------------------------------------------
     // Protected
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * Builds the GUI.
      */
-    protected void buildView()
-    {
+    protected void buildView(){
+        
         setLayout(new FlowLayout());
 
-        if (service_ instanceof Initializable)
-        {    
+        if (service_ instanceof Initializable) {
             actions_.put(ServiceTransition.INITIALIZE, new InitializeAction());
-            
-            add(new JSmartButton((Action) 
-                actions_.get(ServiceTransition.INITIALIZE)));
+
+            add(new JSmartButton(
+                (Action) actions_.get(ServiceTransition.INITIALIZE)));
         }
 
-        if (service_ instanceof Startable)
-        {    
+        if (service_ instanceof Startable) {
             actions_.put(ServiceTransition.START, new StartAction());
             actions_.put(ServiceTransition.STOP, new StopAction());
-            
+
             add(new JSmartButton((Action) 
                 actions_.get(ServiceTransition.START)));
             
             add(new JSmartButton((Action) 
                 actions_.get(ServiceTransition.STOP)));
         }
-        
-        if (service_ instanceof Suspendable)
-        {
+
+        if (service_ instanceof Suspendable) {
             actions_.put(ServiceTransition.SUSPEND, new SuspendAction());
             actions_.put(ServiceTransition.RESUME, new ResumeAction());
-            
+
             add(new JSmartButton((Action) 
                 actions_.get(ServiceTransition.SUSPEND)));
-            
+
             add(new JSmartButton((Action) 
                 actions_.get(ServiceTransition.RESUME)));
         }
-        
-        if (service_ instanceof Destroyable)
-        {    
+
+        if (service_ instanceof Destroyable) {
             actions_.put(ServiceTransition.DESTROY, new DestroyAction());
-            
+
             add(new JSmartButton((Action) 
                 actions_.get(ServiceTransition.DESTROY)));
         }
+        
+        add(currentStateLabel_);
     }
-    
-    
-    //--------------------------------------------------------------------------
+
+
+    // --------------------------------------------------------------------------
     // MyServicelistener
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * The internal listener is only interested in the INITIALIZED state so that
      * the initial state of the buttons can be set.
      */
-    class MyServiceListener implements ServiceListener 
-    {
-        /**
-         * @see toolbox.util.service.ServiceListener#serviceStateChanged(
-         *      toolbox.util.service.Service)
+    class MyServiceListener implements ServiceListener{
+        
+        /*
+         * @see toolbox.util.service.ServiceListener#serviceStateChanged(toolbox.util.service.Service)
          */
-        public void serviceStateChanged(Service service) throws ServiceException
-        {
+        public void serviceStateChanged(Service service)
+            throws ServiceException{
             ServiceState current = service.getState();
-            
+
             logger_.debug("New state = " + service.getState());
-            
-            if (service instanceof ObservableService)
-            {
+
+            if (service instanceof ObservableService) {
                 ObservableService obs = (ObservableService) service;
                 StateMachine sm = obs.getStateMachine();
                 List transitions = sm.getTransitionsFrom(current);
-                
-                for (Iterator iter = actions_.entrySet().iterator(); 
-                     iter.hasNext();)
-                {
+
+                for (Iterator iter = actions_.entrySet().iterator(); iter
+                    .hasNext();) {
                     Map.Entry entry = (Map.Entry) iter.next();
-                    
+
                     ServiceTransition tran = (ServiceTransition) entry.getKey();
                     Action action = (Action) entry.getValue();
                     action.setEnabled(transitions.contains(tran));
                 }
+                
+                currentStateLabel_.setText(sm.getState().toString());
             }
         }
     }
-    
-    //--------------------------------------------------------------------------
+
+    // --------------------------------------------------------------------------
     // InitializeAction
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * Initializes the service.
      */
-    class InitializeAction extends SmartAction
-    {
+    class InitializeAction extends SmartAction{
+        
         /**
          * Creates an InitializeAction.
          */
-        public InitializeAction()
-        {
+        public InitializeAction(){
             super("Init", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Initializable) service_).initialize(MapUtils.EMPTY_MAP);
         }
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // StartAction
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * StartAction starts the attached service and sets the state of the buttons
      * according to the next available set of state transitions.
      */
-    class StartAction extends SmartAction
-    {
+    class StartAction extends SmartAction{
+        
         /**
          * Creates a StartAction.
          */
-        public StartAction()
-        {
+        public StartAction(){
             super("Start", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Startable) service_).start();
         }
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // StopAction
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * StopAction stops the attached service and sets the state of the buttons
      * according to the next available set of state transitions.
      */
-    class StopAction extends SmartAction
-    {
+    class StopAction extends SmartAction{
+        
         /**
          * Creates a StopAction.
          */
-        public StopAction()
-        {
+        public StopAction(){
             super("Stop", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Startable) service_).stop();
         }
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // SuspendAction
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
-     * SuspendAction suspends the attached service and sets the state of the 
+     * SuspendAction suspends the attached service and sets the state of the
      * buttons according to the next available set of state transitions.
      */
-    class SuspendAction extends SmartAction
-    {
+    class SuspendAction extends SmartAction{
+        
         /**
          * Creates a SuspendAction.
          */
-        public SuspendAction()
-        {
+        public SuspendAction(){
             super("Suspend", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Suspendable) service_).suspend();
         }
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // ResumeAction
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
     /**
      * ResumeAction
      */
-    class ResumeAction extends SmartAction
-    {
+    class ResumeAction extends SmartAction{
+        
         /**
          * Creates a ResumeAction.
          */
-        public ResumeAction()
-        {
+        public ResumeAction() {
             super("Resume", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Suspendable) service_).resume();
         }
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // DestroyAction
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Destroys the service.
      */
-    class DestroyAction extends SmartAction
-    {
+    class DestroyAction extends SmartAction{
+        
         /**
          * Creates an DestroyAction.
          */
-        public DestroyAction()
-        {
+        public DestroyAction(){
             super("Destroy", true, false, null);
         }
 
-        
-        /**
-         * @see toolbox.util.ui.SmartAction#runAction(
-         *      java.awt.event.ActionEvent)
+
+        /*
+         * @see toolbox.util.ui.SmartAction#runAction(java.awt.event.ActionEvent)
          */
-        public void runAction(ActionEvent e) throws Exception
-        {
+        public void runAction(ActionEvent e) throws Exception{
             ((Destroyable) service_).destroy();
         }
     }
