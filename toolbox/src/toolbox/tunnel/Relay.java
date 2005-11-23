d0eae0a4ca72b3795aadf1a49fc0b8aec59a42ce
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-11-23 20:00:54 +0000

    Formatting updates

--- toolbox/src/toolbox/tunnel/Relay.java
+++ toolbox/src/toolbox/tunnel/Relay.java
@@ -12,44 +12,44 @@ import org.apache.commons.io.IOUtils;
  * 
  * @see toolbox.tunnel.TcpTunnel
  */
-public class Relay implements Runnable
-{
-    //--------------------------------------------------------------------------
+public class Relay implements Runnable {
+
+    // -------------------------------------------------------------------------
     // Constants
-    //--------------------------------------------------------------------------
-    
-    /** 
-     * Size of data window. 
+    // -------------------------------------------------------------------------
+
+    /**
+     * Size of data window.
      */
     private static final int BUFFER_SIZE = 1024;
 
-    //--------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // Fields
-    //--------------------------------------------------------------------------
-    
-    /** 
-     * InputStream data is read from. 
-     */    
+    // -------------------------------------------------------------------------
+
+    /**
+     * InputStream data is read from.
+     */
     private InputStream in_;
-    
-    /** 
-     * OutputStream data is forwarded to. 
+
+    /**
+     * OutputStream data is forwarded to.
      */
     private OutputStream out_;
-    
-    /** 
-     * Number of bytes transferred. 
+
+    /**
+     * Number of bytes transferred.
      */
     private int count_;
-    
-    /** 
-     * Copy buffer. 
-     */    
+
+    /**
+     * Copy buffer.
+     */
     private byte[] buffer_;
 
-    //--------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // Constructors
-    //--------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
 
     /**
      * Creates a new relay between an input and output stream.
@@ -57,40 +57,34 @@ public class Relay implements Runnable
      * @param in Input stream to read bytes from.
      * @param out Output stream to write bytes to.
      */
-    public Relay(InputStream in, OutputStream out)
-    {
-        in_     = in;
-        out_    = out;
-        count_  = 0;
+    public Relay(InputStream in, OutputStream out) {
+        in_ = in;
+        out_ = out;
+        count_ = 0;
         buffer_ = new byte[BUFFER_SIZE];
     }
 
-    //--------------------------------------------------------------------------
+    // -------------------------------------------------------------------------
     // Runnable Interface
-    //--------------------------------------------------------------------------
-    
+    // -------------------------------------------------------------------------
+
     /**
      * Starts the relay.
      */
-    public void run()
-    {
+    public void run() {
         int n;
 
-        try
-        {
-            while ((n = in_.read(buffer_)) > 0)
-            {
+        try {
+            while ((n = in_.read(buffer_)) > 0) {
                 out_.write(buffer_, 0, n);
                 out_.flush();
                 count_ += n;
             }
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             ; // Ignore
         }
-        finally
-        {
+        finally {
             IOUtils.closeQuietly(in_);
             IOUtils.closeQuietly(out_);
         }
--- toolbox/src/toolbox/util/io/JTextAreaOutputStream.java
+++ toolbox/src/toolbox/util/io/JTextAreaOutputStream.java
@@ -7,42 +7,40 @@ import javax.swing.JTextArea;
 /**
  * An outputStream that dumps into a {@link javax.swing.JTextArea}.
  */
-public class JTextAreaOutputStream extends OutputStream
-{
-    //--------------------------------------------------------------------------
+public class JTextAreaOutputStream extends OutputStream {
+
+    // --------------------------------------------------------------------------
     // Fields
-    //--------------------------------------------------------------------------
-    
-    /** 
-     * Textarea to dump to. 
+    // --------------------------------------------------------------------------
+
+    /**
+     * Textarea to dump to.
      */
     private JTextArea textArea_;
 
-    //--------------------------------------------------------------------------
-    // Constructors 
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+    // Constructors
+    // --------------------------------------------------------------------------
+
     /**
      * Creates a JTextAreaOutputStream.
      * 
      * @param textArea JTextArea sink for the output stream.
      */
-    public JTextAreaOutputStream(JTextArea textArea)
-    { 
-        textArea_ = textArea; 
+    public JTextAreaOutputStream(JTextArea textArea) {
+        textArea_ = textArea;
     }
- 
-    //--------------------------------------------------------------------------
-    // Implements Abstract from java.io.OutputStream 
-    //--------------------------------------------------------------------------
 
-    /**
+    // --------------------------------------------------------------------------
+    // Overrides java.io.OutputStream
+    // --------------------------------------------------------------------------
+
+    /*
      * Writes a byte to the textarea.
      * 
-     * @param b Byte
+     * @see java.io.OutputStream#write(int)
      */
-    public void write(int b)
-    {
-        textArea_.append(new String(new byte[] {(byte) b })); 
+    public void write(int b) {
+        textArea_.append(new String(new byte[] { (byte) b }));
     }
 }
