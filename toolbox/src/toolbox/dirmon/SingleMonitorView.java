062d31aacb66c4e2cff245a614ced7e9cac7ae6e
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-11-30 21:40:39 +0000

    Created as a composite of the EventTableView and the ControllerView

---
+++ toolbox/src/toolbox/dirmon/SingleMonitorView.java
@@ -0,0 +1,99 @@
+package toolbox.dirmon;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.JPanel;
+
+import org.apache.log4j.Logger;
+
+import toolbox.util.dirmon.DirectoryMonitor;
+import toolbox.util.ui.tabbedpane.JSmartTabbedPane;
+import toolbox.util.ui.tabbedpane.SmartTabbedPaneListener;
+
+/**
+ * SingleMonitorView is responsible for viewing and controlling a single
+ * DirectoryMonitor.
+ */
+public class SingleMonitorView extends JPanel implements SmartTabbedPaneListener {
+
+    private static final Logger logger_ = 
+        Logger.getLogger(SingleMonitorView.class);
+    
+    //--------------------------------------------------------------------------
+    // Fields
+    //--------------------------------------------------------------------------
+    
+    private DirectoryMonitor monitor;
+    private EventTableView tableView;
+ 
+    //--------------------------------------------------------------------------
+    // Constructors
+    //--------------------------------------------------------------------------
+    
+    public SingleMonitorView(DirectoryMonitor monitor) {
+        this.monitor = monitor;
+        buildView();
+    }
+    
+    //--------------------------------------------------------------------------
+    // Protected
+    //--------------------------------------------------------------------------
+    
+    protected void buildView() {
+        setLayout(new BorderLayout());
+        
+        tableView = new EventTableView();
+        this.monitor.addDirectoryMonitorListener(tableView);
+        
+        ControllerView controllerView = new ControllerView(this.monitor);
+        
+        add(BorderLayout.CENTER, tableView);
+        add(BorderLayout.SOUTH, controllerView);
+    }
+ 
+    //--------------------------------------------------------------------------
+    // SmartTabbedPaneListener Interface
+    //--------------------------------------------------------------------------
+    
+    /*
+     * @see toolbox.util.ui.tabbedpane.SmartTabbedPaneListener#tabClosing(toolbox.util.ui.tabbedpane.JSmartTabbedPane, int)
+     */
+    public void tabClosing(JSmartTabbedPane tabbedPane, int tabIndex) {
+        Component source = tabbedPane.getComponentAt(tabIndex);
+        if (source == this) {
+            logger_.debug("Recognized tab closing for directory monitor " + monitor);
+            
+            if (monitor.isSuspended())
+                monitor.resume();
+            
+            if (monitor.isRunning())
+                monitor.stop();
+            
+            monitor.removeDirectoryMonitorListener(tableView);
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Public
+    //--------------------------------------------------------------------------
+    
+    /**
+     * Returns the monitor.
+     * 
+     * @return DirectoryMonitor
+     */
+    public DirectoryMonitor getMonitor() {
+        return monitor;
+    }
+
+    
+    /**
+     * Sets the value of monitor.
+     * 
+     * @param monitor The monitor to set.
+     */
+    public void setMonitor(DirectoryMonitor monitor) {
+        this.monitor = monitor;
+    }
+}
