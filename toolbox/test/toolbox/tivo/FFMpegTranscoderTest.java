dac8a0077a3a45c3799122ac9d68310c3e99d9a5
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-12-13 23:00:51 +0000

    Renamed testTranscode() to testTranscode_H264()

--- toolbox/test/toolbox/tivo/FFMpegTranscoderTest.java
+++ toolbox/test/toolbox/tivo/FFMpegTranscoderTest.java
@@ -8,7 +8,9 @@ import org.apache.log4j.Logger;
 
 import toolbox.util.FileUtil;
 
-
+/**
+ * Unit test for {@link toolbox.tivo.FFMpegTranscoder}.
+ */
 public class FFMpegTranscoderTest extends TivoTestCase {
 
     private static final Logger logger_ = 
@@ -26,14 +28,14 @@ public class FFMpegTranscoderTest extends TivoTestCase {
     // Unit Tests
     // -------------------------------------------------------------------------
     
-    public void testTranscode() throws Exception {
+    public void testTranscode_H264() throws Exception {
         logger_.info("Running testTranscode...");
         
         MovieInfoParser parser = new MovieInfoParser();
-        MovieInfo info = parser.parse(getTestFilename());
+        MovieInfo info = parser.parse(getH264Filename());
         
         String destFilename = 
-            FileUtil.getTempDir() + File.separator + "test.mpg";
+            FileUtil.getTempDir() + File.separator + "tivoH264.mpg";
         
         ITranscoder transcoder = new FFMpegTranscoder();
         transcoder.transcode(info, destFilename);
