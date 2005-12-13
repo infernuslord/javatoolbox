2eb7a861cee166e6b84fe60c7e38f6315342c00b
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-12-13 23:14:44 +0000

    Added statusChanged() so clients can receie notification of non-file discovery type events

--- trunk/toolbox/src/toolbox/util/dirmon/IDirectoryMonitorListener.java
+++ trunk/toolbox/src/toolbox/util/dirmon/IDirectoryMonitorListener.java
@@ -2,17 +2,29 @@ package toolbox.util.dirmon;
 
 import java.util.EventListener;
 
+import toolbox.util.dirmon.event.FileEvent;
+import toolbox.util.dirmon.event.StatusEvent;
+
 /**
  * Listener interface for notification of events from a DirectoryMonitor.
  */
 public interface IDirectoryMonitorListener extends EventListener {
     
     /**
-     * Delivers an event to the DirectoryMonitor listener.
+     * Delivers notification that file activity has been recognized.
      * 
-     * @param directoryMonitorEvent Event to deliver.
+     * @param changeEvent Event to deliver.
      * @throws Exception on error.
      */
-    void directoryActivity(DirectoryMonitorEvent directoryMonitorEvent)
-        throws Exception;
+    void directoryActivity(FileEvent changeEvent) throws Exception;
+    
+    
+    /**
+     * Delivers notification that the status of the directory monitor has
+     * changed.
+     * 
+     * @param statusEvent Event
+     * @throws Exception on error.
+     */
+    void statusChanged(StatusEvent statusEvent) throws Exception;
 }
