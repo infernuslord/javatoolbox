f83d9285b4800b0519100fa03fdfa74844d9669f
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-12-16 22:36:39 +0000

    Added support to add listeners

--- toolbox/src/toolbox/tivo/ITranscoder.java
+++ toolbox/src/toolbox/tivo/ITranscoder.java
@@ -7,4 +7,8 @@ public interface ITranscoder {
     
     void transcode(MovieInfo movieInfo, String destFilename)
         throws IOException, InterruptedException;
+    
+    void addTranscoderListener(ITranscoderListener listener);
+    
+    void removeTranscoderListener(ITranscoderListener listener);
 }
