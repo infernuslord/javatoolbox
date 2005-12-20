d8cde2d277e022dbad4a24db0510df85353256ba
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2005-12-20 21:33:24 +0000

    Coding convention updates

--- trunk/toolbox/src/toolbox/plugin/tunnel/ConnectionTrackerView.java
+++ trunk/toolbox/src/toolbox/plugin/tunnel/ConnectionTrackerView.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package toolbox.plugin.tunnel;
 
 import java.awt.GridLayout;
@@ -12,20 +9,33 @@ import javax.swing.border.BevelBorder;
 
 import toolbox.util.io.MonitoredOutputStream;
 import toolbox.util.io.MonitoredOutputStream.OutputStreamListener;
+import toolbox.util.ui.ImageCache;
 import toolbox.util.ui.JSmartLabel;
 
+/**
+ * View that shows the number of incoming, outgoing, and total transferred
+ * bytes over two (one in each direction) 
+ * {@link toolbox.util.io.MonitoredOutputStream}s.
+ */
 public class ConnectionTrackerView extends JPanel {
+
+    // -------------------------------------------------------------------------
+    // Fields
+    // -------------------------------------------------------------------------
     
-    private int bytesTransferred = 0;
-    private MonitoredOutputStream incomingSink;
-    private MonitoredOutputStream outgoingSink;
-    private OutputStreamListener incomingListener;
-    private OutputStreamListener outgoingListener;
+    private int bytesTransferred_ = 0;
+    private MonitoredOutputStream incomingSink_;
+    private MonitoredOutputStream outgoingSink_;
+    private OutputStreamListener incomingListener_;
+    private OutputStreamListener outgoingListener_;
     
-    private JLabel incomingLabel;
-    private JLabel outgoingLabel;
-    private JLabel transferredLabel;
+    private JLabel incomingLabel_;
+    private JLabel outgoingLabel_;
+    private JLabel transferredLabel_;
     
+    // -------------------------------------------------------------------------
+    // Constructors
+    // -------------------------------------------------------------------------
     
     public ConnectionTrackerView(
         MonitoredOutputStream incomingSink,
@@ -33,49 +43,60 @@ public class ConnectionTrackerView extends JPanel {
         
         buildView();
         
-        this.incomingSink = incomingSink;
-        this.outgoingSink = outgoingSink;
+        incomingSink_ = incomingSink;
+        outgoingSink_ = outgoingSink;
         
-        incomingListener = new UniDirectionalListener();
-        incomingSink.addOutputStreamListener(incomingListener);
+        incomingListener_ = new UniDirectionalListener();
+        incomingSink.addOutputStreamListener(incomingListener_);
         
-        outgoingListener = new UniDirectionalListener();
-        outgoingSink.addOutputStreamListener(outgoingListener);
+        outgoingListener_ = new UniDirectionalListener();
+        outgoingSink.addOutputStreamListener(outgoingListener_);
     }
 
+    // -------------------------------------------------------------------------
+    // Protected
+    // -------------------------------------------------------------------------
+    
     protected void buildView() {
         setLayout(new GridLayout(3,2));
         
         add(new JLabel("Incoming bytes:"));
-        add(incomingLabel = new JLabel("0"));
+        add(incomingLabel_ = new JLabel("0"));
         add(new JLabel("Outgoing bytes:"));
-        add(outgoingLabel = new JLabel("0"));
+        add(outgoingLabel_ = new JLabel("0"));
         add(new JLabel("Total butes:"));
-        add(transferredLabel = new JSmartLabel("0"));
+        add(transferredLabel_ = new JSmartLabel("0"));
         
         setBorder(BorderFactory.createBevelBorder(BevelBorder.RAISED));
     }
     
+    // -------------------------------------------------------------------------
+    // UniDirectionalListener 
+    // -------------------------------------------------------------------------
     
     class UniDirectionalListener implements OutputStreamListener {
     
         public void streamClosed(MonitoredOutputStream stream) {
-            long in = incomingSink.getCount();
-            long out = outgoingSink.getCount();
+            long in = incomingSink_.getCount();
+            long out = outgoingSink_.getCount();
             
-            incomingLabel.setText("" + in);
-            outgoingLabel.setText("" + out);
-            transferredLabel.setText(
-                "Closed " + (in + out));
+            incomingLabel_.setText("" + in);
+            outgoingLabel_.setText("" + out);
+            transferredLabel_.setText((in + out) + "");
+            transferredLabel_.setIcon(ImageCache.getIcon(ImageCache.IMAGE_DELETE));
+            
+            // Cleanup
+            incomingSink_.removeOutputStreamListener(incomingListener_);
+            outgoingSink_.removeOutputStreamListener(outgoingListener_);
         }
         
         public void streamFlushed(MonitoredOutputStream stream) {
-            long in = incomingSink.getCount();
-            long out = outgoingSink.getCount();
+            long in = incomingSink_.getCount();
+            long out = outgoingSink_.getCount();
             
-            incomingLabel.setText("" + in);
-            outgoingLabel.setText("" + out);
-            transferredLabel.setText("" + (in + out));
+            incomingLabel_.setText("" + in);
+            outgoingLabel_.setText("" + out);
+            transferredLabel_.setText("" + (in + out));
         }
     }
 }
