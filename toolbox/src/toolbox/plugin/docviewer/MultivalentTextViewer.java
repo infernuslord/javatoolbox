f57a63f8a552150deef4b1a126823e3519f50443
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-01-09 23:21:21 +0000

    Updates for Multivalent20060102.jar

--- toolbox/src/toolbox/plugin/docviewer/MultivalentTextViewer.java
+++ toolbox/src/toolbox/plugin/docviewer/MultivalentTextViewer.java
@@ -15,74 +15,74 @@ import toolbox.util.collections.CaseInsensetiveSet;
  * A viewer that operates on all Multivalent supported formats (PDF, HTML, DVI)
  * and extracts only the plain text for display.
  */
-public class MultivalentTextViewer extends TextViewer
-{
-    //--------------------------------------------------------------------------
+public class MultivalentTextViewer extends TextViewer {
+
+    // --------------------------------------------------------------------------
     // Fields
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Set of supported file extensions.
      */
-    private Set extensions_; 
-    
-    //--------------------------------------------------------------------------
+    private Set extensions_;
+
+    // --------------------------------------------------------------------------
     // Constructors
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Creates a MultivalentTextViewer.
      */
-    public MultivalentTextViewer()
-    {
+    public MultivalentTextViewer() {
         super("Multivalent Text Viewer");
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Overrides TextViewer
-    //--------------------------------------------------------------------------
-    
+    // --------------------------------------------------------------------------
+
     /**
      * Builds the list of viewable file types.
      * 
      * @see toolbox.util.service.Initializable#initialize(java.util.Map)
      */
-    public void initialize(Map init)
-    {
+    public void initialize(Map init) {
         super.initialize(init);
         extensions_ = new CaseInsensetiveSet(new HashSet());
-        
-        extensions_.addAll(Arrays.asList(
-            new String[] {"pdf", "html", "htm", "dvi", "xml"}));
+
+        extensions_.addAll(Arrays.asList(new String[] {
+            "pdf",
+            "html",
+            "htm",
+            "dvi",
+            "xml"
+            }
+        ));
     }
-    
-    
-    /**
+
+
+    /*
      * @see toolbox.plugin.docviewer.TextViewer#canView(java.io.File)
      */
-    public boolean canView(File file)
-    {
+    public boolean canView(File file) {
         return extensions_.contains(FileUtil.getExtension(file));
     }
 
-    
+
     /**
      * Converts the passed in PDF, HTML, DVI to plain text and displays it in
      * the textarea.
      * 
      * @see toolbox.plugin.docviewer.DocumentViewer#view(java.io.File)
      */
-    public void view(File file) throws DocumentViewerException
-    {
-        try
-        {
+    public void view(File file) throws DocumentViewerException {
+        try {
             ExtractText extract = new ExtractText();
-            String text = extract.extract(file.toURI(), null);;
+            String text = extract.extract(file.toURI());
             getTextArea().setText(text);
             getTextArea().setCaretPosition(0);
         }
-        catch (Exception e)
-        {
+        catch (Exception e) {
             throw new DocumentViewerException(e);
         }
     }
