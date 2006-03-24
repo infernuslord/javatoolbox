675260f9cf06b89b5dfa68d3cc1c6447e8ca257a
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-03-24 21:22:49 +0000

    Added additional launch strategy if an Access Denied error occurs

--- trunk/toolbox/src/toolbox/util/AppLauncher.java
+++ trunk/toolbox/src/toolbox/util/AppLauncher.java
@@ -1,8 +1,12 @@
 package toolbox.util;
 
+import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.SystemUtils;
 import org.apache.log4j.Logger;
 
 public final class AppLauncher  {
@@ -11,15 +15,18 @@ public final class AppLauncher  {
     
     private AppLauncher() {
     }
-    
+     
     public static final void launch(String filename) 
         throws IOException, InterruptedException {
 
+        if (!SystemUtils.IS_OS_WINDOWS)
+            throw new IllegalArgumentException("App launching supported on windows only.");
+    
         // Only works for windows
         String[] cmdArray = new String[3];
         cmdArray[0] = "cmd";
         cmdArray[1] = "/c";
-        cmdArray[2] = "\"" + filename + "\"";
+        cmdArray[2] = "\"\"" + filename + "\"\"";
         
         Process p = Runtime.getRuntime().exec(cmdArray);
         StringBuffer stdout = new StringBuffer();
@@ -27,21 +34,52 @@ public final class AppLauncher  {
         int exitValue = ProcessUtil.getProcessOutput(p, stdout, stderr);
         
         if (exitValue > 0) {
-            StringBuffer sb = new StringBuffer();
-            sb.append("Launch failed with command '" + ArrayUtil.toString(cmdArray) + "'\n");
+
+            File file = new File(filename);
             
-            if (StringUtils.isNotEmpty(stdout.toString())) {
-                sb.append("stdout:\n");
-                sb.append(stdout);
-                sb.append("\n");
+            // Failed...lets try to get around access denied errors on mounted
+            // clearcase view if we're certain the file exists and is not a 
+            // directory
+            
+            if (StringUtil.containsIgnoreCase(stderr.toString(), "Access Denied") && 
+                            file.exists() && file.isFile()) {
+                
+                File tmpDir = FileUtil.getTempDir();
+                FileUtils.copyFileToDirectory(file, tmpDir);
+                File tmpFile = new File(tmpDir, FilenameUtils.getName(filename));
+                
+                if (tmpFile.exists()) {
+                    
+                    // launch is async so just queue up to be deleted when the
+                    // jvm is exited.
+                    FileUtils.forceDeleteOnExit(tmpFile);
+                    launch (tmpFile.getAbsolutePath());
+                }
+                else {
+                    throw new RuntimeException("Tried to copy file "
+                        + filename
+                        + " to tmp dir "
+                        + tmpDir
+                        + " and launch but failed.");
+                }
             }
-            
-            if (StringUtils.isNotEmpty(stderr.toString())) {
-                sb.append("stderr:\n");
-                sb.append(stderr);
+            else { 
+                StringBuffer sb = new StringBuffer();
+                sb.append("Launch failed with command '" + ArrayUtil.toString(cmdArray) + "'\n");
+                
+                if (StringUtils.isNotEmpty(stdout.toString())) {
+                    sb.append("stdout:\n");
+                    sb.append(stdout);
+                    sb.append("\n");
+                }
+                
+                if (StringUtils.isNotEmpty(stderr.toString())) {
+                    sb.append("stderr:\n");
+                    sb.append(stderr);
+                }
+                
+                throw new RuntimeException(sb.toString());
             }
-            
-            throw new RuntimeException(sb.toString());
         }
     }
 }
