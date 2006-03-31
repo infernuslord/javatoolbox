5133494596271140f3b6df29a6e29280e46ffa23
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-03-31 20:45:17 +0000

    Added toStringVerbose() which internalls uses AsMap.of()

--- toolbox/src/toolbox/util/MapUtil.java
+++ toolbox/src/toolbox/util/MapUtil.java
@@ -3,6 +3,8 @@ package toolbox.util;
 import java.util.Iterator;
 import java.util.Map;
 
+import toolbox.util.collections.AsMap;
+
 /**
  * Utility functionality for Maps.
  */
@@ -34,6 +36,27 @@ public final class MapUtil
         {
             Map.Entry entry = (Map.Entry) i.next();
             sb.append(entry.getKey() + " : " + entry.getValue() + "\n");
+        }
+        
+        return sb.toString();
+    }
+    
+    
+    /**
+     * Alternative toString() implementation that displays map entries, one
+     * per line.
+     * 
+     * @param map Map to stringify
+     * @return String representing map's contents
+     */
+    public static String toStringVerbose(Map map)
+    {
+        StringBuffer sb = new StringBuffer();
+        
+        for (Iterator i = map.entrySet().iterator(); i.hasNext();)
+        {
+            Map.Entry entry = (Map.Entry) i.next();
+            sb.append("Key= " + entry.getKey() + " \nValue= " + AsMap.of(entry.getValue()) + "\n");
         }
         
         return sb.toString();
