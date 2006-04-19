c899662af8983c187a4da8374b23bb647248ae93
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-04-19 21:48:30 +0000

    Added unit tests

--- trunk/toolbox/test/toolbox/util/dirmon/recognizer/FileDeletedRecognizerTest.java
+++ trunk/toolbox/test/toolbox/util/dirmon/recognizer/FileDeletedRecognizerTest.java
@@ -1,13 +1,19 @@
 package toolbox.util.dirmon.recognizer;
 
 import java.io.File;
+import java.util.List;
 
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.log4j.Logger;
 
 import toolbox.util.FileUtil;
+import toolbox.util.dirmon.DirSnapshot;
+import toolbox.util.dirmon.DirectoryMonitor;
+import toolbox.util.dirmon.IFileActivityRecognizer;
+import toolbox.util.dirmon.event.FileEvent;
 
 /**
  * Unit test for {@link toolbox.util.dirmon.trash.FileDeletedActivity}.
@@ -29,42 +35,150 @@ public class FileDeletedRecognizerTest extends TestCase {
     // Unit Tests
     // --------------------------------------------------------------------------
 
-    public void testGetAffectedFiles_DeleteFile() throws Exception {
-        logger_.info("Running testGetAffectedFiles_DeleteFile ...");
+    public void testGetRecognizedEvents_NewFilesAdded() throws Exception {
+        logger_.info("Running testGetRecognizedEvents_NewFilesAdded ...");
 
         // Create a base line dir with two files
         File dir = FileUtil.createTempDir();
+        DirSnapshot snapshot1 = new DirSnapshot(dir);
+        DirectoryMonitor dm = new DirectoryMonitor(dir);
 
+        try {
+            File file1 = FileUtil.createTempFile(dir);
+            File file2 = FileUtil.createTempFile(dir);
+            FileUtils.writeStringToFile(file1, "file1", null);
+            FileUtils.writeStringToFile(file2, "file2", null);
+            DirSnapshot snapshot2 = new DirSnapshot(dir);
+            
+            IFileActivityRecognizer recognizer = new FileDeletedRecognizer(dm);
+            List firstRun = recognizer.getRecognizedEvents(snapshot1, snapshot2);
+            assertEquals("No deleted files should have been recognized", 0, firstRun.size());
+        }
+        finally {
+            FileUtils.forceDelete(dir);
+        }
+    }
+
+    
+    public void testGetRecognizedEvents_NoChange() throws Exception {
+        logger_.info("Running testGetRecognizedEvents_NoChange ...");
+
+        File dir = FileUtil.createTempDir();
+        DirectoryMonitor dm = new DirectoryMonitor(dir);
+
+        try {
+            File file1 = FileUtil.createTempFile(dir);
+            File file2 = FileUtil.createTempFile(dir);
+            FileUtils.writeStringToFile(file1, "file1", null);
+            FileUtils.writeStringToFile(file2, "file2", null);
+            DirSnapshot snapshot1 = new DirSnapshot(dir);
+            DirSnapshot snapshot2 = new DirSnapshot(dir);
+            
+            IFileActivityRecognizer recognizer = new FileDeletedRecognizer(dm);
+            List firstRun = recognizer.getRecognizedEvents(snapshot1, snapshot2);
+            assertEquals("No deleted files should have been recognized", 0, firstRun.size());
+        }
+        finally {
+            FileUtils.forceDelete(dir);
+        }
+    }
+    
+    
+    public void testGetRecognizedEvents_SingleFileDeleted() throws Exception {
+        logger_.info("Running testGetRecognizedEvents_SingleFileDeleted ...");
+
+        File dir = FileUtil.createTempDir();
+        DirectoryMonitor dm = new DirectoryMonitor(dir);
+        IFileActivityRecognizer recognizer = new FileDeletedRecognizer(dm);
+
+        try {
+            File file1 = FileUtil.createTempFile(dir);
+            File file2 = FileUtil.createTempFile(dir);
+            FileUtils.writeStringToFile(file1, "file1", null);
+            FileUtils.writeStringToFile(file2, "file2", null);
+            String expectedPath = file2.getAbsolutePath();
+            
+            DirSnapshot snapshot1 = new DirSnapshot(dir);
+            FileUtils.forceDelete(file2);
+            DirSnapshot snapshot2 = new DirSnapshot(dir);
+            
+            List events = recognizer.getRecognizedEvents(snapshot1, snapshot2);
+            assertEquals(1, events.size());
+            
+            FileEvent e = (FileEvent) events.get(0);
+            assertEquals(FileEvent.TYPE_FILE_DELETED, e.getEventType());
+            assertEquals(dm, e.getSource());
+            assertNull(e.getAfterSnapshot());
+            assertNotNull(e.getBeforeSnapshot());
+            assertEquals(dm, e.getDirectoryMonitor());
+            assertEquals(expectedPath, e.getBeforeSnapshot().getAbsolutePath());
+        }
+        finally {
+            FileUtils.forceDelete(dir);
+        }
+    }
+    
+    
+//    public void testGetRecognizedEvents_SingleFileDeleted() throws Exception {
+//        logger_.info("Running testGetRecognizedEvents_SingleFileDeleted ...");
+//
+//        File dir = FileUtil.createTempDir();
+//        DirectoryMonitor dm = new DirectoryMonitor(dir);
+//
 //        try {
-//            String file1 = FileUtil.createTempFilename(dir);
-//            String file2 = FileUtil.createTempFilename(dir);
-//
-//            FileUtil.setFileContents(file1, "file1", false);
-//            FileUtil.setFileContents(file2, "file2", false);
-//
-//            // Get list of new files..should be zero on first run
-//            IFileActivity activity = new FileDeletedActivity();
-//            List firstRun = activity.getAffectedFiles(dir);
-//            assertEquals("first run should be empty", 0, firstRun.size());
-//
-//            // Delete a file from the directory
-//            FileUtil.delete(file1);
-//
-//            // Run the activity again..should report 1 deleted file
-//            List secondRun = activity.getAffectedFiles(dir);
-//
-//            assertEquals(
-//                "second run should contain one file", 1, secondRun.size());
-//
-//            logger_.debug("Deleted file activity: "
-//                + ArrayUtil.toString(secondRun.toArray()));
-//
-//            // Run the activity again.. should report no new files
-//            List thirdRun = activity.getAffectedFiles(dir);
-//            assertEquals(0, thirdRun.size());
+//            File file1 = FileUtil.createTempFile(dir);
+//            File file2 = FileUtil.createTempFile(dir);
+//            FileUtils.writeStringToFile(file1, "file1", null);
+//            FileUtils.writeStringToFile(file2, "file2", null);
+//            DirSnapshot snapshot1 = new DirSnapshot(dir);
+//            FileUtils.forceDelete(file2);
+//            DirSnapshot snapshot2 = new DirSnapshot(dir);
+//            
+//            IFileActivityRecognizer recognizer = new FileDeletedRecognizer(dm);
+//            List firstRun = recognizer.getRecognizedEvents(snapshot1, snapshot2);
+//            assertEquals(1, firstRun.size());
+////
+////            // Delete a file from the directory
+////            FileUtil.delete(file1);
+////
+////            // Run the activity again..should report 1 deleted file
+////            List secondRun = activity.getAffectedFiles(dir);
+////
+////            assertEquals(
+////                "second run should contain one file", 1, secondRun.size());
+////
+////            logger_.debug("Deleted file activity: "
+////                + ArrayUtil.toString(secondRun.toArray()));
+////
+////            // Run the activity again.. should report no new files
+////            List thirdRun = activity.getAffectedFiles(dir);
+////            assertEquals(0, thirdRun.size());
 //        }
 //        finally {
-//            FileUtil.removeDir(dir);
+//            FileUtils.forceDelete(dir);
 //        }
-    }
+//    }
+    
+    
+//    public void testFileEquality() {
+//        logger_.info("Running testFileEquality ...");
+//        
+//        
+//        File a = new File("c:\\nresults.txt");
+//        File b = new File("c:\\nresults.txt");
+//        File c = new File("c:\\bin\\..\\nresults.txt");
+//        
+//        List x = new ArrayList();
+//        
+//        x.add(a);
+//        x.remove(b);
+//        x.add(b);
+//        x.remove(a);
+//        x.add(a);
+//        x.add(a);
+//        
+//        logger_.error("Size = " + x.size());
+//    }
+    
+    
 }
