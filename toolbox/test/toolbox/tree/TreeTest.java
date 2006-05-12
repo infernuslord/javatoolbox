74fabcd542dada44f2b57522d9013a390c5570cb
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-05-12 05:25:46 +0000

    Updated to use refactored Tree with TreeConfig

--- toolbox/test/toolbox/tree/TreeTest.java
+++ toolbox/test/toolbox/tree/TreeTest.java
@@ -2,7 +2,6 @@ package toolbox.tree;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.StringWriter;
 
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
@@ -12,12 +11,14 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import toolbox.util.FileUtil;
+import toolbox.util.RandomUtil;
+import toolbox.util.file.FileComparator;
 
 /**
  * Unit test for {@link toolbox.tree.Tree}.
  */
-public class TreeTest extends TestCase
-{
+public class TreeTest extends TestCase {
+    
     private static final Logger logger_ = Logger.getLogger(TreeTest.class);
     
     //--------------------------------------------------------------------------
@@ -33,13 +34,7 @@ public class TreeTest extends TestCase
     // Main
     //--------------------------------------------------------------------------
     
-    /**
-     * Entrypoint.
-     * 
-     * @param args None recognized.
-     */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         TestRunner.run(TreeTest.class);
     }
 
@@ -50,10 +45,9 @@ public class TreeTest extends TestCase
     /**
      * Create a temp directory to play around in.
      * 
-     * @throws IOException on I/O error.
+     * @see junit.framework.TestCase#setUp()
      */
-    public void setUp() throws IOException
-    {
+    public void setUp() throws IOException{
         System.out.println(StringUtils.repeat("=", 80));
         rootDir_ = FileUtil.createTempDir();
     }
@@ -61,59 +55,24 @@ public class TreeTest extends TestCase
     
     /**
      * Removes the temp directory.
+     * 
+     * @see junit.framework.TestCase#tearDown()
      */
-    public void tearDown() throws IOException
-    {
+    public void tearDown() throws IOException {
         FileUtils.deleteDirectory(rootDir_);
     }
 
     //--------------------------------------------------------------------------
     // Unit Tests
     //--------------------------------------------------------------------------
-
-    /**
-     * Tests the constructors.
-     * 
-     * @throws Exception on error.
-     */
-    public void testConstructors() throws Exception
-    {
-        logger_.info("Running testConstructors...");
-        
-        Tree t = new Tree(FileUtil.getTempDir());
-        assertNotNull(t);
-        
-        Tree t2 = new Tree(FileUtil.getTempDir(), true);
-        assertNotNull(t2);
-        
-        Tree t3 = new Tree(FileUtil.getTempDir(), new StringWriter());
-        assertNotNull(t3);
-        
-        Tree t4 = new Tree(FileUtil.getTempDir(), new StringWriter());        
-        assertNotNull(t4);
-        
-        Tree t5 = new Tree(FileUtil.getTempDir(), true, true);
-        assertNotNull(t5);
-        
-        Tree t6 = new Tree(FileUtil.getTempDir(), true, true, Tree.SORT_NAME);
-        assertNotNull(t6);
-
-        Tree t7 = new Tree(
-            FileUtil.getTempDir(), true, true, true, Tree.SORT_NAME, ".*");
-        
-        assertNotNull(t7);
-        
-    }
-
     
     /**
      * Tests for a simple cascading structure.
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeSimpleCascade() throws Exception
-    {
-        logger_.info("Running testShowTreeSimpleCascade...");
+    public void testShowTree_Cascade_Dirs() throws Exception {
+        logger_.info("Running testShowTree_Cascade_Dirs...");
         logger_.debug("Tree with cascading dirs: \n");
         
         File a = new File(rootDir_, "a");
@@ -140,8 +99,7 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeSimpleFlat() throws Exception
-    {
+    public void testShowTreeSimpleFlat() throws Exception {
         logger_.info("Running testShowTreeSimpleFlat...");
         logger_.debug("Tree with flat struct at level 2: \n");
         
@@ -169,8 +127,7 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeExtensionBar() throws Exception
-    {
+    public void testShowTreeExtensionBar() throws Exception {
         logger_.info("Running testShowTreeExtensionBar...");
         logger_.debug("Tree with an extension bar: \n");
                 
@@ -210,9 +167,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeManyInRoot() throws Exception
-    {
-        logger_.info("Running testShowTreeManyInRoot...");
+    public void testShowTree_Root_Dirs_Many() throws Exception {
+        logger_.info("Running testShowTree_Root_Dirs_Many...");
         logger_.debug("Tree with > 1 dir in root: \n");
                 
         File a = new File(rootDir_, "a");
@@ -244,9 +200,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeEmptyRoot() throws Exception
-    {
-        logger_.info("Running testShowTreeEmptyRoot...");
+    public void testShowTree_Root_Dirs_Zero() throws Exception {
+        logger_.info("Running testShowTree_Root_Dirs_Zero...");
         logger_.debug("Tree with an empty root: \n");
         Tree tree = new Tree(rootDir_);
         tree.showTree();
@@ -267,9 +222,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeOneDir() throws Exception
-    {
-        logger_.info("Running testShowTreeOneDir...");
+    public void testShowTree_Root_Dirs_One() throws Exception {
+        logger_.info("Running testShowTree_Root_Dirs_One...");
         logger_.debug("Tree with a single directory: \n");
 
         File a = new File(rootDir_, "a");
@@ -287,9 +241,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void xtestShowTreeLargeTree() throws Exception
-    {
-        logger_.info("Running testShowTreeLargeTree...");
+    public void xtestShowTreeLargeTree() throws Exception {
+        logger_.info("Running xtestShowTreeLargeTree...");
         Tree tree = new Tree(new File("/"));
         tree.showTree();
         
@@ -302,9 +255,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeSimpleCascadeFile() throws Exception
-    {
-        logger_.debug("Running testShowTreeSimpleCascadeFile...");
+    public void testShowTree_Cascade_DirAndFile() throws Exception {
+        logger_.info("Running testShowTree_Cascade_DirAndFile...");
         logger_.debug("Tree with cascading dirs and one file: \n");
         
         createFile(rootDir_);
@@ -324,8 +276,10 @@ public class TreeTest extends TestCase
         File d = new File(c, "d");
         assertTrue(d.mkdir());
         createFile(d);
-               
-        Tree tree = new Tree(rootDir_, true);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        Tree tree = new Tree(rootDir_, config);
         tree.showTree();
         
         printNativeFileTree(rootDir_);
@@ -337,9 +291,8 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testShowTreeRootFiles() throws Exception
-    {
-        logger_.info("Running testShowTreeRootFiles...");
+    public void testShowTree_Root_Files_Many() throws Exception {
+        logger_.info("Running testShowTree_Root_Files_Many...");
         logger_.debug("Tree with cascading dirs and one file: \n");
         
         createFile(rootDir_);
@@ -348,7 +301,9 @@ public class TreeTest extends TestCase
         createFile(rootDir_);
         createFile(rootDir_);
                
-        Tree tree = new Tree(rootDir_, true);
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        Tree tree = new Tree(rootDir_, config);
         tree.showTree();
         
         printNativeFileTree(rootDir_);
@@ -360,8 +315,7 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void testPrintUsage() throws Exception
-    {
+    public void testPrintUsage() throws Exception {
         logger_.info("Running testPrintUsage...");
         
         // Send in an invalid flag so usage information is shown
@@ -374,8 +328,7 @@ public class TreeTest extends TestCase
      * 
      * @throws Exception on error.
      */
-    public void xxxtestMain() throws Exception
-    {
+    public void xxxtestMain() throws Exception {
         // TODO: Fix me to use options when upgrading to cli2
         
         logger_.info("Running testMain...");
@@ -383,10 +336,168 @@ public class TreeTest extends TestCase
         Tree.main(new String[] {"-os", FileUtil.getTempDir().getAbsolutePath()});
     }
     
+
+    public void testShowTree_Show_FileSizes() throws Exception {
+        logger_.info("Running testShowTree_Show_FileSizes...");
+        logger_.debug("Tree with file sizes: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFilesize(true);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Show_FileDates() throws Exception {
+        logger_.info("Running testShowTree_Show_FileDates...");
+        logger_.debug("Tree with file dates: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFileDate(true);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Show_FileDateAndSize() throws Exception {
+        logger_.info("Running testShowTree_Show_FileDateAndSize...");
+        logger_.debug("Tree with file dates and size: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFileDate(true);
+        config.setShowFilesize(true);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Show_FileDateAndSize_Sort_Size() throws Exception {
+        logger_.info("Running testShowTree_Show_FileDateAndSize_Sort_Size...");
+        logger_.debug("Tree with file dates and size sorted by size: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFileDate(true);
+        config.setShowFilesize(true);
+        config.setSortBy(FileComparator.COMPARE_SIZE);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+    
+    public void testShowTree_Sort_FileSize() throws Exception {
+        logger_.info("Running testShowTree_Sort_FileSize...");
+        logger_.debug("Tree sorted by file size: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFilesize(true);
+        config.setSortBy(FileComparator.COMPARE_SIZE);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Sort_FileName() throws Exception {
+        logger_.info("Running testShowTree_Sort_FileName...");
+        logger_.debug("Tree sorted by file name: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFilesize(true);
+        config.setSortBy(FileComparator.COMPARE_NAME);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Sort_Date() throws Exception {
+        logger_.info("Running testShowTree_Sort_Date...");
+        logger_.debug("Tree sorted by file date: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setShowFilesize(true);
+        config.setSortBy(FileComparator.COMPARE_DATE);
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
+
+    public void testShowTree_Regex_Match() throws Exception {
+        logger_.info("Running testShowTree_Regex_Match...");
+        logger_.debug("Tree matching regex - files containing 2: \n");
+        
+        createFile(rootDir_);
+        createRandomTree(rootDir_);
+        
+        TreeConfig config = new TreeConfig();
+        config.setShowFiles(true);
+        config.setRegexFilter("2");
+        Tree tree = new Tree(rootDir_, config);
+        tree.showTree();
+        
+        printNativeFileTree(rootDir_);
+    }
     
     //--------------------------------------------------------------------------
     // Helper Methods
     //--------------------------------------------------------------------------
+
+    private void createRandomTree(File root) throws IOException{
+        File a = new File(root, "a");
+        assertTrue(a.mkdir());
+        createSomeFiles(a, 5);
+                
+        File b = new File(a, "b");
+        assertTrue(b.mkdir());
+        createSomeFiles(b, 5);
+        
+        File c = new File (b, "c");
+        assertTrue(c.mkdir());
+        createSomeFiles(c, 5);
+        
+        File d = new File(c, "d");
+        assertTrue(d.mkdir());
+        createSomeFiles(d, 5);
+    }
+    
+    private void createSomeFiles(File a, int max) throws IOException {
+        int n = RandomUtil.nextInt(1, max);
+        for (int i = 0; i < n; i++)
+            createFile(a);
+    }
 
     /**
      * Creates a temp file in the given directory.
@@ -398,7 +509,7 @@ public class TreeTest extends TestCase
     protected File createFile(File dir) throws IOException
     {
         File f = FileUtil.createTempFile(dir);
-        FileUtil.setFileContents(f, "testing", false);
+        FileUtil.setFileContents(f, StringUtils.repeat("testing", RandomUtil.nextInt(1, 1000)), false);
         return f;
     }
 
@@ -411,18 +522,11 @@ public class TreeTest extends TestCase
      */
     public void printNativeTree(File dir) throws IOException
     {
-        /*
-        Process p = Runtime.getRuntime().exec(
-            "tree.com /a " + dir.getAbsolutePath());
-            
-        InputStream is = p.getInputStream();
-        String output = StreamUtil.asString(is);
-        
-        output = output.substring(
-            output.indexOf("\n", output.indexOf("\n") + 1));
-            
-        System.out.println(output);
-        */
+//        Process p = Runtime.getRuntime().exec("tree.com /a " + dir.getAbsolutePath());
+//        InputStream is = p.getInputStream();
+//        String output = IOUtils.toString(is);
+//        output = output.substring(output.indexOf("\n", output.indexOf("\n") + 1));
+//        System.out.println(output);
     }
 
     
@@ -434,17 +538,10 @@ public class TreeTest extends TestCase
      */
     public void printNativeFileTree(File dir) throws IOException
     {
-        /*
-        Process p = Runtime.getRuntime().exec(
-            "tree.com /a /f " + dir.getAbsolutePath());
-            
-        InputStream is = p.getInputStream();
-        String output = StreamUtil.asString(is);
-        
-        output = output.substring(
-            output.indexOf("\n", output.indexOf("\n") + 1));
-            
-        System.out.println(output);
-        */
+//        Process p = Runtime.getRuntime().exec("tree.com /a /f " + dir.getAbsolutePath());
+//        InputStream is = p.getInputStream();
+//        String output = IOUtils.toString(is);
+//        output = output.substring(output.indexOf("\n", output.indexOf("\n") + 1));
+//        System.out.println(output);
     }
 }
