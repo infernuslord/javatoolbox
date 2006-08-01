45609e1654ac3a09b04fd7799aa83bb357c29944
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-08-01 21:12:04 +0000

    Added command line option support to ip2hostname to control lookup strategy, caching, and verbose logging.

--- toolbox/src/toolbox/ip2hostname/IP2Hostname.java
+++ toolbox/src/toolbox/ip2hostname/IP2Hostname.java
@@ -5,8 +5,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Iterator;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
 import org.apache.commons.io.IOUtils;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 /**
@@ -17,15 +26,117 @@ public class IP2Hostname {
 
     private static final Logger logger = Logger.getLogger(IP2Hostname.class);
     
+    // -------------------------------------------------------------------------
+    // Fields
+    // -------------------------------------------------------------------------
+    
+    private boolean async;
+    private boolean caching;
+    
+    // -------------------------------------------------------------------------
+    // Main
+    // -------------------------------------------------------------------------
+    
     public static void main(String[] args) {
-        IP2Hostname ip2hostname = new IP2Hostname();
-        ip2hostname.start(System.in, System.out);
+        
+        try {
+            CommandLineParser parser = new PosixParser();
+            Options options = new Options();
+
+            // Create command line options            
+            Option asyncOption = new Option("a", "async", false, "Asynchronous replacement of hostname (default)");
+            Option syncOption = new Option("s", "sync", false, "Synchronous replacement of hostname");
+            Option cachingOption = new Option("c", "caching", false, "Cache resolved hostnames (default)");
+            Option nonCachingOption = new Option("n", "noncaching", false, "Do not cache resolved hostnames");
+            Option verboseOption = new Option("v", "verbose", false, "Verbose logging");
+            Option helpOption = new Option("h", "help", false, "Print usage");
+            
+            options.addOption(helpOption);
+            options.addOption(asyncOption);        
+            options.addOption(syncOption);
+            options.addOption(cachingOption);
+            options.addOption(nonCachingOption);
+            options.addOption(verboseOption);
+    
+            // Parse options
+            CommandLine cmdLine = parser.parse(options, args, true);
+
+            boolean async = true;
+            boolean caching = true;
+            
+            // Handle options
+            for (Iterator i = cmdLine.iterator(); i.hasNext();) {
+                Option option = (Option) i.next();
+                String opt = option.getOpt();
+
+                if (opt.equals(asyncOption.getOpt())) {
+                    async = true;
+                }
+                else if (opt.equals(syncOption.getOpt())) {
+                    async = false;
+                }
+                else if (opt.equals(cachingOption.getOpt())) {
+                    caching = true;
+                }
+                else if (opt.equals(nonCachingOption.getOpt())) {
+                    caching = false;
+                }
+                else if (opt.equals(verboseOption.getOpt())) {
+                    Logger l = Logger.getLogger("toolbox.ip2hostname");
+                    l.setAdditivity(true);
+                    l.setLevel(Level.DEBUG);
+                }
+                else if (opt.equals(helpOption.getOpt())) {
+                    printUsage(options);
+                    return;
+                }
+                else {
+                    logger.error("Option not handled: " + option);
+                }
+                
+                logger.debug("handling option: " + option);
+            }
+
+            // there is not support for arguments - flag as improper usage
+            switch (cmdLine.getArgs().length) {
+                
+                case 0:
+                    // OK
+                    break;
+
+                default:
+                    // Invalid
+                    printUsage(options);
+                    return;
+            }
+            
+            IP2Hostname ip2hostname = new IP2Hostname();
+            ip2hostname.setAsync(async);
+            ip2hostname.setCaching(caching);
+            ip2hostname.start(System.in, System.out);
+            
+        }
+        catch (Exception e) {
+            logger.error("main", e);
+        }
+        
     }
+    
+    // -------------------------------------------------------------------------
+    // Constructors
+    // -------------------------------------------------------------------------
+    
+    public IP2Hostname() {
+    }
+    
+    // -------------------------------------------------------------------------
+    // Public
+    // -------------------------------------------------------------------------
     
     public void start(InputStream is, OutputStream os) {
         
         IP2HostnameReader reader = new IP2HostnameReader(
-            new InputStreamReader(is));
+            new InputStreamReader(is), new HostnameResolver(caching, async));
         
         PrintWriter writer = new PrintWriter(os);
         String line = null;
@@ -43,5 +154,50 @@ public class IP2Hostname {
             IOUtils.closeQuietly(reader);
             IOUtils.closeQuietly(writer);
         }
+    }
+
+    public boolean isAsync() {
+        return async;
+    }
+
+    
+    public void setAsync(boolean async) {
+        this.async = async;
+    }
+
+    
+    public boolean isCaching() {
+        return caching;
+    }
+
+    
+    public void setCaching(boolean caching) {
+        this.caching = caching;
+    }
+    
+    // -------------------------------------------------------------------------
+    // Private 
+    // -------------------------------------------------------------------------
+    
+    /**
+     * Prints program usage. 
+     */
+    private static void printUsage(Options options) {
+        HelpFormatter f = new HelpFormatter();
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        
+        f.printHelp(
+            pw, 
+            80, 
+            "ip2hostname " + "[options]", 
+            "", 
+            options, 
+            2, 
+            4,
+            "",
+            false);
+        
+        System.out.println(sw.toString());
     }
 }
---
+++ toolbox/test/toolbox/ip2hostname/IP2HostnameTest.java
@@ -0,0 +1,32 @@
+package toolbox.ip2hostname;
+
+import org.apache.log4j.Logger;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+/**
+ * Unit test for {@link toolbox.ip2hostname.IP2Hostname}.
+ */
+public class IP2HostnameTest extends TestCase {
+
+    private static final Logger logger = Logger.getLogger(IP2HostnameTest.class);
+    
+    // -------------------------------------------------------------------------
+    // Main
+    // -------------------------------------------------------------------------
+    
+    public static void main(String[] args) {
+        TestRunner.run(IP2HostnameTest.class);
+    }
+    
+    // -------------------------------------------------------------------------
+    // Unit Tests
+    // -------------------------------------------------------------------------
+    
+    public void testPrintUsage() {
+        logger.info("Running testPrintUsage ...");
+        
+        IP2Hostname.main(new String[] {"-help"} );
+    }
+}
