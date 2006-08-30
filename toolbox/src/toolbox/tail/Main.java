b03ccd3f78af239514f184fb3495a7f5747f4bdd
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2006-08-30 20:08:17 +0000

    Added -f flag with prints the filename as a prefix to each line of output

--- toolbox/src/toolbox/tail/Main.java
+++ toolbox/src/toolbox/tail/Main.java
@@ -2,9 +2,11 @@ package toolbox.tail;
 
 import java.io.File;
 import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import org.apache.log4j.Logger;
 
+import toolbox.util.io.NullWriter;
 import toolbox.util.service.ServiceException;
 
 /**
@@ -14,15 +16,12 @@ public class Main extends TailAdapter
 {
     private static final Logger logger_ = Logger.getLogger(Main.class);
 
+    private boolean prefixWithFilename = false;
+    
     //--------------------------------------------------------------------------
     // Main
     //--------------------------------------------------------------------------
 
-    /**
-     * Entrypoint.
-     *
-     * @param args List of files to tail.
-     */
     public static void main(String args[])
     {
         new Main(args);
@@ -46,17 +45,33 @@ public class Main extends TailAdapter
         }
 
         String files[] = args;
+        int fi = 0;
+        
+        if ("-f".equals(files[0]))
+        {
+            prefixWithFilename = true;
+            fi = 1;
+        }
+                        
 
-        for (int i = 0; i < files.length; i++)
+        for (int i = fi; i < files.length; i++)
         {
             try
             {
                 Tail tail = new Tail();
-
-                tail.follow(
-                    new File(files[i]),
-                    new OutputStreamWriter(System.out));
-
+                
+                if (!prefixWithFilename)
+                    tail.follow(new File(files[i]), new OutputStreamWriter(System.out));
+                else
+                {
+                    tail.follow(new File(files[i]), new NullWriter());
+                    tail.addTailListener(
+                        new LineBeginsWithFilenameDecorator(
+                            new TailToWriter(
+                                new OutputStreamWriter(System.out)), 
+                            files[i]));
+                }
+                
                 tail.start();
             }
             catch (ServiceException se)
@@ -75,11 +90,45 @@ public class Main extends TailAdapter
      */
     private void printUsage()
     {
-        System.out.println(
-            "Program : Tails a list of files");
-        System.out.println(
-            "Usage   : java toolbox.tail.Main [file1 file2 ... filex]");
-        System.out.println(
-            "Example : java toolbox.tail.Main appserver.log");
+        System.out.println("Program : Tails a list of files");
+        System.out.println("Usage   : java toolbox.tail.Main [file1 file2 ... file n]");
+        System.out.println("Example : java toolbox.tail.Main appserver.log");
     }
+    
+    // -------------------------------------------------------------------------
+    // 
+    // -------------------------------------------------------------------------
+    
+    class LineBeginsWithFilenameDecorator extends TailAdapter
+    {
+        private TailToWriter delegate;
+        private String filename;
+        
+        public LineBeginsWithFilenameDecorator(TailToWriter delegate, String filename)
+        {
+            this.delegate = delegate;
+            this.filename = filename;
+        }
+ 
+        public void nextLine(Tail tail, String line) {
+            
+            Writer w = delegate.getWriter();
+            
+            try 
+            {
+                w.write("[");
+                w.write(filename);
+                w.write("] ");
+            }
+            catch (Exception e) 
+            {
+                System.err.println(e);
+            }
+            finally
+            {
+                delegate.nextLine(tail, line);
+            }
+        }
+    }
+    
 }
