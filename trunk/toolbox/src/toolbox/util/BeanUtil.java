c4d11686c9c50bf199c94b6245d5bf170f72337a
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2007-01-24 19:51:07 +0000

    Added setAllValuesForType(...)

--- trunk/toolbox/src/toolbox/util/BeanUtil.java
+++ trunk/toolbox/src/toolbox/util/BeanUtil.java
@@ -4,7 +4,10 @@ package toolbox.util;
 import java.beans.BeanInfo;
 import java.beans.Introspector;
 import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
+import org.apache.commons.beanutils.PropertyUtils;
 
 /**
  * Javabean Utilities.
@@ -76,4 +79,44 @@ public class BeanUtil
         
         return sb.toString();
     }
+    
+    /**
+     * For all javabean properties of type clazz on a given javabean, sets the 
+     * value of those javabean properties to the passed in value.
+     * <p>
+     * Example:
+     * <p> 
+     * Assume you have a javabean with 100 {@link Integer} attributes with getters
+     * and setters. How do you set all the attributes to the value 2 without
+     * calling <code>setAttrX(new Integer(2))</code> a 100 times (assuming you 
+     * don't care about the performance overhead of using refletion)?
+     * <p>
+     * <pre>
+     * setAllValuesForType(myBean, Integer.class, new Integer(2));
+     * </pre>
+     * 
+     * @param javabean Javabean to set values on.
+     * @param clazz Type of the javabean properties to set.
+     * @param value The value to set each property to.
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InvocationTargetException
+     */
+    public static void setAllValuesForType(Object javabean, Class clazz, Object value) 
+      throws 
+        IllegalArgumentException, 
+        IllegalAccessException, 
+        InvocationTargetException {
+      
+      PropertyDescriptor[] descriptors = PropertyUtils.getPropertyDescriptors(javabean);
+
+      for (int i = 0; i < descriptors.length; i++) {
+        PropertyDescriptor pd = descriptors[i];
+        if (pd.getPropertyType() == clazz) {
+          Method m = pd.getWriteMethod();
+          if (m != null)
+            pd.getWriteMethod().invoke(javabean, new Object[] {value});
+        }
+      }
+    }    
 }
