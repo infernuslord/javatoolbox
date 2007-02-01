f79142dafdfd1a5350f263a9ffeb8b094c15e069
analogue <analogue@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2007-02-01 21:59:03 +0000

    More exhaustive unit tests for getStartOfDay(...) and getEndOfDay(...)

--- trunk/toolbox/test/toolbox/util/DateTimeUtilTest.java
+++ trunk/toolbox/test/toolbox/util/DateTimeUtilTest.java
@@ -15,55 +15,24 @@ import toolbox.util.DateTimeUtil;
  */
 public class DateTimeUtilTest extends TestCase
 {
-    private static final Logger logger_ =
-        Logger.getLogger(DateTimeUtilTest.class);
+    private static final Logger logger_ = Logger.getLogger(DateTimeUtilTest.class);
 
     //--------------------------------------------------------------------------
     // Main
     //--------------------------------------------------------------------------
             
-    /**
-     * Entrypoint.
-     * 
-     * @param args None recognized
-     */
-    public static void main(String[] args)
-    {
-        TestRunner.run(DateTimeUtilTest.class);
-    }
+    public static void main(String[] args) {
+		TestRunner.run(DateTimeUtilTest.class);
+	}
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Unit Tests
     //--------------------------------------------------------------------------
         
-    /**
-     * Tests getBeginningOfDay()
-     * 
-     * @throws Exception on error
-     */
-    public void testGetBeginningOfDay() throws Exception
-    {
-        logger_.info("Running testGetBeginningOfDay...");
-        
-        Date d = DateTimeUtil.getBeginningOfDay();
-
-        Calendar c = Calendar.getInstance();
-        c.setTime(d);
-        assertTrue("hour not zero", c.get(Calendar.HOUR_OF_DAY) == 0);
-        assertTrue("minute not zero", c.get(Calendar.MINUTE) == 0);
-        assertTrue("sedond not zero", c.get(Calendar.SECOND) == 0);
-        assertTrue("millis not zero", c.get(Calendar.MILLISECOND) == 0);
-    }
-
-    
-    /**
-     * Tests add()
-     */
-    public void testAdd()
-    {
+    public void testAdd() {
         logger_.info("Running testAdd...");
         
-        Date d = DateTimeUtil.getBeginningOfDay();
+        Date d = DateTimeUtil.getStartOfDay();
         logger_.debug("Before adding: " + DateTimeUtil.format(d));
         
         DateTimeUtil.add(d, 1, 1, 1, 1, 1, 1);
@@ -71,35 +40,15 @@ public class DateTimeUtilTest extends TestCase
     }
     
     
-    /**
-     * Tests getBeginningOfTime()
-     */
-    public void testGetBeginningOfTime()
-    {
+    public void testGetStartOfTime() {
         logger_.info("Running testGetBeginningOfTime...");
 
-        Date d = DateTimeUtil.getBeginningOfTime();
+        Date d = DateTimeUtil.getStartOfTime();
         logger_.debug("Beginning of time: " + DateTimeUtil.format(d));
     }
 
     
-    /**
-     * Test getEndOfDay()
-     */
-    public void testGetEndOfDay()
-    {
-        logger_.info("Running testGetEndOfDay...");
-
-        Date d = DateTimeUtil.getEndOfDay();
-        logger_.debug("End of day: " + DateTimeUtil.format(d));
-    }
-
-    
-    /**
-     * Tests getEndOfTime()
-     */
-    public void testGetEndOfTime()
-    {
+    public void testGetEndOfTime() {
         logger_.info("Running testGetEndOfTime...");
 
         Date d = DateTimeUtil.getEndOfTime();
@@ -107,14 +56,121 @@ public class DateTimeUtilTest extends TestCase
     }
 
     
-    /**
-     * Tests formatToSecond()
-     */
-    public void testFormatToSecond()
-    {
+    public void testFormatToSecond() {
         logger_.info("Running testFormatToSecond...");
 
         Date d = new Date();
         logger_.debug("Formatted to seconds: " + DateTimeUtil.formatToSecond(d));
     }
+    
+    
+    public void testGetStartOfDay() throws Exception {
+		logger_.info("Running testGetStartOfDay...");
+
+		Date now = new Date();
+		Date start = DateTimeUtil.getStartOfDay();
+		logger_.debug("Start of day: " + start);
+
+		Calendar startCal = Calendar.getInstance();
+		startCal.setTime(start);
+
+		// There is the possibility that if the first two lines of this test run 
+		// on the end of day boundary (23:59:59 999) that this test will fail.
+		Calendar nowCal = Calendar.getInstance();
+		nowCal.setTime(now);
+
+		assertEquals(nowCal.get(Calendar.YEAR), startCal.get(Calendar.YEAR));
+		assertEquals(nowCal.get(Calendar.MONTH), startCal.get(Calendar.MONTH));
+		assertEquals(nowCal.get(Calendar.DATE), startCal.get(Calendar.DATE));
+		assertEquals(0, startCal.get(Calendar.HOUR_OF_DAY));
+		assertEquals(0, startCal.get(Calendar.MINUTE));
+		assertEquals(0, startCal.get(Calendar.SECOND));
+		assertEquals(0, startCal.get(Calendar.MILLISECOND));
+	}
+
+	public void testGetStartOfDay_Date() throws Exception {
+		logger_.info("Running testGetStartOfDay_Date...");
+
+		Date now = new Date();
+		Date start = DateTimeUtil.getStartOfDay(now);
+		logger_.debug("Start of day: " + start);
+
+		Calendar startCal = Calendar.getInstance();
+		startCal.setTime(start);
+
+		Calendar nowCal = Calendar.getInstance();
+		nowCal.setTime(now);
+
+		assertEquals(nowCal.get(Calendar.YEAR), startCal.get(Calendar.YEAR));
+		assertEquals(nowCal.get(Calendar.MONTH), startCal.get(Calendar.MONTH));
+		assertEquals(nowCal.get(Calendar.DATE), startCal.get(Calendar.DATE));
+		assertEquals(0, startCal.get(Calendar.HOUR_OF_DAY));
+		assertEquals(0, startCal.get(Calendar.MINUTE));
+		assertEquals(0, startCal.get(Calendar.SECOND));
+		assertEquals(0, startCal.get(Calendar.MILLISECOND));
+	}
+
+	public void testGetStartOfDay_Date_AlreadyAtStart() throws Exception {
+		logger_.info("Running testGetStartOfDay_Date_AlreadyAtStart...");
+
+		Date now = new Date();
+		Date start = DateTimeUtil.getStartOfDay(now);
+		Date startAgain = DateTimeUtil.getStartOfDay(start);
+		assertEquals(start, startAgain);
+	}
+
+	public void testGetEndOfDay_Date() {
+		logger_.info("Running testGetEndOfDay_Date...");
+
+		Date now = new Date();
+		Date end = DateTimeUtil.getEndOfDay(now);
+		logger_.debug("End of day: " + end);
+
+		Calendar endCal = Calendar.getInstance();
+		endCal.setTime(end);
+
+		Calendar nowCal = Calendar.getInstance();
+		nowCal.setTime(now);
+
+		assertEquals(nowCal.get(Calendar.YEAR), endCal.get(Calendar.YEAR));
+		assertEquals(nowCal.get(Calendar.MONTH), endCal.get(Calendar.MONTH));
+		assertEquals(nowCal.get(Calendar.DATE), endCal.get(Calendar.DATE));
+		assertEquals(23, endCal.get(Calendar.HOUR_OF_DAY));
+		assertEquals(59, endCal.get(Calendar.MINUTE));
+		assertEquals(59, endCal.get(Calendar.SECOND));
+		assertEquals(999, endCal.get(Calendar.MILLISECOND));
+	}
+
+	public void testGetEndOfDay() {
+		logger_.info("Running testGetEndOfDay...");
+
+		Date now = new Date();
+		Date end = DateTimeUtil.getEndOfDay();
+		logger_.debug("End of day: " + end);
+
+		Calendar endCal = Calendar.getInstance();
+		endCal.setTime(end);
+
+		// There is the possibility that if the first two lines of this test run 
+		// on the end of day boundary (23:59:59 999) that this test will fail.
+		Calendar nowCal = Calendar.getInstance();
+		nowCal.setTime(now);
+
+		assertEquals(nowCal.get(Calendar.YEAR), endCal.get(Calendar.YEAR));
+		assertEquals(nowCal.get(Calendar.MONTH), endCal.get(Calendar.MONTH));
+		assertEquals(nowCal.get(Calendar.DATE), endCal.get(Calendar.DATE));
+		assertEquals(23, endCal.get(Calendar.HOUR_OF_DAY));
+		assertEquals(59, endCal.get(Calendar.MINUTE));
+		assertEquals(59, endCal.get(Calendar.SECOND));
+		assertEquals(999, endCal.get(Calendar.MILLISECOND));
+	}
+
+	public void testGetEndOfDay_Date_AlreadyAtEnd() throws Exception {
+		logger_.info("Running testGetEndOfDay_Date_AlreadyAtEnd...");
+
+		Date now = new Date();
+		Date end = DateTimeUtil.getEndOfDay(now);
+		Date endAgain = DateTimeUtil.getEndOfDay(end);
+		assertEquals(end, endAgain);
+	}    
 }
