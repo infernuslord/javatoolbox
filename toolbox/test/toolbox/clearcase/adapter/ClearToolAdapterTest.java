3506a4e09285b9637253a140e8aebe3fe089151a
analogue@yahoo.com <analogue@yahoo.com@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2008-04-22 22:26:41 +0000

    Skip test if cleartool executable not found on system path

--- toolbox/test/toolbox/clearcase/adapter/ClearToolAdapterTest.java
+++ toolbox/test/toolbox/clearcase/adapter/ClearToolAdapterTest.java
@@ -14,6 +14,7 @@ import org.apache.log4j.Logger;
 import toolbox.clearcase.IClearCaseAdapter;
 import toolbox.clearcase.domain.Revision;
 import toolbox.clearcase.domain.VersionedFile;
+import toolbox.util.ClassUtil;
 
 /**
  * Unit test for {@link toolbox.clearcase.adapter.ClearToolAdapter}. 
@@ -38,10 +39,17 @@ public class ClearToolAdapterTest extends TestCase
     
     public void testFindChangedFiles() throws Exception
     {
-        logger_.info("Running testFindChangedFiles...");
-        
+    	logger_.info("Running testFindChangedFiles...");
+
+    	// Skip if cleartool executable not found
+    	if ((ClassUtil.findInPath("cleartool") == null) &&
+    	   (ClassUtil.findInPath("cleartool.exe") == null)) {
+    		logger_.debug("Skipping testFindChangedFiles...");
+    		return;
+    	}
+    	
         IClearCaseAdapter cc = ClearCaseAdapterFactory.create();
-        cc.setViewPath(new File("m:\\x1700_sandbox\\staffplanning"));
+        cc.setViewPath(new File("c:\\clearcase\\myDynamicView"));
         
         List changed = 
             cc.findChangedFiles(
