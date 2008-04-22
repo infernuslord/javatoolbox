f6ec3aa1d56d366f026e43959b941c6e9f54e1fb
analogue@yahoo.com <analogue@yahoo.com@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2008-04-22 16:03:34 +0000

    cleanup

--- trunk/toolbox/src/toolbox/util/collections/AsMap.java
+++ trunk/toolbox/src/toolbox/util/collections/AsMap.java
@@ -1,64 +1,22 @@
 package toolbox.util.collections;
 
+import java.util.Collections;
 import java.util.Map;
 
-import toolbox.util.typecast.Typecast;
+public final class AsMap {
+	
+	// --------------------------------------------------------------------------
+	// Public
+	// --------------------------------------------------------------------------
 
-/**
- * Used to convert an object into a Map.
- * <p>
- * Example:
- * <pre class="snippet">
- *  Map map = AsMap.of(someObject);
- * 
- *  // The toString() implementation dumps the objects attributes.
- *  // Very nice for debugging.
- *  System.out.println(map.toString());
- * 
- * </pre>
- */
-public final class AsMap
-{
-    //--------------------------------------------------------------------------
-    // Static
-    //--------------------------------------------------------------------------
-
-    /**
-     * Instance.
-     */
-    public static final Typecast TYPECAST = new Typecast(Map.class);
-
-    static
-    {
-        TYPECAST.addSearchPackage(AsMap.class);
-    }
-
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    /**
-     * Maps an object.
-     * 
-     * @param object Object to map.
-     * @return Map of object.
-     */
-    public static Map of(Object object)
-    {
-        return of(object, null);
-    }
-
-
-    /**
-     * Maps an object.
-     * 
-     * @param object Object to map.
-     * @param policy Mapping policy.
-     * @return Map of object.
-     */
-    public static Map of(Object object, String policy)
-    {
-        return (Map) TYPECAST.coerce(object, policy);
-    }
+	/**
+	 * Maps an object.
+	 * 
+	 * @param object
+	 *            Object to map.
+	 * @return Map of object.
+	 */
+	public static Map of(Object object) {
+		return Collections.EMPTY_MAP;
+	}
 }
