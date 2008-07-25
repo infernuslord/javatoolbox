72bca5e2ab592ed63410b672c10448af9f31209e
analogue@yahoo.com <analogue@yahoo.com@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2008-07-25 20:04:42 +0000

    Upgraded ant to 1.7.1; Fixed FigletTaskTest and PropertyPromptTaskTest

--- toolbox/test/toolbox/ant/PropertyPromptTaskTest.java
+++ toolbox/test/toolbox/ant/PropertyPromptTaskTest.java
@@ -5,6 +5,7 @@ import java.io.File;
 import junit.framework.TestCase;
 import junit.textui.TestRunner;
 
+import org.apache.log4j.Logger;
 import org.apache.tools.ant.Main;
 
 import toolbox.junit.testcase.StandaloneTestCase;
@@ -12,38 +13,30 @@ import toolbox.util.FileUtil;
 import toolbox.util.ResourceUtil;
 
 /**
- * Unit test for {@link toolbox.ant.PropertyPromptTask}. This test is marked as 
- * standalone because the call into Ant results in call to System.exit() on 
+ * Unit test for {@link toolbox.ant.PropertyPromptTask}. This test is marked as
+ * standalone because the call into Ant results in call to System.exit() on
  * completion.
  */
-public class PropertyPromptTaskTest extends TestCase 
-                                    implements StandaloneTestCase 
-{
-    //--------------------------------------------------------------------------
+public class PropertyPromptTaskTest extends TestCase implements StandaloneTestCase {
+
+    private static final Logger log = Logger.getLogger(PropertyPromptTaskTest.class);
+
+    // --------------------------------------------------------------------------
     // Main
-    //--------------------------------------------------------------------------
-    
-    public static void main(String[] args)
-    {
+    // --------------------------------------------------------------------------
+
+    public static void main(String[] args) {
         TestRunner.run(PropertyPromptTaskTest.class);
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Unit Tests
-    //--------------------------------------------------------------------------
-    
-    public void testPropertyPrompt() throws Exception
-    {
-        File f = ResourceUtil.getResourceAsTempFile(
-            "/toolbox/ant/PropertyPromptTaskTest.xml");
-        
-        Main.main(new String[]{
-            "-debug", 
-            "-verbose", 
-            "-f", 
-            f.getCanonicalPath()
-        });
-        
+    // --------------------------------------------------------------------------
+
+    public void testPropertyPrompt() throws Exception {
+        File f = ResourceUtil.getResourceAsTempFile("test/toolbox/ant/PropertyPromptTaskTest.xml");
+        log.debug("Ant file = " + f.getAbsolutePath());
+        Main.main(new String[] { "-debug", "-verbose", "-f", f.getCanonicalPath() });
         FileUtil.deleteQuietly(f);
     }
 }
