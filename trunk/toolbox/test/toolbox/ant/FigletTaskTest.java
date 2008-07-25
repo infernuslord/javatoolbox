2b281277c5da66dd760b74478c4172da73c48628
analogue@yahoo.com <analogue@yahoo.com@1d61b40d-8c4a-0410-a6ba-a5e3ed80748e>
2008-07-25 20:04:42 +0000

    Upgraded ant to 1.7.1; Fixed FigletTaskTest and PropertyPromptTaskTest

--- trunk/toolbox/test/toolbox/ant/FigletTaskTest.java
+++ trunk/toolbox/test/toolbox/ant/FigletTaskTest.java
@@ -8,40 +8,35 @@ import org.apache.tools.ant.Main;
 import toolbox.junit.testcase.StandaloneTestCase;
 
 /**
- * Unit test for {@link toolbox.ant.FigletTask}. This test is marked as 
- * standalone because the call into Ant results in call to System.exit() on 
+ * Unit test for {@link toolbox.ant.FigletTask}. This test is marked as
+ * standalone because the call into Ant results in call to System.exit() on
  * completion.
  */
-public class FigletTaskTest extends TestCase implements StandaloneTestCase
-{
-    //--------------------------------------------------------------------------
-    // Main
-    //--------------------------------------------------------------------------
+public class FigletTaskTest extends TestCase implements StandaloneTestCase {
     
-    public static void main(String[] args)
-    {
+    // --------------------------------------------------------------------------
+    // Main
+    // --------------------------------------------------------------------------
+
+    public static void main(String[] args) {
         TestRunner.run(FigletTaskTest.class);
     }
 
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
     // Unit Tests
-    //--------------------------------------------------------------------------
+    // --------------------------------------------------------------------------
 
-    public void testBannerTask()
-    {
-        //-Dant.home=c:\ant 
-        
-        System.out.println("Anthome=" + System.getProperty("ant.home"));
-        System.out.println("userhome=" + System.getProperty("user.home"));
-        System.out.println("userdir=" + System.getProperty("user.dir"));
-        
-        String args[] =
-        {
-            "-f", "test/toolbox/ant/FigletTaskTest.xml"
-        };
-            
-        Main.main(args); 
-        
-        //[options] [target]
+    public void testBannerTask() {
+        // -Dant.home=c:\ant
+
+        //System.out.println("Anthome=" + System.getProperty("ant.home"));
+        //System.out.println("userhome=" + System.getProperty("user.home"));
+        //System.out.println("userdir=" + System.getProperty("user.dir"));
+
+        String args[] = { "-f", "test/toolbox/ant/FigletTaskTest.xml" };
+
+        Main.main(args);
+
+        // [options] [target]
     }
 }
